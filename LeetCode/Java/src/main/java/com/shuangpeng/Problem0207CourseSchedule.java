b542f664300e2a22f76c657f3dbd3a2a7a106b6b
shuangpengpang <15913598312@163.com>
2020-11-13 22:27:54 +0800

    课程表

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem0207CourseSchedule.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem0207CourseSchedule.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class Problem0207CourseSchedule {
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    // 深度优先
+    public boolean canFinish0(int numCourses, int[][] prerequisites) {
         if (prerequisites == null || prerequisites.length == 0) {
             return true;
         }
@@ -52,5 +53,169 @@ public class Problem0207CourseSchedule {
         set.remove(vertex);
         result.add(vertex);
         return true;
+    }
+
+
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    // 深度优先（官方）
+    public boolean canFinish1(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v : edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+
+    // 深度优先（哈希表）
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < prerequisites.length; i++) {
+            int[] edges = prerequisites[i];
+            int start = edges[1];
+            int end = edges[0];
+            if (map.containsKey(start)) {
+                map.get(start).add(end);
+            } else {
+                List<Integer> list = new ArrayList<>(2);
+                list.add(end);
+                map.put(start, list);
+            }
+        }
+        int[] visited = new int[numCourses];
+        Set<Integer> keySet = map.keySet();
+        for (int key : keySet) {
+            if (visited[key] == 0 && !dfs(map, key, visited)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(Map<Integer, List<Integer>> map, int start, int[] visited) {
+        visited[start] = 1;
+        if (map.containsKey(start)) {
+            List<Integer> list = map.get(start);
+            for (int i : list) {
+                if (visited[i] == 0 && !dfs(map, i, visited)) {
+                    return false;
+                } else if (visited[i] == 1) {
+                    return false;
+                } else if (visited[i] == 2) {
+                    continue;
+                }
+            }
+        }
+        visited[start] = 2;
+        return true;
+    }
+
+
+    // 深度优先（邻接表）
+    public boolean canFinish3(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+        List<List<Integer>> edges = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            int[] edge = prerequisites[i];
+            int start = edge[1];
+            int end = edge[0];
+            edges.get(start).add(end);
+        }
+        int[] visited = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && !dfs(edges, i, visited)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(List<List<Integer>> edges, int start, int[] visited) {
+        visited[start] = 1;
+        List<Integer> list = edges.get(start);
+        for (int i : list) {
+            if (visited[i] == 0 && !dfs(edges, i, visited)) {
+                return false;
+            } else if (visited[i] == 1) {
+                return false;
+            } else if (visited[i] == 2) {
+                continue;
+            }
+        }
+        visited[start] = 2;
+        return true;
+    }
+
+    // 广度优先
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+        List<List<Integer>> edges = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            int[] edge = prerequisites[i];
+            int start = edge[1];
+            int end = edge[0];
+            indegree[end]++;
+            edges.get(start).add(end);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            List<Integer> list = edges.get(vertex);
+            for (int edge : list) {
+                indegree[edge]--;
+                if (indegree[edge] == 0) {
+                    queue.offer(edge);
+                }
+            }
+            count++;
+        }
+        return count == numCourses;
     }
 }

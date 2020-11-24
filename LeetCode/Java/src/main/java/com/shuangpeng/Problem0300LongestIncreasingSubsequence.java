eeda0e06722fe0d15ef0f48e90b3c5887a8d4a90
shuangpengpang <15913598312@163.com>
2020-11-24 23:19:58 +0800

    1、删除无效的括号 2、最佳买卖股票时机含冷冻期

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem0300LongestIncreasingSubsequence.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem0300LongestIncreasingSubsequence.java
@@ -5,7 +5,7 @@ public class Problem0300LongestIncreasingSubsequence {
     // [4,10,4,3,8,9]
 
     // 动态规划
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS0(int[] nums) {
         int length = nums.length;
         int[] dp = new int[length];
         dp[0] = 1;
@@ -23,5 +23,61 @@ public class Problem0300LongestIncreasingSubsequence {
             max = Math.max(max, dp[i]);
         }
         return max;
+    }
+
+    public int lengthOfLIS2(int[] nums) {
+        int length = nums.length;
+        int[] dp = new int[length + 1];
+        dp[0] = Integer.MIN_VALUE;
+        dp[1] = nums[0];
+        int max = 1;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] > dp[max]) {
+                dp[++max] = nums[i];
+            } else {
+                int left = 0;
+                int right = max;
+                while (left < right) {
+                    int mid = (left + right) >>> 1;
+                    if (nums[i] > dp[mid]) {
+                        left = mid + 1;
+                    } else if (nums[i] < dp[mid]) {
+                        right = mid;
+                    } else {
+                        left = mid;
+                        break;
+                    }
+                }
+                dp[left] = nums[i];
+            }
+        }
+        return max;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
     }
 }
---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem0301RemoveInvalidParentheses.java
@@ -0,0 +1,98 @@
+package com.shuangpeng;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Problem0301RemoveInvalidParentheses {
+
+//    Set<String> resultSet = new HashSet<>();
+    int minOperation = Integer.MAX_VALUE;
+    public List<String> removeInvalidParentheses0(String s) {
+        resultSet.clear();
+        recurse(s, 0, 0, 0, 0, new StringBuilder());
+        return new ArrayList<>(resultSet);
+    }
+
+    // 回溯
+    public void recurse(String s, int index, int removeCount, int leftCount, int rightCount, StringBuilder result) {
+        if (index == s.length()) {
+            if (leftCount == rightCount && removeCount <= minOperation) {
+                if (removeCount < minOperation) {
+                    resultSet.clear();
+                    minOperation = removeCount;
+                }
+                resultSet.add(result.toString());
+            }
+        } else {
+            char ch = s.charAt(index);
+            int length = result.length();
+            if (ch != '(' && ch != ')') {
+                result.append(ch);
+                recurse(s, index + 1, removeCount, leftCount, rightCount, result);
+                result.deleteCharAt(length);
+            } else if (ch == '(') {
+                recurse(s, index + 1, removeCount + 1, leftCount, rightCount, result);
+
+                result.append(ch);
+                recurse(s, index + 1, removeCount, leftCount + 1, rightCount, result);
+                result.deleteCharAt(length);
+            } else {
+                recurse(s, index + 1, removeCount + 1, leftCount, rightCount, result);
+                if (leftCount > rightCount) {
+                    result.append(ch);
+                    recurse(s, index + 1, removeCount, leftCount, rightCount + 1, result);
+                    result.deleteCharAt(length);
+                }
+            }
+        }
+    }
+
+
+    Set<String> resultSet = new HashSet<>();
+    public List<String> removeInvalidParentheses(String s) {
+        resultSet.clear();
+        int leftRem = 0;
+        int rightRem = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                leftRem++;
+            } else if (ch == ')') {
+                rightRem += leftRem > 0 ? 0 : 1;
+                leftRem -= leftRem > 0 ? 1 : 0;
+            }
+        }
+        backtrack(s, 0, 0, 0, leftRem, rightRem, new StringBuilder());
+        return new ArrayList<>(resultSet);
+    }
+
+    public void backtrack(String s, int index, int leftCount, int rightCount, int leftRem, int rightRem, StringBuilder result) {
+        if (index == s.length()) {
+            if (leftRem == 0 && rightRem == 0) {
+                resultSet.add(result.toString());
+            }
+        } else {
+            char ch = s.charAt(index);
+            // 删除
+            if (ch == '(' && leftRem > 0) {
+                backtrack(s, index + 1, leftCount, rightCount, leftRem - 1, rightRem, result);
+            } else if (ch == ')' && rightRem > 0) {
+                backtrack(s, index + 1, leftCount, rightCount, leftRem, rightRem - 1, result);
+            }
+
+            int length = result.length();
+            result.append(ch);
+            if (ch != '(' && ch != ')') {
+                backtrack(s, index + 1, leftCount, rightCount, leftRem, rightRem, result);
+            } else if (ch == '(') {
+                backtrack(s, index + 1, leftCount + 1, rightCount, leftRem, rightRem, result);
+            } else if (ch == ')' && leftCount > rightCount) {
+                backtrack(s, index + 1, leftCount, rightCount + 1, leftRem, rightRem, result);
+            }
+            result.deleteCharAt(length);
+        }
+    }
+}

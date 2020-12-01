730b8bb623fb7575fcac6cc080f31f3e63f2ddc9
shuangpengpang <15913598312@163.com>
2020-12-01 23:48:55 +0800

    1、合并二叉树 2、任务调度器

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem0581ShortestUnsortedContinuousSubarray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem0581ShortestUnsortedContinuousSubarray.java
@@ -1,50 +1,177 @@
 package com.shuangpeng;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Problem0581ShortestUnsortedContinuousSubarray {
 
-    public static void main(String[] args) {
-//        int[] nums = {1, 2, 3, 4};
-        int[] nums = {2,6,4,8,10,9,15};
-        Problem0581ShortestUnsortedContinuousSubarray a = new Problem0581ShortestUnsortedContinuousSubarray();
-        a.findUnsortedSubarray(nums);
+//    public static void main(String[] args) {
+////        int[] nums = {1, 2, 3, 4};
+//        int[] nums = {2,6,4,8,10,9,15};
+//        Problem0581ShortestUnsortedContinuousSubarray a = new Problem0581ShortestUnsortedContinuousSubarray();
+//        a.findUnsortedSubarray(nums);
+//    }
+
+    public int findUnsortedSubarray0(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+        List<Integer> minStack = new ArrayList<>(nums.length);
+        List<Integer> maxStack = new ArrayList<>(nums.length);
+        minStack.add(nums[0]);
+        maxStack.add(nums[nums.length - 1]);
+        int leftCount = 1;
+        int rightCount = 1;
+        for (int i = 1; i < nums.length; i++) {
+            while (leftCount > 0 && !minStack.isEmpty() && minStack.get(minStack.size() - 1) > nums[i]) {
+                minStack.remove(minStack.size() - 1);
+                leftCount--;
+            }
+            if (leftCount == i) {
+                minStack.add(nums[i]);
+                leftCount++;
+            }
+            int j = nums.length - 1 - i;
+            while (rightCount > 0 && !maxStack.isEmpty() && maxStack.get(maxStack.size() - 1) < nums[j]) {
+                maxStack.remove(maxStack.size() - 1);
+                rightCount--;
+            }
+            if (rightCount == i) {
+                maxStack.add(nums[j]);
+                rightCount++;
+            }
+            if ((leftCount == 0 && rightCount == 0) || (leftCount + rightCount) > nums.length + 1) {
+                break;
+            }
+        }
+        if (leftCount + rightCount >= nums.length) {
+            return 0;
+        }
+        return nums.length - leftCount - rightCount;
+    }
+
+    public int findUnsortedSubarray1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        int l = 0;
+        int r = length - 1;
+        for (int i = 0; i < length; i++) {
+            for (int j = l - 1; j >= 0; j--) {
+                if (nums[j] > nums[i]) {
+                    l--;
+                } else {
+                    break;
+                }
+            }
+            if (l == i) {
+                l++;
+            }
+            for (int j = r + 1; j < length; j++) {
+                if (nums[length - 1 - i] > nums[j]) {
+                    r++;
+                } else {
+                    break;
+                }
+            }
+            if (r == length - 1 - i) {
+                r--;
+            }
+        }
+        return r - l + 1 >= 0 ? r - l + 1 : 0;
+    }
+
+    public int findUnsortedSubarray2(int[] nums) {
+        int left = nums.length;
+        int right = -1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    left = Math.min(left, i);
+                    right = Math.max(right, j);
+                }
+            }
+        }
+        return right - left + 1 < 0 ? 0 : right - left + 1;
+    }
+
+    public int findUnsortedSubarray3(int[] nums) {
+        int[] copy = nums.clone();
+        Arrays.sort(copy);
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            int j = nums.length - 1 - i;
+            if (left == i && nums[i] == copy[i]) {
+                left++;
+            }
+            if (j == right && nums[j] == copy[j]) {
+                right--;
+            }
+        }
+        return right - left + 1 < 0 ? 0 : right - left + 1;
+    }
+
+    public int findUnsortedSubarray4(int[] nums) {
+        int left = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                left = Math.min(left, stack.pop());
+            }
+            if (i == stack.size()) {
+                stack.push(i);
+            }
+            if (left == 0) {
+                break;
+            }
+        }
+        stack.clear();
+        int right = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int j = nums.length - i - 1;
+            while (!stack.isEmpty() && nums[j] > nums[stack.peek()]) {
+                right = Math.max(right, stack.pop());
+            }
+            if (i == stack.size()) {
+                stack.push(j);
+            }
+            if (right >= nums.length - 1) {
+                break;
+            }
+        }
+        return right - left + 1 < 0 ? 0 : right - left + 1;
     }
 
     public int findUnsortedSubarray(int[] nums) {
-        List<Integer> stack = new ArrayList<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            int data = nums[i];
-            while (!stack.isEmpty() && nums[stack.get(stack.size() - 1)] > data) {
-                stack.remove(stack.size() - 1);
+        int length = nums.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                min = Math.min(min, nums[i]);
             }
-            stack.add(i);
+            int j = length - i - 1;
+            if (nums[j] > nums[j + 1]) {
+                max = Math.max(max, nums[j]);
+            }
         }
         int left = 0;
-        int right = stack.size() - 1;
-        int i = 0;
-        boolean leftStop = false;
-        boolean rightStop = false;
-        int count = 0;
-        while (left <= right && (!leftStop || !rightStop)) {
-            if (!leftStop && stack.get(left) == i) {
+        int right = length - 1;
+        for (int i = 0; i < length; i++) {
+            if ((left != i && right != length - i - 1) || (right - left < -1)) {
+                break;
+            }
+            if (left == i && nums[i] <= min) {
                 left++;
-                count++;
-            } else {
-                leftStop = true;
             }
-            if (!rightStop && stack.get(right) == nums.length - i - 1) {
+            if (right == length - i - 1 && nums[right] >= max) {
                 right--;
-                count++;
-            } else {
-                rightStop = true;
             }
-            i++;
         }
-        if (left > right) {
-            return 0;
-        }
-        return nums.length - count;
+        return right - left + 1 < 0 ? 0 : right - left + 1;
     }
 }
---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem0621TaskScheduler.java
@@ -0,0 +1,143 @@
+package com.shuangpeng;
+
+import java.util.*;
+
+public class Problem0621TaskScheduler {
+
+//    public static void main(String[] args) {
+//        Problem0621TaskScheduler a = new Problem0621TaskScheduler();
+//        char[] tasks = {'A','A','A','B','B','B'};
+////        char[] tasks = {'A','A','A','A','A','A','B','C','D','E','F','G'};
+//        a.leastInterval(tasks, 2);
+//    }
+
+    public int leastInterval0(char[] tasks, int n) {
+        int[] taskCount = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            taskCount[tasks[i] - 'A']++;
+        }
+        List<Integer> list = new ArrayList<>(taskCount.length);
+        for (int i = 0; i < taskCount.length; i++) {
+            if (taskCount[i] > 0) {
+                list.add(taskCount[i]);
+            }
+        }
+        Collections.sort(list, (a, b) -> b - a);
+        int interval = 0;
+        while (list.size() > 0) {
+            int count = list.size();
+            if (count > n + 1) {
+                interval += n + 1;
+                for (int i = 0; i < n + 1; i++) {
+                    list.set(i, list.get(i) - 1);
+                }
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i) == 0) {
+                        list.remove(i);
+                        i = -1;
+                    }
+                }
+                Collections.sort(list, (a, b) -> b - a);
+            } else if (count > 1) {
+                int min = list.get(count - 1);
+                interval += (n + 1) * min;
+                for (int i = count - 1; i >= 0; i--) {
+                    list.set(i, list.get(i) - min);
+                    if (list.get(i) == 0) {
+                        list.remove(i);
+                    }
+                }
+                if (list.size() == 0) {
+                    interval -= n + 1 - count;
+                }
+            } else {
+                interval += (n + 1) * (list.get(0) - 1) + 1;
+                list.remove(0);
+                break;
+            }
+        }
+        return interval;
+    }
+
+    public int leastInterval1(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        Arrays.sort(map);
+        int time = 0;
+        int length = map.length;
+        int count = 0;
+        while (map[length - 1] > 0) {
+            count = 0;
+            for (int i = 0; i < n + 1; i++) {
+                if (length - i - 1 >= 0 && map[length - i - 1] > 0) {
+                    map[length - i - 1]--;
+                    count++;
+                }
+                time++;
+            }
+            Arrays.sort(map);
+        }
+        return time - (n + 1 - count);
+    }
+
+    public int leastInterval2(char[] tasks, int n) {
+        int length = 26;
+        int[] map = new int[length];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        Arrays.sort(map);
+        int time = 0;
+        while (map[length - 1] > 0) {
+            for (int i = 0; i < n + 1; i++) {
+                if (map[length - 1] == 0) {
+                    break;
+                }
+                if (length - i - 1 >= 0 && map[length - i - 1] > 0) {
+                    map[length - i - 1]--;
+                }
+                time++;
+            }
+            Arrays.sort(map);
+        }
+        return time;
+    }
+
+    public int leastInterval(char[] tasks, int n) {
+        int length = 26;
+        int[] map = new int[length];
+        for (int i = 0; i < tasks.length; i++) {
+            map[tasks[i] - 'A']++;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>(length, (a, b) -> b - a);
+        for (int i = 0; i < length; i++) {
+            if (map[i] > 0) {
+                queue.offer(map[i]);
+            }
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int[] array = new int[n + 1];
+            for (int i = 0; i < n + 1; i++) {
+                if (!queue.isEmpty()) {
+                    array[i] = queue.poll();
+                    array[i]--;
+                    time++;
+                    if (array[0] == 0) {
+                        break;
+                    }
+                } else {
+                    time++;
+                }
+            }
+            for (int i = 0; i < n + 1; i++) {
+                if (array[i] > 0) {
+                    queue.offer(array[i]);
+                }
+            }
+        }
+        return time;
+    }
+}

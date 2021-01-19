34ff67645534fa950d58d54048fcfa079e2d76c4
shuangpengpang <15913598312@163.com>
2021-01-20 00:22:52 +0800

    摆动排序II

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/Problem0324WiggleSortII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/Problem0324WiggleSortII.java
@@ -13,16 +13,35 @@ public class Problem0324WiggleSortII {
         Arrays.sort(nums);
         int length = nums.length;
         int mid = (length - 1) / 2;
-        int[] temp = new int[length];
+        int[] left = new int[mid + 1];
+        int[] right = new int[length - mid - 1];
         for (int i = 0; i < length; i++) {
-            if ((i & 1) == 0) {
-                temp[i] = nums[i / 2];
+            if (i <= mid) {
+                left[i] = nums[i];
             } else {
-                temp[i] = nums[mid + 1 + i / 2];
+                right[i - mid - 1] = nums[i];
             }
         }
+        int m = mid / 2;
+        for (int i = 0; i <= m; i++) {
+            int temp = left[i];
+            left[i] = left[mid - i];
+            left[mid - i] = temp;
+        }
+        m = (length - mid) / 2 - 1;
+        for (int i = 0; i <= m; i++) {
+            int temp = right[i];
+            right[i] = right[length - mid - i - 2];
+            right[length - mid - i - 2] = temp;
+        }
+        int j = 0;
+        int k = 0;
         for (int i = 0; i < length; i++) {
-            nums[i] = temp[i];
+            if (j == k) {
+                nums[i] = left[j++];
+            } else {
+                nums[i] = right[k++];
+            }
         }
     }
 }

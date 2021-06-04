3e347efd550fcf6e735a0b800812ccdb44f80711
shuangpengpang <15913598312@163.com>
2021-06-04 09:49:54 +0800

    1、最长有效括号 2、连续数组

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/Problem0525ContiguousArray.java
@@ -0,0 +1,26 @@
+package com.shuangpeng.Problem;
+
+import java.util.*;
+
+public class Problem0525ContiguousArray {
+
+    public int findMaxLength(int[] nums) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                sum++;
+            } else {
+                sum--;
+            }
+            if (map.containsKey(sum)) {
+                max = Math.max(max, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return max;
+    }
+}

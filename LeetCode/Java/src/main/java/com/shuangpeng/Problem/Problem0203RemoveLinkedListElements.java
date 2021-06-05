5c4813d98f04fd094a4bb06c5bf059f616c5e13b
shuangpengpang <15913598312@163.com>
2021-06-05 15:54:04 +0800

    1、交错字符串 2、删除链表中的元素

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/Problem0203RemoveLinkedListElements.java
@@ -0,0 +1,47 @@
+package com.shuangpeng.Problem;
+
+import com.shuangpeng.common.ListNode;
+
+public class Problem0203RemoveLinkedListElements {
+
+    public ListNode removeElements0(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode previous = dummy;
+        ListNode node = head;
+        while (node != null) {
+            if (node.val == val) {
+                previous.next = node.next;
+            } else {
+                previous = node;
+            }
+            node = node.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode removeElements1(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode node = dummy;
+        while (node.next != null) {
+            if (node.next.val == val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return removeElements(head.next, val);
+        }
+        head.next = removeElements(head.next, val);
+        return head;
+    }
+}

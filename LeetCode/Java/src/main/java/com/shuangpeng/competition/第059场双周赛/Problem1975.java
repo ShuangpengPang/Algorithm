d8ec3348b61309cc8000d4d3209014057a5ee661
shuangpengpang <15913598312@163.com>
2021-08-27 19:54:00 +0800

    第59场双周赛

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第059场双周赛/Problem1975.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.competition.第059场双周赛;
+
+public class Problem1975 {
+    public long maxMatrixSum(int[][] matrix) {
+        int n = matrix.length;
+        int minValue = Integer.MAX_VALUE;
+        boolean flag = true;
+        long sum = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int value = Math.abs(matrix[i][j]);
+                minValue = Math.min(minValue, value);
+                sum += value;
+                if (matrix[i][j] < 0) {
+                    flag = !flag;
+                }
+            }
+        }
+        return flag ? sum : sum - (minValue << 1);
+    }
+}

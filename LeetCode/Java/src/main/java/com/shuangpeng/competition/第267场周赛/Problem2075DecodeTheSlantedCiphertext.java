a38899590e5816897177417c4b6ccd331b83c0a1
shuangpengpang <15913598312@163.com>
2021-11-20 21:55:37 +0800

    第267场周赛： 1、买票需要的时间（2073） 2、反转偶数长度组的节点（2074） 3、解码斜向换位密码（2075） 4、处理含限制条件的好友请求（2076）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第267场周赛/Problem2075DecodeTheSlantedCiphertext.java
@@ -0,0 +1,47 @@
+package com.shuangpeng.competition.第267场周赛;
+
+public class Problem2075DecodeTheSlantedCiphertext {
+
+    // 比赛时写法
+    public String decodeCiphertext0(String encodedText, int rows) {
+        int length = encodedText.length();
+        int cols = length / rows;
+        int idx = 0;
+        char[][] chars = new char[rows][cols];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                chars[i][j] = encodedText.charAt(idx++);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < cols; ++j) {
+            for (int i = 0; i < rows && i + j < cols; ++i) {
+                sb.append(chars[i][i + j]);
+            }
+        }
+        int n = sb.length();
+        for (int i = n - 1; i >= 0; --i) {
+            if (sb.charAt(i) == ' ') {
+                sb.deleteCharAt(i);
+            } else {
+                break;
+            }
+        }
+        return sb.toString();
+    }
+
+    public String decodeCiphertext(String encodedText, int rows) {
+        int n = encodedText.length();
+        int cols = n / rows;
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < cols; ++j) {
+            for (int i = 0; i < rows && i * cols + i + j < n; ++i) {
+                sb.append(encodedText.charAt(i * cols + i + j));
+            }
+        }
+        for (int i = sb.length() - 1; i >= 0 && sb.charAt(i) == ' '; --i) {
+            sb.deleteCharAt(i);
+        }
+        return sb.toString();
+    }
+}

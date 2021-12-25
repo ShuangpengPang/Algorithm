cd8e5cabde81b57897fe1da661caec9c04b531bb
shuangpengpang <15913598312@163.com>
2021-12-25 20:40:12 +0800

    第272场周赛： 1、找出数组中的第一个回文字符串（2108） 2、向字符串添加空格（2109） 3、股票平滑下跌阶段的数目（2110） 4、使数组 K 递增的最少操作次数（2111）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第272场周赛/Problem2111MinimumOperationsToMakeTheArrayKIncreasing.java
@@ -0,0 +1,42 @@
+package com.shuangpeng.competition.第272场周赛;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem2111MinimumOperationsToMakeTheArrayKIncreasing {
+
+    // 比赛时写法
+    public int kIncreasing(int[] arr, int k) {
+        int n = arr.length;
+        int ans = 0;
+        for (int i = 0; i < k; ++i) {
+            List<Integer> list = new ArrayList<>();
+            int count = 0;
+            for (int j = i; j < n; j += k) {
+                ++count;
+                int idx = binarySearch(list, arr[j]);
+                if (idx >= list.size()) {
+                    list.add(arr[j]);
+                } else {
+                    list.set(idx, arr[j]);
+                }
+            }
+            ans += count - list.size();
+        }
+        return ans;
+    }
+
+    private int binarySearch(List<Integer> list, int num) {
+        int left = 0, right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            int data = list.get(mid);
+            if (data <= num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}

1a9acd153d8031c27cf29b34bb02aa4e5823b5b4
shuangpengpang <15913598312@163.com>
2022-02-15 20:00:36 +0800

    commit

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0540SingleElementInASortedArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0540SingleElementInASortedArray.java
@@ -15,7 +15,7 @@ public class Problem0540SingleElementInASortedArray {
         return ans;
     }
 
-    public int singleNonDuplicate(int[] nums) {
+    public int singleNonDuplicate1(int[] nums) {
         int n = nums.length;
         int left = 0, right = n - 1;
         while (left < right) {
@@ -31,6 +31,33 @@ public class Problem0540SingleElementInASortedArray {
                 right = mid - 2;
             } else {
                 return nums[mid];
+            }
+        }
+        return nums[left];
+    }
+
+    public int singleNonDuplicate2(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == nums[mid ^ 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            mid -= mid & 1;
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
             }
         }
         return nums[left];

ee6a59f841cabafb29c3403ac059663407c228f6
shuangpengpang <15913598312@163.com>
2022-04-22 12:00:29 +0800

    尽量减少恶意软件的传播II

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0396RotateFunction.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0396RotateFunction.java
@@ -2,7 +2,7 @@ package com.shuangpeng.Problem.p0301_0400;
 
 public class Problem0396RotateFunction {
 
-    public int maxRotateFunction(int[] nums) {
+    public int maxRotateFunction0(int[] nums) {
         int n = nums.length;
         int sum = 0, origin = 0;
         for (int i = 0; i < n; i++) {
@@ -15,5 +15,36 @@ public class Problem0396RotateFunction {
             answer = Math.max(answer, origin);
         }
         return answer;
+    }
+
+    public int maxRotateFunction1(int[] nums) {
+        int result = 0;
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            result += nums[i] * i;
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; ++i) {
+            result += nums[i] * n - sum;
+            ans = Math.max(ans, result);
+        }
+        return ans;
+    }
+
+    public int maxRotateFunction(int[] nums) {
+        int sum = 0, f = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            f += nums[i] * i;
+        }
+        int ans = f;
+        for (int i = n - 1; i > 0; --i) {
+            f += sum - nums[i] * n;
+            ans = Math.max(ans, f);
+        }
+        return ans;
     }
 }

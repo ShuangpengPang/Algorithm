dd35d62d0dbebed32a04d345f0b19e911a05e2e4
shuangpengpang <15913598312@163.com>
2022-05-28 17:34:11 +0800

    环绕字符串中的唯一子字符串（467）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0467UniqueSubstringsInWraparoundString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0467UniqueSubstringsInWraparoundString.java
@@ -1,8 +1,10 @@
 package com.shuangpeng.Problem.p0401_0500;
 
+import java.util.Arrays;
+
 public class Problem0467UniqueSubstringsInWraparoundString {
 
-    public int findSubstringInWraproundString(String p) {
+    public int findSubstringInWraproundString0(String p) {
         int N = 26;
         int[] array = new int[N];
         array[p.charAt(0) - 'a'] = 1;
@@ -24,5 +26,63 @@ public class Problem0467UniqueSubstringsInWraparoundString {
             right++;
         }
         return answer;
+    }
+
+    public int findSubstringInWraproundString1(String p) {
+        int N = 26;
+        int[] firstIndex = new int[N];
+        int[] maxLength = new int[N];
+        Arrays.fill(firstIndex, -1);
+        int n = p.length();
+        for (int i = 0, s = 0; i < n; ++i) {
+            int j = p.charAt(i) - 'a';
+            if ((i == 0 || j == 0 && p.charAt(i - 1) != 'z') || (j > 0 && j != p.charAt(i - 1) - 'a' + 1)) {
+                s = i;
+            }
+            if (firstIndex[j] < s) {
+                firstIndex[j] = i;
+            }
+            for (int k = 0; k < N; ++k) {
+                if (firstIndex[k] >= s) {
+                    maxLength[k] = Math.max(maxLength[k], i - firstIndex[k] + 1);
+                }
+            }
+        }
+        return Arrays.stream(maxLength).sum();
+    }
+
+    public int findSubstringInWraproundString2(String p) {
+        int[] dp = new int[26];
+        int n = p.length();
+        for (int i = 0, k = 0; i < n; ++i) {
+            int j = p.charAt(i) - 'a';
+            if (i > 0 && (j == p.charAt(i - 1) - 'a' + 1 || (j == 0 && p.charAt(i - 1) == 'z'))) {
+                ++k;
+            } else {
+                k = 1;
+            }
+            dp[j] = Math.max(dp[j], k);
+        }
+        return Arrays.stream(dp).sum();
+    }
+
+    public int findSubstringInWraproundString(String p) {
+        int N = 26;
+        int[] dp = new int[N];
+        int n = p.length();
+        for (int i = 0, k = 0; i < n; ++i) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) + N) % N == 1) {
+                ++k;
+            } else {
+                k = 1;
+            }
+            int j = p.charAt(i) - 'a';
+            dp[j] = Math.max(dp[j], k);
+        }
+        int ans = 0;
+        for (int i = 0; i < N; ++i) {
+            ans += dp[i];
+        }
+        return ans;
     }
 }

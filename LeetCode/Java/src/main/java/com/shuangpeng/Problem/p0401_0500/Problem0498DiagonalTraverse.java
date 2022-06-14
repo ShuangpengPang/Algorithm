40a141b92124d8b68c516ea204f0d17c93b5874e
shuangpengpang <15913598312@163.com>
2022-06-14 17:27:17 +0800

    对角线遍历（498）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0498DiagonalTraverse.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0498DiagonalTraverse.java
@@ -7,7 +7,7 @@ package com.shuangpeng.Problem.p0401_0500;
  */
 public class Problem0498DiagonalTraverse {
 
-    public int[] findDiagonalOrder(int[][] mat) {
+    public int[] findDiagonalOrder0(int[][] mat) {
         int m = mat.length, n = mat[0].length;
         int N = m * n;
         int[] ans = new int[N];
@@ -37,6 +37,28 @@ public class Problem0498DiagonalTraverse {
                     } else {
                         ++x;
                     }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] ans = new int[m * n];
+        int pos = 0;
+        for (int i = 0; i < m + n - 1; ++i) {
+            if (i % 2 == 0) {
+                int x = i < m ? i : m - 1;
+                int y = i < m ? 0 : i - m + 1;
+                while (x >= 0 && y < n) {
+                    ans[pos++] = mat[x--][y++];
+                }
+            } else {
+                int x = i < n ? 0 : i - n + 1;
+                int y = i < n ? i : n - 1;
+                while (x < m && y >= 0) {
+                    ans[pos++] = mat[x++][y--];
                 }
             }
         }

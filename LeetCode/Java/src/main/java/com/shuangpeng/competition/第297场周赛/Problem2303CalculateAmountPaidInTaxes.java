8269bcac8305db2f87f68caa1eea9a3b1fefe285
shuangpengpang <15913598312@163.com>
2022-06-30 17:32:10 +0800

    第297场周赛： 1、计算应缴税款总额（2303） 2、网格中的最小路径代价（2304） 3、公平分发饼干（2305） 4、公司命名（2306）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第297场周赛/Problem2303CalculateAmountPaidInTaxes.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第297场周赛/Problem2303CalculateAmountPaidInTaxes.java
@@ -7,7 +7,28 @@ package com.shuangpeng.competition.第297场周赛;
  */
 public class Problem2303CalculateAmountPaidInTaxes {
 
-    public double calculateTax(int[][] brackets, int income) {
+    // 比赛时写法
+    public double calculateTax0(int[][] brackets, int income) {
+        double ans = 0;
+        int n = brackets.length;
+        int prev = 0;
+        for (int[] pair : brackets) {
+            int up = pair[0], p = pair[1];
+            if (income <= prev) {
+                break;
+            }
+            if (income <= up) {
+                ans += (double) (income - prev) * p / 100;
+                break;
+            } else {
+                ans += (double) (up - prev) * p / 100;
+            }
+            prev = up;
+        }
+        return ans;
+    }
+
+    public double calculateTax1(int[][] brackets, int income) {
         int n = brackets.length;
         int prev = 0;
         double ans = 0;
@@ -16,5 +37,16 @@ public class Problem2303CalculateAmountPaidInTaxes {
             prev = brackets[i][0];
         }
         return ans;
+    }
+
+    public double calculateTax(int[][] brackets, int income) {
+        int n = brackets.length;
+        int sum = 0;
+        int prev = 0;
+        for (int i = 0; i < n && income > prev; i++) {
+            sum += (Math.min(income, brackets[i][0]) - prev) * brackets[i][1];
+            prev = brackets[i][0];
+        }
+        return sum / 100.0;
     }
 }
--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第297场周赛/Problem2304MinimumPathCostInAGrid.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第297场周赛/Problem2304MinimumPathCostInAGrid.java
@@ -9,7 +9,33 @@ import java.util.Arrays;
  */
 public class Problem2304MinimumPathCostInAGrid {
 
-    public int minPathCost(int[][] grid, int[][] moveCost) {
+    // 比赛时写法
+    public int minPathCost0(int[][] grid, int[][] moveCost) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = grid[m - 1][i];
+        }
+        for (int i = m - 2; i >= 0; --i) {
+            int[] temp = new int[n];
+            for (int j = 0; j < n; ++j) {
+                int ans = Integer.MAX_VALUE;
+                int num = grid[i][j];
+                for (int k = 0; k < n; ++k) {
+                    ans = Math.min(ans, num + moveCost[num][k] + dp[k]);
+                }
+                temp[j] = ans;
+            }
+            dp = temp;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.min(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    public int minPathCost1(int[][] grid, int[][] moveCost) {
         int m = grid.length, n = grid[0].length;
         int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
@@ -30,5 +56,32 @@ public class Problem2304MinimumPathCostInAGrid {
             temp = s;
         }
         return Arrays.stream(dp).min().getAsInt();
+    }
+
+    public int minPathCost(int[][] grid, int[][] moveCost) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = grid[0][i];
+        }
+        int[] temp = new int[n];
+        int[] s;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int min = Integer.MAX_VALUE;
+                for (int k = 0; k < n; k++) {
+                    min = Math.min(min, dp[k] + moveCost[grid[i - 1][k]][j]);
+                }
+                temp[j] = min + grid[i][j];
+            }
+            s = dp;
+            dp = temp;
+            temp = s;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i : dp) {
+            ans = Math.min(ans, i);
+        }
+        return ans;
     }
 }

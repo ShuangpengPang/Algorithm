1b52a790004fef2265bb79e2a1a022fe885e8c97
shuangpengpang <15913598312@163.com>
2022-07-04 11:58:41 +0800

    下一个更大元素（556）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0556NextGreaterElementIII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0556NextGreaterElementIII.java
@@ -49,3 +49,65 @@ public class Problem0556NextGreaterElementIII {
         arr[j] = temp;
     }
 }
+
+class Problem0556NextGreaterElementIII0 {
+
+    public int nextGreaterElement(int n) {
+        char[] chars = Integer.toString(n).toCharArray();
+        int m = chars.length;
+        int idx = m - 2;
+        while (idx >= 0 && chars[idx] >= chars[idx + 1]) {
+            idx--;
+        }
+        if (idx < 0) {
+            return -1;
+        }
+        int index = m - 1;
+        while (chars[index] <= chars[idx]) {
+            index--;
+        }
+        swap(chars, idx, index);
+        for (int i = idx + 1, j = m - 1; i < j; i++, j--) {
+            swap(chars, i, j);
+        }
+        long ans = Long.parseLong(new String(chars));
+        return (int) (ans > Integer.MAX_VALUE ? -1 : ans);
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char c = chars[i];
+        chars[i] = chars[j];
+        chars[j] = c;
+    }
+}
+
+class Problem0556NextGreaterElementIII1 {
+
+    public int nextGreaterElement(int n) {
+        int x = n, cnt1 = 1;
+        while (x / 10 % 10 >= x % 10) {
+            x /= 10;
+            cnt1++;
+        }
+        x /= 10;
+        if (x == 0) {
+            return -1;
+        }
+        int num = x % 10;
+        int t = n, cnt2 = 0;
+        while (t % 10 <= num) {
+            t /= 10;
+            cnt2++;
+        }
+        x += t % 10 - num;
+        for (int i = 0; i < cnt1; i++) {
+            int digit = i != cnt2 ? n % 10 : num;
+            if (x > Integer.MAX_VALUE / 10 || (x == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return -1;
+            }
+            x = x * 10 + digit;
+            n /= 10;
+        }
+        return x;
+    }
+}

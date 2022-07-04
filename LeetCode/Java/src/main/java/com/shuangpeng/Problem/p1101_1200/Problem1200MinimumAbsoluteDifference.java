71d87b4b3b9c030b637bdd49947530824bf5c797
shuangpengpang <15913598312@163.com>
2022-07-04 10:44:40 +0800

    最小绝对差（1200）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1200MinimumAbsoluteDifference.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1200MinimumAbsoluteDifference.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class Problem1200MinimumAbsoluteDifference {
 
-    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+    public List<List<Integer>> minimumAbsDifference0(int[] arr) {
         Arrays.sort(arr);
         int n = arr.length;
         int diff = Integer.MAX_VALUE;
@@ -22,6 +22,86 @@ public class Problem1200MinimumAbsoluteDifference {
         for (int i = 0; i < n - 1; i++) {
             if (arr[i + 1] - arr[i] == diff) {
                 List<Integer> list = new ArrayList<>();
+                list.add(arr[i]);
+                list.add(arr[i + 1]);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> minimumAbsDifference1(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int diff = Integer.MAX_VALUE;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            int d = arr[i + 1] - arr[i];
+            if (d < diff || d == diff) {
+                if (d < diff) {
+                    diff = d;
+                    ans.clear();
+                }
+                List<Integer> list = new ArrayList<>(2);
+                list.add(arr[i]);
+                list.add(arr[i + 1]);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> minimumAbsDifference2(int[] arr) {
+        return new java.util.AbstractList<List<Integer>>() {
+            private int[] buffer;
+            private int size;
+            private boolean initialed;
+            private int min = Integer.MAX_VALUE;
+            private void init() {
+                if (initialed) {
+                    return;
+                }
+                buffer = new int[arr.length];
+                Arrays.sort(arr);
+                for (int i = 1; i < arr.length; i++) {
+                    int diff = arr[i] - arr[i - 1];
+                    if (diff > min) {
+                        continue;
+                    } else if (diff < min) {
+                        size = 0;
+                        min = diff;
+                    }
+                    buffer[size++] = arr[i - 1];
+                }
+                initialed = true;
+            }
+            @Override
+            public List<Integer> get(int index) {
+                init();
+                return Arrays.asList(buffer[index], buffer[index] + min);
+            }
+
+            @Override
+            public int size() {
+                init();
+                return size;
+            }
+        };
+    }
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int diff = Integer.MAX_VALUE;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            int d = arr[i + 1] - arr[i];
+            if (d < diff) {
+                diff = d;
+                ans.clear();
+            }
+            if (d == diff) {
+                List<Integer> list = new ArrayList<>(2);
                 list.add(arr[i]);
                 list.add(arr[i + 1]);
                 ans.add(list);

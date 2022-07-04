5cbbfc9de9473a23709fea08fc56a5c1a879610c
shuangpengpang <15913598312@163.com>
2022-07-04 15:39:02 +0800

    操作后的最大异或和（2317）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第081场双周赛/Problem2317MaximumXORAfterOperations.java
@@ -0,0 +1,17 @@
+package com.shuangpeng.competition.第081场双周赛;
+
+/**
+ * @Description: Problem2317MaximumXORAfterOperations（操作后的最大异或和）
+ * @Date 2022/7/4 3:17 PM
+ * @Version 1.0
+ */
+public class Problem2317MaximumXORAfterOperations {
+
+    public int maximumXOR(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans |= num;
+        }
+        return ans;
+    }
+}

43e19fd8933b5bffd59890dc4e8f62939feb465d
shuangpengpang <15913598312@163.com>
2022-07-07 11:27:21 +0800

    单词替换（648）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0648ReplaceWords.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0648ReplaceWords.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p0601_0700;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -52,5 +53,58 @@ public class Problem0648ReplaceWords {
             j++;
         }
         return sb.toString();
+    }
+}
+
+class Problem0648ReplaceWords0 {
+    static final int M = (int) 1e5 + 1, N = 26;
+    static int[][] tr = new int[M][N];
+    static boolean[] isWord = new boolean[M];
+    static int idx = 0;
+
+    void add(String s) {
+        int p = 0;
+        int n = s.length();
+        for (int i = 0; i < n && !isWord[p]; i++) {
+            int j = s.charAt(i) - 'a';
+            if (tr[p][j] == 0) {
+                tr[p][j] = ++idx;
+            }
+            p = tr[p][j];
+        }
+        isWord[p] = true;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        for (int i = 0; i <= idx; i++) {
+            Arrays.fill(tr[i], 0);
+            isWord[i] = false;
+        }
+        idx = 0;
+        for (String s : dictionary) {
+            add(s);
+        }
+        StringBuilder sb = new StringBuilder();
+        int n = sentence.length();
+        for (int i = 0, j = 0; i < n; i = j) {
+            while (j < n && sentence.charAt(j) != ' ') {
+                j++;
+            }
+            int p = 0;
+            for (int k = i; k < j && (p == -1 || !isWord[p]); k++) {
+                char c = sentence.charAt(k);
+                sb.append(c);
+                if (p == -1) {
+                    continue;
+                }
+                p = tr[p][c - 'a'];
+                if (p == 0) {
+                    p = -1;
+                }
+            }
+            sb.append(' ');
+            j++;
+        }
+        return sb.deleteCharAt(sb.length() - 1).toString();
     }
 }

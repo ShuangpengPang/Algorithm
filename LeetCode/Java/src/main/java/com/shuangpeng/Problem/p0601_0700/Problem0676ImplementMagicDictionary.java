f2c618f7a7b2dbce87d10e4cd802955c9257471d
shuangpengpang <15913598312@163.com>
2022-07-11 12:19:37 +0800

    实现一个魔法字典（676）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0676ImplementMagicDictionary.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0676ImplementMagicDictionary.java
@@ -1,6 +1,6 @@
 package com.shuangpeng.Problem.p0601_0700;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @Description: Problem0676ImplementMagicDictionary（实现一个魔法字典）
@@ -82,17 +82,171 @@ public class Problem0676ImplementMagicDictionary {
  */
 }
 
-//class MagicDictionary {
-//
-//    Map<Integer, List<String>> map;
-//
-//    public MagicDictionary() {
-//    }
-//
-//    public void buildDict(String[] dictionary) {
-//    }
-//
-//    public boolean search(String searchWord) {
-//    }
-//}
+class Problem0676ImplementMagicDictionary0 {
+
+    class MagicDictionary {
+
+        Map<Integer, List<String>> map = new HashMap<>(100);
+
+        public MagicDictionary() {
+            map.clear();
+        }
+
+        public void buildDict(String[] dictionary) {
+            for (String d : dictionary) {
+                int n = d.length();
+                map.putIfAbsent(n, new ArrayList<>());
+                map.get(n).add(d);
+            }
+        }
+
+        public boolean search(String searchWord) {
+            int n = searchWord.length();
+            if (!map.containsKey(n)) {
+                return false;
+            }
+            for (String s : map.get(n)) {
+                if (check(s, searchWord)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private boolean check(String s1, String s2) {
+            int n = s1.length();
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    cnt++;
+                    if (cnt > 1) {
+                        return false;
+                    }
+                }
+            }
+            return cnt == 1;
+        }
+    }
+}
+
+class Problem0676ImplementMagicDictionary1 {
+
+    class MagicDictionary {
+
+        class Trie {
+            Trie[] tries;
+            boolean isWord;
+
+            Trie() {
+                tries = new Trie[26];
+            }
+        }
+
+        Trie root;
+
+        public MagicDictionary() {
+            root = new Trie();
+        }
+
+        public void buildDict(String[] dictionary) {
+            for (String dict : dictionary) {
+                Trie trie = root;
+                int n = dict.length();
+                for (int i = 0; i < n; i++) {
+                    int j = dict.charAt(i) - 'a';
+                    if (trie.tries[j] == null) {
+                        trie.tries[j] = new Trie();
+                    }
+                    trie = trie.tries[j];
+                }
+                trie.isWord = true;
+            }
+        }
+
+        public boolean search(String searchWord) {
+            return dfs(searchWord, root, 0, false);
+        }
+
+        private boolean dfs(String searchWord, Trie trie, int pos, boolean isModified) {
+            if (pos == searchWord.length()) {
+                return isModified && trie.isWord;
+            }
+            int c = searchWord.charAt(pos) - 'a';
+            if (trie.tries[c] != null && dfs(searchWord, trie.tries[c], pos + 1, isModified)) {
+                return true;
+            }
+            if (!isModified) {
+                for (int i = 0; i < 26; i++) {
+                    if (i == c || trie.tries[i] == null) {
+                        continue;
+                    }
+                    if (dfs(searchWord, trie.tries[i], pos + 1, true)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
+
+class MagicDictionary {
+    Trie root;
+
+    public MagicDictionary() {
+        root = new Trie();
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String word : dictionary) {
+            Trie cur = root;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                int idx = ch - 'a';
+                if (cur.child[idx] == null) {
+                    cur.child[idx] = new Trie();
+                }
+                cur = cur.child[idx];
+            }
+            cur.isFinished = true;
+        }
+    }
+
+    public boolean search(String searchWord) {
+        return dfs(searchWord, root, 0, false);
+    }
+
+    private boolean dfs(String searchWord, Trie node, int pos, boolean modified) {
+        if (pos == searchWord.length()) {
+            return modified && node.isFinished;
+        }
+        int idx = searchWord.charAt(pos) - 'a';
+        if (node.child[idx] != null) {
+            if (dfs(searchWord, node.child[idx], pos + 1, modified)) {
+                return true;
+            }
+        }
+        if (!modified) {
+            for (int i = 0; i < 26; ++i) {
+                if (i != idx && node.child[i] != null) {
+                    if (dfs(searchWord, node.child[i], pos + 1, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+class Trie {
+    boolean isFinished;
+    Trie[] child;
+
+    public Trie() {
+        isFinished = false;
+        child = new Trie[26];
+    }
+}
+
 

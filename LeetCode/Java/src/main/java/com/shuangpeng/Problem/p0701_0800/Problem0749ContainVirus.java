9252f5d362a177e53ee469235d51cb2a392701d9
shuangpengpang <15913598312@163.com>
2022-07-18 19:14:08 +0800

    隔离病毒（749）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0749ContainVirus.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0749ContainVirus.java
@@ -90,11 +90,96 @@ public class Problem0749ContainVirus {
     }
 }
 
-//class Problem0749ContainVirus0 {
-//
-//    static final int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-//    static final int M = (1 << 16) - 1;
-//
-//    public int containVirus(int[][] isInfected) {
-//    }
-//}
+class Problem0749ContainVirus0 {
+
+    static final int[] dirs = {-1, 0, 1, 0, -1};
+
+    public int containVirus(int[][] isInfected) {
+        int m = isInfected.length, n = isInfected[0].length;
+        int ans = 0;
+        while (true) {
+            int[][] states = new int[m][n];
+            int color = -1, maxCount = 0, r = 0, c = 0, w = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (isInfected[i][j] == 1 && states[i][j] == 0) {
+                        int[] wall = new int[1];
+                        int count = getNeighborCount(isInfected, states, color, i, j, wall);
+                        if (count > maxCount) {
+                            maxCount = count;
+                            r = i;
+                            c = j;
+                            w = wall[0];
+                        }
+                        color--;
+                    }
+                }
+            }
+            if (maxCount == 0) {
+                break;
+            }
+            ans += w;
+            buildWall(isInfected, r, c);
+            states = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (isInfected[i][j] == 1 && states[i][j] == 0) {
+                        spread(isInfected, states, i, j);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void spread(int[][] grid, int[][] states, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        states[i][j] = 1;
+        for (int d = 0; d < 4; d++) {
+            int x = i + dirs[d], y = j + dirs[d + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || states[x][y] != 0) {
+                continue;
+            }
+            if (grid[x][y] == 0) {
+                states[x][y] = 1;
+                grid[x][y] = 1;
+            } else if (grid[x][y] == 1) {
+                spread(grid, states, x, y);
+            }
+        }
+    }
+
+    private void buildWall(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        grid[i][j] = -1;
+        for (int d = 0; d < 4; d++) {
+            int x = i + dirs[d], y = j + dirs[d + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != 1) {
+                continue;
+            }
+            buildWall(grid, x, y);
+        }
+    }
+
+    private int getNeighborCount(int[][] grid, int[][] states, int color, int i, int j, int[] wall) {
+        int m = grid.length, n = grid[0].length;
+        states[i][j] = 1;
+        int ans = 0;
+        for (int d = 0; d < 4; d++) {
+            int x = i + dirs[d], y = j + dirs[d + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || states[x][y] == 1) {
+                continue;
+            }
+            if (grid[x][y] == 0) {
+                wall[0]++;
+                if (states[x][y] != color) {
+                    states[x][y] = color;
+                    ans++;
+                }
+            } else if (grid[x][y] == 1) {
+                ans += getNeighborCount(grid, states, color, x, y, wall);
+            }
+        }
+        return ans;
+    }
+}

adf63de7101651271abf363d45355571f9e327e7
shuangpengpang <15913598312@163.com>
2022-07-20 10:53:10 +0800

    二维网格迁移（1260）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1260Shift2DGrid.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1260Shift2DGrid.java
@@ -1,6 +1,7 @@
 package com.shuangpeng.Problem.p1201_1300;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,7 +11,7 @@ import java.util.List;
  */
 public class Problem1260Shift2DGrid {
 
-    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+    public List<List<Integer>> shiftGrid0(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length, N = m * n;
         List<List<Integer>> ans = new ArrayList<>(m);
         for (int i = 0; i < m; i++) {
@@ -28,5 +29,27 @@ public class Problem1260Shift2DGrid {
             }
         }
         return ans;
+    }
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length, N = m * n;
+        k = k % N;
+        int x = k / n, y = k % n;
+        int[][] map = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map[x][y] = grid[i][j];
+                y++;
+                if (y == n) {
+                    x++;
+                    y = 0;
+                    if (x == m) {
+                        x = 0;
+                    }
+                }
+            }
+        }
+        List list = Arrays.asList(map);
+        return list;
     }
 }

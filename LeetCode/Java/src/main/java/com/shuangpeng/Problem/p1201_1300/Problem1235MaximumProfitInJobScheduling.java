1c2f4493fb715f5fc48b20574bd3336092a1f1a1
shuangpengpang <15913598312@163.com>
2022-07-26 15:40:38 +0800

    规划兼职工作（1235）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1235MaximumProfitInJobScheduling.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1235MaximumProfitInJobScheduling.java
@@ -12,7 +12,7 @@ import java.util.stream.IntStream;
  */
 public class Problem1235MaximumProfitInJobScheduling {
 
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    public int jobScheduling0(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
         Integer[] ids = IntStream.range(0, n).boxed().toArray(Integer[]::new);
         Arrays.sort(ids, Comparator.comparingInt(a -> endTime[a]));
@@ -23,5 +23,27 @@ public class Problem1235MaximumProfitInJobScheduling {
             dp.put(e, Math.max(dp.get(dp.floorKey(s)) + profit[i], dp.lastEntry().getValue()));
         }
         return dp.lastEntry().getValue();
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        Integer[] ids = IntStream.range(0, n).boxed().toArray(Integer[]::new);
+        Arrays.sort(ids, Comparator.comparingInt(a -> endTime[a]));
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            int idx = ids[i], s = startTime[idx];
+            int left = 0, right = i - 1;
+            while (left <= right) {
+                int mid = left + (right - left >> 1);
+                if (s >= endTime[ids[mid]]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            int p = profit[idx] + (left == 0 ? 0 : dp[left - 1]);
+            dp[i] = Math.max(i > 0 ? dp[i - 1] : 0, p);
+        }
+        return dp[n - 1];
     }
 }

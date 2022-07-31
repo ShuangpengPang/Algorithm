765cd1ab8439a9a9888bbe2a15cdc182557fb07e
shuangpengpang <15913598312@163.com>
2022-07-31 18:20:35 +0800

    相等行列对（2352）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2352EqualRowAndColumnPairs.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2352EqualRowAndColumnPairs.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.competition.第303场周赛;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description: Problem2352EqualRowAndColumnPairs（相等行列对）
  * @Date 2022/7/31 5:45 PM
@@ -7,7 +10,7 @@ package com.shuangpeng.competition.第303场周赛;
  */
 public class Problem2352EqualRowAndColumnPairs {
 
-    public int equalPairs(int[][] grid) {
+    public int equalPairs0(int[][] grid) {
         int n = grid.length;
         int ans = 0;
         for (int i = 0; i < n; i++) {
@@ -23,6 +26,26 @@ public class Problem2352EqualRowAndColumnPairs {
                     ans++;
                 }
             }
+        }
+        return ans;
+    }
+
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        Map<String, Integer> map1 = new HashMap<>(), map2 = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb1 = new StringBuilder(), sb2 = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb1.append(grid[i][j]).append(",");
+                sb2.append(grid[j][i]).append(",");
+            }
+            String s1 = sb1.toString(), s2 = sb2.toString();
+            map1.put(s1, map1.getOrDefault(s1, 0) + 1);
+            map2.put(s2, map2.getOrDefault(s2, 0) + 1);
+        }
+        int ans = 0;
+        for (String s : map1.keySet()) {
+            ans += map1.get(s) * map2.getOrDefault(s, 0);
         }
         return ans;
     }

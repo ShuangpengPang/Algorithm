63c91f4c833b2a7352b73ae2d15a2d7c07a3d0f6
shuangpengpang <15913598312@163.com>
2022-07-31 10:20:04 +0800

    第一次出现两次的字母（2351）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2351FirstLetterToAppearTwice.java
@@ -0,0 +1,22 @@
+package com.shuangpeng.competition.第303场周赛;
+
+/**
+ * @Description: Problem2351FirstLetterToAppearTwice（第一个出现两次的字母）
+ * @Date 2022/7/29 7:37 PM
+ * @Version 1.0
+ */
+public class Problem2351FirstLetterToAppearTwice {
+
+    public char repeatedCharacter(String s) {
+        int n = s.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            cnt[c - 'a']++;
+            if (cnt[c - 'a'] == 2) {
+                return c;
+            }
+        }
+        return 'a';
+    }
+}

ab5147319d4dddbc8054bc4fdce02ba8b986cdb1
shuangpengpang <15913598312@163.com>
2022-07-31 17:39:01 +0800

    最大层内元素和（1161）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1161MaximumLevelSumOfABinaryTree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1161MaximumLevelSumOfABinaryTree.java
@@ -2,7 +2,9 @@ package com.shuangpeng.Problem.p1101_1200;
 
 import com.shuangpeng.common.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -12,7 +14,7 @@ import java.util.Queue;
  */
 public class Problem1161MaximumLevelSumOfABinaryTree {
 
-    public int maxLevelSum(TreeNode root) {
+    public int maxLevelSum0(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
         int level = 0;
@@ -36,5 +38,70 @@ public class Problem1161MaximumLevelSumOfABinaryTree {
             }
         }
         return maxLevel;
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, 0, list);
+        int maxSum = Integer.MIN_VALUE;
+        int n = list.size();
+        int maxLevel = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = list.get(i);
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxLevel = i;
+            }
+        }
+        return maxLevel + 1;
+    }
+
+    private void dfs(TreeNode root, int level, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        if (level == list.size()) {
+            list.add(root.val);
+        } else {
+            list.set(level, list.get(level) + root.val);
+        }
+        dfs(root.left, level + 1, list);
+        dfs(root.right, level + 1, list);
+    }
+}
+
+class Problem1161MaximumLevelSumOfABinaryTree0 {
+
+    int maxSum;
+    int maxLevel;
+
+    public int maxLevelSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        maxLevel = 0;
+        dfs(1, 1, root);
+        return maxLevel;
+    }
+
+    private void dfs(int layer, int len, TreeNode... nodes) {
+        TreeNode[] arr = new TreeNode[len << 1];
+        int sum = 0;
+        int size = 0;
+        for (int i = 0; i < len; i++) {
+            TreeNode node = nodes[i];
+            sum += node.val;
+            if (node.left != null) {
+                arr[size++] = node.left;
+            }
+            if (node.right != null) {
+                arr[size++] = node.right;
+            }
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxLevel = layer;
+        }
+        if (size > 0) {
+            dfs(layer + 1, size, arr);
+        }
     }
 }

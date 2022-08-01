d2c7d8b4fbd042f99f0333a443b170164c422e59
shuangpengpang <15913598312@163.com>
2022-08-01 16:55:18 +0800

    第303场周赛： 1、第一个出现两次的字母（2351） 2、相等行列对（2352） 3、设计食物评分系统（2353） 4、优质数对的数目（2354）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2353DesignAFoodRatingSystem.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2353DesignAFoodRatingSystem.java
@@ -1,8 +1,8 @@
 package com.shuangpeng.competition.第303场周赛;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeSet;
+import javafx.util.Pair;
+
+import java.util.*;
 
 /**
  * @Description: Problem2353DesignAFoodRatingSystem（设计食物评分系统）
@@ -70,3 +70,207 @@ public class Problem2353DesignAFoodRatingSystem {
  * String param_2 = obj.highestRated(cuisine);
  */
 }
+
+class Problem2353DesignAFoodRatingSystem0 {
+
+    class FoodRatings {
+
+        class Info implements Comparable<Info> {
+            String food;
+            int rating;
+
+            Info(String food, int rating) {
+                this.food = food;
+                this.rating = rating;
+            }
+
+            @Override
+            public int compareTo(Info info) {
+                if (this.rating != info.rating) {
+                    return info.rating - this.rating;
+                }
+                return this.food.compareTo(info.food);
+            }
+        }
+
+        Map<String, Integer> ratingMap;
+        Map<String, TreeSet<Info>> cuisinesMap;
+        Map<String, String> foodMap;
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            ratingMap = new HashMap<>();
+            cuisinesMap = new HashMap<>();
+            foodMap = new HashMap<>();
+            int n = foods.length;
+            for (int i = 0; i < n; i++) {
+                String f = foods[i];
+                String c = cuisines[i];
+                int r = ratings[i];
+                ratingMap.put(f, r);
+                cuisinesMap.putIfAbsent(c, new TreeSet<Info>());
+                cuisinesMap.get(c).add(new Info(f, r));
+                foodMap.put(f, c);
+            }
+        }
+
+        public void changeRating(String food, int newRating) {
+            ratingMap.put(food, newRating);
+            String c = foodMap.get(food);
+            cuisinesMap.get(c).add(new Info(food, newRating));
+        }
+
+        public String highestRated(String cuisine) {
+            TreeSet<Info> set = cuisinesMap.get(cuisine);
+            while (set.first().rating != ratingMap.get(set.first().food)) {
+                set.remove(set.first());
+            }
+            return set.first().food;
+        }
+    }
+}
+
+class Problem2353DesignAFoodRatingSystem1 {
+
+    class FoodRatings {
+
+        Map<String, Pair<String, Integer>> fs ;
+        Map<String, TreeSet<Pair<String, Integer>>> cs;
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            fs = new HashMap<>();
+            cs = new HashMap<>();
+            int n = foods.length;
+            for (int i = 0; i < n; i++) {
+                String f = foods[i];
+                String c = cuisines[i];
+                int r = ratings[i];
+                fs.put(f, new Pair<>(c, r));
+                cs.computeIfAbsent(c, k -> new TreeSet<>((a, b) -> !Objects.equals(a.getValue(), b.getValue()) ? b.getValue() - a.getValue(): a.getKey().compareTo(b.getKey()))).add(new Pair<>(f, r));
+            }
+        }
+
+        public void changeRating(String food, int newRating) {
+            Pair<String, Integer> old = fs.get(food);
+            String c = old.getKey();
+            int r = old.getValue();
+            cs.get(c).remove(new Pair<>(food, r));
+            cs.get(c).add(new Pair<>(food, newRating));
+            fs.put(food, new Pair<>(c, newRating));
+        }
+
+        public String highestRated(String cuisine) {
+            return cs.get(cuisine).first().getKey();
+        }
+    }
+}
+
+class Problem2353DesignAFoodRatingSystem2 {
+
+    class FoodRatings {
+
+        class Item {
+            String s;
+            int i;
+
+            Item(String s, int i) {
+                this.s = s;
+                this.i = i;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                Item other = (Item) o;
+                return s.equals(other.s) && i == other.i;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(s, i);
+            }
+        }
+
+        Map<String, Item> fs ;
+        Map<String, TreeSet<Item>> cs;
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            fs = new HashMap<>();
+            cs = new HashMap<>();
+            int n = foods.length;
+            for (int i = 0; i < n; i++) {
+                String f = foods[i];
+                String c = cuisines[i];
+                int r = ratings[i];
+                fs.put(f, new Item(c, r));
+                cs.computeIfAbsent(c, k -> new TreeSet<>((a, b) -> a.i != b.i ? b.i - a.i : a.s.compareTo(b.s))).add(new Item(f, r));
+            }
+        }
+
+        public void changeRating(String food, int newRating) {
+            Item old = fs.get(food);
+            String c = old.s;
+            int r = old.i;
+            cs.get(c).remove(new Item(food, r));
+            cs.get(c).add(new Item(food, newRating));
+            old.i = newRating;
+        }
+
+        public String highestRated(String cuisine) {
+            return cs.get(cuisine).first().s;
+        }
+    }
+}
+
+class Problem2353DesignAFoodRatingSystem3 {
+
+    class FoodRatings {
+
+        class Item {
+            String c;
+            String f;
+            int i;
+            boolean invalid;
+
+            Item(String f, String c, int i) {
+                this.f = f;
+                this.c = c;
+                this.i = i;
+                this.invalid = false;
+            }
+        }
+
+        Map<String, Item> fs ;
+        Map<String, PriorityQueue<Item>> cs;
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            fs = new HashMap<>();
+            cs = new HashMap<>();
+            int n = foods.length;
+            for (int i = 0; i < n; i++) {
+                String f = foods[i];
+                String c = cuisines[i];
+                int r = ratings[i];
+                Item item = new Item(f, c, r);
+                fs.put(f, item);
+                cs.computeIfAbsent(c, k -> new PriorityQueue<>((a, b) -> a.i != b.i ? b.i - a.i : a.f.compareTo(b.f))).add(item);
+            }
+        }
+
+        public void changeRating(String food, int newRating) {
+            Item old = fs.get(food);
+            String c = old.c;
+            old.invalid = true;
+            Item item = new Item(food, c, newRating);
+            fs.put(food, item);
+            cs.get(c).add(item);
+        }
+
+        public String highestRated(String cuisine) {
+            PriorityQueue<Item> q = cs.get(cuisine);
+            while (q.peek().invalid) {
+                q.poll();
+            }
+            return q.peek().f;
+        }
+    }
+}
+
--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2354NumberOfExcellentPairs.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第303场周赛/Problem2354NumberOfExcellentPairs.java
@@ -2,6 +2,7 @@ package com.shuangpeng.competition.第303场周赛;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * @Description: Problem2354NumberOfExcellentPairs（优质数对的数目）
@@ -10,7 +11,35 @@ import java.util.Set;
  */
 public class Problem2354NumberOfExcellentPairs {
 
-    public long countExcellentPairs(int[] nums, int k) {
+    // 比赛时写法
+    public long countExcellentPairs0(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        TreeMap<Integer, Integer> map = new TreeMap<>((a, b) -> b - a);
+        for (int num : set) {
+            int count = Integer.bitCount(num);
+            map.put(count, map.getOrDefault(count, 0) + 1);
+        }
+        TreeMap<Integer, Long> preSum = new TreeMap<>();
+        long count = 0L;
+        for (int num : map.keySet()) {
+            count += map.get(num);
+            preSum.put(num, count);
+        }
+        long ans = 0L;
+        for (int num : map.keySet()) {
+            int cnt = map.get(num);
+            Integer key = preSum.ceilingKey(k - num);
+            if (key != null) {
+                ans += cnt * preSum.get(key);
+            }
+        }
+        return ans;
+    }
+
+    public long countExcellentPairs1(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
@@ -27,6 +56,30 @@ public class Problem2354NumberOfExcellentPairs {
                     count += cnt[j];
                 }
                 ans += (long) cnt[i] * count;
+            }
+        }
+        return ans;
+    }
+
+    public long countExcellentPairs(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int N = 30;
+        int[] cnt = new int[N];
+        for (int num : nums) {
+            if (set.add(num)) {
+                cnt[Integer.bitCount(num)]++;
+            }
+        }
+        int s = 0;
+        for (int i = k; i < N; i++) {
+            s += cnt[i];
+        }
+        long ans = 0L;
+        for (int i = 0; i < N; i++) {
+            ans += (long) cnt[i] * s;
+            int j = k - i - 1;
+            if (j >= 0 && j < N) {
+                s += cnt[j];
             }
         }
         return ans;

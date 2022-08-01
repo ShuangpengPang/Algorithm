babe4c5afad6ec1da7743ab3f19ab1b765ae0ef1
shuangpengpang <15913598312@163.com>
2022-08-01 12:16:29 +0800

    生成每种字符都是奇数个的字符串（1374）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1374GenerateAStringWithCharactersThatHaveOddCounts.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1374GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -1,5 +1,7 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.util.Arrays;
+
 /**
  * @Description: Problem1374GenerateAStringWithCharactersThatHaveOddCounts（生成每种字符都是奇数个的字符串）
  * @Date 2022/8/1 11:12 AM
@@ -7,7 +9,7 @@ package com.shuangpeng.Problem.p1301_1400;
  */
 public class Problem1374GenerateAStringWithCharactersThatHaveOddCounts {
 
-    public String generateTheString(int n) {
+    public String generateTheString0(int n) {
         StringBuilder sb = new StringBuilder();
         if ((n & 1) == 0) {
             sb.append('b');
@@ -17,5 +19,14 @@ public class Problem1374GenerateAStringWithCharactersThatHaveOddCounts {
             sb.append('a');
         }
         return sb.toString();
+    }
+
+    public String generateTheString(int n) {
+        char[] chars = new char[n];
+        Arrays.fill(chars, 'a');
+        if ((n & 1) == 0) {
+            chars[0] = 'b';
+        }
+        return new String(chars);
     }
 }

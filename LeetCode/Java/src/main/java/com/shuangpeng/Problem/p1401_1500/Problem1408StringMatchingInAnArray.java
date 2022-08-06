5925624e1c74f2f1ddd7be15eea18991d8959f86
shuangpengpang <15913598312@163.com>
2022-08-06 15:22:38 +0800

    数组中的字符串匹配（1408）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1408StringMatchingInAnArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1408StringMatchingInAnArray.java
@@ -44,7 +44,7 @@ public class Problem1408StringMatchingInAnArray {
         return false;
     }
 
-    public List<String> stringMatching(String[] words) {
+    public List<String> stringMatching1(String[] words) {
         Arrays.sort(words, Comparator.comparingInt(a -> a.length()));
         int n = words.length;
         List<String> ans = new ArrayList<>();
@@ -86,5 +86,21 @@ public class Problem1408StringMatchingInAnArray {
             idx = s1.charAt(j) == c ? j + 1 : j;
         }
         return idx == n1;
+    }
+
+    public List<String> stringMatching(String[] words) {
+        int n = words.length;
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    if (words[j].contains(words[i])) {
+                        ans.add(words[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }

7ed2ee5acafae2c471c8c4b35e923d0000cb34aa
shuangpengpang <15913598312@163.com>
2022-08-23 16:21:44 +0800

    变为棋盘（782）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0782TransformToChessboard.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0782TransformToChessboard.java
@@ -1,11 +1,14 @@
 package com.shuangpeng.Problem.p0701_0800;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * @Description: 变为棋盘
+ * @Date 2022/8/23 4:20 PM
+ **/
 public class Problem0782TransformToChessboard {
 
-    public int movesToChessboard(int[][] board) {
+    public int movesToChessboard0(int[][] board) {
         int n = board.length;
         Map<Integer, Integer> count = new HashMap<>();
         for (int[] row : board) {
@@ -65,4 +68,51 @@ public class Problem0782TransformToChessboard {
         k1 = Integer.bitCount(k1) > Integer.bitCount(k2) ? k1 : k2;
         return Integer.bitCount(k1 ^ M2) >> 1;
     }
+
+    public int movesToChessboard(int[][] board) {
+        int n = board.length;
+        int rowMask = 0, colMask = 0;
+        for (int i = 0; i < n; i++) {
+            rowMask |= board[0][i] << i;
+            colMask |= board[i][0] << i;
+        }
+        if (Math.abs(Integer.bitCount(rowMask) * 2 - n) > 1 || Math.abs(Integer.bitCount(colMask) * 2 - n) > 1) {
+            return -1;
+        }
+        int reverseRowMask = ((1 << n) - 1) ^ rowMask, reverseColMask = ((1 << n) - 1) ^ colMask;
+        int rowCnt = 0, colCnt = 0;
+        for (int i = 0; i < n; i++) {
+            int curRow = 0, curCol = 0;
+            for (int j = 0; j < n; j++) {
+                curRow |= board[i][j] << j;
+                curCol |= board[j][i] << j;
+            }
+            if (curRow != rowMask && curRow != reverseRowMask || curCol != colMask && curCol != reverseColMask) {
+                return -1;
+            }
+            if (curRow == rowMask) {
+                rowCnt++;
+            }
+            if (curCol == colMask) {
+                colCnt++;
+            }
+        }
+        if (Math.abs(rowCnt * 2 - n) > 1 || Math.abs(colCnt * 2 - n) > 1) {
+            return -1;
+        }
+        return getCount(rowMask, n) + getCount(colMask, n);
+    }
+
+    private int getCount(int mask, int n) {
+        int m1 = 0xAAAAAAAA, m2 = 0x55555555;
+        int cnt = Integer.bitCount(mask);
+        if ((n & 1) == 1) {
+            if (cnt * 2 - n == 1) {
+                return cnt - Integer.bitCount(mask & m2);
+            }
+            return cnt - Integer.bitCount(mask & m1);
+        }
+        return cnt - Math.max(Integer.bitCount(mask & m1), Integer.bitCount(mask & m2));
+    }
 }
+

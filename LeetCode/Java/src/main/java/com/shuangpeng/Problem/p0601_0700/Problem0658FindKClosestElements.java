809901515a77d5851bcabfaa8e70bff77226a6ad
shuangpengpang <15913598312@163.com>
2022-08-25 14:58:07 +0800

    找到K个最接近的元素（658）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0658FindKClosestElements.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0658FindKClosestElements.java
@@ -1,9 +1,6 @@
 package com.shuangpeng.Problem.p0601_0700;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Description: Problem0658FindKClosestElements（找到K个最接近的元素）
@@ -59,7 +56,7 @@ public class Problem0658FindKClosestElements {
         return left - 1;
     }
 
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    public List<Integer> findClosestElements1(int[] arr, int k, int x) {
         int n = arr.length;
         int left = 0, right = k;
         for (int l = 0, r = k; r < n; l++, r++) {
@@ -77,4 +74,57 @@ public class Problem0658FindKClosestElements {
         }
         return ans;
     }
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> copy = new ArrayList<>(arr.length);
+        for (int num : arr) {
+            copy.add(num);
+        }
+        copy.sort((a, b) -> {
+            if (Math.abs(x - a) != Math.abs(x - b)) {
+                return Math.abs(x - a) - Math.abs(x - b);
+            }
+            return a - b;
+        });
+        List<Integer> ans = copy.subList(0, k);
+        ans.sort(Comparator.comparingInt(a -> a));
+        return ans;
+    }
 }
+
+class Problem0658FindKClosestElements0 {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = binarySearch(arr, x), right = left, n = arr.length;
+        while (k-- > 0) {
+            if (right == n) {
+                left--;
+            } else if (left == 0) {
+                right++;
+            } else if (x - arr[left - 1] <= arr[right] - x) {
+                left--;
+            } else {
+                right++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>(right - left);
+        for (int i = left; i < right; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+
+    private int binarySearch(int[] arr, int x) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (x >= arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+

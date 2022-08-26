a46f8d043541f958d18797ec723aecb5c8e5d163
shuangpengpang <15913598312@163.com>
2022-08-26 22:20:08 +0800

    形成目标数组的子数组最少增加次数（1526）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1526MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1526MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
@@ -7,13 +7,21 @@ package com.shuangpeng.Problem.p1501_1600;
  */
 public class Problem1526MinimumNumberOfIncrementsOnSubarraysToFormATargetArray {
 
-    public int minNumberOperations(int[] target) {
+    public int minNumberOperations0(int[] target) {
         int ans = 0, pre = 0;
         for (int num : target) {
             if (num > pre) {
                 ans += num - pre;
             }
             pre = num;
+        }
+        return ans;
+    }
+
+    public int minNumberOperations(int[] target) {
+        int ans = target[0], n = target.length;
+        for (int i = 1; i < n; i++) {
+            ans += Math.max(target[i] - target[i - 1], 0);
         }
         return ans;
     }

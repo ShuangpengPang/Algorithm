18187b3d7fabf90278e13773dcd44838dd179ec9
shuangpengpang <15913598312@163.com>
2022-08-28 18:00:15 +0800

    阶乘函数后K个零（793）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0793PreimageSizeOfFactorialZeroesFunction.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0793PreimageSizeOfFactorialZeroesFunction.java
@@ -2,7 +2,7 @@ package com.shuangpeng.Problem.p0701_0800;
 
 public class Problem0793PreimageSizeOfFactorialZeroesFunction {
 
-    public int preimageSizeFZF(int k) {
+    public int preimageSizeFZF0(int k) {
         long left = 0, right = (long) k * 5;
         while (left <= right) {
             long mid = left + ((right - left) >> 1);
@@ -21,6 +21,31 @@ public class Problem0793PreimageSizeOfFactorialZeroesFunction {
             }
         }
         return 0;
+    }
+
+    public int preimageSizeFZF(int k) {
+        long left = 0, right = k * 5L;
+        while (left <= right) {
+            long mid = left + (right - left >> 1);
+            long cnt = getCount(mid);
+            if (k > cnt) {
+                left = mid + 1;
+            } else if (k < cnt) {
+                right = mid - 1;
+            } else {
+                return 5;
+            }
+        }
+        return 0;
+    }
+
+    private long getCount(long n) {
+        long base = 5, cnt = 0;
+        while (base <= n) {
+            cnt += n / base;
+            base *= 5;
+        }
+        return cnt;
     }
 
 //    public static void main(String[] args) {

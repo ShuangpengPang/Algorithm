c742c141c3dfe7ecbac6f7aa705ed782b479629e
shuangpengpang <15913598312@163.com>
2022-08-30 10:34:10 +0800

    最大二叉树II（998）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0998MaximumBinaryTreeII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0998MaximumBinaryTreeII.java
@@ -2,9 +2,13 @@ package com.shuangpeng.Problem.p0901_1000;
 
 import com.shuangpeng.common.TreeNode;
 
+/**
+ * @Description:（最大二叉树II）
+ * @Date 2022/8/30 10:26 AM
+ **/
 public class Problem0998MaximumBinaryTreeII {
 
-    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+    public TreeNode insertIntoMaxTree0(TreeNode root, int val) {
         if (root == null) {
             return new TreeNode(val);
         }
@@ -35,5 +39,20 @@ public class Problem0998MaximumBinaryTreeII {
             treeNode.right = getTreeNode(node.right, val, true);
         }
         return treeNode;
+    }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode node = new TreeNode(val);
+        if (val > root.val) {
+            node.left = root;
+            return node;
+        }
+        TreeNode cur = root;
+        while (cur.right != null && cur.right.val > val) {
+            cur = cur.right;
+        }
+        node.left = cur.right;
+        cur.right = node;
+        return root;
     }
 }

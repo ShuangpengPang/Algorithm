427d0cf41236ffc0ed98a527fcb80ec3cf1655b3
shuangpengpang <15913598312@163.com>
2022-08-31 16:23:11 +0800

    石子游戏（1563）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1563StoneGameV.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1563StoneGameV.java
@@ -7,7 +7,7 @@ package com.shuangpeng.Problem.p1501_1600;
  */
 public class Problem1563StoneGameV {
 
-    public int stoneGameV(int[] stoneValue) {
+    public int stoneGameV0(int[] stoneValue) {
         int n = stoneValue.length;
         int[] preSum = new int[n + 1];
         for (int i = 1; i <= n; i++) {
@@ -32,4 +32,31 @@ public class Problem1563StoneGameV {
         }
         return dp[0][n - 1];
     }
+
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[][] dp = new int[n][n], maxLeft = new int[n][n], maxRight = new int[n][n];
+        for (int l = n - 1; l >= 0; l--) {
+            maxLeft[l][l] = maxRight[l][l] = stoneValue[l];
+            for (int r = l + 1, sum = stoneValue[l], i = l - 1, sumLeft = 0; r < n; r++) {
+                sum += stoneValue[r];
+                while (i + 1 < r && (sumLeft + stoneValue[i + 1]) << 1 <= sum) {
+                    sumLeft += stoneValue[++i];
+                }
+                if (l <= i) {
+                    dp[l][r] = Math.max(dp[l][r], maxLeft[l][i]);
+                }
+                if (i + 2 <= r) {
+                    dp[l][r] = Math.max(dp[l][r], maxRight[i + 2][r]);
+                }
+                if (sumLeft << 1 == sum) {
+                    dp[l][r] = Math.max(dp[l][r], maxRight[i + 1][r]);
+                }
+                maxLeft[l][r] = Math.max(maxLeft[l][r - 1], dp[l][r] + sum);
+                maxRight[l][r] = Math.max(maxRight[l + 1][r], dp[l][r] + sum);
+            }
+        }
+        return dp[0][n - 1];
+    }
 }
+

aee7e5668bfeaf6ce16cf5a3d63f488c1318088e
shuangpengpang <15913598312@163.com>
2022-09-02 17:06:28 +0800

    连通两组点的最小成本（1595）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1595MinimumCostToConnectTwoGroupsOfPoints.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1595MinimumCostToConnectTwoGroupsOfPoints.java
@@ -10,6 +10,31 @@ import java.util.List;
  */
 public class Problem1595MinimumCostToConnectTwoGroupsOfPoints {
 
+    // 复杂度较高
+    public int connectTwoGroups0(List<List<Integer>> cost) {
+        int m = cost.size(), n = cost.get(0).size();
+        int[][] costMatrix = new int[m][1 << n];
+        for (int k = 0; k < m; k++) {
+            for (int i = 0; i < (1 << n); i++) {
+                int sum = 0;
+                for (int j = 0; j < n; j++) {
+                    if ((i & (1 << j)) > 0)
+                        sum += cost.get(k).get(j);
+                }
+                costMatrix[k][i] = sum;
+            }
+        }
+        int[][] dp = new int[m][1 << n];
+        for (int i = 1; i < m; i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        dp[0] = costMatrix[0];
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < (1 << n); j++)
+                for (int k = 1; k < (1 << n); k++)
+                    dp[i][j | k] = Math.min(dp[i][j | k], dp[i - 1][k] + costMatrix[i][j]);
+        return dp[m - 1][(1 << n) - 1];
+    }
+
     public int connectTwoGroups(List<List<Integer>> cost) {
         int m = cost.size(), n = cost.get(0).size(), N = 1 << n;
         int[][] dp = new int[m + 1][N];

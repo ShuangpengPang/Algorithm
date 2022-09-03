30dfe4bc960e0d139ebc9b7dd38618f41c060e0b
shuangpengpang <15913598312@163.com>
2022-09-03 18:59:33 +0800

    最长数对链（646）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0646MaximumLengthOfPairChain.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0646MaximumLengthOfPairChain.java
@@ -3,7 +3,27 @@ package com.shuangpeng.Problem.p0601_0700;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * @Description:d（最长数对链）
+ * @Date 2022/9/3 6:58 PM
+ **/
 public class Problem0646MaximumLengthOfPairChain {
+
+    public int findLongestChain0(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[0]));
+        int n = pairs.length;
+        int[] dp = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int j = i - 1;
+            while (j >= 0 && pairs[j][1] >= pairs[i][0]) {
+                j--;
+            }
+            dp[i] = j >= 0 ? dp[j] + 1 : 1;
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
+    }
 
     public int findLongestChain(int[][] pairs) {
         Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));

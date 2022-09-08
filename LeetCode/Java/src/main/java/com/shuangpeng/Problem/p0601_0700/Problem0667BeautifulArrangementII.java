e5e6e9884f9d05b7ed1f262ce54c8c4beca69f23
shuangpengpang <15913598312@163.com>
2022-09-08 11:09:01 +0800

    优美的排列II（667）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0667BeautifulArrangementII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0667BeautifulArrangementII.java
@@ -7,7 +7,7 @@ package com.shuangpeng.Problem.p0601_0700;
  */
 public class Problem0667BeautifulArrangementII {
 
-    public int[] constructArray(int n, int k) {
+    public int[] constructArray0(int n, int k) {
         int[] ans = new int[n];
         for (int i = 0; i < n - k; i++) {
             ans[i] = i + 1;
@@ -19,6 +19,20 @@ public class Problem0667BeautifulArrangementII {
                 ans[i] = l++;
             }
             f ^= 1;
+        }
+        return ans;
+    }
+
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n - k; i++) {
+            ans[i] = i + 1;
+        }
+        for (int idx = n - k, i = n, j = n - k + 1; i >= j; i--, j++) {
+            ans[idx++] = i;
+            if (i > j) {
+                ans[idx++] = j;
+            }
         }
         return ans;
     }

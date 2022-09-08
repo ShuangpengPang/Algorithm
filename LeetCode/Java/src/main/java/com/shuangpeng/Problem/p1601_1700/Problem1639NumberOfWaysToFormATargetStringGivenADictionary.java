f209f7f493a76147719865a9d6cafbbf9ff8b0d5
shuangpengpang <15913598312@163.com>
2022-09-08 15:08:51 +0800

    通过给定词典构造目标字符串的方案数（1639）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1639NumberOfWaysToFormATargetStringGivenADictionary.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1639NumberOfWaysToFormATargetStringGivenADictionary.java
@@ -1,5 +1,7 @@
 package com.shuangpeng.Problem.p1601_1700;
 
+import java.util.Arrays;
+
 /**
  * @Description: Problem1639NumberOfWaysToFormATargetStringGivenADictionary（通过给定词典构造目标字符串的方案数）
  * @Date 2022/9/8 12:01 PM
@@ -7,7 +9,7 @@ package com.shuangpeng.Problem.p1601_1700;
  */
 public class Problem1639NumberOfWaysToFormATargetStringGivenADictionary {
 
-    public int numWays(String[] words, String target) {
+    public int numWays0(String[] words, String target) {
         int m = words[0].length(), n = target.length(), M = (int) 1e9 + 7;
         int[][] cnt = new int[m][26];
         for (String w : words) {
@@ -27,4 +29,27 @@ public class Problem1639NumberOfWaysToFormATargetStringGivenADictionary {
         }
         return dp[n][m];
     }
+
+    public int numWays(String[] words, String target) {
+        int m = words[0].length(), n = target.length(), M = (int) 1e9 + 7;
+        int[][] cnt = new int[m][26];
+        for (String w : words) {
+            for (int i = 0; i < m; i++) {
+                cnt[i][w.charAt(i) - 'a']++;
+            }
+        }
+        int[] dp = new int[m];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) {
+            int c = target.charAt(i) - 'a', last = i == 0 ? 1 : 0, prev = 0;
+            for (int j = 0; j < m; j++) {
+                int ans = (int) (((long) last * cnt[j][c] + prev) % M);
+                last = dp[j];
+                dp[j] = ans;
+                prev = ans;
+            }
+        }
+        return dp[m - 1];
+    }
 }
+

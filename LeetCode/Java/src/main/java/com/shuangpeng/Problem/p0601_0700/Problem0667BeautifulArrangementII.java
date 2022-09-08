fe079fbbf7678139238539b00dcc4ea681a68a3b
shuangpengpang <15913598312@163.com>
2022-09-08 10:47:12 +0800

    优美的排列II（667）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0667BeautifulArrangementII.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p0601_0700;
+
+/**
+ * @Description: Problem0667BeautifulArrangementII（优美的排列II）
+ * @Date 2022/9/8 10:14 AM
+ * @Version 1.0
+ */
+public class Problem0667BeautifulArrangementII {
+
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n - k; i++) {
+            ans[i] = i + 1;
+        }
+        for (int i = n - k, l = n - k + 1, r = n, f = 0; i < n; i++) {
+            if ((f & 1) == 0) {
+                ans[i] = r--;
+            } else {
+                ans[i] = l++;
+            }
+            f ^= 1;
+        }
+        return ans;
+    }
+}

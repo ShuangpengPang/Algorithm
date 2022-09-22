60cb7fd8a80f56a5a370da6dad40d0ac3dc4e859
shuangpengpang <15913598312@163.com>
2022-09-22 10:52:24 +0800

    是否连接形成数组（1640）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1640CheckArrayFormationThroughConcatenation.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1640CheckArrayFormationThroughConcatenation.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class Problem1640CheckArrayFormationThroughConcatenation {
 
-    public boolean canFormArray(int[] arr, int[][] pieces) {
+    public boolean canFormArray0(int[] arr, int[][] pieces) {
         int[] map = new int[101];
         int n = arr.length;
         Arrays.fill(map, -1);
@@ -23,6 +23,29 @@ public class Problem1640CheckArrayFormationThroughConcatenation {
             }
             for (int i = 1; i < m; i++) {
                 if (map[p[i]] != map[p[i - 1]] + 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        int n = arr.length, m = pieces.length;
+        int[] map = new int[101];
+        Arrays.fill(map, -1);
+        for (int i = 0; i < m; i++) {
+            map[pieces[i][0]] = i;
+        }
+        int i = 0;
+        while (i < n) {
+            int idx = map[arr[i]];
+            if (idx == -1) {
+                return false;
+            }
+            int[] p = pieces[idx];
+            for (int j = 0; j < p.length; j++) {
+                if (arr[i++] != p[j]) {
                     return false;
                 }
             }

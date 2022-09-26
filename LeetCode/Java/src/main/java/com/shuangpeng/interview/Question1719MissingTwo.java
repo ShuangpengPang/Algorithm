5508ff408af28b03b32a1498abe356487dab5b2f
shuangpengpang <15913598312@163.com>
2022-09-26 15:26:53 +0800

    面试题1719 消失的两个数字

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1719MissingTwo.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1719MissingTwo.java
@@ -7,7 +7,7 @@ package com.shuangpeng.interview;
  */
 public class Question1719MissingTwo {
 
-    public int[] missingTwo(int[] nums) {
+    public int[] missingTwo0(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             while (nums[i] <= n && nums[i] != i + 1) {
@@ -34,5 +34,89 @@ public class Question1719MissingTwo {
             b = (n << 1) + 3 - c;
         }
         return new int[]{a, b};
+    }
+
+    public int[] missingTwo1(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        for (int i = 1; i <= n + 2; i++) {
+            xor ^= i;
+        }
+        int lowBit = xor & (-xor);
+        int x1 = 0, x2 = 0;
+        for (int num : nums) {
+            if ((num & lowBit) == 0) {
+                x1 ^= num;
+            } else {
+                x2 ^= num;
+            }
+        }
+        for (int i = 1; i <= n + 2; i++) {
+            if ((i & lowBit) == 0) {
+                x1 ^= i;
+            } else {
+                x2 ^= i;
+            }
+        }
+        return new int[]{x1, x2};
+    }
+
+    public int[] missingTwo2(int[] nums) {
+        int n = nums.length + 2, sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int sumTwo = ((n + 1) * n >> 1) - sum, h = sumTwo >> 1;
+        sum = 0;
+        for (int num : nums) {
+            if (num <= h) {
+                sum += num;
+            }
+        }
+        int x = ((h + 1) * h >> 1) - sum;
+        return new int[]{x, sumTwo - x};
+    }
+
+    public int[] missingTwo(int[] nums) {
+        int n = nums.length, a = -1, b = -1;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != -1 && nums[i] != i + 1) {
+                int j = nums[i] - 1;
+                if (j < n) {
+                    nums[i] = nums[j];
+                    nums[j] = j + 1;
+                } else if (j == n) {
+                    nums[i] = a;
+                    a = j + 1;
+                } else {
+                    nums[i] = b;
+                    b = j + 1;
+                }
+            }
+        }
+        int x = -1, y = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == -1) {
+                if (x == -1) {
+                    x = i + 1;
+                } else {
+                    y = i + 1;
+                }
+            }
+        }
+        if (a == -1) {
+            if (x == -1) {
+                x = n + 1;
+            } else {
+                y = n + 1;
+            }
+        }
+        if (b == -1) {
+            y = n + 2;
+        }
+        return new int[]{x, y};
     }
 }

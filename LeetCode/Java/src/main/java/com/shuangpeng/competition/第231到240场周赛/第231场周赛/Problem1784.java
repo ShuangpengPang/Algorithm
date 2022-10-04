82929716c17317aca60820248198070b53a7c523
shuangpengpang <15913598312@163.com>
2022-10-04 14:26:17 +0800

    检查二进制字符串字段（1784）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第231到240场周赛/第231场周赛/Problem1784.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第231到240场周赛/第231场周赛/Problem1784.java
@@ -1,8 +1,12 @@
 package com.shuangpeng.competition.第231到240场周赛.第231场周赛;
 
+/**
+ * @Description:（检查二进制字符串字段）
+ * @Date 2022/10/4 2:24 PM
+ **/
 public class Problem1784 {
 
-    public boolean checkOnesSegment(String s) {
+    public boolean checkOnesSegment0(String s) {
         if (s == null || s.length() == 0) {
             return false;
         }
@@ -13,6 +17,22 @@ public class Problem1784 {
                 return false;
             } else if (s.charAt(i) == '0') {
                 flag = true;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkOnesSegment1(String s) {
+        return !s.contains("01");
+    }
+
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                if (i > 0 && s.charAt(i - 1) == '0') {
+                    return false;
+                }
             }
         }
         return true;

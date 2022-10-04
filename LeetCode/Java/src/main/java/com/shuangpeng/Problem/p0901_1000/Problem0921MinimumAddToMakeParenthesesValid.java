a6efc279f949cc72ed2a8ce017d4dec9d34faa4b
shuangpengpang <15913598312@163.com>
2022-10-04 13:14:36 +0800

    使括号有效的最少添加（921）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0921MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,27 @@
+package com.shuangpeng.Problem.p0901_1000;
+
+/**
+ * @Description: Problem0921MinimumAddToMakeParenthesesValid（使括号有效的最少添加）
+ * @Date 2022/10/4 1:12 PM
+ * @Version 1.0
+ */
+public class Problem0921MinimumAddToMakeParenthesesValid {
+
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        int cnt = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                if (cnt < 0) {
+                    ans += -cnt;
+                    cnt = 1;
+                } else {
+                    cnt++;
+                }
+            } else {
+                cnt--;
+            }
+        }
+        return ans + Math.abs(cnt);
+    }
+}

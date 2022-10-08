9c9ec738a076620956930538fec1906dd4ee13d8
shuangpengpang <15913598312@163.com>
2022-10-08 18:22:45 +0800

    最大升序子数组和（1800）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第231到240场周赛/第233场周赛/Problem1800.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第231到240场周赛/第233场周赛/Problem1800.java
@@ -1,8 +1,12 @@
 package com.shuangpeng.competition.第231到240场周赛.第233场周赛;
 
+/**
+ * @Description:（最大升序子数组和）
+ * @Date 2022/10/8 6:21 PM
+ **/
 public class Problem1800 {
 
-    public int maxAscendingSum(int[] nums) {
+    public int maxAscendingSum0(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -17,5 +21,18 @@ public class Problem1800 {
             right++;
         }
         return answer;
+    }
+
+    public int maxAscendingSum(int[] nums) {
+        int ans = 0, sum = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || nums[i] > nums[i - 1]) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
     }
 }

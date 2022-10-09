6d42bc506d5e1e9e6bd323312867ebbf526f9a55
shuangpengpang <15913598312@163.com>
2022-10-09 16:04:42 +0800

    括号的分数（856）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0856ScoreOfParentheses.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0856ScoreOfParentheses.java
@@ -10,7 +10,7 @@ import java.util.Deque;
  */
 public class Problem0856ScoreOfParentheses {
 
-    public int scoreOfParentheses(String s) {
+    public int scoreOfParentheses0(String s) {
         int n = s.length();
         Deque<String> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
@@ -34,4 +34,50 @@ public class Problem0856ScoreOfParentheses {
         }
         return ans;
     }
+
+    public int scoreOfParentheses1(String s) {
+        int n = s.length();
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(0);
+            } else {
+                int v = stack.pop();
+                stack.push(stack.pop() + Math.max(v << 1, 1));
+            }
+        }
+        return stack.pop();
+    }
+
+    public int scoreOfParentheses2(String s) {
+        int n = s.length();
+        int ans = 0, cnt = 0;
+        int i = 0;
+        while (i < n) {
+            while (i < n && s.charAt(i) == '(') {
+                cnt++;
+                i++;
+            }
+            ans += 1 << (cnt - 1);
+            while (i < n && s.charAt(i) == ')') {
+                cnt--;
+                i++;
+            }
+        }
+        return ans;
+    }
+
+    public int scoreOfParentheses(String s) {
+        int n = s.length(), ans = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            cnt += c == '(' ? 1 : -1;
+            if (c == ')' && s.charAt(i - 1) == '(') {
+                ans += 1 << cnt;
+            }
+        }
+        return ans;
+    }
 }
+

837016061a414e441e5eaa0aaccaa37104693abe
shuangpengpang <15913598312@163.com>
2022-10-10 11:16:44 +0800

    使序列递增的最小交换次数（801）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0801MinimumSwapsToMakeSequencesIncreasing.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0801MinimumSwapsToMakeSequencesIncreasing.java
@@ -1,5 +1,9 @@
 package com.shuangpeng.Problem.p0801_0900;
 
+/**
+ * @Description:（使序列递增的最小交换次数）
+ * @Date 2022/10/10 10:37 AM
+ **/
 public class Problem0801MinimumSwapsToMakeSequencesIncreasing {
 
     public int minSwap0(int[] nums1, int[] nums2) {
@@ -32,7 +36,7 @@ public class Problem0801MinimumSwapsToMakeSequencesIncreasing {
         return Math.min(noSwap, swap);
     }
 
-    public int minSwap(int[] A, int[] B) {
+    public int minSwap1(int[] A, int[] B) {
         int n = A.length;
         int noSwap = 0, swap = 1;
         for (int i = 1; i < n; ++i) {
@@ -49,5 +53,19 @@ public class Problem0801MinimumSwapsToMakeSequencesIncreasing {
             swap = tempSwap;
         }
         return Math.min(noSwap, swap);
+    }
+
+    public int minSwap(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int swap = 1, noSwap = 0, INF = Integer.MAX_VALUE >> 1;
+        for (int i = 1; i < n; i++) {
+            boolean b1 = nums1[i - 1] < nums1[i] && nums2[i - 1] < nums2[i];
+            boolean b2 = nums1[i - 1] < nums2[i] && nums2[i - 1] < nums1[i];
+            int swapTemp = 1 + Math.min(b1 ? swap : INF, b2 ? noSwap : INF);
+            int noSwapTemp = Math.min(b1 ? noSwap : INF, b2 ? swap : INF);
+            swap = swapTemp;
+            noSwap = noSwapTemp;
+        }
+        return Math.min(swap, noSwap);
     }
 }

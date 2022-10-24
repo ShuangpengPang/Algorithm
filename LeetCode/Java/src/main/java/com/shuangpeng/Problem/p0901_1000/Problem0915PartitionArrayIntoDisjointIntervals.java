50d09b9bacdec69b376313f65d738ef96d51eb17
shuangpengpang <15913598312@163.com>
2022-10-24 11:18:59 +0800

    分割数组（915）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0915PartitionArrayIntoDisjointIntervals.java
@@ -0,0 +1,27 @@
+package com.shuangpeng.Problem.p0901_1000;
+
+/**
+ * @Description: Problem0915PartitionArrayIntoDisjointIntervals（分割数组）
+ * @Date 2022/10/24 10:10 AM
+ * @Version 1.0
+ */
+public class Problem0915PartitionArrayIntoDisjointIntervals {
+
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int[] maxValue = new int[n];
+        maxValue[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            maxValue[i] = Math.max(maxValue[i - 1], nums[i]);
+        }
+        int ans = 0, idx = n - 2, minValue = nums[n - 1];
+        while (idx >= 0) {
+            if (minValue >= maxValue[idx]) {
+                ans = idx;
+            }
+            minValue = Math.min(minValue, nums[idx]);
+            idx--;
+        }
+        return ans + 1;
+    }
+}

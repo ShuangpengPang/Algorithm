4a4fa975e4dce691b4ace908d5f02bff59c5504f
shuangpengpang <15913598312@163.com>
2022-10-25 11:23:37 +0800

    石头游戏VIII（1872）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1872StoneGameVIII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1872StoneGameVIII.java
@@ -7,7 +7,7 @@ package com.shuangpeng.Problem.p1801_1900;
  */
 public class Problem1872StoneGameVIII {
 
-    public int stoneGameVIII(int[] stones) {
+    public int stoneGameVIII0(int[] stones) {
         int n = stones.length;
         int[] preSum = new int[n];
         preSum[0] = stones[0];
@@ -20,5 +20,18 @@ public class Problem1872StoneGameVIII {
             dp[i] = Math.max(dp[i + 1], preSum[i] - dp[i + 1]);
         }
         return dp[1];
+    }
+
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += stones[i];
+        }
+        int ans = sum;
+        for (int i = n - 2; i >= 1; i--) {
+            sum -= stones[i + 1];
+            ans = Math.max(ans, sum - ans);
+        }
+        return ans;
     }
 }

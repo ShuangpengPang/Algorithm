c60c3727684cfd3e2566471b44198a3d8e671ee0
shuangpengpang <15913598312@163.com>
2022-10-28 19:40:43 +0800

    统计特殊整数（2376）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第301到310场周赛/第306场周赛/Problem2376CountSpecialIntegers.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第301到310场周赛/第306场周赛/Problem2376CountSpecialIntegers.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class Problem2376CountSpecialIntegers {
 
-    public int countSpecialNumbers(int n) {
+    public int countSpecialNumbers0(int n) {
         char[] cs = Integer.toString(n).toCharArray();
         int m = cs.length;
         int[] dp = new int[m];
@@ -40,5 +40,37 @@ public class Problem2376CountSpecialIntegers {
             }
         }
         return cnt;
+    }
+
+    public int countSpecialNumbers(int n) {
+        char[] cs = Integer.toString(n).toCharArray();
+        int[][] dp = new int[cs.length][1 << 10];
+        Arrays.setAll(dp, i -> {
+            Arrays.fill(dp[i], -1);
+            return dp[i];
+        });
+        return dfs(cs, 0, 0, true, false, dp);
+    }
+
+    private int dfs(char[] cs, int idx, int mask, boolean isLimit, boolean isNum, int[][] memo) {
+        if (idx == cs.length) {
+            return isNum ? 1 : 0;
+        }
+        if (!isLimit && isNum && memo[idx][mask] >= 0) {
+            return memo[idx][mask];
+        }
+        int ans = 0;
+        if (!isNum) {
+            ans = dfs(cs, idx + 1, mask, false, isNum, memo);
+        }
+        for (int i = isNum ? 0 : 1; i <= (isLimit ? cs[idx] - '0' : 9); i++) {
+            if (((mask >> i) & 1) == 0) {
+                ans += dfs(cs, idx + 1, mask | 1 << i, isLimit && i == (cs[idx] - '0'), true, memo);
+            }
+        }
+        if (!isLimit && isNum) {
+            memo[idx][mask] = ans;
+        }
+        return ans;
     }
 }

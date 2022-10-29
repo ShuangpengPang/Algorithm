73bfd69b2c3df3c6a30522429af9b620224d9e2e
shuangpengpang <15913598312@163.com>
2022-10-29 10:24:58 +0800

    统计匹配检索规则的物品数量（1773）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第221到230场周赛/第230场周赛/Problem1773CountItemsMatchingARule.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第221到230场周赛/第230场周赛/Problem1773CountItemsMatchingARule.java
@@ -2,9 +2,13 @@ package com.shuangpeng.competition.第221到230场周赛.第230场周赛;
 
 import java.util.List;
 
-public class Problem1773 {
+/**
+ * @Description: 统计匹配检索规则的物品数量
+ * @Date 2022/10/29 10:23 AM
+ **/
+public class Problem1773CountItemsMatchingARule {
 
-    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+    public int countMatches0(List<List<String>> items, String ruleKey, String ruleValue) {
         int answer = 0;
         int size = items.size();
         for (int i = 0; i < size; i++) {
@@ -18,5 +22,16 @@ public class Problem1773 {
             }
         }
         return answer;
+    }
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int ans = 0;
+        int idx = ruleKey.equals("type") ? 0 : (ruleKey.equals("color") ? 1 : 2);
+        for (List<String> item : items) {
+            if (item.get(idx).equals(ruleValue)) {
+                ans++;
+            }
+        }
+        return ans;
     }
 }

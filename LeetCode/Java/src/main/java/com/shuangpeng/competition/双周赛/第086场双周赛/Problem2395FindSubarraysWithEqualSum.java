15bc82a306f1e34fbbb47dc3bf82b595aa73f572
shuangpengpang <15913598312@163.com>
2022-11-01 17:50:44 +0800

    和相等的子数组（2395）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/双周赛/第086场双周赛/Problem2395FindSubarraysWithEqualSum.java
@@ -0,0 +1,24 @@
+package com.shuangpeng.competition.双周赛.第086场双周赛;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: Problem2395FindSubarraysWithEqualSum（和相等的子数组）
+ * @Date 2022/11/1 5:44 PM
+ * @Version 1.0
+ */
+public class Problem2395FindSubarraysWithEqualSum {
+
+    public boolean findSubarrays(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i < n; i++) {
+            int sum = nums[i] + nums[i - 1];
+            if (!set.add(sum)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

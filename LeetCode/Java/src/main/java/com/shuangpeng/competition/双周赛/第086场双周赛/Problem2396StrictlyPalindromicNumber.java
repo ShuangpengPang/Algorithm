7f64f0640d15de5c1cd6e978637566aa60d66ec6
shuangpengpang <15913598312@163.com>
2022-11-01 18:01:18 +0800

    严格回文的数字（2396）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/双周赛/第086场双周赛/Problem2396StrictlyPalindromicNumber.java
@@ -0,0 +1,13 @@
+package com.shuangpeng.competition.双周赛.第086场双周赛;
+
+/**
+ * @Description: Problem2396StrictlyPalindromicNumber（严格回文的数字）
+ * @Date 2022/11/1 5:55 PM
+ * @Version 1.0
+ */
+public class Problem2396StrictlyPalindromicNumber {
+
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}

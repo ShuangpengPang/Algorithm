74b8df2a46fee05243cc23fa0cf06515558650da
shuangpengpang <15913598312@163.com>
2022-11-01 16:45:37 +0800

    预算内的最多机器人数目（2398）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/双周赛/第086场双周赛/Problem2398MaximumNumberOfRobotsWithinBudget.java
@@ -0,0 +1,30 @@
+package com.shuangpeng.competition.双周赛.第086场双周赛;
+
+import java.util.PriorityQueue;
+
+/**
+ * @Description: Problem2398MaximumNumberOfRobotsWithinBudget（预算内的最多机器人数目）
+ * @Date 2022/11/1 4:18 PM
+ * @Version 1.0
+ */
+public class Problem2398MaximumNumberOfRobotsWithinBudget {
+
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+        int n = chargeTimes.length, ans = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        long sum = 0L;
+        for (int l = 0, r = 0; r < n; r++) {
+            sum += runningCosts[r];
+            pq.offer(new int[]{chargeTimes[r], r});
+            while (l <= r && (pq.peek()[1] < l || (r - l + 1) * sum + pq.peek()[0] > budget)) {
+                if (pq.peek()[1] < l) {
+                    pq.poll();
+                    continue;
+                }
+                sum -= runningCosts[l++];
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}

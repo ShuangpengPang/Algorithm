2645693d138b6380e2d5dee4eaa7b6e1025c3853
shuangpengpang <15913598312@163.com>
2022-11-09 11:20:58 +0800

    最大加号标志（764）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0764LargestPlusSign.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0764LargestPlusSign.java
@@ -2,9 +2,13 @@ package com.shuangpeng.Problem.p0701_0800;
 
 import java.util.Arrays;
 
+/**
+ * @description: 最大加号标志
+ * @date 2022/11/9 10:21 AM
+ **/
 public class Problem0764LargestPlusSign {
 
-    public int orderOfLargestPlusSign(int n, int[][] mines) {
+    public int orderOfLargestPlusSign0(int n, int[][] mines) {
         int[][] array = new int[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(array[i], 1);
@@ -38,5 +42,36 @@ public class Problem0764LargestPlusSign {
             }
         }
         return k;
+    }
+
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        boolean[][] grid = new boolean[n][n];
+        for (int[] m : mines) {
+            grid[m[0]][m[1]] = true;
+        }
+        int[][] left = new int[n][n], right = new int[n][n], top = new int[n][n], down = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!grid[i][j]) {
+                    left[i][j] = j == 0 ? 1 : left[i][j - 1] + 1;
+                    top[i][j] = i == 0 ? 1 : top[i - 1][j] + 1;
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (!grid[i][j]) {
+                    right[i][j] = j == n - 1 ? 1 : right[i][j + 1] + 1;
+                    down[i][j] = i == n - 1 ? 1 : down[i + 1][j] + 1;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans = Math.max(ans, Math.min(Math.min(left[i][j], right[i][j]), Math.min(top[i][j], down[i][j])));
+            }
+        }
+        return ans;
     }
 }

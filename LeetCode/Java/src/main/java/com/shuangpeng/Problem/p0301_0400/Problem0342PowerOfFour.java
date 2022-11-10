f55d1f03cc9aa4e40f327a735362e7552915b3cf
shuangpengpang <15913598312@163.com>
2022-11-10 15:38:11 +0800

    4的幂（342）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0342PowerOfFour.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0342PowerOfFour.java
@@ -7,11 +7,23 @@ package com.shuangpeng.Problem.p0301_0400;
  */
 public class Problem0342PowerOfFour {
 
-    public boolean isPowerOfFour(int n) {
+    public boolean isPowerOfFour0(int n) {
         if (n < 1) {
             return false;
         }
         int m = n % 10;
         return (n & (n - 1)) == 0 && (m == 4 || m == 6 || m == 1);
+    }
+
+    public boolean isPowerOfFour1(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (Integer.numberOfTrailingZeros(n) & 1) == 0;
+    }
+
+    public boolean isPowerOfFour2(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+    }
+
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
     }
 }

12f734455dc6b2eaee1d8482781c2dfd260670a8
shuangpengpang <15913598312@163.com>
2022-11-16 11:46:43 +0800

    全局倒置与局部倒置（775）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0775GlobalAndLocalInversions.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0775GlobalAndLocalInversions.java
@@ -8,10 +8,31 @@ package com.shuangpeng.Problem.p0701_0800;
  */
 public class Problem0775GlobalAndLocalInversions {
 
-    public boolean isIdealPermutation(int[] nums) {
+    public boolean isIdealPermutation0(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] > i && (nums[i] != i + 1 || nums[i + 1] != i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isIdealPermutation1(int[] nums) {
+        int n = nums.length, min = nums[n - 1];
+        for (int i = n - 3; i >= 0; i--) {
+            if (nums[i] > min) {
+                return false;
+            }
+            min = Math.min(min, nums[i + 1]);
+        }
+        return true;
+    }
+
+    public boolean isIdealPermutation(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(nums[i] - i) > 1) {
                 return false;
             }
         }

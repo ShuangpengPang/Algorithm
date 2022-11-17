31bc4eea92d3bf943a129e22fd4ba905bdebbead
shuangpengpang <15913598312@163.com>
2022-11-17 15:53:53 +0800

    合并多棵二叉搜索树（1932）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1901_2000/Problem1932MergeBSTsToCreateSingleBST.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1901_2000/Problem1932MergeBSTsToCreateSingleBST.java
@@ -48,37 +48,30 @@ public class Problem1932MergeBSTsToCreateSingleBST {
             return null;
         }
         TreeNode root = nodeMap.get(set.iterator().next());
-        int[] arr = build(root, nodeMap);
-        if (arr == null || arr[0] != trees.size() - 1) {
-            return null;
-        }
-        return root;
+        return build(root, Integer.MIN_VALUE, nodeMap, visited) != Integer.MAX_VALUE && visited.isEmpty() ? root : null;
     }
 
-    private int[] build(TreeNode root, Map<Integer, TreeNode> nodeMap) {
-        int ans = 0;
-        int min = root.left == null ? root.val : root.left.val;
-        int max = root.right == null ? root.val : root.right.val;
-        if (root.left != null && nodeMap.containsKey(root.left.val)) {
-            TreeNode node = nodeMap.get(root.left.val);
-            int[] arr = build(node, nodeMap);
-            if (arr == null || arr[2] >= root.val) {
-                return null;
-            }
-            root.left = node;
-            min = arr[1];
-            ans += arr[0] + 1;
+    // 中序遍历
+    private int build(TreeNode root, int minValue, Map<Integer, TreeNode> nodeMap, Set<Integer> visited) {
+        if (root.val <= minValue) {
+            return Integer.MAX_VALUE;
         }
-        if (root.right != null && nodeMap.containsKey(root.right.val)) {
-            TreeNode node = nodeMap.get(root.right.val);
-            int[] arr = build(node, nodeMap);
-            if (arr == null || arr[1] <= root.val) {
-                return null;
+        if (root.left != null) {
+            if (nodeMap.containsKey(root.left.val)) {
+                root.left = nodeMap.get(root.left.val);
             }
-            root.right = node;
-            max = arr[2];
-            ans += arr[0] + 1;
+            if (build(root.left, minValue, nodeMap, visited) >= root.val) {
+                return Integer.MAX_VALUE;
+            }
         }
-        return new int[]{ans, min, max};
+        visited.remove(root.val);
+        int ans = root.val;
+        if (root.right != null) {
+            if (nodeMap.containsKey(root.right.val)) {
+                root.right = nodeMap.get(root.right.val);
+            }
+            ans = build(root.right, root.val, nodeMap, visited);
+        }
+        return ans;
     }
 }

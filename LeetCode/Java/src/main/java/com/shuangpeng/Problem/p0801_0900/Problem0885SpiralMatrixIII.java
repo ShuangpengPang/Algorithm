4085e91881ea4dda15f7e7293335f5c21fe5c4f3
shuangpengpang <15913598312@163.com>
2022-11-20 23:50:18 +0800

    螺旋矩阵III（885）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0885SpiralMatrixIII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0801_0900/Problem0885SpiralMatrixIII.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p0801_0900;
  */
 public class Problem0885SpiralMatrixIII {
 
-    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+    public int[][] spiralMatrixIII0(int rows, int cols, int rStart, int cStart) {
         int top = rStart - 1, bottom = rStart + 1, left = cStart - 1, right = cStart + 1, cnt = 1, n = rows * cols;
         int[][] ans = new int[n][2];
         ans[0] = new int[]{rStart, cStart};
@@ -54,6 +54,27 @@ public class Problem0885SpiralMatrixIII {
             bottom++;
             left--;
             right++;
+        }
+        return ans;
+    }
+
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int n = rows * cols, d = 0, idx = 0, r = rStart, c = cStart, step = 1;
+        int[][] ans = new int[n][2];
+        while (idx < n) {
+            int x = r + dirs[d][0] * step, y = c + dirs[d][1] * step;
+            while (idx < n && (r != x || c != y)) {
+                if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                    ans[idx++] = new int[]{r, c};
+                }
+                r += dirs[d][0];
+                c += dirs[d][1];
+            }
+            d = (d + 1) % 4;
+            if ((d & 1) == 0) {
+                step++;
+            }
         }
         return ans;
     }

394faca4db1847b3670d3ab9c43a98a8748a71b4
shuangpengpang <15913598312@163.com>
2022-11-21 18:02:31 +0800

    移除所有载有违禁货物车厢所需的最少时间（2167）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2101_2200/Problem2167MinimumTimeToRemoveAllCarsContainingIllegalGoods.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p2101_2200;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2167MinimumTimeToRemoveAllCarsContainingIllegalGoods（移除所有载有违禁货物车厢所需的最少时间）
+ * @date 2022/11/21 5:20 PM
+ */
+public class Problem2167MinimumTimeToRemoveAllCarsContainingIllegalGoods {
+
+    public int minimumTime(String s) {
+        int n = s.length();
+        int[] right = new int[n];
+        right[n - 1] = s.charAt(n - 1) == '1' ? 1 : 0;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.min(n - i, right[i + 1] + (s.charAt(i) == '1' ? 2 : 0));
+        }
+        int ans = right[0], min = 0;
+        for (int i = 0; i < n - 1; i++) {
+            min = Math.min(i + 1, min + (s.charAt(i) == '1' ? 2 : 0));
+            ans = Math.min(ans, min + right[i + 1]);
+        }
+        return ans;
+    }
+}

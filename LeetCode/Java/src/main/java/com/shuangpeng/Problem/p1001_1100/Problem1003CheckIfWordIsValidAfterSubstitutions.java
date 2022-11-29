2107151c1d3dbd33f1a5f28966395b5b439f1086
shuangpengpang <15913598312@163.com>
2022-11-30 00:02:53 +0800

    检查替换后的词是否有效（1003）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1003CheckIfWordIsValidAfterSubstitutions.java
@@ -0,0 +1,38 @@
+package com.shuangpeng.Problem.p1001_1100;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1003CheckIfWordIsValidAfterSubstitutions（检查替换后的词是否有效）
+ * @date 2022/11/29 11:58 PM
+ */
+public class Problem1003CheckIfWordIsValidAfterSubstitutions {
+
+    public boolean isValid(String s) {
+        int n = s.length();
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            int j = s.charAt(i) - 'a';
+            if (j == 0) {
+                q.push(j);
+            } else if (j == 1) {
+                if (q.isEmpty() || q.peek() != 0) {
+                    return false;
+                }
+                q.push(j);
+            } else {
+                if (q.size() < 2) {
+                    return false;
+                }
+                int a = q.pop(), b = q.pop();
+                if (a != 1 || b != 0) {
+                    return false;
+                }
+            }
+        }
+        return q.isEmpty();
+    }
+}

7f9d3be6aed3240c907fe08be57a9a1f7a681f62
shuangpengpang <15913598312@163.com>
2022-12-02 11:14:04 +0800

    移动所有球到每个盒子所需的最小操作数（1769）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1769MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1769MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1701_1800;
  */
 public class Problem1769MinimumNumberOfOperationsToMoveAllBallsToEachBox {
 
-    public int[] minOperations(String boxes) {
+    public int[] minOperations0(String boxes) {
         int n = boxes.length(), sum = 0;
         int[] preSum = new int[n + 1];
         for (int i = 0; i < n; i++) {
@@ -22,6 +22,26 @@ public class Problem1769MinimumNumberOfOperationsToMoveAllBallsToEachBox {
         ans[0] = sum;
         for (int i = 1; i < n; i++) {
             ans[i] = ans[i - 1] + preSum[i] - (preSum[n] - preSum[i]);
+        }
+        return ans;
+    }
+
+    public int[] minOperations(String boxes) {
+        int n = boxes.length(), cnt = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (boxes.charAt(i) == '1') {
+                cnt++;
+                sum += i;
+            }
+        }
+        int[] ans = new int[n];
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            ans[i] = i == 0 ? sum : ans[i - 1] + left - cnt;
+            if (boxes.charAt(i) == '1') {
+                left++;
+                cnt--;
+            }
         }
         return ans;
     }

7fcc6688ad77054385c552e2e57436a76fa3200e
shuangpengpang <15913598312@163.com>
2022-12-07 11:29:52 +0800

    通过最少操作次数使数组的和相等（1775）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第221到230场周赛/第230场周赛/Problem1775EqualSumArraysWithMinimumNumberOfOperations.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第221到230场周赛/第230场周赛/Problem1775EqualSumArraysWithMinimumNumberOfOperations.java
@@ -2,7 +2,11 @@ package com.shuangpeng.competition.第221到230场周赛.第230场周赛;
 
 import java.util.Arrays;
 
-public class Problem1775 {
+/**
+ * @description: 通过最少操作次数使数组的和相等
+ * @date 2022/12/7 11:14 AM
+ **/
+public class Problem1775EqualSumArraysWithMinimumNumberOfOperations {
 
     public int minOperations0(int[] nums1, int[] nums2) {
         int sum1 = 0;
@@ -125,5 +129,84 @@ public class Problem1775 {
             }
         }
         return diff <= 0 ? answer : -1;
+    }
+}
+
+class Problem1775EqualSumArraysWithMinimumNumberOfOperations0 {
+
+    public int minOperations0(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        if (n1 * 6 < n2 || n2 * 6 < n1) {
+            return -1;
+        }
+        int[] cnt1 = new int[6], cnt2 = new int[6];
+        int s1 = 0, s2 = 0;
+        for (int num : nums1) {
+            cnt1[num - 1]++;
+            s1 += num;
+        }
+        for (int num : nums2) {
+            cnt2[num - 1]++;
+            s2 += num;
+        }
+        if (s1 > s2) {
+            int t = s1;
+            s1 = s2;
+            s2 = t;
+            int[] cnt = cnt1;
+            cnt1 = cnt2;
+            cnt2 = cnt;
+        }
+        int d = s2 - s1, ans = 0;
+        for (int i = 0, j = 5; i < 5 && d > 0; i++, j--) {
+            int add = 5 - i;
+            int c = Math.min((d - 1) / add + 1, cnt1[i]);
+            ans += c;
+            d -= c * add;
+            if (d <= 0) {
+                break;
+            }
+            c = Math.min((d - 1) / add + 1, cnt2[j]);
+            ans += c;
+            d -= c * add;
+        }
+        return ans;
+    }
+
+    public int minOperations(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        if (n1 * 6 < n2 || n2 * 6 < n1) {
+            return -1;
+        }
+        int[] cnt1 = new int[6], cnt2 = new int[6];
+        int diff = 0;
+        for (int num : nums1) {
+            cnt1[num - 1]++;
+            diff += num;
+        }
+        for (int num : nums2) {
+            cnt2[num - 1]++;
+            diff -= num;
+        }
+        if (diff == 0) {
+            return 0;
+        }
+        if (diff > 0) {
+            return getCount(cnt1, cnt2, diff);
+        }
+        return getCount(cnt2, cnt1, -diff);
+    }
+
+    private int getCount(int[] cnt1, int[] cnt2, int diff) {
+        for (int i = 0; i < 6; i++) {
+            cnt1[5 - i] += cnt2[i];
+        }
+        int ans = 0;
+        for (int i = 5; i > 0 && diff > 0; i--) {
+            int c = Math.min((diff + i - 1) / i, cnt1[i]);
+            ans += c;
+            diff -= c * i;
+        }
+        return ans;
     }
 }

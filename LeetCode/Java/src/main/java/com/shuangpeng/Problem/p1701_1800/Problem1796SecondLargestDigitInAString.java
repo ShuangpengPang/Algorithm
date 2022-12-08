5db82700cfed79d07d5c88871d808c4cfc515231
shuangpengpang <15913598312@163.com>
2022-12-09 00:25:37 +0800

    字符串中第二大的数字（1796）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1796SecondLargestDigitInAString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1796SecondLargestDigitInAString.java
@@ -8,13 +8,30 @@ package com.shuangpeng.Problem.p1701_1800;
  */
 public class Problem1796SecondLargestDigitInAString {
 
-    public int secondHighest(String s) {
+    public int secondHighest0(String s) {
         int first = -1, second = -1;
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
             if (c >= '0' && c <= '9') {
                 int j = c - '0';
+                if (j > first) {
+                    second = first;
+                    first = j;
+                } else if (j < first && j > second) {
+                    second = j;
+                }
+            }
+        }
+        return second;
+    }
+
+    public int secondHighest(String s) {
+        int first = -1, second = -1;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int j = s.charAt(i) - '0';
+            if (j >= 0 && j < 10) {
                 if (j > first) {
                     second = first;
                     first = j;

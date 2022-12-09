b9c6e661cc1785ffabc0f7a8e9acf66b47117d2d
shuangpengpang <15913598312@163.com>
2022-12-09 19:45:17 +0800

    使数组唯一的最小增量（945）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0945MinimumIncrementToMakeArrayUnique.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0945MinimumIncrementToMakeArrayUnique.java
@@ -10,13 +10,32 @@ import java.util.Arrays;
  */
 public class Problem0945MinimumIncrementToMakeArrayUnique {
 
-    public int minIncrementForUnique(int[] nums) {
+    public int minIncrementForUnique0(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length, ans = 0, prev = -1;
         for (int i = 0; i < n; i++) {
             ans += Math.max(prev + 1 - nums[i], 0);
             prev = Math.max(prev + 1, nums[i]);
         }
+        return ans;
+    }
+
+    public int minIncrementForUnique(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        int[] cnt = new int[max + 1];
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        int ans = 0;
+        for (int i = 1; i <= max; i++) {
+            if (cnt[i - 1] > 1) {
+                int c = cnt[i - 1] - 1;
+                cnt[i] += c;
+                ans += c;
+            }
+        }
+        int c = cnt[max] - 1;
+        ans += (long) (c + 1) * c >> 1;
         return ans;
     }
 }

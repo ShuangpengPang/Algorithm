8e1c3625dd6c58862a614f71f31a753f419e318d
shuangpengpang <15913598312@163.com>
2022-12-12 17:51:19 +0800

    下一个更大元素IV（2454）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2454NextGreaterElementIV.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2454NextGreaterElementIV.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p2401_2500;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @author ShuangPengPang
@@ -9,6 +11,28 @@ import java.util.Arrays;
  * @date 2022/12/12 2:46 PM
  */
 public class Problem2454NextGreaterElementIV {
+
+    public int[] secondGreaterElement(int[] nums) {
+        int n = nums.length;
+        PriorityQueue<Integer>[] pqs = new PriorityQueue[2];
+        Arrays.setAll(pqs, i -> new PriorityQueue<Integer>(Comparator.comparingInt(a -> nums[a])));
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            while (!pqs[1].isEmpty() && nums[pqs[1].peek()] < x) {
+                ans[pqs[1].poll()] = x;
+            }
+            while (!pqs[0].isEmpty() && nums[pqs[0].peek()] < x) {
+                pqs[1].offer(pqs[0].poll());
+            }
+            pqs[0].offer(i);
+        }
+        return ans;
+    }
+}
+
+class Problem2454NextGreaterElementIV0 {
 
     static final int N = (int) 1e5;
     static final int[] first = new int[N], second = new int[N];

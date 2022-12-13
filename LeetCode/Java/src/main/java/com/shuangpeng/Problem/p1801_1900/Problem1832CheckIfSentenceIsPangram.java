8ad309ed4240dfaadce653eb917a32ed8e6818c2
shuangpengpang <15913598312@163.com>
2022-12-13 10:27:03 +0800

    判断句子是否为全字母句（1832）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1832CheckIfSentenceIsPangram.java
@@ -0,0 +1,23 @@
+package com.shuangpeng.Problem.p1801_1900;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1832CheckIfSentenceIsPangram（判断句子是否为全字母句）
+ * @date 2022/12/13 10:19 AM
+ */
+public class Problem1832CheckIfSentenceIsPangram {
+
+    public boolean checkIfPangram(String sentence) {
+        int n = sentence.length(), cnt = 0;
+        boolean[] has = new boolean[26];
+        for (int i = 0; i < n; i++) {
+            int c = sentence.charAt(i) - 'a';
+            if (!has[c]) {
+                cnt++;
+            }
+            has[c] = true;
+        }
+        return cnt == 26;
+    }
+}

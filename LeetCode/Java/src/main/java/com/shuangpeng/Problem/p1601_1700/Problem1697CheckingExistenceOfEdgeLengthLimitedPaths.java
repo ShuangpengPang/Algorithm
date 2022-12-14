ab6d356f14dcda1eb18173c3bc11d0da37aec544
shuangpengpang <15913598312@163.com>
2022-12-14 12:13:48 +0800

    检查边长度限制的路径是否存在（1697）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1697CheckingExistenceOfEdgeLengthLimitedPaths.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1697CheckingExistenceOfEdgeLengthLimitedPaths.java
@@ -47,3 +47,37 @@ public class Problem1697CheckingExistenceOfEdgeLengthLimitedPaths {
         }
     }
 }
+
+class Problem1697CheckingExistenceOfEdgeLengthLimitedPaths0 {
+
+    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
+        Arrays.sort(edgeList, Comparator.comparingInt(e -> e[2]));
+        int m1 = edgeList.length, m2 = queries.length;
+        Integer[] ids = new Integer[m2];
+        Arrays.setAll(ids, i -> i);
+        Arrays.sort(ids, Comparator.comparingInt(e -> queries[e][2]));
+        int[] parent = new int[n];
+        Arrays.setAll(parent, i -> i);
+        boolean[] ans = new boolean[m2];
+        for (int i = 0, j = 0; i < m2; i++) {
+            int id = ids[i];
+            while (j < m1 && edgeList[j][2] < queries[id][2]) {
+                union(parent, edgeList[j][0], edgeList[j][1]);
+                j++;
+            }
+            ans[id] = find(parent, queries[id][0]) == find(parent, queries[id][1]);
+        }
+        return ans;
+    }
+
+    private void union(int[] parent, int x, int y) {
+        int px = find(parent, x), py = find(parent, y);
+        if (px != py) {
+            parent[py] = px;
+        }
+    }
+
+    private int find(int[] parent, int x) {
+        return parent[x] = parent[x] == x ? x : find(parent, parent[x]);
+    }
+}

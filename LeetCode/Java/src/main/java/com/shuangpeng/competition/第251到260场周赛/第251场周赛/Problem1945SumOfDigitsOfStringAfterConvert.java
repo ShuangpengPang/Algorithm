ac522ce0d397cdb906ef0ca05da97672fd39d7fe
shuangpengpang <15913598312@163.com>
2022-12-15 11:15:37 +0800

    字符串转化后的各位数字之和（1945）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第251到260场周赛/第251场周赛/Problem1945SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,50 @@
+package com.shuangpeng.competition.第251到260场周赛.第251场周赛;
+
+/**
+ * @author shuangpeng
+ * @description: 字符串转化后的各位数字之和
+ * @date 2022/12/15 10:32 AM
+ **/
+public class Problem1945SumOfDigitsOfStringAfterConvert {
+
+    public int getLucky0(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append((int) (c - 'a' + 1));
+        }
+        String string = sb.toString();
+        int sum = 0;
+        for (char c : string.toCharArray()) {
+            sum += (c - '0');
+        }
+        for (int i = 1; i < k; i++) {
+            int temp = sum;
+            sum = 0;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+        }
+        return sum;
+    }
+
+    public int getLucky(String s, int k) {
+        int n = s.length(), sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += getDigitSum(s.charAt(i) - 'a' + 1);
+        }
+        for (int i = 1; i < k; i++) {
+            sum = getDigitSum(sum);
+        }
+        return sum;
+    }
+
+    private int getDigitSum(int i) {
+        int ans = 0;
+        while (i > 0) {
+            ans += i % 10;
+            i /= 10;
+        }
+        return ans;
+    }
+}

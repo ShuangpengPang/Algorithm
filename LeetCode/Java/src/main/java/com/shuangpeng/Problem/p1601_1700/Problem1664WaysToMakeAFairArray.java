1e7f04c6f7a7a1ffbd6bc75c38851ce04c26e6e1
shuangpengpang <15913598312@163.com>
2023-01-30 20:07:06 +0800

    生成平衡数组的方案数（1664）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1664WaysToMakeAFairArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1664WaysToMakeAFairArray.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1601_1700;
  */
 public class Problem1664WaysToMakeAFairArray {
 
-    public int waysToMakeFair(int[] nums) {
+    public int waysToMakeFair0(int[] nums) {
         int n = nums.length, totalEven = 0, totalOdd = 0;
         for (int i = 0; i < n; i++) {
             if ((i & 1) == 0) {
@@ -28,6 +28,19 @@ public class Problem1664WaysToMakeAFairArray {
             if (even + totalOdd - sumOdd == odd + totalEven - sumEven) {
                 ans++;
             }
+        }
+        return ans;
+    }
+
+    public int waysToMakeFair(int[] nums) {
+        int n = nums.length, ans = 0, s = 0;
+        for (int i = 0; i < n; i++) {
+            s += (i & 1) == 0 ? nums[i] : -nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            s -= (i & 1) == 0 ? nums[i] : -nums[i];
+            ans += s == 0 ? 1 : 0;
+            s -= (i & 1) == 0 ? nums[i] : -nums[i];
         }
         return ans;
     }

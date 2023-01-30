b06a37a99833b668d743635da17886b4d67ce6d4
shuangpengpang <15913598312@163.com>
2023-01-30 16:47:55 +0800

    合并两个链表（1669）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1669MergeInBetweenLinkedLists.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1669MergeInBetweenLinkedLists.java
@@ -10,7 +10,7 @@ import com.shuangpeng.common.ListNode;
  */
 public class Problem1669MergeInBetweenLinkedLists {
 
-    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+    public ListNode mergeInBetween0(ListNode list1, int a, int b, ListNode list2) {
         ListNode node1 = list1;
         for (int i = 1; i < a; i++) {
             node1 = node1.next;
@@ -24,6 +24,23 @@ public class Problem1669MergeInBetweenLinkedLists {
             node1 = node1.next;
         }
         node1.next = node2;
+        return list1;
+    }
+
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode n1 = list1, n2 = list1;
+        int gap = b - a + 1;
+        for (int i = 0; i <= b; i++) {
+            if (i > gap) {
+                n1 = n1.next;
+            }
+            n2 = n2.next;
+        }
+        n1.next = list2;
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+        list2.next = n2;
         return list1;
     }
 }

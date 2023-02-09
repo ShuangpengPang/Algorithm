68c5b5f707ea52bcc5b3098dcf71a9c82fb75adc
shuangpengpang <15913598312@163.com>
2023-02-09 17:55:13 +0800

    警告一小时内使用相同员工卡大于等于三次的人（1604）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1604AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1604AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod.java
@@ -1,10 +1,8 @@
 package com.shuangpeng.Problem.p1601_1700;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -24,22 +22,21 @@ public class Problem1604AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod {
         Integer[] ids = new Integer[n];
         Arrays.setAll(ids, i -> i);
         Arrays.sort(ids, Comparator.comparing(a -> keyTime[a]));
-        Map<String, Deque<Integer>> map = new HashMap<>();
+        Map<String, List<Integer>> map = new HashMap<>();
         Set<String> set = new HashSet<>();
         List<String> ans = new ArrayList<>();
         for (int id : ids) {
             String name = keyName[id], t = keyTime[id];
             if (!set.contains(name)) {
                 int time = Integer.parseInt(t.substring(0, 2)) * 60 + Integer.parseInt(t.substring(3));
-                Deque<Integer> q = map.computeIfAbsent(name, k -> new ArrayDeque<>());
-                while (!q.isEmpty() && q.peekFirst() < time - 60) {
-                    q.pollFirst();
-                }
-                q.addLast(time);
-                if (q.size() >= 3) {
+                List<Integer> q = map.computeIfAbsent(name, k -> new ArrayList<>());
+                int size = q.size();
+                if (size >= 2 && q.get(size - 2) >= time - 60) {
                     set.add(name);
                     ans.add(name);
+                    continue;
                 }
+                q.add(time);
             }
         }
         ans.sort(String::compareTo);

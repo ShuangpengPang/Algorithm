afb0e84cb63e8945d70c4fd5066d8b13a91e7b55
shuangpengpang <15913598312@163.com>
2023-02-25 16:33:34 +0800

    交换字符使得字符串相同（1247）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1247MinimumSwapsToMakeStringsEqual.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1247MinimumSwapsToMakeStringsEqual.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1201_1300;
  */
 public class Problem1247MinimumSwapsToMakeStringsEqual {
 
-    public int minimumSwap(String s1, String s2) {
+    public int minimumSwap0(String s1, String s2) {
         int n = s1.length(), N = 26;
         int count1 = 0, count2 = 0;
         for (int i = 0; i < n; i++) {
@@ -26,5 +26,17 @@ public class Problem1247MinimumSwapsToMakeStringsEqual {
             return -1;
         }
         return (count1 >> 1) + (count2 >> 1) + (count1 & 1) + (count2 & 1);
+    }
+
+    public int minimumSwap(String s1, String s2) {
+        int n = s1.length();
+        int[] cnt = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                cnt[s1.charAt(i) & 1]++;
+            }
+        }
+        int s = cnt[0] + cnt[1];
+        return (s & 1) == 1 ? -1 : (s >> 1) + (cnt[0] & 1);
     }
 }

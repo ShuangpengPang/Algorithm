ceec7a7348a532c8447790eb67cfbde801c0bc2a
shuangpengpang <15913598312@163.com>
2023-02-27 17:54:33 +0800

    Dota2 参议院（649）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0649Dota2Senate.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0649Dota2Senate.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p0601_0700;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +11,7 @@ package com.shuangpeng.Problem.p0601_0700;
  */
 public class Problem0649Dota2Senate {
 
-    public String predictPartyVictory(String senate) {
+    public String predictPartyVictory0(String senate) {
         char[] cs = senate.toCharArray();
         int n = cs.length, c1 = 0, c2 = 0;
         for (char c : cs) {
@@ -40,5 +43,26 @@ public class Problem0649Dota2Senate {
             }
         }
         return c1 > 0 ? "Radiant" : "Dire";
+    }
+
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> radiant = new LinkedList<>(), dire = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiant.offer(i);
+            } else {
+                dire.offer(i);
+            }
+        }
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int radiantIndex = radiant.poll(), direIndex = dire.poll();
+            if (radiantIndex < direIndex) {
+                radiant.offer(radiantIndex + n);
+            } else {
+                dire.offer(direIndex + n);
+            }
+        }
+        return radiant.isEmpty() ? "Dire" : "Radiant";
     }
 }

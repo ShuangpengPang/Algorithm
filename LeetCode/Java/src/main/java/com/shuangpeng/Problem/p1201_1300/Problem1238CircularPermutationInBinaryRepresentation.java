365f3f66eb458b7c9e112433224d6c6d0f4b9a9e
shuangpengpang <15913598312@163.com>
2023-02-28 15:56:47 +0800

    循环码排列（1238）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1238CircularPermutationInBinaryRepresentation.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1238CircularPermutationInBinaryRepresentation.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p1201_1300;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
  */
 public class Problem1238CircularPermutationInBinaryRepresentation {
 
-    public List<Integer> circularPermutation(int n, int start) {
+    public List<Integer> circularPermutation0(int n, int start) {
         List<Integer> list = new LinkedList<>();
         list.add(0);
         for (int i = 0; i < n; i++) {
@@ -23,5 +24,26 @@ public class Problem1238CircularPermutationInBinaryRepresentation {
             list.add(list.remove(0));
         }
         return list;
+    }
+
+    public List<Integer> circularPermutation(int n, int start) {
+        int N = 1 << n;
+        List<Integer> list = new ArrayList<>(N);
+        list.add(0);
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = list.size() - 1; j >= 0; j--) {
+                int num = list.get(j) | 1 << i;
+                if (num == start) {
+                    index = list.size();
+                }
+                list.add(num);
+            }
+        }
+        List<Integer> ans = new ArrayList<>(N);
+        for (int i = index; i < index + N; i++) {
+            ans.add(list.get(i % N));
+        }
+        return ans;
     }
 }

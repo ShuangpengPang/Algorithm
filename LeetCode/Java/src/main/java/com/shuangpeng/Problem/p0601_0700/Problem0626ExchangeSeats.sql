ef00e1b3e1e1f8ae378712506bc040178e5f2cbc
shuangpengpang <15913598312@163.com>
2023-02-28 14:31:21 +0800

    换座位（626）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0626ExchangeSeats.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0626ExchangeSeats.sql
@@ -7,3 +7,45 @@ SELECT t1.id,
     END AS student
 FROM Seat t1
     LEFT JOIN Seat t2 ON (t2.id = t1.id + 1 && t1.id % 2 = 1) || (t2.id = t1.id - 1 && t1.id % 2 = 0)
+;
+
+SELECT
+    s1.id, COALESCE(s2.student, s1.student) AS student
+FROM
+    Seat s1
+        LEFT JOIN Seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
+ORDER BY s1.id ASC
+;
+
+SELECT
+    CASE
+        WHEN MOD(id, 2) = 1 AND id = count
+            THEN id
+        WHEN MOD(id, 2) = 1
+            THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM
+    Seat, (SELECT count(*) AS count FROM Seat) AS c
+ORDER BY id ASC
+;
+
+SELECT
+    s1.id, COALESCE(s2.student, s1.student) AS student
+FROM
+    seat s1
+        LEFT JOIN
+    seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
+ORDER BY s1.id;
+
+SELECT
+    id,
+    CASE
+        WHEN MOD(id, 2) = 1 THEN LEAD(student, 1, student) OVER (ORDER BY id)
+        WHEN MOD(id, 2) = 0 THEN LAG(student, 1) OVER (ORDER BY id)
+    END AS student
+FROM
+    Seat
+ORDER BY id
+;

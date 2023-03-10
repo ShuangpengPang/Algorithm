17625ee97d9ffb3c0526bdb84be4d7d29c21a8b0
shuangpengpang <15913598312@163.com>
2023-03-10 10:45:18 +0800

    使数组和能被P整除（1590）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1590MakeSumDivisibleByP.java
@@ -0,0 +1,33 @@
+package com.shuangpeng.Problem.p1501_1600;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1590MakeSumDivisibleByP（使数组和能被P整除）
+ * @date 2023/3/10 10:22 AM
+ */
+public class Problem1590MakeSumDivisibleByP {
+
+    public int minSubarray(int[] nums, int p) {
+        int m = 0;
+        for (int num : nums) {
+            m = (m + num) % p;
+        }
+        if (m == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        int n = nums.length, ans = n;
+        for (int i = 0, s = 0; i < n; i++) {
+            s = (s + nums[i]) % p;
+            int pre = Math.max(map.getOrDefault(s - m, -n), map.getOrDefault(s + p - m, -n));
+            ans = Math.min(ans, i - pre + 1);
+            map.put(s, i + 1);
+        }
+        return ans == n ? -1 : ans;
+    }
+}

bdff7a5678982f42448995ac27ff30190675f3d4
shuangpengpang <15913598312@163.com>
2023-03-10 11:04:56 +0800

    使数组和能被P整除（1590）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1590MakeSumDivisibleByP.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1590MakeSumDivisibleByP.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public class Problem1590MakeSumDivisibleByP {
 
-    public int minSubarray(int[] nums, int p) {
+    public int minSubarray0(int[] nums, int p) {
         int m = 0;
         for (int num : nums) {
             m = (m + num) % p;
@@ -27,6 +27,23 @@ public class Problem1590MakeSumDivisibleByP {
             int pre = Math.max(map.getOrDefault(s - m, -n), map.getOrDefault(s + p - m, -n));
             ans = Math.min(ans, i - pre + 1);
             map.put(s, i + 1);
+        }
+        return ans == n ? -1 : ans;
+    }
+
+    public int minSubarray(int[] nums, int p) {
+        int m = 0, n = nums.length, ans = n;
+        for (int num : nums) {
+            m = (m + num) % p;
+        }
+        if (m == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; i < n; i++) {
+            map.put(j, i);
+            j = (j + nums[i]) % p;
+            ans = Math.min(ans, i - map.getOrDefault((j - m + p) % p, -n) + 1);
         }
         return ans == n ? -1 : ans;
     }

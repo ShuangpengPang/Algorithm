7183bdf6ffe4d0524758993f07e38865592e1b2d
shuangpengpang <15913598312@163.com>
2023-03-11 11:35:00 +0800

    面试题17.05 字母与数字

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1705FindLongestSubarray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1705FindLongestSubarray.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.interview;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +12,7 @@ import java.util.Map;
  */
 public class Question1705FindLongestSubarray {
 
-    public String[] findLongestSubarray(String[] array) {
+    public String[] findLongestSubarray0(String[] array) {
         int n = array.length;
         Map<Integer, Integer> map = new HashMap<>();
         int maxLength = 0, index = 0;
@@ -25,6 +26,64 @@ public class Question1705FindLongestSubarray {
             if (length > maxLength) {
                 maxLength = length;
                 index = j;
+            }
+        }
+        String[] ans = new String[maxLength];
+        System.arraycopy(array, index - maxLength, ans, 0, maxLength);
+        return ans;
+    }
+
+    public String[] findLongestSubarray1(String[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = array.length, maxLength = 0, index = 0;
+        for (int i = 0, s = 0; i < n; i++) {
+            map.putIfAbsent(s, i);
+            s += Character.isDigit(array[i].charAt(0)) ? 1 : -1;
+            int j = i + 1, length = j - map.getOrDefault(s, j);
+            if (length > maxLength) {
+                maxLength = length;
+                index = j;
+            }
+        }
+        String[] ans = new String[maxLength];
+        System.arraycopy(array, index - maxLength, ans, 0, maxLength);
+        return ans;
+    }
+
+    public String[] findLongestSubarray2(String[] array) {
+        int n = array.length;
+        int[] first = new int[(n << 1) + 1];
+        Arrays.fill(first, -1);
+        int maxLength = 0, index = 0;
+        for (int i = 0, s = n; i < n; i++) {
+            if (first[s] == -1) {
+                first[s] = i;
+            }
+            s += ((array[i].charAt(0) >> 6 & 1) << 1) - 1;
+            int j = i + 1, length = j - (first[s] == -1 ? j : first[s]);
+            if (length > maxLength) {
+                maxLength = length;
+                index = j;
+            }
+        }
+        String[] ans = new String[maxLength];
+        System.arraycopy(array, index - maxLength, ans, 0, maxLength);
+        return ans;
+    }
+
+    public String[] findLongestSubarray(String[] array) {
+        int n = array.length;
+        int[] first = new int[(n << 1) + 1];
+        int maxLength = 0, index = 0;
+        for (int i = 0, s = n; i < n; i++) {
+            if (first[s] == 0) {
+                first[s] = i + 1;
+            }
+            s += ((array[i].charAt(0) >> 6 & 1) << 1) - 1;
+            int j = i + 2, length = j - (first[s] == 0 ? j : first[s]);
+            if (length > maxLength) {
+                maxLength = length;
+                index = j - 1;
             }
         }
         String[] ans = new String[maxLength];

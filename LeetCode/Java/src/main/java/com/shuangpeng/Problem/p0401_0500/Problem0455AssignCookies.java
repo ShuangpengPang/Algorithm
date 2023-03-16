334405a5b1f3c1e7a572d5f4e15fae8bea6e9a23
shuangpengpang <15913598312@163.com>
2023-03-16 16:05:07 +0800

    分发饼干（455）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0455AssignCookies.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0455AssignCookies.java
@@ -10,18 +10,30 @@ import java.util.Arrays;
  */
 public class Problem0455AssignCookies {
 
-    public int findContentChildren(int[] g, int[] s) {
+    public int findContentChildren0(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
         int n1 = g.length, n2 = s.length, ans = 0;
-        for (int i = 0, j = 0; j < n2 && i < n1; i++) {
+        for (int i = 0, j = 0; j < n2 && i < n1; i++, j++) {
             while (j < n2 && s[j] < g[i]) {
                 j++;
             }
             if (j < n2) {
                 ans++;
             }
-            j++;
+        }
+        return ans;
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans = 0;
+        for (int i = g.length - 1, j = s.length - 1; i >= 0 && j >= 0; i--) {
+            if (s[j] >= g[i]) {
+                ans++;
+                j--;
+            }
         }
         return ans;
     }

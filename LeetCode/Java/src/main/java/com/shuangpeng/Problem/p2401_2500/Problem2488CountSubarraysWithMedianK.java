f31f78c12e52b0e0f93206d975f93206d02d9e6f
shuangpengpang <15913598312@163.com>
2023-03-16 11:45:44 +0800

    统计中位数为K的子数组（2488）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2488CountSubarraysWithMedianK.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2488CountSubarraysWithMedianK.java
@@ -36,7 +36,7 @@ public class Problem2488CountSubarraysWithMedianK {
         return ans;
     }
 
-    public int countSubarrays(int[] nums, int k) {
+    public int countSubarrays1(int[] nums, int k) {
         int n = nums.length, pos = 0;
         while (nums[pos] != k) {
             pos++;
@@ -55,9 +55,68 @@ public class Problem2488CountSubarraysWithMedianK {
         return ans;
     }
 
+    public int countSubarrays(int[] nums, int k) {
+        int n = nums.length, s = n, ans = 0;
+        int[] cnt = new int[n << 1];
+        boolean find = false;
+        for (int i = 0; i < n; i++) {
+            if (!find) {
+                cnt[s]++;
+            }
+            if (nums[i] == k) {
+                find = true;
+            } else {
+                s += nums[i] > k ? 1 : -1;
+            }
+            if (find) {
+                ans += cnt[s] + cnt[s - 1];
+            }
+        }
+        return ans;
+    }
+
 //    public static void main(String[] args) {
 //        Problem2488CountSubarraysWithMedianK a = new Problem2488CountSubarraysWithMedianK();
 //        int[] nums = {3, 2, 1, 4, 5};
 //        a.countSubarrays(nums, 4);
 //    }
 }
+
+//class Solution {
+//    public:
+//    int countSubarrays(vector<int>& nums, int K) {
+//        int n = nums.size();
+//
+//        // 求中位数大等于 LIM 的子数组有几个
+//        auto gao = [&](int LIM) {
+//            // 把每个数改成 1 或 -1
+//            vector<int> vec;
+//            for (int x : nums) {
+//                if (x >= LIM) vec.push_back(1);
+//                else vec.push_back(-1);
+//            }
+//
+//            // 用数组维护 d 的值，由于下标取值范围是 [-n, n]，所以加一个偏移量 n
+//            int d[n * 2 +1];
+//            memset(d, 0, sizeof(d));
+//            d[n] = 1;
+//
+//            long long ret = 0;
+//            for (int i = 0, s = n, c = 0; i < n; i++) {
+//                // 分类讨论 c 的变化情况
+//                if (vec[i] == 1) {
+//                    c += d[s];
+//                    s++;
+//                } else {
+//                    c -= d[s - 1];
+//                    s--;
+//                }
+//                ret += c;
+//                d[s]++;
+//            }
+//            return ret;
+//        };
+//
+//        return gao(K) - gao(K + 1);
+//    }
+//};

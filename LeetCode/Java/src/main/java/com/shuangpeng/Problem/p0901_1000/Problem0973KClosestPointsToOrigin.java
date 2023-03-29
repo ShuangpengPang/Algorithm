658faebd7422b9c4de09a5bd0e96e1f3b2f9043d
shuangpengpang <15913598312@163.com>
2023-03-29 19:53:45 +0800

    最接近原点的K个点（973）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0973KClosestPointsToOrigin.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0973KClosestPointsToOrigin.java
@@ -26,7 +26,7 @@ public class Problem0973KClosestPointsToOrigin {
         return Arrays.copyOfRange(points, 0, k);
     }
 
-    public int[][] kClosest(int[][] points, int k) {
+    public int[][] kClosest2(int[][] points, int k) {
         int n = points.length;
         PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> b[0] * b[0] + b[1] * b[1] - a[0] * a[0] - a[1] * a[1]);
         for (int i = 0; i < k; i++) {
@@ -44,5 +44,42 @@ public class Problem0973KClosestPointsToOrigin {
             ans[i] = q.poll();
         }
         return ans;
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        partition(points, 0, points.length - 1, k);
+        return Arrays.copyOfRange(points, 0, k);
+    }
+
+    private void partition(int[][] points, int start, int end, int k) {
+        if (start == end) {
+            return;
+        }
+        int d = points[end][0] * points[end][0] + points[end][1] * points[end][1];
+        int j = start;
+        for (int i = start; i < end; i++) {
+            int[] p = points[i];
+            if (p[0] * p[0] + p[1] * p[1] <= d) {
+                swap(points, i, j);
+                j++;
+            }
+        }
+        swap(points, j, end);
+        if (j == k - 1 || j == k) {
+            return;
+        }
+        if (j > k) {
+            partition(points, start, j - 1, k);
+        } else {
+            partition(points, j + 1, end, k);
+        }
+    }
+
+    private void swap(int[][] points, int i, int j) {
+        if (i != j) {
+            int[] p = points[i];
+            points[i] = points[j];
+            points[j] = p;
+        }
     }
 }

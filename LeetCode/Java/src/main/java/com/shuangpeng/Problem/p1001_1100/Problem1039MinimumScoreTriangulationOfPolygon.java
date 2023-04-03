9f555e20aa3ca75aab336b501f7ed515f2a5798c
shuangpengpang <15913598312@163.com>
2023-04-03 18:04:40 +0800

    多边形三角剖分的最低得分（1039）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1039MinimumScoreTriangulationOfPolygon.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1039MinimumScoreTriangulationOfPolygon.java
@@ -1,8 +1,12 @@
 package com.shuangpeng.Problem.p1001_1100;
 
+/**
+ * @description: 多边形三角剖分的最低得分
+ * @date 2023/4/3 6:02 PM
+ **/
 public class Problem1039MinimumScoreTriangulationOfPolygon {
 
-    public int minScoreTriangulation(int[] values) {
+    public int minScoreTriangulation0(int[] values) {
         int n = values.length;
         int[][] dp = new int[n][n];
         for (int i = n - 3; i >= 0; --i) {
@@ -13,6 +17,21 @@ public class Problem1039MinimumScoreTriangulationOfPolygon {
                 for (int k = i + 1; k < j - 1; ++k) {
                     int sum = dp[i][k] + values[k] * product + dp[k][j];
                     dp[i][j] = Math.min(dp[i][j], sum);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        for (int i = n - 3; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                int product = values[i] * values[j];
+                dp[i][j] = dp[i + 1][j] + product * values[i + 1];
+                for (int k = i + 2; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + product * values[k]);
                 }
             }
         }

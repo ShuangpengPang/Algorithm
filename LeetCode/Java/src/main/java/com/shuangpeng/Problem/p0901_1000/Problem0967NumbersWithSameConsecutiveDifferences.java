e51f0090a4af3eb4b50cc8596a2fd55c23e6bbe9
shuangpengpang <15913598312@163.com>
2023-04-05 18:42:00 +0800

    连续差相同的数字（967）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0967NumbersWithSameConsecutiveDifferences.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0967NumbersWithSameConsecutiveDifferences.java
@@ -14,7 +14,7 @@ public class Problem0967NumbersWithSameConsecutiveDifferences {
     List<Integer> ans;
     int n, k;
 
-    public int[] numsSameConsecDiff(int n, int k) {
+    public int[] numsSameConsecDiff0(int n, int k) {
         ans = new ArrayList<>();
         this.n = n;
         this.k = k;
@@ -41,5 +41,36 @@ public class Problem0967NumbersWithSameConsecutiveDifferences {
         if (k != 0 && d + k < 10) {
             dfs(num + d + k, index + 1);
         }
+    }
+
+    public int[] numsSameConsecDiff(int n, int k) {
+        if (k == 0) {
+            int num = ((int) Math.pow(10, n) - 1) / 9;
+            int[] ans = new int[9];
+            for (int i = 0; i < 9; i++) {
+                ans[i] = (i + 1) * num;
+            }
+            return ans;
+        }
+        int N = 1 << (n - 1);
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < N; j++) {
+                int num = i, last = i % 10;
+                boolean valid = true;
+                for (int b = n - 2; b >= 0; b--) {
+                    last += ((((j >> b) & 1) << 1) - 1) * k;
+                    if (last < 0 || last > 9) {
+                        valid = false;
+                        break;
+                    }
+                    num = num * 10 + last;
+                }
+                if (valid) {
+                    ans.add(num);
+                }
+            }
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }

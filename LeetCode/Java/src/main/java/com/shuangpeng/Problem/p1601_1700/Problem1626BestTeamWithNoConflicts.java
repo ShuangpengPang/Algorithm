2f7693da6567b199d073402d2af6db381a32a64a
shuangpengpang <15913598312@163.com>
2023-04-06 00:06:53 +0800

    无矛盾的最佳球队（1626）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1626BestTeamWithNoConflicts.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1626BestTeamWithNoConflicts.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p1601_1700;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author ShuangPengPang
@@ -10,7 +12,7 @@ import java.util.Arrays;
  */
 public class Problem1626BestTeamWithNoConflicts {
 
-    public int bestTeamScore(int[] scores, int[] ages) {
+    public int bestTeamScore0(int[] scores, int[] ages) {
         int n = scores.length;
         Integer[] ids = new Integer[n];
         Arrays.setAll(ids, i -> i);
@@ -31,6 +33,36 @@ public class Problem1626BestTeamWithNoConflicts {
                 }
             }
             ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = scores.length;
+        Integer[] ids = new Integer[n];
+        Arrays.setAll(ids, i -> i);
+        Arrays.sort(ids, (a, b) -> {
+            if (ages[a] != ages[b]) {
+                return ages[a] - ages[b];
+            }
+            return scores[a] - scores[b];
+        });
+        int ans = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            int s = scores[ids[i]];
+            Map.Entry<Integer, Integer> entry = map.floorEntry(s);
+            if (entry == null) {
+                map.put(s, s);
+                ans = Math.max(ans, s);
+            } else {
+                int key = entry.getKey(), value = s + entry.getValue();
+                for (Map.Entry<Integer, Integer> e = map.higherEntry(key); e != null && e.getValue() <= value; e = map.higherEntry(e.getKey())) {
+                    map.remove(e.getKey());
+                }
+                map.put(s, value);
+                ans = Math.max(ans, value);
+            }
         }
         return ans;
     }

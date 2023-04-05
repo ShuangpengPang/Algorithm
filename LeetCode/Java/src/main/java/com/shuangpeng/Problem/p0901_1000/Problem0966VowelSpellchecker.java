102ee3a0b37abca2ec68759c5b145f2e125ab7ed
shuangpengpang <15913598312@163.com>
2023-04-05 20:06:32 +0800

    元音拼写检查器（966）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0966VowelSpellchecker.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0966VowelSpellchecker.java
@@ -49,3 +49,48 @@ public class Problem0966VowelSpellchecker {
         return ans;
     }
 }
+
+class Problem0966VowelSpellchecker0 {
+
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> set = new HashSet<>();
+        Map<String, String> m1 = new HashMap<>(), m2 = new HashMap<>();
+        for (String w : wordlist) {
+            set.add(w);
+            String lowerCase = w.toLowerCase();
+            m1.putIfAbsent(lowerCase, w);
+            m2.putIfAbsent(convert(lowerCase), w);
+        }
+        int n = queries.length;
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++) {
+            String w = queries[i];
+            if (set.contains(w)) {
+                ans[i] = w;
+                continue;
+            }
+            String lowerCase = w.toLowerCase();
+            if (m1.containsKey(lowerCase)) {
+                ans[i] = m1.get(lowerCase);
+                continue;
+            }
+            String s = convert(lowerCase);
+            if (m2.containsKey(s)) {
+                ans[i] = m2.get(s);
+            } else {
+                ans[i] = "";
+            }
+        }
+        return ans;
+    }
+
+    private String convert(String w) {
+        char[] cs = w.toCharArray();
+        for (int i = cs.length - 1; i >= 0; i--) {
+            if ("aeiou".indexOf(cs[i]) >= 0) {
+                cs[i] = '*';
+            }
+        }
+        return new String(cs);
+    }
+}

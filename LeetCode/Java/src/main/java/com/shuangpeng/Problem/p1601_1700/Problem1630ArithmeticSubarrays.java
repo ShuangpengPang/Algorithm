050b80e4c6be4dcefdb15c5e190ce70eca36a595
shuangpengpang <15913598312@163.com>
2023-04-05 16:39:22 +0800

    等差子数组（1630）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1630ArithmeticSubarrays.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1630ArithmeticSubarrays.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 public class Problem1630ArithmeticSubarrays {
 
-    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+    public List<Boolean> checkArithmeticSubarrays0(int[] nums, int[] l, int[] r) {
         int m = l.length;
         Set<Integer> set = new HashSet<>();
         List<Boolean> ans = new ArrayList<>(m);
@@ -48,6 +48,48 @@ public class Problem1630ArithmeticSubarrays {
                 }
                 ans.add(valid);
             }
+        }
+        return ans;
+    }
+
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        int n = l.length;
+        List<Boolean> ans = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            int left = l[i], right = r[i], count = right - left + 1;
+            if (count == 1) {
+                ans.add(false);
+                continue;
+            }
+            int min = nums[left], max = nums[left];
+            for (int j = left + 1; j <= right; j++) {
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+            }
+            if (min == max) {
+                ans.add(true);
+                continue;
+            }
+            if ((max - min) % (count - 1) != 0) {
+                ans.add(false);
+                continue;
+            }
+            int d = (max - min) / (count - 1);
+            boolean[] visited = new boolean[count];
+            boolean flag = true;
+            for (int j = left; j <= right; j++) {
+                if ((nums[j] - min) % d != 0) {
+                    flag = false;
+                    break;
+                }
+                int index = (nums[j] - min) / d;
+                if (visited[index]) {
+                    flag = false;
+                    break;
+                }
+                visited[index] = true;
+            }
+            ans.add(flag);
         }
         return ans;
     }

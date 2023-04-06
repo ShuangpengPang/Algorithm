4c55aac0c477ff660b199d68c633b1745da2ecf0
shuangpengpang <15913598312@163.com>
2023-04-06 11:25:23 +0800

    负二进制转换（1017）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1017ConvertToBaseNeg2.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1017ConvertToBaseNeg2.java
@@ -8,13 +8,44 @@ package com.shuangpeng.Problem.p1001_1100;
  */
 public class Problem1017ConvertToBaseNeg2 {
 
+    public String baseNeg20(int n) {
+        if (n == 0) {
+            return "0";
+        }
+        int[] bits = new int[32];
+        for (int i = 0; i < 32 && n != 0; i++) {
+            if ((n & 1) == 1) {
+                bits[i]++;
+                if ((i & 1) == 1) {
+                    bits[i + 1]++;
+                }
+            }
+            n >>= 1;
+        }
+        int carry = 0;
+        for (int i = 0; i < 32; i++) {
+            carry += bits[i];
+            bits[i] = carry & 1;
+            carry = (carry - bits[i]) / -2;
+        }
+        int pos = 31;
+        while (pos >= 0 && bits[pos] == 0) {
+            pos--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (pos >= 0) {
+            sb.append(bits[pos--]);
+        }
+        return sb.toString();
+    }
+
     public String baseNeg2(int n) {
         if (n == 0) {
             return "0";
         }
         StringBuilder sb = new StringBuilder();
         while (n != 0) {
-            int v = Math.abs(n) & 1;
+            int v = n & 1;
             n = (n - v) / -2;
             sb.append(v);
         }

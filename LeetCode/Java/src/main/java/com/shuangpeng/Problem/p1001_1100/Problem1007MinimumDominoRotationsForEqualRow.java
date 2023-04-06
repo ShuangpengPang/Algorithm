aca5a8f691a54183776855c7943b00b26713c997
shuangpengpang <15913598312@163.com>
2023-04-06 17:57:07 +0800

    行相等的最少多米诺旋转（1007）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1007MinimumDominoRotationsForEqualRow.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1007MinimumDominoRotationsForEqualRow.java
@@ -1,5 +1,7 @@
 package com.shuangpeng.Problem.p1001_1100;
 
+import java.util.Arrays;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -58,5 +60,71 @@ class Problem1007MinimumDominoRotationsForEqualRow0 {
             }
         }
         return Math.min(c1, c2);
+    }
+}
+
+class Problem1007MinimumDominoRotationsForEqualRow1 {
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int n = tops.length;
+        int[] cnt1 = new int[7], cnt2 = new int[7], total = new int[7];
+        Arrays.fill(total, n);
+        for (int i = 0; i < n; i++) {
+            int x = tops[i], y = bottoms[i];
+            if (x == y) {
+                total[x]--;
+            } else {
+                cnt1[x]++;
+                cnt2[y]++;
+            }
+        }
+        int ans = n + 1;
+        for (int i = 0; i < 7; i++) {
+            if (cnt1[i] + cnt2[i] == total[i]) {
+                ans = Math.min(ans, Math.min(cnt1[i], cnt2[i]));
+            }
+        }
+        return ans == n + 1 ? -1 : ans;
+    }
+}
+
+class Problem1007MinimumDominoRotationsForEqualRow2 {
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int n = tops.length, a = tops[0], b = bottoms[0];
+        int[][] cnt = new int[2][2];
+        int[] total = new int[2], nums = new int[2], t = {a, b};
+        total[0] = total[1] = n;
+        for (int i = 0; i < n; i++) {
+            int x = tops[i], y = bottoms[i];
+            if (x != a && x != b && y != a && y != b) {
+                return -1;
+            }
+            if (x == y) {
+                if (x == a) {
+                    total[0]--;
+                } else {
+                    total[1]--;
+                }
+            } else {
+                nums[0] = x;
+                nums[1] = y;
+                for (int j = 0; j < 2; j++) {
+                    for (int k = 0; k < 2; k++) {
+                        if (nums[j] == t[k]) {
+                            cnt[j][k]++;
+                        }
+                    }
+                }
+            }
+        }
+        int ans = n + 1;
+        if (cnt[0][0] + cnt[1][0] == total[0]) {
+            ans = Math.min(cnt[0][0], cnt[1][0]);
+        }
+        if (cnt[0][1] + cnt[1][1] == total[1]) {
+            ans = Math.min(ans, Math.min(cnt[0][1], cnt[1][1]));
+        }
+        return ans == n + 1 ? -1 : ans;
     }
 }

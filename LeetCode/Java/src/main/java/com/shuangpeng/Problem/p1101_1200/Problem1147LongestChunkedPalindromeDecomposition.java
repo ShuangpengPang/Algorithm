1476a01dc3b522d38eec1a8b0f1f54513ba218f6
shuangpengpang <15913598312@163.com>
2023-04-13 12:03:14 +0800

    段式回文（1147）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1147LongestChunkedPalindromeDecomposition.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1147LongestChunkedPalindromeDecomposition.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1101_1200;
  */
 public class Problem1147LongestChunkedPalindromeDecomposition {
 
-    public int longestDecomposition(String text) {
+    public int longestDecomposition0(String text) {
         char[] cs = text.toCharArray();
         return dfs(cs, 0, new int[(cs.length + 1) >> 1]);
     }
@@ -36,6 +36,35 @@ public class Problem1147LongestChunkedPalindromeDecomposition {
         int n = end - start + 1;
         for (int i = start, j = cs.length - i - n; i <= end; i++, j++) {
             if (cs[i] != cs[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Problem1147LongestChunkedPalindromeDecomposition0 {
+
+    public int longestDecomposition(String text) {
+        char[] cs = text.toCharArray();
+        int n = cs.length, h = (n + 1) >> 1, m = n >> 1;
+        int[] dp = new int[h + 1];
+        for (int i = h - 1; i >= 0; i--) {
+            int ans = 1;
+            for (int j = i; j < m; j++) {
+                if (check(cs, i, n - j - 1, j - i + 1)) {
+                    ans = Math.max(ans, dp[j + 1] + 2);
+                    break;
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
+    }
+
+    private boolean check(char[] cs, int s1, int s2, int n) {
+        for (int i = 0; i < n; i++) {
+            if (cs[s1 + i] != cs[s2 + i]) {
                 return false;
             }
         }

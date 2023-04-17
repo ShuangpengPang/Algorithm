243afe7c8c59032c32ee9f841d9881f1611f16b4
shuangpengpang <15913598312@163.com>
2023-04-17 17:34:26 +0800

    困于环中的机器人（1041）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1041RobotBoundedInCircle.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1041RobotBoundedInCircle.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1001_1100;
  */
 public class Problem1041RobotBoundedInCircle {
 
-    public boolean isRobotBounded(String instructions) {
+    public boolean isRobotBounded0(String instructions) {
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int x = 0, y = 0, d = 0, n = instructions.length();
         for (int i = 0; i < n; i++) {
@@ -21,5 +21,21 @@ public class Problem1041RobotBoundedInCircle {
             }
         }
         return x == 0 && y == 0 || d != 0;
+    }
+
+    public boolean isRobotBounded(String instructions) {
+        int n = instructions.length(), d = 0;
+        int[] dist = new int[4];
+        for (int i = 0; i < n; i++) {
+            char c = instructions.charAt(i);
+            if (c == 'L') {
+                d = (d + 1) % 4;
+            } else if (c == 'R') {
+                d = (d + 3) % 4;
+            } else {
+                dist[d]++;
+            }
+        }
+        return dist[0] == dist[1] && dist[2] == dist[4] || d != 0;
     }
 }

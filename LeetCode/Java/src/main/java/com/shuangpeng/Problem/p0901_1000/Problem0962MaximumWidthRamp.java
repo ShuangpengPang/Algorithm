e0e4b41209ea9f224b8cdb4ab29338619ae1b1f5
shuangpengpang <15913598312@163.com>
2023-04-18 15:58:02 +0800

    最大宽度坡（962）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0962MaximumWidthRamp.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0962MaximumWidthRamp.java
@@ -2,6 +2,9 @@ package com.shuangpeng.Problem.p0901_1000;
 
 import javafx.util.Pair;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 /**
@@ -12,7 +15,7 @@ import java.util.TreeSet;
  */
 public class Problem0962MaximumWidthRamp {
 
-    public int maxWidthRamp(int[] nums) {
+    public int maxWidthRamp0(int[] nums) {
         TreeSet<Pair<Integer, Integer>> set = new TreeSet<>((a, b) -> {
             int aNum = a.getKey(), aIndex = a.getValue();
             int bNum = b.getKey(), bIndex = b.getValue();
@@ -32,4 +35,96 @@ public class Problem0962MaximumWidthRamp {
         }
         return ans;
     }
+
+    public int maxWidthRamp1(int[] nums) {
+        TreeSet<int[]> set = new TreeSet<>(Comparator.comparingInt(a -> a[0]));
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int[] arr = {nums[i], i};
+            int[] value = set.floor(arr);
+            if (value != null) {
+                ans = Math.max(ans, i - value[1]);
+            } else {
+                set.add(arr);
+            }
+        }
+        return ans;
+    }
+
+    public int maxWidthRamp2(int[] nums) {
+        int ans = 0, n = nums.length;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            Map.Entry<Integer, Integer> entry = map.floorEntry(nums[i]);
+            if (entry != null) {
+                ans = Math.max(ans, i - entry.getValue());
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return ans;
+    }
+
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length, top = 1;
+        int[] stack = new int[n];
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            int left = 0, right = top - 1;
+            while (left <= right) {
+                int mid = left + (right - left >> 1);
+                if (nums[stack[mid]] > nums[i]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans = Math.max(ans, left < top ? i - stack[left] : 0);
+            if (nums[i] < nums[stack[top - 1]]) {
+                stack[top++] = i;
+            }
+        }
+        return ans;
+    }
+
+    //    public static void main(String[] args) {
+//        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
+//        executor.setKeepAliveTime(7 * 1000, TimeUnit.MILLISECONDS);
+//        executor.allowCoreThreadTimeOut(true);
+//        for (int k = 0; k < 2; k++) {
+//            System.err.println("main time: " + new Date(System.currentTimeMillis()));
+//            for (int i = 0; i < 20; i++) {
+//                int j = i;
+//                RunnableScheduledFuture task = (RunnableScheduledFuture<?>) executor.schedule(() -> {
+//                    System.err.println("activeCount: " + executor.getActiveCount() + ", pollSize: " + executor.getPoolSize());
+//                    System.err.println("startTime: " + j + ", " + new Date(System.currentTimeMillis()));
+//                    try {
+//                        Thread.sleep(50);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.err.println("endTime: " + j + ", " + new Date(System.currentTimeMillis()));
+//                }, 0, TimeUnit.SECONDS);
+//                try {
+//                    Thread.sleep(200);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            try {
+//                Thread.sleep(6 * 1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.err.println("==============");
+//            for (int i = 0; i < 10; i++) {
+//                System.err.println("---------activeCount: " + executor.getPoolSize());
+//                try {
+//                    Thread.sleep(200);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 }

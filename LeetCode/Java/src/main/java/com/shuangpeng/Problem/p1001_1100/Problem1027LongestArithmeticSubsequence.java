4ddb325a5e9e5e3526c9ca520c9331000a5d5203
shuangpengpang <15913598312@163.com>
2023-04-23 00:34:09 +0800

    最长等差数列（1027）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1027LongestArithmeticSubsequence.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1027LongestArithmeticSubsequence.java
@@ -5,9 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @description: 最长等差数列
+ * @date 2023/4/23 12:33 AM
+ **/
 public class Problem1027LongestArithmeticSubsequence {
 
-    public int longestArithSeqLength(int[] nums) {
+    public int longestArithSeqLength0(int[] nums) {
         int n = nums.length;
         List<Map<Integer, Integer>> list = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
@@ -22,6 +26,23 @@ public class Problem1027LongestArithmeticSubsequence {
                 if (c > count) {
                     list.get(i).put(d, c);
                     ans = Math.max(ans, c);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int longestArithSeqLength(int[] nums) {
+        int n = nums.length, ans = 0;
+        Map<Integer, Integer>[] m = new Map[n];
+        for (int i = 0; i < n; i++) {
+            m[i] = new HashMap<>();
+            for (int j = i - 1; j >= 0; j--) {
+                int d = nums[i] - nums[j];
+                if (!m[i].containsKey(d)) {
+                    int count = m[j].getOrDefault(d, 1) + 1;
+                    m[i].put(d, count);
+                    ans = Math.max(ans, count);
                 }
             }
         }

b49c0af9bb4efdf94ccf63c7083d4949c68a72df
shuangpengpang <15913598312@163.com>
2023-04-23 10:47:03 +0800

    填充书架（1105）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1105FillingBookcaseShelves.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1105FillingBookcaseShelves.java
@@ -2,9 +2,13 @@ package com.shuangpeng.Problem.p1101_1200;
 
 import java.util.Arrays;
 
+/**
+ * @description: 填充书架
+ * @date 2023/4/23 10:45 AM
+ **/
 public class Problem1105FillingBookcaseShelves {
 
-    public int minHeightShelves(int[][] books, int shelfWidth) {
+    public int minHeightShelves0(int[][] books, int shelfWidth) {
         int n = books.length;
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
@@ -15,6 +19,23 @@ public class Problem1105FillingBookcaseShelves {
                 maxHeight = Math.max(maxHeight, books[j - 1][1]);
                 dp[i] = Math.min(dp[i], dp[j - 1] + maxHeight);
                 width += books[j - 1][0];
+            }
+        }
+        return dp[n];
+    }
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = i, w = 0, h = 0; j >= 1; j--) {
+                w += books[j - 1][0];
+                h = Math.max(h, books[j - 1][1]);
+                if (w > shelfWidth) {
+                    break;
+                }
+                dp[i] = Math.min(dp[i], dp[j - 1] + h);
             }
         }
         return dp[n];

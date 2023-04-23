10e9ab406a3c5f4215bcf7305339a9150efb6e3e
shuangpengpang <15913598312@163.com>
2023-04-23 18:47:27 +0800

    重塑矩阵（566）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0566ReshapeTheMatrix.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0566ReshapeTheMatrix.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p0501_0600;
  */
 public class Problem0566ReshapeTheMatrix {
 
-    public int[][] matrixReshape(int[][] mat, int r, int c) {
+    public int[][] matrixReshape0(int[][] mat, int r, int c) {
         int m = mat.length, n = mat[0].length;
         if (m * n != r * c || m == r && n == c) {
             return mat;
@@ -21,6 +21,26 @@ public class Problem0566ReshapeTheMatrix {
                     x++;
                     y = 0;
                 }
+            }
+        }
+        return ans;
+    }
+
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length, n = mat[0].length, t = m * n;
+        if (t != r * c || m == r) {
+            return mat;
+        }
+        int[][] ans = new int[r][c];
+        for (int i = 0, j = 0, x = 0, y = 0; i < r;) {
+            ans[i][j] = mat[x][y];
+            if (++j == c) {
+                i++;
+                j = 0;
+            }
+            if (++y == n) {
+                x++;
+                y = 0;
             }
         }
         return ans;

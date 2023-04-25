47e506df4296cad6441bed9a07a3b60793f88070
shuangpengpang <15913598312@163.com>
2023-04-25 20:07:58 +0800

    最大连续1的个数（485）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0485MaxConsecutiveOnes.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0485MaxConsecutiveOnes.java
@@ -8,18 +8,27 @@ package com.shuangpeng.Problem.p0401_0500;
  */
 public class Problem0485MaxConsecutiveOnes {
 
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public int findMaxConsecutiveOnes0(int[] nums) {
         int n = nums.length, ans = 0, count = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 1) {
                 count++;
-                if (i == n - 1 || nums[i + 1] == 0) {
-                    ans = Math.max(ans, count);
-                }
+                ans = Math.max(ans, count);
             } else {
                 count = 0;
             }
         }
         return ans;
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length, ans = 0, start = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                ans = Math.max(ans, i - start);
+                start = i + 1;
+            }
+        }
+        return Math.max(ans, n - start);
     }
 }

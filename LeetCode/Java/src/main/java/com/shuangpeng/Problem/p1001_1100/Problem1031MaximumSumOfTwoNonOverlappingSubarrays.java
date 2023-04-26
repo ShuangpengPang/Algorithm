67b71a0e29e35ec5447ac320c1275245750d3075
shuangpengpang <15913598312@163.com>
2023-04-26 11:13:44 +0800

    两个非重叠子数组的最大和（1031）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1031MaximumSumOfTwoNonOverlappingSubarrays.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1031MaximumSumOfTwoNonOverlappingSubarrays.java
@@ -1,8 +1,12 @@
 package com.shuangpeng.Problem.p1001_1100;
 
+/**
+ * @description: 两个非重叠子数组的最大和
+ * @date 2023/4/26 10:33 AM
+ **/
 public class Problem1031MaximumSumOfTwoNonOverlappingSubarrays {
 
-    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+    public int maxSumTwoNoOverlap0(int[] nums, int firstLen, int secondLen) {
         int n = nums.length;
         int[] sum1 = new int[n];
         int[] sum2 = new int[n];
@@ -39,5 +43,53 @@ public class Problem1031MaximumSumOfTwoNonOverlappingSubarrays {
             }
             sum[i] = s;
         }
+    }
+
+    public int maxSumTwoNoOverlap1(int[] nums, int firstLen, int secondLen) {
+        int n = nums.length, ans = 0;
+        int[] m1 = new int[n + 1], m2 = new int[n + 1];
+        for (int i = 1, s1 = 0, s2 = 0; i <= n; i++) {
+            int num = nums[i - 1];
+            s1 += num;
+            s2 += num;
+            if (i > firstLen) {
+                s1 -= nums[i - firstLen - 1];
+            }
+            if (i > secondLen) {
+                s2 -= nums[i - secondLen - 1];
+            }
+            if (i >= firstLen) {
+                m1[i] = Math.max(m1[i - 1], s1);
+                ans = Math.max(ans, s1 + m2[i - firstLen]);
+            }
+            if (i >= secondLen) {
+                m2[i] = Math.max(m2[i - 1], s2);
+                ans = Math.max(ans, s2 + m1[i - secondLen]);
+            }
+        }
+        return ans;
+    }
+
+    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+        return Math.max(getMaxSum(nums, firstLen, secondLen), getMaxSum(nums, secondLen, firstLen));
+    }
+
+    private int getMaxSum(int[] nums, int n1, int n2) {
+        int n = nums.length, m = 0, s1 = 0, s2 = 0, t = n1 + n2;
+        for (int i = 0; i < n1; i++) {
+            s1 += nums[i];
+        }
+        for (int i = n1; i < t; i++) {
+            s2 += nums[i];
+        }
+        m = s1;
+        int ans = s1 + s2;
+        for (int i = n1, j = t; j < n; i++, j++) {
+            s1 += nums[i] - nums[i - n1];
+            s2 += nums[j] - nums[j - n2];
+            m = Math.max(m, s1);
+            ans = Math.max(ans, s2 + m);
+        }
+        return ans;
     }
 }

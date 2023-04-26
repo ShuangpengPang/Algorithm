9e40e651eafb83e53fe510f9324992d8f0529ddb
shuangpengpang <15913598312@163.com>
2023-04-26 11:49:22 +0800

    给定行和列的和求可行矩阵（1605）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1605FindValidMatrixGivenRowAndColumnSums.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1601_1700/Problem1605FindValidMatrixGivenRowAndColumnSums.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1601_1700;
  */
 public class Problem1605FindValidMatrixGivenRowAndColumnSums {
 
-    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+    public int[][] restoreMatrix0(int[] rowSum, int[] colSum) {
         int m = rowSum.length, n = colSum.length;
         int[][] ans = new int[m][n];
         for (int i = 0; i < m; i++) {
@@ -17,6 +17,25 @@ public class Problem1605FindValidMatrixGivenRowAndColumnSums {
                 ans[i][j] = num;
                 rowSum[i] -= num;
                 colSum[j] -= num;
+            }
+        }
+        return ans;
+    }
+
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length, n = colSum.length;
+        int[][] ans = new int[m][n];
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            int num = Math.min(rowSum[i], colSum[j]);
+            ans[i][j] = num;
+            rowSum[i] -= num;
+            colSum[j] -= num;
+            if (rowSum[i] == 0) {
+                i++;
+            }
+            if (colSum[j] == 0) {
+                j++;
             }
         }
         return ans;

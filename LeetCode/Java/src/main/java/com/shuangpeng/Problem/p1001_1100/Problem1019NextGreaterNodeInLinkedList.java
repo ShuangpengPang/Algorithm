fb5bd286be25639f0b995e4c15726cd93c7c9fc8
shuangpengpang <15913598312@163.com>
2023-04-26 16:43:35 +0800

    链表中的下一个更大节点（1019）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1019NextGreaterNodeInLinkedList.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1019NextGreaterNodeInLinkedList.java
@@ -61,7 +61,7 @@ public class Problem1019NextGreaterNodeInLinkedList {
         return ans;
     }
 
-    public int[] nextLargerNodes(ListNode head) {
+    public int[] nextLargerNodes2(ListNode head) {
         return recurse(head, 0, new ArrayDeque<>());
     }
 
@@ -75,6 +75,29 @@ public class Problem1019NextGreaterNodeInLinkedList {
         }
         ans[n] = stack.isEmpty() ? 0 : stack.peek();
         stack.push(node.val);
+        return ans;
+    }
+
+    public int[] nextLargerNodes(ListNode head) {
+        ListNode prev = null;
+        int n = 0;
+        for (ListNode node = head, next; node != null; node = next) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            n++;
+        }
+        int[] ans = new int[n], stack = new int[n];
+        int top = 0;
+        ListNode node = prev;
+        for (int i = n - 1; i >= 0; i--) {
+            while (top > 0 && stack[top - 1] <= node.val) {
+                top--;
+            }
+            ans[i] = top > 0 ? stack[top - 1] : 0;
+            stack[top++] = node.val;
+            node = node.next;
+        }
         return ans;
     }
 }

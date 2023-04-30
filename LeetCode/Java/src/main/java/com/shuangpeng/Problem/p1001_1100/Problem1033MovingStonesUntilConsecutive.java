8ead7013dc07fb4814f77fcd1a6db922fd398960
shuangpengpang <15913598312@163.com>
2023-04-30 16:20:12 +0800

    移动石子直到连续（1033）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1033MovingStonesUntilConsecutive.java
@@ -0,0 +1,24 @@
+package com.shuangpeng.Problem.p1001_1100;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1033MovingStonesUntilConsecutive（移动石子直到连续）
+ * @date 2023/4/30 4:18 PM
+ */
+public class Problem1033MovingStonesUntilConsecutive {
+
+    public int[] numMovesStones(int a, int b, int c) {
+        int[] arr = {a, b, c};
+        Arrays.sort(arr);
+        int min = 2;
+        if (arr[2] - arr[0] == 2) {
+            min = 0;
+        } else if (arr[1] - arr[0] <= 2 || arr[2] - arr[1] <= 2) {
+            min = 1;
+        }
+        return new int[]{min, arr[2] - arr[0] - 2};
+    }
+}

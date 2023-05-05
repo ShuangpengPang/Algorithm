0762a1b8a6b6fad0e986dcd9aa92eb43a43207b4
shuangpengpang <15913598312@163.com>
2023-05-05 11:12:49 +0800

    处理用时最长的那个任务的员工（2432）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2432TheEmployeeThatWorkedOnTheLongestTask.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p2401_2500;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2432TheEmployeeThatWorkedOnTheLongestTask（处理用时最长的那个任务的员工）
+ * @date 2023/5/5 10:38 AM
+ */
+public class Problem2432TheEmployeeThatWorkedOnTheLongestTask {
+
+    public int hardestWorker(int n, int[][] logs) {
+        int ans = 0, maxTime = 0, startTime = 0;
+        for (int[] log : logs) {
+            int id = log[0], time = log[1] - startTime;
+            if (time > maxTime) {
+                maxTime = time;
+                ans = id;
+            } else if (time == maxTime && id < ans) {
+                ans = id;
+            }
+            startTime = log[1];
+        }
+        return ans;
+    }
+}

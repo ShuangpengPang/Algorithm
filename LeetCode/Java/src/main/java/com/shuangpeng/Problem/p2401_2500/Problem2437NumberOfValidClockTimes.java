7a31d1117616413445ae8fd929cd9a7e463e6427
shuangpengpang <15913598312@163.com>
2023-05-09 11:14:17 +0800

    有效时间的数目（2437）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2437NumberOfValidClockTimes.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2437NumberOfValidClockTimes.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p2401_2500;
  */
 public class Problem2437NumberOfValidClockTimes {
 
-    public int countTime(String time) {
+    public int countTime0(String time) {
         int ans = 0;
         char c1 = time.charAt(0), c2 = time.charAt(1);
         char c3 = time.charAt(3), c4 = time.charAt(4);
@@ -37,5 +37,37 @@ public class Problem2437NumberOfValidClockTimes {
             }
         }
         return ans;
+    }
+
+    public int countTime(String time) {
+        int[] r1 = getRange(time.charAt(0)), r2 = getRange(time.charAt(1));
+        int[] r3 = getRange(time.charAt(3)), r4 = getRange(time.charAt(4));
+        int ans = 0;
+        for (int h1 = r1[0]; h1 <= r1[1]; h1++) {
+            if (h1 > 2) {
+                break;
+            }
+            for (int h2 = r2[0]; h2 <= r2[1]; h2++) {
+                if (h1 == 2 && h2 > 3) {
+                    break;
+                }
+                for (int m1 = r3[0]; m1 <= r3[1]; m1++) {
+                    if (m1 > 5) {
+                        break;
+                    }
+                    for (int m2 = r4[0]; m2 <= r4[1]; m2++) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private int[] getRange(char c) {
+        if (c == '?') {
+            return new int[]{0, 9};
+        }
+        return new int[]{c - '0', c - '0'};
     }
 }

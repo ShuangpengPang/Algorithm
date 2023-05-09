d1aebf55072bb899b36d7682d38f9590efad1f2c
shuangpengpang <15913598312@163.com>
2023-05-09 14:48:07 +0800

    总持续时间可被60整除的歌曲（1010）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1010PairsOfSongsWithTotalDurationsDivisibleBy60.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1010PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -8,6 +8,18 @@ package com.shuangpeng.Problem.p1001_1100;
  */
 public class Problem1010PairsOfSongsWithTotalDurationsDivisibleBy60 {
 
+    public int numPairsDivisibleBy600(int[] time) {
+        int[] cnt = new int[60];
+        for (int t : time) {
+            cnt[t % 60]++;
+        }
+        long ans = (long) cnt[0] * (cnt[0] - 1) / 2 + (long) cnt[30] * (cnt[30] - 1) / 2;
+        for (int i = 1; i < 30; i++) {
+            ans += cnt[i] * cnt[60 - i];
+        }
+        return (int) ans;
+    }
+
     public int numPairsDivisibleBy60(int[] time) {
         int[] cnt = new int[60];
         int ans = 0;

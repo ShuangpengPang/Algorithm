04d5b464d6762272b389751d03bc41773b334d1a
shuangpengpang <15913598312@163.com>
2023-05-10 11:09:34 +0800

    可被K整除的最小整数（1015）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1015SmallestIntegerDivisibleByK.java
@@ -0,0 +1,47 @@
+package com.shuangpeng.Problem.p1001_1100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1015SmallestIntegerDivisibleByK（可被K整除的最小整数）
+ * @date 2023/5/10 10:04 AM
+ */
+public class Problem1015SmallestIntegerDivisibleByK {
+
+    public int smallestRepunitDivByK0(int k) {
+        if (k == 1) {
+            return 1;
+        }
+        boolean[] mod = new boolean[k];
+        int len = 0, s = 0, m = 1;
+        while (!mod[m]) {
+            len++;
+            s = (s + m) % k;
+            if (s == 0) {
+                return len;
+            }
+            mod[m] = true;
+            m = m * 10 % k;
+        }
+        boolean[] sum = new boolean[k];
+        while (s != 0 && !sum[s]) {
+            sum[s] = true;
+            len++;
+            s = (s + m) % k;
+            m = m * 10 % k;
+        }
+        return s == 0 ? len : -1;
+    }
+
+    public int smallestRepunitDivByK(int k) {
+        boolean[] mod = new boolean[k];
+        for (int i = 0, n = 0; !mod[n]; i++) {
+            mod[n] = true;
+            n = (n * 10 + 1) % k;
+            if (n == 0) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}

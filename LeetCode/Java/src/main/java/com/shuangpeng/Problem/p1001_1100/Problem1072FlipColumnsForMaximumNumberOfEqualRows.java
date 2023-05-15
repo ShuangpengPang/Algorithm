522f993f75b29d3fd9b3a672750e545a9c453178
shuangpengpang <15913598312@163.com>
2023-05-15 16:07:30 +0800

    按列翻转得到最大值等行数（1072）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1072FlipColumnsForMaximumNumberOfEqualRows.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1072FlipColumnsForMaximumNumberOfEqualRows.java
@@ -11,23 +11,16 @@ import java.util.Map;
  */
 public class Problem1072FlipColumnsForMaximumNumberOfEqualRows {
 
-    public int maxEqualRowsAfterFlips0(int[][] matrix) {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         Map<String, Integer> map = new HashMap<>(m);
-        StringBuilder sb = new StringBuilder(n);
+        char[] cs = new char[n];
         int ans = 0;
-        for (int[] r : matrix) {
-            sb.setLength(0);
-            if (r[0] == 0) {
-                for (int b : r) {
-                    sb.append(b);
-                }
-            } else {
-                for (int b : r) {
-                    sb.append(b ^ 1);
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                cs[j] = (char) ('0' + (matrix[i][j] ^ matrix[i][0]));
             }
-            String s = sb.toString();
+            String s = new String(cs);
             int count = map.getOrDefault(s, 0) + 1;
             map.put(s, count);
             ans = Math.max(ans, count);

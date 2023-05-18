3c9ce82f3c0f9b11cdd4c6fae045e704ef6dbcdf
shuangpengpang <15913598312@163.com>
2023-05-18 16:36:12 +0800

    负二进制数相加（1073）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1073AddingTwoNegabinaryNumbers.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1073AddingTwoNegabinaryNumbers.java
@@ -9,7 +9,7 @@ package com.shuangpeng.Problem.p1001_1100;
 public class Problem1073AddingTwoNegabinaryNumbers {
 
     public int[] addNegabinary(int[] arr1, int[] arr2) {
-        int n1 = arr1.length, n2 = arr2.length, n = Math.max(n1, n2);
+        int n1 = arr1.length, n2 = arr2.length, n = Math.max(n1, n2) + 2;
         int[] arr = new int[n];
         int carry = 0;
         for (int idx = n - 1, i = n1 - 1, j = n2 - 1; idx >= 0; idx--, i--, j--) {
@@ -22,27 +22,13 @@ public class Problem1073AddingTwoNegabinaryNumbers {
             arr[idx] = carry % (-2) == 0 ? 0 : 1;
             carry = (carry - arr[idx]) / (-2);
         }
-        int[] ans = null;
-        if (carry == 0) {
-            int i = 0;
-            while (i < n && arr[i] == 0) {
-                i++;
-            }
-            if (i == n) {
-                return new int[1];
-            }
-            int m = n - i;
-            ans = new int[m];
-            System.arraycopy(arr, i, ans, 0, m);
-        } else if (carry == 1) {
-            ans = new int[n + 1];
-            ans[0] = 1;
-            System.arraycopy(arr, 0, ans, 1, n);
-        } else if (carry == -1) {
-            ans = new int[n + 2];
-            ans[0] = ans[1] = 1;
-            System.arraycopy(arr, 0, ans, 2, n);
+        int i = 0;
+        while (i < n - 1 && arr[i] == 0) {
+            i++;
         }
+        int m = n - i;
+        int[] ans = new int[m];
+        System.arraycopy(arr, i, ans, 0, m);
         return ans;
     }
 }

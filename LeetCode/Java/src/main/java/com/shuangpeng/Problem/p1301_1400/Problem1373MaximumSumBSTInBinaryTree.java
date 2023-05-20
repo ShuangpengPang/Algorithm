bf55f8ca9668b379ca6dca5da424e9cae83306d3
shuangpengpang <15913598312@163.com>
2023-05-20 11:21:02 +0800

    二叉搜索子树的最大键值和（1373）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1373MaximumSumBSTInBinaryTree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1373MaximumSumBSTInBinaryTree.java
@@ -55,3 +55,28 @@ class Problem1373MaximumSumBSTInBinaryTree0 {
         return new int[]{Math.min(root.val, left[0]), Math.max(root.val, right[1]), sum};
     }
 }
+
+class Problem1373MaximumSumBSTInBinaryTree1 {
+
+    static int INF = Integer.MAX_VALUE;
+    int ans;
+
+    public int maxSumBST(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{INF, -INF, 0};
+        }
+        int[] left = dfs(root.left), right = dfs(root.right);
+        if (left[1] >= root.val || root.val >= right[0]) {
+            return new int[]{-INF, INF, 0};
+        }
+        int s = left[2] + right[2] + root.val;
+        ans = Math.max(ans, s);
+        return new int[]{Math.min(left[0], root.val), Math.max(right[1], root.val), s};
+    }
+}

17ed657f1e1d290fdb08394fab2528c4bdad58c3
shuangpengpang <15913598312@163.com>
2023-05-20 12:12:00 +0800

    替换子串得到平衡字符串（1234）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1234ReplaceTheSubstringForBalancedString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1234ReplaceTheSubstringForBalancedString.java
@@ -22,14 +22,14 @@ public class Problem1234ReplaceTheSubstringForBalancedString {
         }
         int ans = n;
         boolean valid = false;
-        for (int l = -1, r = 0; r < n; r++) {
+        for (int l = 0, r = 0; r < n; r++) {
             cnt[map[cs[r]]]--;
-            while (l < r && check(cnt, map[cs[l + 1]], avg)) {
-                cnt[map[cs[++l]]]++;
+            while (l <= r && check(cnt, map[cs[l]], avg)) {
+                cnt[map[cs[l++]]]++;
                 valid = true;
             }
             if (valid || check(cnt, -1, avg)) {
-                ans = Math.min(ans, r - l);
+                ans = Math.min(ans, r - l + 1);
             }
         }
         return ans;
@@ -41,46 +41,6 @@ public class Problem1234ReplaceTheSubstringForBalancedString {
             if (count > avg) {
                 return false;
             }
-        }
-        return true;
-    }
-}
-
-class Solution {
-    public int balancedString(String s) {
-        int[] cnt = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            cnt[idx(c)]++;
-        }
-
-        int partial = s.length() / 4;
-        int res = s.length();
-
-        if (check(cnt, partial)) {
-            return 0;
-        }
-        for (int l = 0, r = 0; l < s.length(); l++) {
-            while (r < s.length() && !check(cnt, partial)) {
-                cnt[idx(s.charAt(r))]--;
-                r++;
-            }
-            if (!check(cnt, partial)) {
-                break;
-            }
-            res = Math.min(res, r - l);
-            cnt[idx(s.charAt(l))]++;
-        }
-        return res;
-    }
-
-    public int idx(char c) {
-        return c - 'A';
-    }
-
-    public boolean check(int[] cnt, int partial) {
-        if (cnt[idx('Q')] > partial || cnt[idx('W')] > partial || cnt[idx('E')] > partial || cnt[idx('R')] > partial) {
-            return false;
         }
         return true;
     }

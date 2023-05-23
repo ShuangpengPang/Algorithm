2a5742ea8a01c68e9e0cdbeb04f22263ff5d2f32
shuangpengpang <15913598312@163.com>
2023-05-23 18:47:15 +0800

    子串能表示从1到N数字的二进制串（1016）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1016BinaryStringWithSubstringsRepresenting1ToN.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1016BinaryStringWithSubstringsRepresenting1ToN.java
@@ -55,43 +55,43 @@ public class Problem1016BinaryStringWithSubstringsRepresenting1ToN {
 
 class Problem1016BinaryStringWithSubstringsRepresenting1ToN0 {
 
-    int count, n, m;
+    int n, m;
     boolean[] visited;
 
     public boolean queryString(String s, int n) {
         this.n = n;
         m = s.length();
-        if (n > (m + 1) * m >> 1) {
-            return false;
-        }
         if (n == 1) {
             return s.contains("1");
         }
         int k = getBitCount(n);
-        count = 0;
+        if (m < Math.min(k + n - (1 << (k - 1)), k + (1 << k - 2) - 2)) {
+            return false;
+        }
         visited = new boolean[1 << k];
         char[] cs = s.toCharArray();
-        check(cs, k);
-        check(cs, k - 1);
-        return count == n - (1 << (k - 2)) + 1;
+        return check(cs, k, n) && check(cs, k - 1, (1 << (k - 1)) - 1);
     }
 
-    private void check(char[] cs, int k) {
-        int num = 0, N = 1 << (k - 1), M = 1 << k;
+    private boolean check(char[] cs, int k, int max) {
+        int num = 0, N = 1 << (k - 1);
         for (int i = 0; i < k; i++) {
             num = (num << 1) + cs[i] - '0';
         }
-        if (num >= N && num <= n && !visited[num]) {
+        int count = 0;
+        if (num >= N && num <= max && !visited[num]) {
             visited[num] = true;
             count++;
         }
+        int mask = (1 << (k - 1)) - 1;
         for (int i = k; i < m; i++) {
-            num = (num << 1) + (cs[i] - '0') - (cs[i - k] - '0') * M;
-            if (num >= N && num <= n && !visited[num]) {
+            num = ((num & mask) << 1) + (cs[i] - '0');
+            if (num >= N && num <= max && !visited[num]) {
                 visited[num] = true;
                 count++;
             }
         }
+        return count == max - N + 1;
     }
 
     private int getBitCount(int n) {
@@ -101,5 +101,50 @@ class Problem1016BinaryStringWithSubstringsRepresenting1ToN0 {
             bit++;
         }
         return bit;
+    }
+}
+
+class Problem1016BinaryStringWithSubstringsRepresenting1ToN1 {
+
+    public boolean queryString(String s, int n) {
+        if (n == 1) {
+            return s.contains("1");
+        }
+        int b = getBit(n), m = s.length();
+        if (m < n - (1 << b) + b + 1 || m < (1 << b) - (1 << (b - 1)) + b - 1) {
+            return false;
+        }
+        boolean[] visited = new boolean[n + 1];
+        return check(s, b + 1, 1 << b, n, visited) && check(s, b, 1 << (b - 1), (1 << b) - 1, visited);
+    }
+
+    private boolean check(String s, int m, int min, int max, boolean[] visited) {
+        int n = s.length();
+        int num = 0;
+        for (int i = 0; i < m; i++) {
+            num = (num << 1) | (s.charAt(i) - '0');
+        }
+        int count = 0;
+        if (num >= min && num <= max) {
+            visited[num] = true;
+            count++;
+        }
+        int mask = (1 << m) - 1;
+        for (int i = m; i < n; i++) {
+            num = ((num << 1) | (s.charAt(i) - '0')) & mask;
+            if (num >= min && num <= max && !visited[num]) {
+                visited[num] = true;
+                count++;
+            }
+        }
+        return count == max - min + 1;
+    }
+
+    private int getBit(int n) {
+        int b = 0;
+        while (n >= (1 << b)) {
+            b++;
+        }
+        return b - 1;
     }
 }

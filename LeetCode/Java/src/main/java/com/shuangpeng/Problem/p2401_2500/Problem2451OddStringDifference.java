6c869856e9328fb100bad198947bc06e56b1b095
shuangpengpang <15913598312@163.com>
2023-05-25 10:58:31 +0800

    差值数组不同的字符串（2451）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2451OddStringDifference.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2451OddStringDifference.java
@@ -1,6 +1,7 @@
 package com.shuangpeng.Problem.p2401_2500;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +14,7 @@ import java.util.Map;
  */
 public class Problem2451OddStringDifference {
 
-    public String oddString(String[] words) {
+    public String oddString0(String[] words) {
         int m = words.length, n = words[0].length();
         Map<String, List<Integer>> map = new HashMap<>(2);
         for (int i = 0; i < m; i++) {
@@ -31,5 +32,26 @@ public class Problem2451OddStringDifference {
             }
         }
         return null;
+    }
+
+    public String oddString(String[] words) {
+        int[] diff0 = get(words[0]), diff1 = get(words[1]);
+        if (Arrays.equals(diff0, diff1)) {
+            for (int i = 2; i < words.length; i++) {
+                if (!Arrays.equals(diff0, get(words[i]))) {
+                    return words[i];
+                }
+            }
+        }
+        return Arrays.equals(diff0, get(words[2])) ? words[1] : words[0];
+    }
+
+    private int[] get(String w) {
+        int n = w.length();
+        int[] ans = new int[n];
+        for (int i = 0; i + 1 < n; i++) {
+            ans[i] = w.charAt(i + 1) - w.charAt(i);
+        }
+        return ans;
     }
 }

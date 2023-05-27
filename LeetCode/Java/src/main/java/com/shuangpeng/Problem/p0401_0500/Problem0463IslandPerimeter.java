0f6c09c6c3631251f7d429626054047aede196c2
shuangpengpang <15913598312@163.com>
2023-05-27 18:31:44 +0800

    岛屿的周长（463）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0463IslandPerimeter.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0401_0500/Problem0463IslandPerimeter.java
@@ -80,3 +80,35 @@ class Problem0463IslandPerimeter0 {
         return ans;
     }
 }
+
+class Problem0463IslandPerimeter1 {
+
+    static int[] dirs = {-1, 0, 1, 0, -1};
+
+    public int islandPerimeter(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        grid[i][j] = -1;
+        int ans = 0;
+        for (int d = 0; d < 4; d++) {
+            int x = i + dirs[d], y = j + dirs[d + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0) {
+                ans++;
+            } else if (grid[x][y] == 1) {
+                ans += dfs(grid, x, y);
+            }
+        }
+        return ans;
+    }
+}

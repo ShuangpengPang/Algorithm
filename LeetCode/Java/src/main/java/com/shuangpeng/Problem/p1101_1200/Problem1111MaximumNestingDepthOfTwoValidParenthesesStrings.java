df3afe13f8122476e2d91e209f5b0f0095f33a06
shuangpengpang <15913598312@163.com>
2023-06-01 15:39:21 +0800

    有效括号的嵌套深度（1111）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p1101_1200;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings（有效括号的嵌套深度）
+ * @date 2023/6/1 2:47 PM
+ */
+public class Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings {
+
+    public int[] maxDepthAfterSplit(String seq) {
+        int n = seq.length(), cnt1 = 0, cnt2 = 0;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = seq.charAt(i) == '(' ? 1 : -1;
+            if (num == 1 && cnt1 < cnt2 || num == -1 && cnt1 > cnt2) {
+                cnt1 += num;
+            } else {
+                cnt2 += num;
+                ans[i] = 1;
+            }
+        }
+        return ans;
+    }
+}

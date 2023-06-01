b8e31a6b9acbfea52047989a879fbc4a3a7c36e9
shuangpengpang <15913598312@163.com>
2023-06-01 16:59:44 +0800

    有效括号的嵌套深度（1111）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1101_1200;
  */
 public class Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings {
 
-    public int[] maxDepthAfterSplit(String seq) {
+    public int[] maxDepthAfterSplit0(String seq) {
         int n = seq.length(), cnt1 = 0, cnt2 = 0;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
@@ -19,6 +19,24 @@ public class Problem1111MaximumNestingDepthOfTwoValidParenthesesStrings {
                 cnt2 += num;
                 ans[i] = 1;
             }
+        }
+        return ans;
+    }
+
+    public int[] maxDepthAfterSplit1(String seq) {
+        int n = seq.length(), cnt = 0;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = seq.charAt(i) == '(' ? (cnt++) & 1 : (--cnt) & 1;
+        }
+        return ans;
+    }
+
+    public int[] maxDepthAfterSplit(String seq) {
+        int n = seq.length();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = i & 1 ^ (seq.charAt(i) - '(');
         }
         return ans;
     }

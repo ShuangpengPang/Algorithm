28f04a444ce017b4aa58068500f1e829f51dc287
shuangpengpang <15913598312@163.com>
2023-06-05 10:55:56 +0800

    工作计划的最低难度（1335）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1335MinimumDifficultyOfAJobSchedule.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1335MinimumDifficultyOfAJobSchedule.java
@@ -1,6 +1,7 @@
 package com.shuangpeng.Problem.p1301_1400;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 /**
@@ -30,6 +31,72 @@ public class Problem1335MinimumDifficultyOfAJobSchedule {
             }
         }
         return dp[n - 1];
+    }
+
+    public int minDifficulty1(int[] jobDifficulty, int d) {
+        int n = jobDifficulty.length, N = Integer.MAX_VALUE >> 1;
+        int[] arr = new int[n + 1];
+        System.arraycopy(jobDifficulty, 0, arr, 1, n);
+        arr[0] = N;
+        int[][] dp = new int[2][n + 1];
+        Arrays.fill(dp[0], N);
+        dp[0][0] = 0;
+        Deque<int[]> q = new ArrayDeque<>();
+        for (int i = 1; i <= d; i++) {
+            int id = i & 1, p = id ^ 1;
+            Arrays.fill(dp[id], N);
+            q.clear();
+            q.push(new int[]{i - 1, dp[p][i - 1]});
+            for (int j = i; j <= n; j++) {
+                int s = N;
+                while (!q.isEmpty() && arr[j] >= arr[q.peek()[0]]) {
+                    s = Math.min(s, q.pop()[1]);
+                }
+                dp[id][j] = Math.min(dp[id][j], arr[j] + Math.min(s, q.isEmpty() ? N : dp[p][q.peek()[0]]));
+                dp[id][j] = Math.min(dp[id][j], q.isEmpty() ? N : dp[id][q.peek()[0]]);
+                q.push(new int[]{j, Math.min(s, dp[p][j])});
+            }
+        }
+        int id = d & 1;
+        q.clear();
+        for (int i = n; i >= 1; i--) {
+            int s = dp[id][i];
+            while (!q.isEmpty() && arr[i] >= arr[q.peek()[0]]) {
+                s = Math.min(s, q.pop()[1]);
+            }
+            if (q.isEmpty()) {
+                q.push(new int[]{i, s});
+            }
+        }
+        int ans = q.pop()[1];
+        return ans == N ? -1 : ans;
+    }
+
+    public int minDifficulty2(int[] jobDifficulty, int d) {
+        int n = jobDifficulty.length, N = Integer.MAX_VALUE >> 1;
+        if (d > n) {
+            return -1;
+        }
+        int[][] dp = new int[2][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = i == 0 ? jobDifficulty[0] : Math.max(jobDifficulty[i], dp[0][i - 1]);
+        }
+        Deque<int[]> q = new ArrayDeque<>();
+        for (int i = 1; i < d; i++) {
+            int id = i & 1, p = id ^ 1;
+            q.clear();
+            for (int j = i; j < n; j++) {
+                int s = N;
+                while (!q.isEmpty() && jobDifficulty[j] >= jobDifficulty[q.peek()[0]]) {
+                    s = Math.min(s, q.pop()[1]);
+                }
+                s = Math.min(s, q.isEmpty() ? dp[p][i - 1] : dp[p][q.peek()[0]]);
+                int num = s + jobDifficulty[j];
+                dp[id][j] = q.isEmpty() ? num : Math.min(num, dp[id][q.peek()[0]]);
+                q.push(new int[]{j, Math.min(s, dp[p][j])});
+            }
+        }
+        return dp[(d - 1) & 1][n - 1];
     }
 
     public int minDifficulty(int[] jobDifficulty, int d) {

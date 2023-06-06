f6c4a555d6d86fdbf4df3ab6b6b30ed9f8813ce7
shuangpengpang <15913598312@163.com>
2023-06-06 11:58:06 +0800

    通知所有员工所需的时间（1376）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1376TimeNeededToInformAllEmployees.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1376TimeNeededToInformAllEmployees.java
@@ -14,7 +14,7 @@ import java.util.Queue;
  */
 public class Problem1376TimeNeededToInformAllEmployees {
 
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+    public int numOfMinutes0(int n, int headID, int[] manager, int[] informTime) {
         List<Integer>[] g = new List[n];
         Arrays.setAll(g, i -> new ArrayList<>());
         for (int i = 0; i < n; i++) {
@@ -32,6 +32,90 @@ public class Problem1376TimeNeededToInformAllEmployees {
             ans = Math.max(ans, time[id]);
             for (int c : g[id]) {
                 q.add(c);
+            }
+        }
+        return ans;
+    }
+
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<Integer>[] g = new List[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (int i = 0; i < n; i++) {
+            if (i != headID) {
+                g[manager[i]].add(i);
+            }
+        }
+        return dfs(g, headID, informTime);
+    }
+
+    private int dfs(List<Integer>[] g, int x, int[] informTime) {
+        int ans = 0, time = informTime[x];
+        for (int y : g[x]) {
+            ans = Math.max(ans, time + dfs(g, y, informTime));
+        }
+        return ans;
+    }
+}
+
+class Problem1376TimeNeededToInformAllEmployees0 {
+
+    public int numOfMinutes0(int n, int headID, int[] manager, int[] informTime) {
+        int[] time = new int[n];
+        Queue<int[]> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (informTime[i] == 0) {
+                q.offer(new int[]{i, 0});
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            int x = p[0], t = p[1];
+            if (x != headID && t >= time[x]) {
+                int m = manager[x], s = t + informTime[m];
+                if (s > time[m]) {
+                    time[m] = s;
+                    q.offer(new int[]{m, s});
+                }
+            }
+        }
+        return time[headID];
+    }
+
+    public int numOfMinutes1(int n, int headID, int[] manager, int[] informTime) {
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfs(i, manager, informTime));
+        }
+        return ans;
+    }
+
+    private int dfs(int x, int[] manager, int[] informTime) {
+        int p = manager[x];
+        if (p == -1) {
+            return informTime[x];
+        }
+        informTime[x] += dfs(p, manager, informTime);
+        manager[x] = -1;
+        return informTime[x];
+    }
+
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int x = i, s = 0;
+            while (x != -1) {
+                s += informTime[x];
+                x = manager[x];
+            }
+            ans = Math.max(ans, s);
+            x = i;
+            while (x != -1) {
+                int t = informTime[x];
+                int p = manager[x];
+                informTime[x] = s;
+                manager[x] = -1;
+                s -= t;
+                x = p;
             }
         }
         return ans;

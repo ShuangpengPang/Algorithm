b3fa6ee9725bc1da53f036ec5ca400baa0acab1b
shuangpengpang <15913598312@163.com>
2023-06-06 14:40:22 +0800

    移动石子直到连续（1033）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1033MovingStonesUntilConsecutive.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1033MovingStonesUntilConsecutive.java
@@ -23,17 +23,11 @@ public class Problem1033MovingStonesUntilConsecutive {
     }
 
     public int[] numMovesStones(int a, int b, int c) {
-        int x = Math.min(Math.min(a, b), c);
-        int z = Math.max(Math.max(a, b), c);
+        int x = Math.min(a, Math.min(b, c));
+        int z = Math.max(a, Math.max(b, c));
         int y = a + b + c - x - z;
-        int[] ans = new int[2];
-        ans[0] = 2;
-        if (z - x == 2) {
-            ans[0] = 0;
-        } else if (y - x <= 2 || z - y <= 2) {
-            ans[0] = 1;
-        }
-        ans[1] = z - x - 2;
-        return ans;
+        int max = z - x - 2, min = max;
+        min = Math.min(min, Math.min(y - x, z - y) <= 2 ? 1 : 2);
+        return new int[]{min, max};
     }
 }

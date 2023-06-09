da4d9a5e8c7388fd8ac5e550d174c5195345f5f5
shuangpengpang <15913598312@163.com>
2023-06-09 11:36:32 +0800

    构建回文串检测（1177）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1177CanMakePalindromeFromSubstring.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1177CanMakePalindromeFromSubstring.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class Problem1177CanMakePalindromeFromSubstring {
 
-    public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
+    public List<Boolean> canMakePaliQueries0(String s, int[][] queries) {
         int n = s.length();
         int[][] dp = new int[n + 1][26];
         for (int i = 0; i < n; i++) {
@@ -26,6 +26,25 @@ public class Problem1177CanMakePalindromeFromSubstring {
             int cnt = 0;
             for (int i = 0; i < 26; i++) {
                 cnt += dp[right][i] - dp[left][i] & 1;
+            }
+            ans.add((cnt >> 1) <= k);
+        }
+        return ans;
+    }
+
+    public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i + 1] = dp[i] ^ (1 << (s.charAt(i) - 'a'));
+        }
+        List<Boolean> ans = new ArrayList<>(queries.length);
+        for (int[] q : queries) {
+            int left = q[0], right = q[1] + 1, k = q[2];
+            int num = dp[right] ^ dp[left], cnt = 0;
+            while (num != 0) {
+                num &= num - 1;
+                cnt++;
             }
             ans.add((cnt >> 1) <= k);
         }

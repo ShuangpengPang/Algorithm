afd937c57332e1ad9f814e1567d24afe5d36c881
shuangpengpang <15913598312@163.com>
2023-06-13 10:30:36 +0800

    数组中不等三元组的数目（2475）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2475NumberOfUnequalTriplesInArray.java
@@ -0,0 +1,59 @@
+package com.shuangpeng.Problem.p2401_2500;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2475NumberOfUnequalTriplesInArray（数组中不等三元组的数目）
+ * @date 2023/6/13 9:55 AM
+ */
+public class Problem2475NumberOfUnequalTriplesInArray {
+
+    public int unequalTriplets0(int[] nums) {
+        int n = nums.length, ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] != nums[j]) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (nums[k] != nums[i] && nums[k] != nums[j]) {
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int unequalTriplets1(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length, ans = 0;
+        for (int i = 0, j = 0; i < n; i = j) {
+            while (j < n && nums[j] == nums[i]) {
+                j++;
+            }
+            ans += i * (j - i) * (n - j);
+        }
+        return ans;
+    }
+
+    public int unequalTriplets(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        int[] cnt = new int[max + 1];
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        int ans = 0, n = nums.length;
+        for (int i = 0, c = 0; i <= max; i++) {
+            if (cnt[i] > 0) {
+                ans += c * cnt[i] * (n - c - cnt[i]);
+                c += cnt[i];
+            }
+        }
+        return ans;
+    }
+}

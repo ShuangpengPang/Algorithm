35a16b04889becaa12410fa0360ef6f864e51b04
shuangpengpang <15913598312@163.com>
2023-06-13 15:13:44 +0800

    飞机座位分配概率（1227）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1227AirPlaneSeatAssignmentProbability.java
@@ -0,0 +1,19 @@
+package com.shuangpeng.Problem.p1201_1300;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1227AirPlaneSeatAssignmentProbability（飞机座位分配概率）
+ * @date 2023/6/13 1:57 PM
+ */
+public class Problem1227AirPlaneSeatAssignmentProbability {
+
+    public double nthPersonGetsNthSeat(int n) {
+        double p = 0, ans = 1.0 / n;
+        for (int i = 1; i < n; i++) {
+            p += i == 1 ? 1.0 / n : (1.0 - ans) / (n - i + 1);
+            ans = 1.0 - p;
+        }
+        return ans;
+    }
+}

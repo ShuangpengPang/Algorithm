08f5849bdcd214c82fb4160525813fade84fb5ec
shuangpengpang <15913598312@163.com>
2023-07-01 10:53:15 +0800

    回环句（2490）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2401_2500/Problem2490CircularSentence.java
@@ -0,0 +1,29 @@
+package com.shuangpeng.Problem.p2401_2500;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2490CircularSentence（回环句）
+ * @date 2023/7/1 10:34 AM
+ */
+public class Problem2490CircularSentence {
+
+    public boolean isCircularSentence(String sentence) {
+        int n = sentence.length();
+        if (sentence.charAt(0) != sentence.charAt(n - 1)) {
+            return false;
+        }
+        char last = sentence.charAt(0), prev = last;
+        for (int i = 1; i < n; i++) {
+            char c = sentence.charAt(i);
+            if (prev == ' ' && c != last) {
+                return false;
+            }
+            if (c != ' ') {
+                last = c;
+            }
+            prev = c;
+        }
+        return true;
+    }
+}

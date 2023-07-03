a34585467174e05cd41cb22160bef67fe17910f7
shuangpengpang <15913598312@163.com>
2023-07-03 16:33:41 +0800

    重构2行二进制矩阵（1253）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1253ReconstructA2RowBinaryMatrix.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1253ReconstructA2RowBinaryMatrix.java
@@ -11,23 +11,47 @@ import java.util.List;
  */
 public class Problem1253ReconstructA2RowBinaryMatrix {
 
-    public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+    public List<List<Integer>> reconstructMatrix0(int upper, int lower, int[] colsum) {
         int n = colsum.length;
-        List<Integer> r1 = new ArrayList<>(n), r2 = new ArrayList<>(n);
-        for (int i = 0; i < n && upper >= 0 && lower >= 0; i++) {
-            int sum = colsum[i];
+        List<Integer> first = new ArrayList<>(n), second = new ArrayList<>(n);
+        for (int sum : colsum) {
             int num1 = sum == 0 || sum == 1 && upper < lower ? 0 : 1, num2 = sum - num1;
-            r1.add(num1);
-            r2.add(num2);
+            first.add(num1);
+            second.add(num2);
             upper -= num1;
             lower -= num2;
         }
-        if (upper != 0 || lower != 0) {
+        if (upper == 0 && lower == 0) {
+            return new ArrayList<List<Integer>>() {{
+                add(first);
+                add(second);
+            }};
+        }
+        return new ArrayList<>();
+    }
+
+    public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+        int n = colsum.length, sum = 0, two = 0;
+        for (int i = 0; i < n; i++) {
+            if (colsum[i] == 2) {
+                two++;
+            }
+            sum += colsum[i];
+        }
+        if (sum != upper + lower || two > Math.min(upper, lower)) {
             return new ArrayList<>();
         }
-        List<List<Integer>> ans = new ArrayList<>(2);
-        ans.add(r1);
-        ans.add(r2);
-        return ans;
+        upper -= two;
+        List<Integer> list1 = new ArrayList<>(n), list2 = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            int num = colsum[i] == 0 || colsum[i] == 1 && upper == 0 ? 0 : 1;
+            list1.add(num);
+            list2.add(colsum[i] - num);
+            upper -= colsum[i] != 2 ? num : 0;
+        }
+        return new ArrayList<List<Integer>>() {{
+            add(list1);
+            add(list2);
+        }};
     }
 }

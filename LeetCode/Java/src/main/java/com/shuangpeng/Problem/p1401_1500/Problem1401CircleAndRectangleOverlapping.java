7c3eea16e67c3192170775a45b36a9962609dbdc
shuangpengpang <15913598312@163.com>
2023-07-03 19:18:08 +0800

    圆和矩形是否有重叠（1401）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1401CircleAndRectangleOverlapping.java
@@ -0,0 +1,35 @@
+package com.shuangpeng.Problem.p1401_1500;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1401CircleAndRectangleOverlapping（圆和矩形是否有重叠）
+ * @date 2023/7/3 6:13 PM
+ */
+public class Problem1401CircleAndRectangleOverlapping {
+
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        if (xCenter >= x1 && xCenter <= x2 && yCenter >= y1 && yCenter <= y2) {
+            return true;
+        }
+        int s = radius * radius;
+        if (check(xCenter, yCenter, x1, y1, s) || check(xCenter, yCenter, x2, y2, s)) {
+            return true;
+        }
+        if (check(xCenter, yCenter, x1, y2, s) || check(xCenter, yCenter, x2, y1, s)) {
+            return true;
+        }
+        if (yCenter >= y1 && yCenter <= y2 && (check(xCenter, yCenter, x1, yCenter, s) || check(xCenter, yCenter, x2, yCenter, s))) {
+            return true;
+        }
+        if (xCenter >= x1 && xCenter <= x2 && (check(xCenter, yCenter, xCenter, y1, s) || check(xCenter, yCenter, xCenter, y2, s))) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean check(int x1, int y1, int x2, int y2, int s) {
+        int x = x1 - x2, y = y1 - y2;
+        return x * x + y * y <= s;
+    }
+}

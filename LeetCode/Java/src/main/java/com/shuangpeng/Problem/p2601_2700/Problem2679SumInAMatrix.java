55ad572fab98ce77f07b4e085ea5b5cdf6773a5e
shuangpengpang <15913598312@163.com>
2023-07-04 11:04:52 +0800

    矩阵中的和（2679）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2679SumInAMatrix.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2679SumInAMatrix.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class Problem2679SumInAMatrix {
 
-    public int matrixSum(int[][] nums) {
+    public int matrixSum0(int[][] nums) {
         int m = nums.length, n = nums[0].length;
         for (int i = 0; i < m; i++) {
             Arrays.sort(nums[i]);
@@ -24,5 +24,30 @@ public class Problem2679SumInAMatrix {
             ans += max;
         }
         return ans;
+    }
+
+    public int matrixSum(int[][] nums) {
+        int n = nums[0].length;
+        int[] maxArray = new int[n], cnt = new int[1001];
+        for (int[] arr : nums) {
+            int max = 0, min = maxArray[0];
+            for (int num : arr) {
+                cnt[num]++;
+                max = Math.max(max, num);
+            }
+            for (int v = max, j = n - 1; v >= min; v--) {
+                for (int c = 0; c < cnt[v]; c++, j--) {
+                    if (maxArray[j] < v) {
+                        maxArray[j] = v;
+                    }
+                }
+                cnt[v] = 0;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += maxArray[i];
+        }
+        return sum;
     }
 }

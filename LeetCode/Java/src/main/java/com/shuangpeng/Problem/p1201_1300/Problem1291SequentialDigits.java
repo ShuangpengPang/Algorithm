347588fa22337b8655541feacadb83496d79f4ac
shuangpengpang <15913598312@163.com>
2023-07-04 14:15:32 +0800

    顺次数（1291）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1291SequentialDigits.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1291SequentialDigits.java
@@ -27,7 +27,7 @@ public class Problem1291SequentialDigits {
         return ans;
     }
 
-    public List<Integer> sequentialDigits(int low, int high) {
+    public List<Integer> sequentialDigits1(int low, int high) {
         String s = "123456789";
         int n1 = Integer.toString(low).length(), n2 = Integer.toString(high).length();
         List<Integer> ans = new ArrayList<>();
@@ -38,6 +38,36 @@ public class Problem1291SequentialDigits {
                     ans.add(num);
                 }
             }
+        }
+        return ans;
+    }
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        int n1 = getLength(low), n2 = getLength(high);
+        List<Integer> ans = new ArrayList<>();
+        for (int len = n1; len <= n2; len++) {
+            int num = 0;
+            for (int i = 1; i <= 10 - len && num < high; i++) {
+                num = 0;
+                for (int j = i; j < i + len; j++) {
+                    num = num * 10 + j;
+                }
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
+        return ans;
+    }
+
+    private int getLength(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        int ans = 0;
+        while (num != 0) {
+            ans++;
+            num /= 10;
         }
         return ans;
     }

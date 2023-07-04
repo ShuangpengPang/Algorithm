5f6cdbffc3be9c3c771f95c8c5d42c5d8b9827ca
shuangpengpang <15913598312@163.com>
2023-07-04 11:49:26 +0800

    顺次数（1291）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1291SequentialDigits.java
@@ -0,0 +1,29 @@
+package com.shuangpeng.Problem.p1201_1300;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1291SequentialDigits（顺次数）
+ * @date 2023/7/4 11:36 AM
+ */
+public class Problem1291SequentialDigits {
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            int num = 0;
+            for (int j = i; j <= 9 && num <= high; j++) {
+                num = num * 10 + j;
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
+        ans.sort(Comparator.comparingInt(a -> a));
+        return ans;
+    }
+}

8aee0efc017262760b54d286a547a34ababa7b18
shuangpengpang <15913598312@163.com>
2023-07-04 17:11:11 +0800

    统计全为1的正方形子矩阵（1277）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1277CountSquareSubmatricesWithAllOne.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1277CountSquareSubmatricesWithAllOne.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1201_1300;
  */
 public class Problem1277CountSquareSubmatricesWithAllOne {
 
-    public int countSquares(int[][] matrix) {
+    public int countSquares0(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         int[] dp = new int[n + 1];
         int leftTop = 0, ans = 0;
@@ -18,6 +18,23 @@ public class Problem1277CountSquareSubmatricesWithAllOne {
                 dp[j + 1] = matrix[i][j] == 0 ? 0 : Math.min(leftTop, Math.min(dp[j], nextLeftTop)) + 1;
                 ans += dp[j + 1];
                 leftTop = nextLeftTop;
+            }
+        }
+        return ans;
+    }
+
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length, ans = 0;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0, p = 0; j < n; j++) {
+                int c = 0;
+                if (matrix[i][j] == 1) {
+                    c = (j == 0 ? 0 : Math.min(dp[j - 1], Math.min(p, dp[j]))) + 1;
+                    ans += c;
+                }
+                p = dp[j];
+                dp[j] = c;
             }
         }
         return ans;

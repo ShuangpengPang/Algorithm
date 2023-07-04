8d0dfa74a88512a1e21dc0f810fc62de39010192
shuangpengpang <15913598312@163.com>
2023-07-04 12:16:43 +0800

    顺次数（1291）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1291SequentialDigits.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1291SequentialDigits.java
@@ -12,18 +12,33 @@ import java.util.List;
  */
 public class Problem1291SequentialDigits {
 
-    public List<Integer> sequentialDigits(int low, int high) {
+    public List<Integer> sequentialDigits0(int low, int high) {
         List<Integer> ans = new ArrayList<>();
         for (int i = 1; i <= 9; i++) {
             int num = 0;
             for (int j = i; j <= 9 && num <= high; j++) {
                 num = num * 10 + j;
-                if (num >= low && num <= high) {
+                if (num >= low) {
                     ans.add(num);
                 }
             }
         }
         ans.sort(Comparator.comparingInt(a -> a));
+        return ans;
+    }
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        String s = "123456789";
+        int n1 = Integer.toString(low).length(), n2 = Integer.toString(high).length();
+        List<Integer> ans = new ArrayList<>();
+        for (int len = n1; len <= n2; len++) {
+            for (int i = 0; i <= 9 - len; i++) {
+                int num = Integer.parseInt(s.substring(i, i + len));
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
         return ans;
     }
 }

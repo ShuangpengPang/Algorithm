2b02cc60974d3f1868317de5de7cef4783f2001c
shuangpengpang <15913598312@163.com>
2023-07-06 18:03:35 +0800

    转变数组后最接近目标值的数组和（1300）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1300SumOfMutatedArrayClosestToTarget.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1300SumOfMutatedArrayClosestToTarget.java
@@ -10,6 +10,79 @@ import java.util.Arrays;
  */
 public class Problem1300SumOfMutatedArrayClosestToTarget {
 
+    public int findBestValue0(int[] arr, int target) {
+        int maxVal = 0, sum = 0;
+        for (int num : arr) {
+            maxVal = Math.max(maxVal, num);
+            sum += num;
+        }
+        if (sum <= target) {
+            return maxVal;
+        }
+        int left = 0, right = maxVal, closest = 0;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            int s = getSum(arr, mid);
+            if (s < target) {
+                closest = s;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return target - closest <= getSum(arr, left) - target ? left - 1 : left;
+    }
+
+    private int getSum(int[] arr, int value) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += Math.min(num, value);
+        }
+        return sum;
+    }
+
+    public int findBestValue1(int[] arr, int target) {
+        int n = arr.length, maxValue = 0, sum = 0;
+        for (int num : arr) {
+            sum += num;
+            maxValue = Math.max(maxValue, num);
+        }
+        if (sum <= target) {
+            return maxValue;
+        }
+        Arrays.sort(arr);
+        int[] preSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            preSum[i + 1] = preSum[i] + arr[i];
+        }
+        int left = 0, right = maxValue, s1 = 0;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            int s = binarySearch(arr, preSum, mid);
+            if (s < target) {
+                left = mid + 1;
+                s1 = s;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int s2 = binarySearch(arr, preSum, left);
+        return target - s1 <= s2 - target ? left - 1 : left;
+    }
+
+    private int binarySearch(int[] arr, int[] preSum, int value) {
+        int left = 0, n = arr.length, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (arr[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return preSum[left] + (n - left) * value;
+    }
+
     public int findBestValue(int[] arr, int target) {
         Arrays.sort(arr);
         int sum = 0, n = arr.length;

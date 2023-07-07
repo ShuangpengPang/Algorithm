6b384ad4c2188ac65d9668bfdd44db8b83c8640b
shuangpengpang <15913598312@163.com>
2023-07-07 18:37:26 +0800

    子数组异或查询（1310）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1310XORQueriesOfASubarray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1310XORQueriesOfASubarray.java
@@ -8,6 +8,33 @@ package com.shuangpeng.Problem.p1301_1400;
  */
 public class Problem1310XORQueriesOfASubarray {
 
+    public int[] xorQueries0(int[] arr, int[][] queries) {
+        int n = arr.length;
+        int[] tree = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int x = i, num = arr[i - 1];
+            while (x <= n) {
+                tree[x] ^= num;
+                x += x & -x;
+            }
+        }
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = getXorSum(tree, queries[i][0]) ^ getXorSum(tree, queries[i][1] + 1);
+        }
+        return ans;
+    }
+
+    private int getXorSum(int[] tree, int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum ^= tree[x];
+            x -= x & -x;
+        }
+        return sum;
+    }
+
     public int[] xorQueries(int[] arr, int[][] queries) {
         int n = arr.length;
         int[] xorSum = new int[n + 1];

7c22cca6b9ca8464792a5827e4a4f437d3e6b4a7
shuangpengpang <15913598312@163.com>
2023-07-09 20:03:58 +0800

    连通网络的操作次数（1319）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1319NumberOfOperationsToMakeNetworkConnected.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1319NumberOfOperationsToMakeNetworkConnected.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author ShuangPengPang
@@ -9,6 +11,37 @@ import java.util.Arrays;
  * @date 2023/7/9 4:50 PM
  */
 public class Problem1319NumberOfOperationsToMakeNetworkConnected {
+
+    public int makeConnected0(int n, int[][] connections) {
+        if (connections.length < n - 1) {
+            return -1;
+        }
+        List<Integer>[] g = new List[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (int[] c : connections) {
+            int x = c[0], y = c[1];
+            g[x].add(y);
+            g[y].add(x);
+        }
+        int cnt = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(g, i, visited);
+                cnt++;
+            }
+        }
+        return cnt - 1;
+    }
+
+    private void dfs(List<Integer>[] g, int x, boolean[] visited) {
+        visited[x] = true;
+        for (int y : g[x]) {
+            if (!visited[y]) {
+                dfs(g, y, visited);
+            }
+        }
+    }
 
     public int makeConnected(int n, int[][] connections) {
         if (connections.length < n - 1) {

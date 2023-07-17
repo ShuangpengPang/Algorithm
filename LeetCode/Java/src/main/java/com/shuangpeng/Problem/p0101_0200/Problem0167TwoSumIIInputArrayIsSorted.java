ed16dbb4cb4b18fe4e9534011f5460dc3b038a04
shuangpengpang <15913598312@163.com>
2023-07-17 20:11:17 +0800

    两数之和II-输入有序数组（167）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0101_0200/Problem0167TwoSumIIInputArrayIsSorted.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0101_0200/Problem0167TwoSumIIInputArrayIsSorted.java
@@ -8,6 +8,27 @@ package com.shuangpeng.Problem.p0101_0200;
  */
 public class Problem0167TwoSumIIInputArrayIsSorted {
 
+    public int[] twoSum0(int[] numbers, int target) {
+        int i = 0, j = getIndex(numbers, 1, numbers.length - 1, target - numbers[i]);
+        while (numbers[i] + numbers[j] != target) {
+            i++;
+            j = getIndex(numbers, i + 1, j, target - numbers[i]);
+        }
+        return new int[]{i + 1, j + 1};
+    }
+
+    private int getIndex(int[] numbers, int left, int right, int num) {
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (numbers[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return Math.min(left, numbers.length - 1);
+    }
+
     public int[] twoSum(int[] numbers, int target) {
         int i = 0, j = numbers.length - 1;
         while (numbers[i] + numbers[j] != target) {

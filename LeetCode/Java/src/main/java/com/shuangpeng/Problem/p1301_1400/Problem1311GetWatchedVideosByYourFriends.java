ceea99a4b4e946eedc2b61e50ce445c0b9ae6689
shuangpengpang <15913598312@163.com>
2023-07-17 19:55:46 +0800

    获取你好友已观看的视频（1311）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1311GetWatchedVideosByYourFriends.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1311GetWatchedVideosByYourFriends.java
@@ -1,9 +1,11 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 /**
  * @author ShuangPengPang
@@ -13,7 +15,7 @@ import java.util.Map;
  */
 public class Problem1311GetWatchedVideosByYourFriends {
 
-    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
+    public List<String> watchedVideosByFriends0(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
         int n = friends.length;
         boolean[] visited = new boolean[n];
         List<Integer> peoples = new ArrayList<>();
@@ -39,6 +41,33 @@ public class Problem1311GetWatchedVideosByYourFriends {
         }
         List<String> ans = new ArrayList<>(freq.keySet());
         ans.sort((a, b) -> freq.get(a).equals(freq.get(b)) ? a.compareTo(b) : freq.get(a) - freq.get(b));
+        return ans;
+    }
+
+    public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
+        int n = friends.length;
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new ArrayDeque<>(n);
+        q.offer(id);
+        visited[id] = true;
+        for (int i = 0; i < level && !q.isEmpty(); i++) {
+            for (int j = q.size() - 1; j >= 0; j--) {
+                for (int y : friends[q.poll()]) {
+                    if (!visited[y]) {
+                        visited[y] = true;
+                        q.offer(y);
+                    }
+                }
+            }
+        }
+        Map<String, Integer> freq = new HashMap<>();
+        for (int i = q.size() - 1; i >= 0; i--) {
+            for (String v : watchedVideos.get(q.poll())) {
+                freq.merge(v, 1, Integer::sum);
+            }
+        }
+        List<String> ans = new ArrayList<>(freq.keySet());
+        ans.sort((a, b) -> freq.get(a) != freq.get(b) ? freq.get(a) - freq.get(b) : a.compareTo(b));
         return ans;
     }
 }

3654f738747567a365fe52106b2d51f4ae8824b7
shuangpengpang <15913598312@163.com>
2023-07-19 18:39:26 +0800

    转变数组后最接近目标值的数组和（1300）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1300SumOfMutatedArrayClosestToTarget.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1300SumOfMutatedArrayClosestToTarget.java
@@ -83,7 +83,7 @@ public class Problem1300SumOfMutatedArrayClosestToTarget {
         return preSum[left] + (n - left) * value;
     }
 
-    public int findBestValue(int[] arr, int target) {
+    public int findBestValue2(int[] arr, int target) {
         Arrays.sort(arr);
         int sum = 0, n = arr.length;
         for (int num : arr) {
@@ -101,5 +101,58 @@ public class Problem1300SumOfMutatedArrayClosestToTarget {
         int add = (target - sum) / (n - i - 1), ans = p + add;
         sum += add * (n - i - 1);
         return target - sum <= sum + (n - i - 1) - target ? ans : ans + 1;
+    }
+
+    public int findBestValue3(int[] arr, int target) {
+        int sum = 0, n = arr.length;
+        for (int num : arr) {
+            sum += num;
+        }
+        Arrays.sort(arr);
+        int ans = arr[n - 1];
+        for (int i = n - 1; i >= 0 && sum > target; i--) {
+            int d = Math.min(i > 0 ? arr[i] - arr[i - 1] : arr[i], (sum - target) / (n - i) + 1);
+            sum -= d * (n - i);
+            if (sum <= target) {
+                ans = target - sum <= sum + n - i - target ? arr[i] - d : arr[i] - d + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int findBestValue(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+        int l = 0, r = arr[n - 1], ans = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int index = Arrays.binarySearch(arr, mid);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            int cur = prefix[index] + (n - index) * mid;
+            if (cur <= target) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        int chooseSmall = check(arr, ans);
+        int chooseBig = check(arr, ans + 1);
+        return Math.abs(chooseSmall - target) <= Math.abs(chooseBig - target) ? ans : ans + 1;
+    }
+
+    public int check(int[] arr, int x) {
+        int ret = 0;
+        for (int num : arr) {
+            ret += Math.min(num, x);
+        }
+        return ret;
     }
 }

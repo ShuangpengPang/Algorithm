a0cbadabeff7a4e877533bfb178bf177763e9713
shuangpengpang <15913598312@163.com>
2023-07-20 14:44:28 +0800

    环形子数组的最大和（918）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0918MaximumSumCircularSubarray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0918MaximumSumCircularSubarray.java
@@ -5,6 +5,10 @@ import java.util.Comparator;
 import java.util.Deque;
 import java.util.PriorityQueue;
 
+/**
+ * @description:（环形子数组的最大和）
+ * @date 2023/7/20 2:43 PM
+ **/
 public class Problem0918MaximumSumCircularSubarray {
 
     public int maxSubarraySumCircular0(int[] nums) {
@@ -100,7 +104,7 @@ public class Problem0918MaximumSumCircularSubarray {
         return Math.max(ans, sum + maxA);
     }
 
-    public int maxSubarraySumCircular(int[] nums) {
+    public int maxSubarraySumCircular4(int[] nums) {
         int n = nums.length;
         if (n == 1) {
             return nums[0];
@@ -132,5 +136,54 @@ public class Problem0918MaximumSumCircularSubarray {
             }
             return ans;
         }
+    }
+
+    public int maxSubarraySumCircular5(int[] nums) {
+        int n = nums.length, M = Integer.MIN_VALUE >> 1;
+        int[] suffix = new int[n + 1];
+        suffix[n] = M;
+        for (int i = n - 1, sum = 0; i >= 0; i--) {
+            sum += nums[i];
+            suffix[i] = Math.max(suffix[i + 1], sum);
+        }
+        int ans = suffix[0], s1 = 0, s2 = M, maxLeft = M;
+        for (int i = 0; i < n; i++) {
+            s1 += nums[i];
+            maxLeft = Math.max(maxLeft, s1);
+            s2 = Math.max(s2 + nums[i], nums[i]);
+            ans = Math.max(ans, Math.max(maxLeft + suffix[i + 1], s2));
+        }
+        return ans;
+    }
+
+    public int maxSubarraySumCircular6(int[] nums) {
+        int n = nums.length, N = Integer.MAX_VALUE >> 1;
+        int sum = 0, ans = -N, minSum = N;
+        for (int i = 0, s1 = 0, s2 = 0; i < n; i++) {
+            sum += nums[i];
+            s1 = Math.max(s1 + nums[i], nums[i]);
+            ans = Math.max(ans, s1);
+            s2 = Math.min(s2 + nums[i], nums[i]);
+            minSum = Math.min(minSum, s2);
+        }
+        return ans < 0 ? ans : Math.max(ans, sum - minSum);
+    }
+
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length, N = (n << 1), ans = Integer.MIN_VALUE;
+        Deque<int[]> q = new ArrayDeque<>(N);
+        for (int i = 0, s = 0; i < N; i++) {
+            int num = i < n ? nums[i] : nums[i - n];
+            s += num;
+            while (!q.isEmpty() && q.peekFirst()[0] < i - n) {
+                q.pollFirst();
+            }
+            ans = Math.max(ans, q.isEmpty() ? s : s - q.peekFirst()[1]);
+            while (!q.isEmpty() && q.peekLast()[1] >= s) {
+                q.pollLast();
+            }
+            q.offerLast(new int[]{i, s});
+        }
+        return ans;
     }
 }

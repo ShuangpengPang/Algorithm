0d1f2feb14ad0c395933f09940cd3f81ac9b760a
shuangpengpang <15913598312@163.com>
2023-07-26 16:53:02 +0800

    餐厅过滤器（1333）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1333FilterRestaurantsByVeganFriendlyPriceAndDistance.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1333FilterRestaurantsByVeganFriendlyPriceAndDistance.java
@@ -1,7 +1,9 @@
 package com.shuangpeng.Problem.p1301_1400;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author ShuangPengPang
@@ -11,7 +13,7 @@ import java.util.List;
  */
 public class Problem1333FilterRestaurantsByVeganFriendlyPriceAndDistance {
 
-    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+    public List<Integer> filterRestaurants0(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
         List<int[]> list = new ArrayList<>();
         for (int[] r : restaurants) {
             if ((veganFriendly == 0 || r[2] == 1) && r[3] <= maxPrice && r[4] <= maxDistance) {
@@ -24,5 +26,10 @@ public class Problem1333FilterRestaurantsByVeganFriendlyPriceAndDistance {
             ans.add(r[0]);
         }
         return ans;
+    }
+
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        return Arrays.stream(restaurants).filter(x -> (veganFriendly == 0 || x[2] == 1) && x[3] <= maxPrice && x[4] <= maxDistance)
+                .sorted((a, b) -> a[1] != b[1] ? b[1] - a[1] : b[0] - a[0]).map(x -> x[0]).collect(Collectors.toList());
     }
 }

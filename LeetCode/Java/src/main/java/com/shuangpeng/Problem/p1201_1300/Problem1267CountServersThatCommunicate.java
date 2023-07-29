ec99a3ce10f0ef2a9ac05aac7275e4d559d58aa1
shuangpengpang <15913598312@163.com>
2023-07-29 19:38:58 +0800

    统计参与通信的服务器（1267）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1267CountServersThatCommunicate.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1267CountServersThatCommunicate.java
@@ -147,7 +147,7 @@ class Problem1267CountServersThatCommunicate2 {
         return ans;
     }
 
-    public int countServers(int[][] grid) {
+    public int countServers1(int[][] grid) {
         int m = grid.length, n = grid[0].length, ans = 0;
         int[] rows = new int[m], cols = new int[n];
         for (int i = 0; i < m; i++) {
@@ -168,6 +168,28 @@ class Problem1267CountServersThatCommunicate2 {
                         }
                         break;
                     }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int countServers(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] row = new int[m], col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) {
+                    ans++;
                 }
             }
         }

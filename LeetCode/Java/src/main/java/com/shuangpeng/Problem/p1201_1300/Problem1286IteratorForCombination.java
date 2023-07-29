835f5c7c4f2217d749967eece7eed591170fbed7
shuangpengpang <15913598312@163.com>
2023-07-29 18:33:59 +0800

    字母组合迭代器（1286）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1286IteratorForCombination.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1286IteratorForCombination.java
@@ -13,45 +13,39 @@ public class Problem1286IteratorForCombination {
     class CombinationIterator {
 
         char[] cs;
-        int[] idx;
+        int[] index;
+        int n;
         boolean next;
 
         public CombinationIterator(String characters, int combinationLength) {
             cs = characters.toCharArray();
-            idx = new int[combinationLength];
-            Arrays.setAll(idx, i -> i);
+            n = combinationLength;
+            index = new int[n];
+            Arrays.setAll(index, i -> i);
             next = true;
         }
 
         public String next() {
-            char[] arr = new char[idx.length];
-            for (int i = 0; i < idx.length; i++) {
-                arr[i] = cs[idx[i]];
+            char[] arr = new char[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = cs[index[i]];
             }
-            String ans = new String(arr);
-            int index = getIndex();
-            if (index >= 0) {
-                idx[index]++;
-                for (int k = index + 1; k < idx.length; k++) {
-                    idx[k] = idx[k - 1] + 1;
+            int i = n - 1, m = cs.length;
+            while (i >= 0 && index[i] == i + m - n) {
+                i--;
+            }
+            if (i >= 0) {
+                for (int j = index[i] + 1; i < n; i++, j++) {
+                    index[i] = j;
                 }
             } else {
                 next = false;
             }
-            return ans;
+            return new String(arr);
         }
 
         public boolean hasNext() {
             return next;
-        }
-
-        private int getIndex() {
-            int i = cs.length - 1, j = idx.length - 1;
-            while (j >= 0 && i == idx[j]) {
-                i--;
-                j--;
-            }
-            return j;
         }
     }
 
@@ -95,6 +89,47 @@ class Problem1286IteratorForCombination0 {
 
         public boolean hasNext() {
             return hash >= N;
+        }
+    }
+}
+
+class Problem1286IteratorForCombination1 {
+
+    class CombinationIterator {
+
+        char[] cs;
+        int m, n, hash = 0;
+
+        public CombinationIterator(String characters, int combinationLength) {
+            cs = characters.toCharArray();
+            m = cs.length;
+            n = combinationLength;
+            hash = (1 << n) - 1 << m - n;
+        }
+
+        public String next() {
+            char[] arr = new char[n];
+            for (int i = 0, j = m - 1; i < n; i++, j--) {
+                while ((hash >> j & 1) == 0) {
+                    j--;
+                }
+                arr[i] = cs[m - j - 1];
+            }
+            hash &= hash + 1;
+            if (hash != 0) {
+                int lowBit = hash & -hash;
+                hash ^= lowBit;
+                int cnt = n - Integer.bitCount(hash);
+                for (int i = 0; i < cnt; i++) {
+                    lowBit >>= 1;
+                    hash |= lowBit;
+                }
+            }
+            return new String(arr);
+        }
+
+        public boolean hasNext() {
+            return hash != 0;
         }
     }
 }

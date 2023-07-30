cb15da689df060deef4b610a51ac57152d304500
shuangpengpang <15913598312@163.com>
2023-07-30 17:36:43 +0800

    环形链表II（142）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0101_0200/Problem0142LinkedListCycle2.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0101_0200/Problem0142LinkedListCycle2.java
@@ -2,7 +2,40 @@ package com.shuangpeng.Problem.p0101_0200;
 
 import com.shuangpeng.common.ListNode;
 
+/**
+ * @description:（环形链表II）
+ * @date 2023/7/30 5:34 PM
+ **/
 public class Problem0142LinkedListCycle2 {
+
+    public ListNode detectCycle0(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow != fast) {
+            return null;
+        }
+        int cnt = 1;
+        ListNode node = slow.next;
+        while (node != slow) {
+            cnt++;
+            node = node.next;
+        }
+        slow = fast = head;
+        for (int i = 0; i < cnt; i++) {
+            fast = fast.next;
+        }
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {

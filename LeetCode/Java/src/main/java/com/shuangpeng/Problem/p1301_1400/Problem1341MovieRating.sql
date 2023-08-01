b8c09cdbb1f0f345b649e0a11c392e23ce30198d
shuangpengpang <15913598312@163.com>
2023-08-01 15:29:14 +0800

    电影评分（1341）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1341MovieRating.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1341MovieRating.sql
@@ -27,3 +27,46 @@ FROM (
     LIMIT 1
 ) t2
 ;
+
+WITH user_rating AS (
+    SELECT COUNT(user_id) AS user_count, user_id
+    FROM MovieRating
+    GROUP BY user_id
+), max_user_rating AS (
+    SELECT name
+    FROM user_rating t
+    LEFT JOIN Users u ON t.user_id = u.user_id
+    WHERE t.user_count = (SELECT MAX(user_count) FROM user_rating)
+), movie_rating AS (
+    SELECT SUM(rating) / COUNT(DISTINCT user_id) AS avg_rating, movie_id
+    FROM MovieRating
+    WHERE DATE_FORMAT(created_at, "%Y-%m") = '2020-02'
+    GROUP BY movie_id
+), max_movie_rating AS (
+    SELECT m.title
+    FROM movie_rating mt
+    LEFT JOIN Movies m ON mt.movie_id = m.movie_id
+    WHERE mt.avg_rating = (SELECT MAX(avg_rating) FROM movie_rating)
+)
+SELECT MIN(name) AS results
+FROM max_user_rating
+UNION ALL
+SELECT MIN(title) AS results
+FROM max_movie_rating
+;
+
+SELECT results
+FROM (
+  SELECT name AS results, RANK() OVER(ORDER BY COUNT(*) DESC, name) AS RANKING
+  FROM Users
+  INNER JOIN MovieRating USING(user_id)
+  GROUP BY user_id
+  UNION ALL
+  SELECT title AS results, RANK() OVER(ORDER BY AVG(rating) DESC, title) AS RANKING
+  FROM MovieRating
+  INNER JOIN Movies USING(movie_id)
+  WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+  GROUP BY movie_id
+) T
+WHERE T.RANKING = 1
+;

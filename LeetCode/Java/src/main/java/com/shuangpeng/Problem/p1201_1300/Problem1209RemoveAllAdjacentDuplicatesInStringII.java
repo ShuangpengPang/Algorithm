2a2e9b4024a5ba0aab99988e9121c2faf85d62d1
shuangpengpang <15913598312@163.com>
2023-08-02 11:22:45 +0800

    删除字符串中的所有相邻重复项（1209）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1209RemoveAllAdjacentDuplicatesInStringII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1209RemoveAllAdjacentDuplicatesInStringII.java
@@ -37,7 +37,7 @@ public class Problem1209RemoveAllAdjacentDuplicatesInStringII {
         return sb.toString();
     }
 
-    public String removeDuplicates(String s, int k) {
+    public String removeDuplicates1(String s, int k) {
         int n = s.length();
         Deque<int[]> q = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
@@ -64,42 +64,24 @@ public class Problem1209RemoveAllAdjacentDuplicatesInStringII {
         return sb.toString();
     }
 
-//    public String removeDuplicates(String s, int k) {
-//        StringBuilder sb = new StringBuilder();
-//        int n = s.length();
-//        int[] cnt = new int[n];
-//        for (int i = 0, j = 0; j < n; i++, j++) {
-//            char c = s.charAt(j);
-//            if (i > 0 && c == sb.charAt(i - 1)) {
-//                if (cnt[i - 1] == k - 1) {
-//                    i -= k - 1;
-//                    sb.setLength(i--);
-//                } else {
-//                    cnt[i] =
-//                }
-//            } else {
-//                cnt[]
-//            }
-//        }
-//    }
-
-//    public String removeDuplicates(String s, int k) {
-//        StringBuilder sb = new StringBuilder(s);
-//        int count[] = new int[sb.length()];
-//        for (int i = 0; i < sb.length(); ++i) {
-//            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
-//                count[i] = 1;
-//            } else {
-//                count[i] = count[i - 1] + 1;
-//                if (count[i] == k) {
-//                    sb.delete(i - k + 1, i + 1);
-//                    i = i - k;
-//                }
-//            }
-//        }
-//        return sb.toString();
-//    }
-
-
-
+    public String removeDuplicates(String s, int k) {
+        char[] cs = s.toCharArray();
+        int n = cs.length, j = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (j == 0 || cs[i] != cs[j - 1]) {
+                cs[j++] = cs[i];
+                q.offerLast(1);
+            } else {
+                int cnt = q.pollLast() + 1;
+                if (cnt == k) {
+                    j = j - k + 1;
+                } else {
+                    cs[j++] = cs[i];
+                    q.offerLast(cnt);
+                }
+            }
+        }
+        return new String(cs, 0, j);
+    }
 }

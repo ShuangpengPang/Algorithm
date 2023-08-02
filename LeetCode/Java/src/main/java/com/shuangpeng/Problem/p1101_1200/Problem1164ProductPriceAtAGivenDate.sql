d704f277981cf22a7a9e46c5840a91467d8e6c0a
shuangpengpang <15913598312@163.com>
2023-08-02 18:02:46 +0800

    指定日期的产品价格（1164）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1164ProductPriceAtAGivenDate.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1164ProductPriceAtAGivenDate.sql
@@ -47,3 +47,18 @@ FROM (
 ) t2
 WHERE row_index = 1
 ;
+
+WITH target_price AS (
+    SELECT product_id, MAX(change_date) AS change_date
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+), distinct_id AS (
+    SELECT DISTINCT product_id
+    FROM Products
+)
+SELECT d.product_id, IFNULL(p.new_price, 10) AS price
+FROM distinct_id d
+    LEFT JOIN target_price t ON t.product_id = d.product_id
+    LEFT JOIN Products p ON p.product_id = t.product_id AND p.change_date = t.change_date
+;

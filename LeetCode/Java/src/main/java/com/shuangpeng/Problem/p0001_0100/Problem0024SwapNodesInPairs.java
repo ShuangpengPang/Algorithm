cfaccd92ce150c5099fb53f89d3e1c57db4244ed
shuangpengpang <15913598312@163.com>
2023-08-07 11:33:47 +0800

    两两交换链表中的节点（24）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0001_0100/Problem0024SwapNodesInPairs.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0001_0100/Problem0024SwapNodesInPairs.java
@@ -2,6 +2,10 @@ package com.shuangpeng.Problem.p0001_0100;
 
 import com.shuangpeng.common.ListNode;
 
+/**
+ * @description:（两两交换链表中的节点）
+ * @date 2023/8/7 11:32 AM
+ **/
 public class Problem0024SwapNodesInPairs {
 
     public ListNode swapPairs0(ListNode head) {
@@ -17,6 +21,18 @@ public class Problem0024SwapNodesInPairs {
             prev.next = temp;
             prev = node;
             node = next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode swapPairs1(ListNode head) {
+        ListNode dummy = new ListNode(0, head), prev = dummy;
+        for (ListNode node = head; node != null && node.next != null; node = node.next) {
+            ListNode next = node.next.next;
+            prev.next = node.next;
+            node.next.next = node;
+            node.next = next;
+            prev = node;
         }
         return dummy.next;
     }

3ff9f853667d9462c60b0faab0751c67ff800c34
shuangpengpang <15913598312@163.com>
2023-08-07 10:39:58 +0800

    反转字符串（344）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0344ReverseString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0344ReverseString.java
@@ -1,13 +1,25 @@
 package com.shuangpeng.Problem.p0301_0400;
 
+/**
+ * @description:（反转字符串）
+ * @date 2023/8/7 10:39 AM
+ **/
 public class Problem0344ReverseString {
 
-    public void reverseString(char[] s) {
+    public void reverseString0(char[] s) {
         int mid = s.length / 2;
         for (int i = 0; i < mid; i++) {
             char ch = s[i];
             s[i] = s[s.length - i - 1];
             s[s.length - i - 1] = ch;
+        }
+    }
+
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
         }
     }
 }

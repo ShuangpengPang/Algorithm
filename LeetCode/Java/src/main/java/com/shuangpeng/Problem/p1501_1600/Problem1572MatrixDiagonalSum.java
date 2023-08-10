9a2dbced35014effe0d02cff4ac8727682153d77
shuangpengpang <15913598312@163.com>
2023-08-11 00:14:40 +0800

    矩阵对角线元素的和（1572）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1572MatrixDiagonalSum.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.Problem.p1501_1600;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1572MatrixDiagonalSum（矩阵对角线元素的和）
+ * @date 2023/8/11 12:13 AM
+ */
+public class Problem1572MatrixDiagonalSum {
+
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length, h = n >> 1, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i] + mat[i][n - i - 1];
+        }
+        if ((n & 1) == 1) {
+            sum -= mat[h][h];
+        }
+        return sum;
+    }
+}

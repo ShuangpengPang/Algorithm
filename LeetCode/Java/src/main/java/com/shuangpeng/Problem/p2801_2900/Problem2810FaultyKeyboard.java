de080f2c1ba88a018a12fc9565b728462f4c1fed
shuangpengpang <15913598312@163.com>
2023-08-11 10:37:29 +0800

    故障键盘（2810）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2810FaultyKeyboard.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2810FaultyKeyboard.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p2801_2900;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +11,7 @@ package com.shuangpeng.Problem.p2801_2900;
  */
 public class Problem2810FaultyKeyboard {
 
-    public String finalString(String s) {
+    public String finalString0(String s) {
         int n = s.length();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
@@ -20,5 +23,27 @@ public class Problem2810FaultyKeyboard {
             }
         }
         return sb.toString();
+    }
+
+    public String finalString(String s) {
+        int n = s.length();
+        boolean reverse = false;
+        Deque<Character> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'i') {
+                reverse = !reverse;
+            } else if (reverse) {
+                q.offerFirst(c);
+            } else {
+                q.offerLast(c);
+            }
+        }
+        int m = q.size();
+        char[] cs = new char[m];
+        for (int i = reverse ? m - 1 : 0, step = reverse ? -1 : 1; i >= 0 && i < m; i += step) {
+            cs[i] = q.pollFirst();
+        }
+        return new String(cs);
     }
 }

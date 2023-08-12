8e1cbc014d341d35c97b8ea250c1a8d65f101504
shuangpengpang <15913598312@163.com>
2023-08-12 16:54:14 +0800

    每个元音包含偶数次的最长子字符串（1371）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1371FindTheLongestSubstringContainingVowelsInEvenCounts.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1371FindTheLongestSubstringContainingVowelsInEvenCounts.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +12,7 @@ import java.util.Map;
  */
 public class Problem1371FindTheLongestSubstringContainingVowelsInEvenCounts {
 
-    public int findTheLongestSubstring(String s) {
+    public int findTheLongestSubstring0(String s) {
         String vowels = "aeiou";
         int n = s.length(), hash = 0, ans = 0;
         Map<Integer, Integer> map = new HashMap<>();
@@ -23,6 +24,23 @@ public class Problem1371FindTheLongestSubstringContainingVowelsInEvenCounts {
             }
             int j = i;
             ans = Math.max(ans, i - map.computeIfAbsent(hash, k -> j));
+        }
+        return ans;
+    }
+
+    public int findTheLongestSubstring(String s) {
+        int[] map = new int[1 << 5];
+        Arrays.fill(map, Integer.MAX_VALUE);
+        String str = "aeiou";
+        int hash = 0, n = s.length(), ans = 0;
+        map[0] = -1;
+        for (int i = 0; i < n; i++) {
+            int j = str.indexOf(s.charAt(i));
+            if (j != -1) {
+                hash ^= 1 << j;
+            }
+            map[hash] = Math.min(map[hash], i);
+            ans = Math.max(ans, i - map[hash]);
         }
         return ans;
     }

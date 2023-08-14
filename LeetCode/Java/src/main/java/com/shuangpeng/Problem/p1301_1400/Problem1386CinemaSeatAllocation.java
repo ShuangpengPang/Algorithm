16bcb431aede88cdb6775a24cc2262474afdef42
shuangpengpang <15913598312@163.com>
2023-08-14 11:50:06 +0800

    安排电影院座位（1386）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1386CinemaSeatAllocation.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1386CinemaSeatAllocation.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p1301_1400;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ShuangPengPang
@@ -10,7 +12,7 @@ import java.util.Arrays;
  */
 public class Problem1386CinemaSeatAllocation {
 
-    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+    public int maxNumberOfFamilies0(int n, int[][] reservedSeats) {
         Arrays.sort(reservedSeats, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
         int ans = n << 1;
         int i = 0, j = 0, m = reservedSeats.length;
@@ -39,5 +41,22 @@ public class Problem1386CinemaSeatAllocation {
             }
         }
         return true;
+    }
+
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] r : reservedSeats) {
+            if (r[1] >= 2 && r[1] <= 9) {
+                map.put(r[0], map.getOrDefault(r[0], 0) | 1 << r[1] - 2);
+            }
+        }
+        int ans = n - map.size() << 1;
+        int m1 = (1 << 8) - 1, m2 = (1 << 4) - 1, a = m1 ^ m2, b = m1 ^ m2 << 2, c = m1 ^ m2 << 4;
+        for (int hash : map.values()) {
+            if ((hash | a) == a || (hash | b) == b || (hash | c) == c) {
+                ans++;
+            }
+        }
+        return ans;
     }
 }

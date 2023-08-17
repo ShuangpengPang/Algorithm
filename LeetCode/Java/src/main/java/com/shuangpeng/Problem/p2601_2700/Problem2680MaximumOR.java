f952bb4232298cda4f650e42cde7a69af1a0812d
shuangpengpang <15913598312@163.com>
2023-08-17 18:06:36 +0800

    最大或值（2680）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2680MaximumOR.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2680MaximumOR.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p2601_2700;
  */
 public class Problem2680MaximumOR {
 
-    public long maximumOr(int[] nums, int k) {
+    public long maximumOr0(int[] nums, int k) {
         int[] cnt = new int[31];
         int or = 0;
         for (int num : nums) {
@@ -28,6 +28,21 @@ public class Problem2680MaximumOR {
                 }
             }
             ans = Math.max(ans, value | ((long) num << k));
+        }
+        return ans;
+    }
+
+    public long maximumOr(int[] nums, int k) {
+        int n = nums.length;
+        int[] right = new int[n];
+        for (int i = n - 1, res = 0; i >= 0; i--) {
+            right[i] = res;
+            res |= nums[i];
+        }
+        long ans = 0;
+        for (int i = 0, or = 0; i < n; i++) {
+            ans = Math.max(ans, ((long) nums[i]) << k | or | right[i]);
+            or |= nums[i];
         }
         return ans;
     }

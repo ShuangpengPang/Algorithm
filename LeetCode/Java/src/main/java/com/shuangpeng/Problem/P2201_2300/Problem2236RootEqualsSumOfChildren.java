d23c21c01b0ffbfc7cf003bb139623d7a9452598
shuangpengpang <15913598312@163.com>
2023-08-20 00:07:54 +0800

    判断根结点是否等于子结点之和（2236）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/P2201_2300/Problem2236RootEqualsSumOfChildren.java
@@ -0,0 +1,23 @@
+package com.shuangpeng.Problem.P2201_2300;
+
+import com.shuangpeng.common.TreeNode;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2236RootEqualsSumOfChildren（判断根结点是否等于子结点之和）
+ * @date 2023/8/20 12:06 AM
+ */
+public class Problem2236RootEqualsSumOfChildren {
+
+    public boolean checkTree(TreeNode root) {
+        return root.val == dfs(root.left) + dfs(root.right);
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.val + dfs(root.left) + dfs(root.right);
+    }
+}

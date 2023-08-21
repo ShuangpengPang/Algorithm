4c2859cc597f1cbf713a71a9e6b4a0f0592bb281
shuangpengpang <15913598312@163.com>
2023-08-21 16:00:49 +0800

    统计和小于目标的下标对数目（2824）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2824CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,24 @@
+package com.shuangpeng.Problem.p2801_2900;
+
+import java.util.List;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2824CountPairsWhoseSumIsLessThanTarget（统计和小于目标的下标对数目）
+ * @date 2023/8/21 3:46 PM
+ */
+public class Problem2824CountPairsWhoseSumIsLessThanTarget {
+
+    public int countPairs(List<Integer> nums, int target) {
+        int n = nums.size(), cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}

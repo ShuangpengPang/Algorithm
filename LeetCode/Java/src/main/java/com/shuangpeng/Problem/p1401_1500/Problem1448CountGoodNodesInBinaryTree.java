b8d2fdcf5c72808140712bb81689e0af9b8a9eec
shuangpengpang <15913598312@163.com>
2023-08-25 10:43:28 +0800

    统计二叉树中好节点的数目（1448）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1448CountGoodNodesInBinaryTree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1448CountGoodNodesInBinaryTree.java
@@ -2,6 +2,10 @@ package com.shuangpeng.Problem.p1401_1500;
 
 import com.shuangpeng.common.TreeNode;
 
+/**
+ * @description:（统计二叉树中好节点的数目）
+ * @date 2023/8/25 10:42 AM
+ **/
 public class Problem1448CountGoodNodesInBinaryTree {
 
     private int answer = 0;
@@ -21,5 +25,24 @@ public class Problem1448CountGoodNodesInBinaryTree {
         }
         dfs(node.left, max);
         dfs(node.right, max);
+    }
+}
+
+class Problem1448CountGoodNodesInBinaryTree0 {
+
+    public int goodNodes(TreeNode root) {
+        return dfs(root, Integer.MIN_VALUE);
+    }
+
+    private int dfs(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 0;
+        if (max <= root.val) {
+            ans = 1;
+            max = root.val;
+        }
+        return ans + dfs(root.left, max) + dfs(root.right, max);
     }
 }

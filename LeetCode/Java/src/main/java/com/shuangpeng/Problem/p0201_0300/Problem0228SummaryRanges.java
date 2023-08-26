bd5f665867f14977a778e8eaf4d8a14a3c14e474
shuangpengpang <15913598312@163.com>
2023-08-26 10:42:14 +0800

    汇总区间（228）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0228SummaryRanges.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0228SummaryRanges.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class Problem0228SummaryRanges {
 
-    public List<String> summaryRanges(int[] nums) {
+    public List<String> summaryRanges0(int[] nums) {
         List<String> ans = new ArrayList<>();
         int n = nums.length;
         for (int l = 0, r = 0; r < n; r++) {
@@ -23,6 +23,34 @@ public class Problem0228SummaryRanges {
                 l = r + 1;
             }
         }
+        return ans;
+    }
+
+    public List<String> summaryRanges1(int[] nums) {
+        int n = nums.length;
+        List<String> ans = new ArrayList<>();
+        for (int i = 0, j = 0; i < n; j++, i = j) {
+            while (j + 1 < n && nums[j + 1] == nums[j] + 1) {
+                j++;
+            }
+            ans.add(i == j ? String.valueOf(nums[j]) : nums[i] + "->" + nums[j]);
+        }
+        return ans;
+    }
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        int n = nums.length, p = 0;
+        if (n == 0) {
+            return ans;
+        }
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] != nums[i - 1] + 1) {
+                ans.add(i - p == 1 ? String.valueOf(nums[i - 1]) : nums[p] + "->" + nums[i - 1]);
+                p = i;
+            }
+        }
+        ans.add(p == n - 1 ? String.valueOf(nums[p]) : nums[p] + "->" + nums[n - 1]);
         return ans;
     }
 }

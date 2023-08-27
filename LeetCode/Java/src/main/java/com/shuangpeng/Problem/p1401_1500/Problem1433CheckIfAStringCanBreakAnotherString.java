56c09720c0e7320aac6801a0799f4f86094e9089
shuangpengpang <15913598312@163.com>
2023-08-27 19:13:27 +0800

    检查一个字符串是否可以打破另一个字符串（1433）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1433CheckIfAStringCanBreakAnotherString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1433CheckIfAStringCanBreakAnotherString.java
@@ -9,29 +9,6 @@ package com.shuangpeng.Problem.p1401_1500;
 public class Problem1433CheckIfAStringCanBreakAnotherString {
 
     public boolean checkIfCanBreak0(String s1, String s2) {
-        int[] diff = new int[26];
-        int n = s1.length();
-        for (int i = 0; i < n; i++) {
-            diff[s1.charAt(i) - 'a']++;
-            diff[s2.charAt(i) - 'a']--;
-        }
-        int i = 0;
-        while (i < 26 && diff[i] == 0) {
-            i++;
-        }
-        if (i == 26) {
-            return true;
-        }
-        for (int j = i + 1, s = diff[i]; j < 26; j++) {
-            s += diff[j];
-            if (diff[i] < 0 && s > 0 || diff[i] > 0 && s < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean checkIfCanBreak(String s1, String s2) {
         int n = s1.length();
         int[] cnt = new int[26];
         for (int i = 0; i < n; i++) {
@@ -51,5 +28,24 @@ public class Problem1433CheckIfAStringCanBreakAnotherString {
             i++;
         }
         return true;
+    }
+
+    public boolean checkIfCanBreak(String s1, String s2) {
+        int n = s1.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[s1.charAt(i) - 'a']++;
+            cnt[s2.charAt(i) - 'a']--;
+        }
+        boolean positive = false, negative = false;
+        for (int s = 0, i = 0; i < 26 && (!positive || !negative); i++) {
+            s += cnt[i];
+            if (s > 0) {
+                positive = true;
+            } else if (s < 0) {
+                negative = true;
+            }
+        }
+        return !positive || !negative;
     }
 }

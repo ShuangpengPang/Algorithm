80ec8f01cf4d0d5bfb6f98132904140f38f44569
shuangpengpang <15913598312@163.com>
2023-08-27 18:34:05 +0800

    检查一个字符串是否可以打破另一个字符串（1433）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1433CheckIfAStringCanBreakAnotherString.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1433CheckIfAStringCanBreakAnotherString.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1401_1500;
  */
 public class Problem1433CheckIfAStringCanBreakAnotherString {
 
-    public boolean checkIfCanBreak(String s1, String s2) {
+    public boolean checkIfCanBreak0(String s1, String s2) {
         int[] diff = new int[26];
         int n = s1.length();
         for (int i = 0; i < n; i++) {
@@ -27,6 +27,28 @@ public class Problem1433CheckIfAStringCanBreakAnotherString {
             if (diff[i] < 0 && s > 0 || diff[i] > 0 && s < 0) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    public boolean checkIfCanBreak(String s1, String s2) {
+        int n = s1.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[s1.charAt(i) - 'a']++;
+            cnt[s2.charAt(i) - 'a']--;
+        }
+        int i = 0;
+        while (i < 26 && cnt[i] == 0) {
+            i++;
+        }
+        int num = cnt[i], sum = 0;
+        while (i < 26) {
+            sum += cnt[i];
+            if (sum != 0 && (sum ^ num) < 0) {
+                return false;
+            }
+            i++;
         }
         return true;
     }

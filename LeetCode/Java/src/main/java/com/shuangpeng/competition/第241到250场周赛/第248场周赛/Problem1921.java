42ff23812957ce4e64617107544d5e5e4720dbd7
shuangpengpang <15913598312@163.com>
2023-09-03 23:13:59 +0800

    消灭怪物的最大数量（1921）

--- LeetCode/Java/src/main/java/com/shuangpeng/competition/第241到250场周赛/第248场周赛/Problem1921.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/competition/第241到250场周赛/第248场周赛/Problem1921.java
@@ -1,10 +1,15 @@
 package com.shuangpeng.competition.第241到250场周赛.第248场周赛;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
+/**
+ * @description:（消灭怪物的最大数量）
+ * @date 2023/9/3 11:10 PM
+ **/
 public class Problem1921 {
 
-    public int eliminateMaximum(int[] dist, int[] speed) {
+    public int eliminateMaximum0(int[] dist, int[] speed) {
         int n = dist.length;
         double[] temp = new double[n];
         for (int i = 0; i < n; i++) {
@@ -13,6 +18,35 @@ public class Problem1921 {
         Arrays.sort(temp);
         for (int i = 0; i < n; i++) {
             if (i >= temp[i]) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    public int eliminateMaximum1(int[] dist, int[] speed) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        int n = dist.length;
+        for (int i = 0; i < n; i++) {
+            q.offer((dist[i] + speed[i] - 1) / speed[i]);
+        }
+        int ans = 0;
+        while (!q.isEmpty() && q.peek() > ans) {
+            ans++;
+            q.poll();
+        }
+        return ans;
+    }
+
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        int[] time = new int[n];
+        for (int i = 0; i < n; i++) {
+            time[i] = (dist[i] + speed[i] - 1) / speed[i];
+        }
+        Arrays.sort(time);
+        for (int i = 0; i < n; i++) {
+            if (i >= time[i]) {
                 return i;
             }
         }

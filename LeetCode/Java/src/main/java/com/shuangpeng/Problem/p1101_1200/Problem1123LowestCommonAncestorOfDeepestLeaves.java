420ed90a4016371add1e3e3dfdea326e081bdd90
shuangpengpang <15913598312@163.com>
2023-09-06 12:15:59 +0800

    最深叶节点的最近公共祖先（1123）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1123LowestCommonAncestorOfDeepestLeaves.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1101_1200/Problem1123LowestCommonAncestorOfDeepestLeaves.java
@@ -1,7 +1,15 @@
 package com.shuangpeng.Problem.p1101_1200;
 
 import com.shuangpeng.common.TreeNode;
+import javafx.util.Pair;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description:（最深叶节点的最近公共祖先）
+ * @date 2023/9/6 12:14 PM
+ **/
 public class Problem1123LowestCommonAncestorOfDeepestLeaves {
 
     class TreeNodeInfo {
@@ -51,6 +59,87 @@ public class Problem1123LowestCommonAncestorOfDeepestLeaves {
         if (left == right && left >= pre) {
             res = node;
             pre = left;
+        }
+        return Math.max(left, right);
+    }
+}
+
+class Problem1123LowestCommonAncestorOfDeepestLeaves0 {
+
+    private Map<Integer, Integer> depth;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        depth = new HashMap<>();
+        dfs(root);
+        return getNode(root);
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int d = Math.max(dfs(root.left), dfs(root.right)) + 1;
+        depth.put(root.val, d);
+        return d;
+    }
+
+    private TreeNode getNode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        int left = root.left == null ? 0 : depth.get(root.left.val);
+        int right = root.right == null ? 0 : depth.get(root.right.val);
+        if (left == right) {
+            return root;
+        }
+        if (left > right) {
+            return getNode(root.left);
+        }
+        return getNode(root.right);
+    }
+}
+
+class Problem1123LowestCommonAncestorOfDeepestLeaves1 {
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).getValue();
+    }
+
+    private Pair<Integer, TreeNode> dfs(TreeNode root) {
+        if (root == null) {
+            return new Pair<>(0, null);
+        }
+        Pair<Integer, TreeNode> left = dfs(root.left), right = dfs(root.right);
+        int leftDepth = left.getKey(), rightDepth = right.getKey();
+        if (leftDepth > rightDepth) {
+            return new Pair<>(leftDepth + 1, left.getValue());
+        } else if (rightDepth > leftDepth) {
+            return new Pair<>(rightDepth + 1, right.getValue());
+        }
+        return new Pair<>(leftDepth + 1, root);
+    }
+}
+
+class Problem1123LowestCommonAncestorOfDeepestLeaves2 {
+
+    TreeNode ans;
+    int maxDepth;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        ans = root;
+        maxDepth = 0;
+        dfs(root, 0);
+        return ans;
+    }
+
+    private int dfs(TreeNode root, int depth) {
+        if (root == null) {
+            maxDepth = Math.max(maxDepth, depth);
+            return depth;
+        }
+        int left = dfs(root.left, depth + 1), right = dfs(root.right, depth + 1);
+        if (left == right && left == maxDepth) {
+            ans = root;
         }
         return Math.max(left, right);
     }

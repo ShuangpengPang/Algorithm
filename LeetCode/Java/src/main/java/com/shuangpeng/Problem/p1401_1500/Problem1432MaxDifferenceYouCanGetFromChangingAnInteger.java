9a2cadb5d838df4bf428cd74e4b88e5199088f2c
shuangpengpang <15913598312@163.com>
2023-09-09 14:39:12 +0800

    改变一个整数能得到的最大差值（1432）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1432MaxDifferenceYouCanGetFromChangingAnInteger.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1432MaxDifferenceYouCanGetFromChangingAnInteger.java
@@ -40,3 +40,71 @@ public class Problem1432MaxDifferenceYouCanGetFromChangingAnInteger {
         return ans;
     }
 }
+
+class Problem1432MaxDifferenceYouCanGetFromChangingAnInteger0 {
+
+    public int maxDiff(int num) {
+        char[] cs = Integer.toString(num).toCharArray();
+        int s = cs[0] - '0', t = 1;
+        if (cs[0] == '1') {
+            for (char c : cs) {
+                if (c != '0' && c != '1') {
+                    s = c - '0';
+                    t = 0;
+                    break;
+                }
+            }
+        }
+        int m = cs[0] - '0';
+        for (char c : cs) {
+            if (c != '9') {
+                m = c - '0';
+                break;
+            }
+        }
+        return (m == '9' ? num : getNumber(cs, m, 9)) - getNumber(cs, s, t);
+    }
+
+
+    private int getNumber(char[] cs, int s, int t) {
+        int num = 0;
+        for (char c : cs) {
+            int d = c - '0';
+            num = num * 10 + (d == s ? t : d);
+        }
+        return num;
+    }
+}
+
+class Problem1432MaxDifferenceYouCanGetFromChangingAnInteger1 {
+
+    public int maxDiff(int num) {
+        char[] cs1 = Integer.toString(num).toCharArray(), cs2 = cs1.clone();
+        if (cs1[0] != '1') {
+            replace(cs1, cs1[0], '1');
+        } else {
+            for (char c : cs1) {
+                if (c != '0' && c != '1') {
+                    replace(cs1, c, '0');
+                    break;
+                }
+            }
+        }
+        for (char c : cs2) {
+            if (c != '9') {
+                replace(cs2, c, '9');
+                break;
+            }
+        }
+        return Integer.parseInt(new String(cs2)) - Integer.parseInt(new String(cs1));
+    }
+
+    private void replace(char[] cs, char s, char t) {
+        int n = cs.length;
+        for (int i = 0; i < n; i++) {
+            if (cs[i] == s) {
+                cs[i] = t;
+            }
+        }
+    }
+}

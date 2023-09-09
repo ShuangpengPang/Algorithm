0696616bb77a8aba45a49a9e35ad56cfcb13360c
shuangpengpang <15913598312@163.com>
2023-09-09 16:08:10 +0800

    对角线遍历II（1424）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1424DiagonalTraverseII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1424DiagonalTraverseII.java
@@ -2,7 +2,9 @@ package com.shuangpeng.Problem.p1401_1500;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * @author ShuangPengPang
@@ -12,24 +14,68 @@ import java.util.List;
  */
 public class Problem1424DiagonalTraverseII {
 
-    public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        int m = nums.size(), n = 0, M = 0;
+    public int[] findDiagonalOrder0(List<List<Integer>> nums) {
+        int cnt = 0, m = nums.size();
         for (List<Integer> list : nums) {
-            n = Math.max(n, list.size());
-            M += list.size();
+            cnt += list.size();
         }
-        int N = m + n - 1;
-        List<Integer>[] lists = new List[N];
-        Arrays.setAll(lists, i -> new ArrayList<>());
-        for (int i = m - 1; i >= 0; i--) {
-            List<Integer> list = nums.get(i);
-            for (int j = 0; j < list.size(); j++) {
-                lists[i + j].add(list.get(j));
+        int[] ans = new int[cnt];
+        int idx = 0;
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        q.offer(new int[]{idx++, 0, 0, nums.get(0).get(0)});
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            ans[p[0]] = p[3];
+            if (p[2] == 0 && p[1] + 1 < m) {
+                q.offer(new int[]{idx++, p[1] + 1, 0, nums.get(p[1] + 1).get(0)});
+            }
+            List<Integer> list = nums.get(p[1]);
+            if (p[2] + 1 < list.size()) {
+                q.offer(new int[]{idx++, p[1], p[2] + 1, list.get(p[2] + 1)});
             }
         }
-        int[] ans = new int[M];
-        for (int i = 0, idx = 0; i < N; i++) {
-            for (int num : lists[i]) {
+        return ans;
+    }
+
+    public int[] findDiagonalOrder1(List<List<Integer>> nums) {
+        List<int[]> arr = new ArrayList<>();
+        int m = nums.size();
+        for (int i = 0; i < m; i++) {
+            List<Integer> list = nums.get(i);
+            int n = list.size();
+            for (int j = 0; j < n; j++) {
+                arr.add(new int[]{i + j, j, list.get(j)});
+            }
+        }
+        arr.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int size = arr.size();
+        int[] ans = new int[size];
+        for (int i = 0; i < size; i++) {
+            ans[i] = arr.get(i)[2];
+        }
+        return ans;
+    }
+
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int m = nums.size(), N = 0, cnt = 0;
+        for (int i = 0; i < m; i++) {
+            int c = nums.get(i).size();
+            N = Math.max(N, i + c);
+            cnt += c;
+        }
+        List<Integer>[] arr = new List[N];
+        Arrays.setAll(arr, i -> new ArrayList<>());
+        for (int i = m - 1; i >= 0; i--) {
+            List<Integer> list = nums.get(i);
+            int n = list.size();
+            for (int j = 0; j < n; j++) {
+                arr[i + j].add(list.get(j));
+            }
+        }
+        int[] ans = new int[cnt];
+        int idx = 0;
+        for (List<Integer> list : arr) {
+            for (int num : list) {
                 ans[idx++] = num;
             }
         }

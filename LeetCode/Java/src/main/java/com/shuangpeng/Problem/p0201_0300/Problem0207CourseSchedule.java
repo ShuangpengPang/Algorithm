8c7d750e09e98bbfd0c761e298d658af27ff3513
shuangpengpang <15913598312@163.com>
2023-09-09 12:03:09 +0800

    课程表（207）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0207CourseSchedule.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0207CourseSchedule.java
@@ -2,6 +2,10 @@ package com.shuangpeng.Problem.p0201_0300;
 
 import java.util.*;
 
+/**
+ * @description:（课程表）
+ * @date 2023/9/9 12:02 PM
+ **/
 public class Problem0207CourseSchedule {
 
     // 深度优先
@@ -217,5 +221,118 @@ public class Problem0207CourseSchedule {
             count++;
         }
         return count == numCourses;
+    }
+}
+
+class Problem0207CourseSchedule0 {
+
+    public boolean canFinish0(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<Integer>[] graph = new List[numCourses];
+        Arrays.setAll(graph, i -> new ArrayList<>());
+        for (int[] p : prerequisites) {
+            inDegree[p[0]]++;
+            graph[p[1]].add(p[0]);
+        }
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[numCourses];
+        int cnt = 0;
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
+                visited[i] = true;
+                cnt++;
+            }
+        }
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            for (int y : graph[x]) {
+                if (visited[y]) {
+                    return false;
+                }
+                if (--inDegree[y] == 0) {
+                    visited[y] = true;
+                    q.offer(y);
+                    cnt++;
+                }
+            }
+        }
+        return cnt == numCourses;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = prerequisites.length, idx = 0;
+        int[] e = new int[n], ne = new int[n], h = new int[numCourses];
+        Arrays.fill(h, -1);
+        int[] inDegree = new int[numCourses];
+        for (int[] p : prerequisites) {
+            e[idx] = p[0];
+            ne[idx] = h[p[1]];
+            h[p[1]] = idx++;
+            inDegree[p[0]]++;
+        }
+        int cnt = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
+                cnt++;
+            }
+        }
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            for (int i = h[x]; i != -1; i = ne[i]) {
+                int y = e[i];
+                if (--inDegree[y] == 0) {
+                    q.offer(y);
+                    cnt++;
+                }
+            }
+        }
+        return cnt == numCourses;
+    }
+}
+
+class Problem0207CourseSchedule1 {
+
+    int[] e, ne, h;
+    int idx;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = prerequisites.length;
+        e = new int[n];
+        ne = new int[n];
+        h = new int[numCourses];
+        Arrays.fill(h, -1);
+        idx = 0;
+        for (int[] p : prerequisites) {
+            e[idx] = p[0];
+            ne[idx] = h[p[1]];
+            h[p[1]] = idx++;
+        }
+        int[] state = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0 && !dfs(i, state)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean dfs(int x, int[] state) {
+        if (state[x] == 1) {
+            return false;
+        }
+        if (state[x] == 2) {
+            return true;
+        }
+        state[x] = 1;
+        for (int i = h[x]; i != -1; i = ne[i]) {
+            if (!dfs(e[i], state)) {
+                return false;
+            }
+        }
+        state[x] = 2;
+        return true;
     }
 }

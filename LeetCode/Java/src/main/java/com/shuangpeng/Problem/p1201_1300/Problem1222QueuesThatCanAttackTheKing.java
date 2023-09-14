11460a10b180c8e006e0233594f257a31dbdfa9d
shuangpengpang <15913598312@163.com>
2023-09-14 12:13:44 +0800

    可以攻击国王的皇后（1222）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1222QueuesThatCanAttackTheKing.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1222QueuesThatCanAttackTheKing.java
@@ -2,7 +2,9 @@ package com.shuangpeng.Problem.p1201_1300;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author ShuangPengPang
@@ -11,6 +13,27 @@ import java.util.List;
  * @date 2023/6/13 12:09 PM
  */
 public class Problem1222QueuesThatCanAttackTheKing {
+
+    public List<List<Integer>> queensAttacktheKing0(int[][] queens, int[] king) {
+        Set<Integer> set = new HashSet<>();
+        for (int[] q : queens) {
+            set.add(q[0] * 8 + q[1]);
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (i != 0 || j != 0) {
+                    for (int x = king[0] + i, y = king[1] + j; x >= 0 && x < 8 && y >= 0 && y < 8; x += i, y += j) {
+                        if (set.contains(x * 8 + y)) {
+                            ans.add(Arrays.asList(x, y));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
         int N = 8;

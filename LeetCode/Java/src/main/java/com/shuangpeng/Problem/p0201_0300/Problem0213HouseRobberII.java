d39a184b08cc86d2d4608eaee28aaf2b4c1e3cb9
shuangpengpang <15913598312@163.com>
2023-09-17 19:08:47 +0800

    打家劫舍II（213）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0213HouseRobberII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0213HouseRobberII.java
@@ -1,8 +1,12 @@
 package com.shuangpeng.Problem.p0201_0300;
 
+/**
+ * @description:（打家劫舍II）
+ * @date 2023/9/17 7:07 PM
+ **/
 public class Problem0213HouseRobberII {
 
-    public int rob(int[] nums) {
+    public int rob0(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
@@ -22,5 +26,23 @@ public class Problem0213HouseRobberII {
             a = Math.max(a, t);
         }
         return Math.max(max, Math.max(a, b));
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        return Math.max(maxMoney(nums, 0, n - 2), maxMoney(nums, 1, n - 1));
+    }
+
+    private int maxMoney(int[] nums, int start, int end) {
+        int first = 0, second = 0;
+        for (int i = start; i <= end; i++) {
+            int tmp = Math.max(second, first + nums[i]);
+            first = second;
+            second = tmp;
+        }
+        return second;
     }
 }

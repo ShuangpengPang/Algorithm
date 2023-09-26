b2290adc56e0a7ca465d6ffc99ea9180d560fe92
shuangpengpang <15913598312@163.com>
2023-09-26 18:19:05 +0800

    递枕头（2582）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2582PassThePillow.java
@@ -0,0 +1,15 @@
+package com.shuangpeng.Problem.p2501_2600;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2582PassThePillow（递枕头）
+ * @date 2023/9/26 11:20 AM
+ */
+public class Problem2582PassThePillow {
+
+    public int passThePillow(int n, int time) {
+        int c = time / (n - 1), m = time % (n - 1);
+        return (c & 1) == 0 ? 1 + m : n - m;
+    }
+}

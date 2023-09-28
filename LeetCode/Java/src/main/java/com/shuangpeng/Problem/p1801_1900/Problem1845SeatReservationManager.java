59de6eb446d95c1d4619d4ca817a1f0a6e986f3f
shuangpengpang <15913598312@163.com>
2023-09-28 12:19:14 +0800

    座位预约管理系统（1845）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1845SeatReservationManager.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1845SeatReservationManager.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p1801_1900;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
@@ -9,32 +10,68 @@ import java.util.TreeSet;
  * @date 2023/9/28 11:18 AM
  */
 public class Problem1845SeatReservationManager {
+
+    class SeatManager {
+
+        int[] parent;
+        TreeSet<Integer> seat;
+
+        public SeatManager(int n) {
+            parent = new int[n + 2];
+            Arrays.setAll(parent, i -> i);
+            seat = new TreeSet<>();
+            seat.add(1);
+        }
+
+        public int reserve() {
+            int s = seat.first();
+            seat.remove(s);
+            parent[s] = find(parent, s + 1);
+            seat.add(parent[s]);
+            return s;
+        }
+
+        public void unreserve(int seatNumber) {
+            parent[seatNumber] = seatNumber;
+            if (parent[seatNumber - 1] != seatNumber - 1) {
+                parent[seatNumber - 1] = seatNumber;
+            }
+            seat.add(seatNumber);
+        }
+
+        private int find(int[] parent, int x) {
+            return parent[x] = x == parent[x] ? x : find(parent, parent[x]);
+        }
+    }
 }
 
-class SeatManager {
+class Problem1845SeatReservationManager0 {
 
-    boolean[] occupied;
-    TreeSet<Integer> seat;
+    class SeatManager {
 
-    public SeatManager(int n) {
-        occupied = new boolean[n + 2];
-        seat = new TreeSet<>();
-        seat.add(1);
-    }
+        boolean[] occupied;
+        TreeSet<Integer> seat;
 
-    public int reserve() {
-        int s = seat.first();
-        occupied[s] = true;
-        seat.remove(s);
-        if (!occupied[s + 1]) {
-            seat.add(s + 1);
+        public SeatManager(int n) {
+            occupied = new boolean[n + 2];
+            seat = new TreeSet<>();
+            seat.add(1);
         }
-        return s;
-    }
 
-    public void unreserve(int seatNumber) {
-        occupied[seatNumber] = false;
-        seat.add(seatNumber);
+        public int reserve() {
+            int s = seat.first();
+            occupied[s] = true;
+            seat.remove(s);
+            if (!occupied[s + 1]) {
+                seat.add(s + 1);
+            }
+            return s;
+        }
+
+        public void unreserve(int seatNumber) {
+            occupied[seatNumber] = false;
+            seat.add(seatNumber);
+        }
     }
 }
 

595f9d447cd8f86329fcef92645f97365224a0d6
shuangpengpang <15913598312@163.com>
2023-10-17 16:37:04 +0800

    找出和为指定值的下标对（1865）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1865FindingPairsWithACertainSum.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1865FindingPairsWithACertainSum.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p1801_1900;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,4 +47,38 @@ public class Problem1865FindingPairsWithACertainSum {
  * obj.add(index,val);
  * int param_2 = obj.count(tot);
  */
+}
+
+class Problem1865FindingPairsWithACertainSum0 {
+
+    class FindSumPairs {
+        HashMap<Integer, Integer> nums2_map = new HashMap<>();
+        int[] nums1;
+        int[] nums2;
+        public FindSumPairs(int[] nums1, int[] nums2) {
+            Arrays.sort(nums1);
+            this.nums1 = nums1;
+            this.nums2 = nums2;
+            for (int i : nums2) {
+                nums2_map.merge(i, 1, Integer::sum);
+            }
+        }
+
+        public void add(int index, int val) {
+            nums2_map.merge(nums2[index], -1, Integer::sum);
+            nums2[index] += val;
+            nums2_map.merge(nums2[index], 1, Integer::sum);
+        }
+
+        public int count(int tot) {
+            int res = 0;
+            for (int i : nums1) {
+                if (i >= tot) {
+                    break;
+                }
+                res += nums2_map.getOrDefault(tot - i, 0);
+            }
+            return res;
+        }
+    }
 }

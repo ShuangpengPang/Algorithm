3d5df754d41a494e901b3292e3e4df0bcc9e23b5
shuangpengpang <15913598312@163.com>
2023-10-23 10:18:00 +0800

    老人的数目（2678）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2678NumberOfSeniorCitizens.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2678NumberOfSeniorCitizens.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p2601_2700;
  */
 public class Problem2678NumberOfSeniorCitizens {
 
-    public int countSeniors(String[] details) {
+    public int countSeniors0(String[] details) {
         int cnt = 0;
         for (String d : details) {
             if (Integer.parseInt(d.substring(11, 13)) > 60) {
@@ -16,5 +16,15 @@ public class Problem2678NumberOfSeniorCitizens {
             }
         }
         return cnt;
+    }
+
+    public int countSeniors(String[] details) {
+        int ans = 0;
+        for (String detail : details) {
+            if ((detail.charAt(11) - '0') * 10 + detail.charAt(12) - '0' > 60) {
+                ans++;
+            }
+        }
+        return ans;
     }
 }

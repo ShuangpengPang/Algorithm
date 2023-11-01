4aba074c915b5c84d3502b365c9e0a35e282e30a
shuangpengpang <15913598312@163.com>
2023-11-01 14:27:29 +0800

    使数组元素相等的减少操作次数（1887）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1887ReductionOperationsToMakeTheArrayElementsEqual.java
@@ -0,0 +1,23 @@
+package com.shuangpeng.Problem.p1801_1900;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1887ReductionOperationsToMakeTheArrayElementsEqual（使数组元素相等的减少操作次数）
+ * @date 2023/11/1 2:02 PM
+ */
+public class Problem1887ReductionOperationsToMakeTheArrayElementsEqual {
+
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int cnt = 0, n = nums.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) {
+                cnt += n - i - 1;
+            }
+        }
+        return cnt;
+    }
+}

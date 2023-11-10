3e08ac0c74845803bfc72d5f5c082f8eabd5af42
shuangpengpang <15913598312@163.com>
2023-11-10 11:07:19 +0800

    删除一个字符串中所有出现的给定子字符串（1910）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1901_2000/Problem1910RemoveAllOccurrencesOfASubstring.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1901_2000/Problem1910RemoveAllOccurrencesOfASubstring.java
@@ -1,7 +1,5 @@
 package com.shuangpeng.Problem.p1901_2000;
 
-import javafx.util.Pair;
-
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -9,6 +7,29 @@ import javafx.util.Pair;
  * @date 2023/11/9 2:49 PM
  */
 public class Problem1910RemoveAllOccurrencesOfASubstring {
+
+    public String removeOccurrences0(String s, String part) {
+        StringBuilder sb  = new StringBuilder();
+        char[] cs1 = s.toCharArray(), cs2 = part.toCharArray();
+        int n = cs2.length;
+        for (char c : cs1) {
+            sb.append(c);
+            if (sb.length() >= cs2.length && check(sb, cs2)) {
+                sb.setLength(sb.length() - n);
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean check(StringBuilder sb, char[] cs) {
+        int n = cs.length;
+        for (int i = sb.length() - n, j = 0; j < n; i++, j++) {
+            if (sb.charAt(i) != cs[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public String removeOccurrences(String s, String part) {
         int n = s.length(), m = part.length();
@@ -22,23 +43,25 @@ public class Problem1910RemoveAllOccurrencesOfASubstring {
             next[i] = part.charAt(j) == c ? j + 1 : j;
         }
         int top = 0;
-        Pair<Character, Integer>[] stack = new Pair[n];
-        for (int i = 0, j = 0; i < n; i++) {
+        int[][] stack = new int[n + 1][2];
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
+            int j = stack[top][1];
             while (j > 0 && part.charAt(j) != c) {
                 j = next[j - 1];
             }
             j = part.charAt(j) == c ? j + 1 : j;
             if (j == m) {
                 top -= m - 1;
-                j = top == 0 ? 0 : stack[top - 1].getValue();
             } else {
-                stack[top++] = new Pair<>(c, j);
+                top++;
+                stack[top][0] = c;
+                stack[top][1] = j;
             }
         }
         char[] cs = new char[top];
         for (int i = 0; i < top; i++) {
-            cs[i] = stack[i].getKey();
+            cs[i] = (char) stack[i + 1][0];
         }
         return new String(cs);
     }

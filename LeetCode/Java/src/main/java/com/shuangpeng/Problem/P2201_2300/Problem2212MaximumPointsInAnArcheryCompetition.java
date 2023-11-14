0fc65c57f497a08240bde5708d63ac46ba1f1b5e
shuangpengpang <15913598312@163.com>
2023-11-14 10:34:20 +0800

    射箭比赛中的最大得分（2212）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/P2201_2300/Problem2212MaximumPointsInAnArcheryCompetition.java
@@ -0,0 +1,32 @@
+package com.shuangpeng.Problem.P2201_2300;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2212MaximumPointsInAnArcheryCompetition（射箭比赛中的最大得分）
+ * @date 2023/11/14 9:55 AM
+ */
+public class Problem2212MaximumPointsInAnArcheryCompetition {
+
+    public int[] maximumBobPoints(int numArrows, int[] aliceArrows) {
+        int maxScore = 0, n = aliceArrows.length, N = 1 << n;
+        int[] ans = aliceArrows;
+        for (int m = 1; m < N; m++) {
+            int num = numArrows, score = 0;
+            int[] arr = new int[n];
+            for (int i = 0; i < n && num >= 0; i++) {
+                if (((m >> i) & 1) == 1) {
+                    arr[i] = aliceArrows[i] + 1;
+                    num -= arr[i];
+                    score += i;
+                }
+            }
+            if (num >= 0 && score > maxScore) {
+                maxScore = score;
+                arr[0] += num;
+                ans = arr;
+            }
+        }
+        return ans;
+    }
+}

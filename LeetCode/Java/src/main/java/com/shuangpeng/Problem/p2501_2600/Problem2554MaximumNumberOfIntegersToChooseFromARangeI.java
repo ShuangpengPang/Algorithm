dc7100d9c64ac4a1b0e6e42d96aa69ec54e0ae58
shuangpengpang <15913598312@163.com>
2023-11-29 15:40:44 +0800

    从一个范围内选择最多整数I（2554）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2554MaximumNumberOfIntegersToChooseFromARangeI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2554MaximumNumberOfIntegersToChooseFromARangeI.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p2501_2600;
  */
 public class Problem2554MaximumNumberOfIntegersToChooseFromARangeI {
 
-    public int maxCount(int[] banned, int n, int maxSum) {
+    public int maxCount0(int[] banned, int n, int maxSum) {
         boolean[] set = new boolean[n + 1];
         for (int b : banned) {
             if (b <= n) {
@@ -26,5 +26,41 @@ public class Problem2554MaximumNumberOfIntegersToChooseFromARangeI {
             sum += i;
         }
         return cnt;
+    }
+
+    public int maxCount(int[] banned, int n, int maxSum) {
+        boolean[] visited = new boolean[n + 1];
+        int[] bit = new int[n + 1], cnt = new int[n + 1];
+        for (int b : banned) {
+            if (b > n || visited[b]) {
+                continue;
+            }
+            visited[b] = true;
+            int x = b;
+            while (x <= n) {
+                bit[x] += b;
+                cnt[x]++;
+                x += x & -x;
+            }
+        }
+        int left = 0, right = n;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (((mid + 1) * mid >> 1) - getResult(bit, mid) <= maxSum) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left - 1 - getResult(cnt, left - 1);
+    }
+
+    private int getResult(int[] bit, int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += bit[x];
+            x -= x & -x;
+        }
+        return sum;
     }
 }

eac5fd9ad32fb81409e53bb80aaf1238028d4b79
shuangpengpang <15913598312@163.com>
2023-11-30 00:11:17 +0800

    将字符串分割成值不超过K的子字符串（2522）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2522PartitionStringIntoSubstringsWithValuesAtMostK.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p2501_2600;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2522PartitionStringIntoSubstringsWithValuesAtMostK（将字符串分割成值不超过K的子字符串）
+ * @date 2023/11/30 12:10 AM
+ */
+public class Problem2522PartitionStringIntoSubstringsWithValuesAtMostK {
+
+    public int minimumPartition(String s, int k) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < n;) {
+            long num = 0;
+            while (i < n && num * 10 + s.charAt(i) - '0' <= k) {
+                num = num * 10 + s.charAt(i++) - '0';
+            }
+            if (num == 0) {
+                return -1;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}

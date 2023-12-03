d77c88c38c2bb2b2d320f076210a225fcb8f2eb7
shuangpengpang <15913598312@163.com>
2023-12-03 22:46:17 +0800

    统计染色格子数（2579）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2579CountTotalNumberOfColoredCells.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2579CountTotalNumberOfColoredCells.java
@@ -8,11 +8,15 @@ package com.shuangpeng.Problem.p2501_2600;
  */
 public class Problem2579CountTotalNumberOfColoredCells {
 
-    public long coloredCells(int n) {
+    public long coloredCells0(int n) {
         long ans = 1;
         for (int i = 2; i <= n; i++) {
             ans += (i - 1) << 2;
         }
         return ans;
+    }
+
+    public long coloredCells(int n) {
+        return 1 + ((long) n * (n - 1) << 1);
     }
 }

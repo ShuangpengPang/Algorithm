59a7ad6affaa827c66a6aa3efd91f9443419e053
shuangpengpang <15913598312@163.com>
2023-12-12 15:39:04 +0800

    使二叉树所有路径值相等的最小代价（2673）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2673MakeCostsOfPathsEqualInABinaryTree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2601_2700/Problem2673MakeCostsOfPathsEqualInABinaryTree.java
@@ -10,7 +10,7 @@ public class Problem2673MakeCostsOfPathsEqualInABinaryTree {
 
     private int ans;
 
-    public int minIncrements(int n, int[] cost) {
+    public int minIncrements0(int n, int[] cost) {
         ans = 0;
         dfs(cost, 0, 0);
         return ans;
@@ -26,5 +26,27 @@ public class Problem2673MakeCostsOfPathsEqualInABinaryTree {
         int max = Math.max(left, right);
         ans += (max << 1) - left - right;
         return max;
+    }
+
+    public int minIncrements1(int n, int[] cost) {
+        int ans = 0;
+        for (int i = n >> 1, j = n; i < j; i >>= 1, j >>= 1) {
+            for (int k = i + 1; k < j; k += 2) {
+                int max = Math.max(cost[k - 1], cost[k]);
+                ans += (max << 1) - cost[k - 1] - cost[k];
+                cost[k - 1 >> 1] += max;
+            }
+        }
+        return ans;
+    }
+
+    public int minIncrements(int n, int[] cost) {
+        int ans = 0;
+        for (int i = n >> 1; i > 0; i--) {
+            int left = (i << 1) - 1, right = i << 1;
+            ans += Math.abs(cost[left] - cost[right]);
+            cost[i - 1] += Math.max(cost[left], cost[right]);
+        }
+        return ans;
     }
 }

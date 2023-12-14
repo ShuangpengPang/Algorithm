f01cc12842a82c2ac34f177b2d3666544cf26962
shuangpengpang <15913598312@163.com>
2023-12-14 16:56:49 +0800

    三个数的最大乘积（628）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0628MaximumProductOfThreeNumbers.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0628MaximumProductOfThreeNumbers.java
@@ -1,5 +1,7 @@
 package com.shuangpeng.Problem.p0601_0700;
 
+import java.util.Arrays;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +10,7 @@ package com.shuangpeng.Problem.p0601_0700;
  */
 public class Problem0628MaximumProductOfThreeNumbers {
 
-    public int maximumProduct(int[] nums) {
+    public int maximumProduct0(int[] nums) {
         int n = nums.length;
         int one1 = Math.min(nums[0], Math.min(nums[1], nums[2]));
         int one2 = Math.max(nums[0], Math.max(nums[1], nums[2]));
@@ -24,5 +26,39 @@ public class Problem0628MaximumProductOfThreeNumbers {
             one2 = Math.max(one2, nums[i]);
         }
         return ans;
+    }
+
+    public int maximumProduct(int[] nums) {
+        int N = Integer.MAX_VALUE;
+        int[] arr1 = new int[3], arr2 = new int[4];
+        Arrays.fill(arr1, N);
+        Arrays.fill(arr2, -N);
+        for (int num : nums) {
+            addMin(arr1, num);
+            addMax(arr2, num);
+        }
+        return Math.max(arr2[0] * arr2[1] * arr2[2], arr1[0] * arr1[1] * arr2[0]);
+    }
+
+    private void addMin(int[] arr, int num) {
+        int n = arr.length - 1, i = 0;
+        while (i < n && num >= arr[i]) {
+            i++;
+        }
+        for (int j = n - 1; j >= i; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[i] = num;
+    }
+
+    private void addMax(int[] arr, int num) {
+        int n = arr.length - 1, i = 0;
+        while (i < n && num <= arr[i]) {
+            i++;
+        }
+        for (int j = n - 1; j >= i; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[i] = num;
     }
 }

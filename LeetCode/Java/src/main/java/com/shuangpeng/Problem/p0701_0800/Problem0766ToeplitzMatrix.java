db5f8880c7619db4a7cfad131108636966884cc3
shuangpengpang <15913598312@163.com>
2023-12-17 17:07:12 +0800

    托普利茨矩阵（766）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0701_0800/Problem0766ToeplitzMatrix.java
@@ -0,0 +1,29 @@
+package com.shuangpeng.Problem.p0701_0800;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem0766ToeplitzMatrix（托普利茨矩阵）
+ * @date 2023/12/17 5:05 PM
+ */
+public class Problem0766ToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int r = i + 1, c = 1; r < m && c < n; r++, c++) {
+                if (matrix[r][c] != matrix[r - 1][c - 1]) {
+                    return false;
+                }
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            for (int r = 1, c = j + 1; r < m && c < n; r++, c++) {
+                if (matrix[r][c] != matrix[r - 1][c - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

09792748c4a902130eaef186547fdb8601d7c5c8
shuangpengpang <15913598312@163.com>
2023-12-25 22:22:46 +0800

    最长相邻不相等子序列I（2900）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2900LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.Problem.p2801_2900;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2900LongestUnequalAdjacentGroupsSubsequenceI（最长相邻不相等子序列I）
+ * @date 2023/12/25 10:21 PM
+ */
+public class Problem2900LongestUnequalAdjacentGroupsSubsequenceI {
+
+    public List<String> getWordsInLongestSubsequence(int n, String[] words, int[] groups) {
+        List<String> ans = new ArrayList<>();
+        ans.add(words[0]);
+        for (int i = 1, g = groups[0]; i < n; i++) {
+            if (groups[i] != g) {
+                ans.add(words[i]);
+                g = groups[i];
+            }
+        }
+        return ans;
+    }
+}

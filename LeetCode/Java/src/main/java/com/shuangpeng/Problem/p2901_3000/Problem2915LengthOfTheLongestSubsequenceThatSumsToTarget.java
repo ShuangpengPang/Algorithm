c17fd8827b2a112edd3b3404f3b45aca7cdfe7bb
shuangpengpang <15913598312@163.com>
2024-01-06 20:18:55 +0800

    和为目标值的最长子序列的长度（2915）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2915LengthOfTheLongestSubsequenceThatSumsToTarget.java
@@ -0,0 +1,26 @@
+package com.shuangpeng.Problem.p2901_3000;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2915LengthOfTheLongestSubsequenceThatSumsToTarget（和为目标值的最长子序列的长度）
+ * @date 2024/1/6 8:17 PM
+ */
+public class Problem2915LengthOfTheLongestSubsequenceThatSumsToTarget {
+
+    public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+        int n = nums.size();
+        int[] dp = new int[target + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = Math.max(dp[i], dp[i - num] + 1);
+            }
+        }
+        return dp[target] < 0 ? -1 : dp[target];
+    }
+}

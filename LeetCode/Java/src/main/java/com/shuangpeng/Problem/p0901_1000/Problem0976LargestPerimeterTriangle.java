03ee4f0fd888ab8c50547ffb91fcd2b96e528d35
shuangpengpang <15913598312@163.com>
2024-01-08 22:48:50 +0800

    三角形的最大周长（976）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0976LargestPerimeterTriangle.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0901_1000/Problem0976LargestPerimeterTriangle.java
@@ -12,12 +12,11 @@ public class Problem0976LargestPerimeterTriangle {
 
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        int ans = 0;
         for (int i = nums.length - 1; i >= 2; i--) {
             if (nums[i] < nums[i - 1] + nums[i - 2]) {
-                ans = Math.max(ans, nums[i] + nums[i - 1] + nums[i - 2]);
+                return nums[i] + nums[i - 1] + nums[i - 2];
             }
         }
-        return ans;
+        return 0;
     }
 }

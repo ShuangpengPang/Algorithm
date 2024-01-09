02394399aa3a7f436ab258bcde13fbf2061a584c
shuangpengpang <15913598312@163.com>
2024-01-09 22:32:53 +0800

    购买水果需要的最少金币数（2944）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2944MinimumNumberOfCoinsForFruits.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2944MinimumNumberOfCoinsForFruits.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p2901_3000;
 
+import java.util.ArrayDeque;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.PriorityQueue;
 
 /**
@@ -23,7 +25,7 @@ public class Problem2944MinimumNumberOfCoinsForFruits {
         return dp[n];
     }
 
-    public int minimumCoins(int[] prices) {
+    public int minimumCoins1(int[] prices) {
         int n = prices.length;
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
         int ans = 0;
@@ -35,5 +37,45 @@ public class Problem2944MinimumNumberOfCoinsForFruits {
             ans = pq.peek()[1];
         }
         return ans;
+    }
+
+    public int minimumCoins2(int[] prices) {
+        int n = prices.length, h = n + 1 >> 1;
+        Deque<int[]> q = new ArrayDeque<>(n);
+        for (int i = n; i >= h; i--) {
+            while (!q.isEmpty() && q.peekLast()[1] >= prices[i - 1]) {
+                q.pollLast();
+            }
+            q.offerLast(new int[]{i, prices[i - 1]});
+        }
+        int ans = q.peekLast()[1];
+        for (int i = h - 1; i >= 1; i--) {
+            while (q.peekFirst()[0] > (i << 1) + 1) {
+                q.pollFirst();
+            }
+            ans = prices[i - 1] + q.peekFirst()[1];
+            while (!q.isEmpty() && q.peekLast()[1] >= ans) {
+                q.pollLast();
+            }
+            q.offerLast(new int[]{i, ans});
+        }
+        return ans;
+    }
+
+    public int minimumCoins(int[] prices) {
+        int n = prices.length;
+        Deque<int[]> q = new ArrayDeque<>(n);
+        q.offerLast(new int[]{n + 1, 0});
+        for (int i = n; i > 0; i--) {
+            while (q.peekLast()[0] > (i << 1) + 1) {
+                q.pollLast();
+            }
+            int num = prices[i - 1] + q.peekLast()[1];
+            while (q.peekFirst()[1] >= num) {
+                q.pollFirst();
+            }
+            q.offerFirst(new int[]{i, num});
+        }
+        return q.pollFirst()[1];
     }
 }

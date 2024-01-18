3bd2a440220da46f79a2ac894845983d93dbcf00
shuangpengpang <15913598312@163.com>
2024-01-18 15:44:36 +0800

    查询树中环的长度（2509）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2509CycleLengthQueriesInATree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2509CycleLengthQueriesInATree.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p2501_2600;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -7,6 +10,46 @@ package com.shuangpeng.Problem.p2501_2600;
  * @date 2024/1/18 12:53 PM
  */
 public class Problem2509CycleLengthQueriesInATree {
+
+    public int[] cycleLengthQueries0(int n, int[][] queries) {
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            int a = queries[i][0], b = queries[i][1];
+            List<Integer> list1 = new ArrayList<>(n), list2 = new ArrayList<>(n);
+            for (int num = a; num > 0; num >>= 1) {
+                list1.add(num);
+            }
+            for (int num = b; num > 0; num >>= 1) {
+                list2.add(num);
+            }
+            int j = list1.size() - 1, k = list2.size() - 1;
+            while (j >= 0 && k >= 0 && list1.get(j).equals(list2.get(k))) {
+                j--;
+                k--;
+            }
+            ans[i] = j + k + 3;
+        }
+        return ans;
+    }
+
+    public int[] cycleLengthQueries1(int n, int[][] queries) {
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            int a = queries[i][0], b = queries[i][1];
+            ans[i] = 1;
+            while (a != b) {
+                if (a > b) {
+                    a >>= 1;
+                } else {
+                    b >>= 1;
+                }
+                ans[i]++;
+            }
+        }
+        return ans;
+    }
 
     public int[] cycleLengthQueries(int n, int[][] queries) {
         int m = queries.length;

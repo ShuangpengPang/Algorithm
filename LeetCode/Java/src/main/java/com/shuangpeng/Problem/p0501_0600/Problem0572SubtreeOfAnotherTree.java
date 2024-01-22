2894f60dcdb9310f740d5e368493e156132d693c
shuangpengpang <15913598312@163.com>
2024-01-22 11:02:39 +0800

    另一棵树的子树（572）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0572SubtreeOfAnotherTree.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0501_0600/Problem0572SubtreeOfAnotherTree.java
@@ -43,3 +43,32 @@ public class Problem0572SubtreeOfAnotherTree {
         return sb;
     }
 }
+
+class Problem0572SubtreeOfAnotherTree0 {
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (check(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean check(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (root.val != subRoot.val) {
+            return false;
+        }
+        return check(root.left, subRoot.left) && check(root.right, subRoot.right);
+    }
+}

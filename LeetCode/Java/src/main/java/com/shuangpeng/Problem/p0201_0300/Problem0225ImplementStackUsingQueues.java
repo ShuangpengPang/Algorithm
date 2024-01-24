1c6d1a7993273ce0791679eda29cb4e53ed5e6c1
shuangpengpang <15913598312@163.com>
2024-01-24 15:28:51 +0800

    用队列实现栈（225）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0225ImplementStackUsingQueues.java
@@ -0,0 +1,55 @@
+package com.shuangpeng.Problem.p0201_0300;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem0225ImplementStackUsingQueues（用队列实现栈）
+ * @date 2024/1/24 12:10 AM
+ */
+public class Problem0225ImplementStackUsingQueues {
+}
+
+class MyStack {
+
+    private Queue<Integer> q;
+    private int topNum;
+
+    public MyStack() {
+        q = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        topNum = x;
+        q.offer(x);
+    }
+
+    public int pop() {
+        int size = q.size();
+        topNum = -1;
+        for (int i = 1; i < size; i++) {
+            topNum = q.poll();
+            q.offer(topNum);
+        }
+        return q.poll();
+    }
+
+    public int top() {
+        return topNum;
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

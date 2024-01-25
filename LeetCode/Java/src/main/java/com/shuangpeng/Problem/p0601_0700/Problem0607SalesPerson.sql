8c37463d5e06569762724f39b0c0e0cfe84152d1
shuangpengpang <15913598312@163.com>
2024-01-25 15:24:13 +0800

    销售员（607）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0607SalesPerson.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0601_0700/Problem0607SalesPerson.sql
@@ -12,3 +12,25 @@ where not exists (
         and c.name = 'RED'
 )
 ;
+
+select name
+from SalesPerson
+where sales_id not in (
+    select s.sales_id
+    from Company c
+        left join Orders o on c.com_id = o.com_id
+        left join SalesPerson s on o.sales_id = s.sales_id
+    where c.name = 'RED'
+        and s.sales_id is not null
+)
+;
+
+select name
+from SalesPerson
+where sales_id not in (
+    select o.sales_id
+    from Orders o
+        left join Company c on c.com_id = o.com_id
+    where c.name = 'RED'
+)
+;

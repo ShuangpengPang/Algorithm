ce66b161623f29ebaded36f4b83a1a1678944102
shuangpengpang <15913598312@163.com>
2024-01-27 23:00:00 +0800

    项目员工I（1075）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1075ProjectEmployeesI.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1001_1100/Problem1075ProjectEmployeesI.sql
@@ -8,3 +8,21 @@ from Project p
 where e.experience_years is not null
 group by p.project_id
 ;
+
+SELECT
+    project_id,
+    ROUND(AVG(e.experience_years),2)  AS average_years
+FROM
+    Project as p
+LEFT JOIN
+    Employee as e
+ON
+    p.employee_id = e.employee_id
+GROUP BY
+    p.project_id
+;
+
+select distinct p.project_id, round(avg(e.experience_years) over (partition by p.project_id), 2) as average_years
+from Project p
+    left join Employee e on p.employee_id = e.employee_id
+;

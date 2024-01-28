f20d21da2e36dcd221af486e67a030306b6faef0
shuangpengpang <15913598312@163.com>
2024-01-28 19:30:38 +0800

    最少得分子序列（2565）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2565SubsequenceWithTheMinimumScore.java
@@ -0,0 +1,44 @@
+package com.shuangpeng.Problem.p2501_2600;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2565SubsequenceWithTheMinimumScore（最少得分子序列）
+ * @date 2024/1/28 7:29 PM
+ */
+public class Problem2565SubsequenceWithTheMinimumScore {
+
+    public int minimumScore(String s, String t) {
+        char[] cs = s.toCharArray(), ct = t.toCharArray();
+        int n1 = cs.length, n2 = ct.length;
+        int[] index1 = new int[n2], index2 = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            index1[i] = n1;
+            index2[i] = -1;
+        }
+        int left = 0;
+        for (int i = 0; left < n2 && i < n1; i++) {
+            if (ct[left] == cs[i]) {
+                index1[left++] = i;
+            }
+        }
+        if (left == n2) {
+            return 0;
+        }
+        int ans = n2 - left;
+        int right = n2 - 1;
+        for (int i = n1 - 1; i >= 0; i--) {
+            if (ct[right] == cs[i]) {
+                index2[right--] = i;
+            }
+        }
+        ans = Math.min(ans, right + 1);
+        for (int i = 0, j = right + 1; i < left && j < n2; i++) {
+            while (j < n2 && index2[j] <= index1[i]) {
+                j++;
+            }
+            ans = Math.min(ans, j - i - 1);
+        }
+        return ans;
+    }
+}

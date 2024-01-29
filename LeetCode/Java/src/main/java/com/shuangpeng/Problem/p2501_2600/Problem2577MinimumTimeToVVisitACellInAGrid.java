9db5970030b2b3b3e226c1b5df93c99ef48a9626
shuangpengpang <15913598312@163.com>
2024-01-29 23:34:06 +0800

    在网格图中访问一个格子的最少时间（2577）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2577MinimumTimeToVVisitACellInAGrid.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2577MinimumTimeToVVisitACellInAGrid.java
@@ -93,3 +93,56 @@ class Problem2577MinimumTimeToVVisitACellInAGrid0 {
         return time[m - 1][n - 1];
     }
 }
+
+class Problem2577MinimumTimeToVVisitACellInAGrid1 {
+
+    private int[] dirs = {-1, 0, 1, 0, -1};
+
+    public int minimumTime(int[][] grid) {
+        if (grid[0][1] > 1 && grid[1][0] > 1) {
+            return -1;
+        }
+        int m = grid.length, n = grid[0].length, maxTime = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxTime = Math.max(maxTime, grid[i][j]);
+            }
+        }
+        int left = grid[m - 1][n - 1], right = maxTime + m * n;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (!check(grid, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + ((left - m - n) & 1);
+    }
+
+    private boolean check(int[][] grid, int step) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{m - 1, n - 1});
+        visited[m - 1][n - 1] = true;
+        while (!q.isEmpty()) {
+            step--;
+            for (int i = q.size() - 1; i >= 0; i--) {
+                int[] p = q.poll();
+                int x = p[0], y = p[1];
+                if (x == 0 && y == 0) {
+                    return true;
+                }
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dirs[d], ny = y + dirs[d + 1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && step >= grid[nx][ny]) {
+                        q.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}

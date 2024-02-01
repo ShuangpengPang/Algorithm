b71c6c28881b2d7cdbc2a3d9eb9827e79d9fac6a
shuangpengpang <15913598312@163.com>
2024-02-01 13:21:32 +0800

    平均售价（1251）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1251AverageSellingPrice.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1251AverageSellingPrice.sql
@@ -12,3 +12,9 @@ select distinct p.product_id, ifnull(s.average_price, 0) as average_price
 from Prices p
     left join summary s on s.product_id = p.product_id
 ;
+
+select p.product_id, ifnull(round(sum(p.price * u.units) / sum(u.units), 2), 0) as average_price
+from Prices p
+    left join UnitsSold u on u.product_id = p.product_id and u.purchase_date >= p.start_date and u.purchase_date <= p.end_date
+group by p.product_id
+;

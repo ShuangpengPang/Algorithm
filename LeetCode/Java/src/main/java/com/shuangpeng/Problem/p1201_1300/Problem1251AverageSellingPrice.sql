501e13b88cff554c2ed42fcb725a237773e26dc5
shuangpengpang <15913598312@163.com>
2024-02-01 12:38:09 +0800

    平均售价（1251）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1251AverageSellingPrice.sql
@@ -0,0 +1,14 @@
+# 平均售价
+
+# Write your MySQL query statement below
+
+with summary as (
+    select u.product_id, round(sum(p.price * u.units) / sum(u.units), 2) as average_price
+    from UnitsSold u
+        left join Prices p on p.product_id = u.product_id and u.purchase_date >= p.start_date and u.purchase_date <= p.end_date
+    group by u.product_id
+)
+select distinct p.product_id, ifnull(s.average_price, 0) as average_price
+from Prices p
+    left join summary s on s.product_id = p.product_id
+;

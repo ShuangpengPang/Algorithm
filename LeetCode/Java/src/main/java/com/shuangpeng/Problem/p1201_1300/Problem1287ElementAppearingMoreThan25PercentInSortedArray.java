7003a0aaddd790be2fba44f3f6973badf9f78331
shuangpengpang <15913598312@163.com>
2024-02-03 22:52:04 +0800

    有序数组中出现次数超过25%的元素（1287）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1287ElementAppearingMoreThan25PercentInSortedArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1201_1300/Problem1287ElementAppearingMoreThan25PercentInSortedArray.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1201_1300;
  */
 public class Problem1287ElementAppearingMoreThan25PercentInSortedArray {
 
-    public int findSpecialInteger(int[] arr) {
+    public int findSpecialInteger0(int[] arr) {
         int n = arr.length,  m = n >> 2;
         for (int i = 0, cnt = 0; i < n; i++) {
             if (++cnt > m) {
@@ -19,5 +19,38 @@ public class Problem1287ElementAppearingMoreThan25PercentInSortedArray {
             }
         }
         return 0;
+    }
+
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length, span = (n >> 2) + 1;
+        for (int i = 0; i < n; i += span) {
+            if (getCount(arr, arr[i]) >= span) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    private int getCount(int[] arr, int x) {
+        int n = arr.length, left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int tmp = left;
+        right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (arr[mid] == x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left - tmp;
     }
 }

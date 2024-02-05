64b54dc082040658437dc2938d88ed379073f9d1
shuangpengpang <15913598312@163.com>
2024-02-05 10:55:24 +0800

    按键变更的次数（3019）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3019NumberOfChangingKeys.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3019NumberOfChangingKeys（按键变更的次数）
+ * @date 2024/2/5 10:47 AM
+ */
+public class Problem3019NumberOfChangingKeys {
+
+    public int countKeyChanges(String s) {
+        int cnt = 0, n = s.length();
+        for (int i = 1; i < n; i++) {
+            int k = s.charAt(i) - s.charAt(i - 1);
+            if (k != 32 && k != -32 && k != 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}

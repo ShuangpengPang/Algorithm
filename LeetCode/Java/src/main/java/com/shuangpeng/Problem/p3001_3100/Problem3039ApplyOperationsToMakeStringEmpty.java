e2824fd85aa91e8e673ce1add8a869bf4ce30c68
shuangpengpang <15913598312@163.com>
2024-02-23 15:33:33 +0800

    进行操作使字符串为空（3039）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3039ApplyOperationsToMakeStringEmpty.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3039ApplyOperationsToMakeStringEmpty.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p3001_3100;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +11,7 @@ package com.shuangpeng.Problem.p3001_3100;
  */
 public class Problem3039ApplyOperationsToMakeStringEmpty {
 
-    public String lastNonEmptyString(String s) {
+    public String lastNonEmptyString0(String s) {
         char first = s.charAt(0);
         StringBuilder sb = new StringBuilder();
         sb.append(first);
@@ -24,6 +27,27 @@ public class Problem3039ApplyOperationsToMakeStringEmpty {
             }
             if (sb.length() == 0 || cnt[sb.charAt(sb.length() - 1) - 'a'] == cnt[j]) {
                 sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String lastNonEmptyString(String s) {
+        int[] cnt = new int[26], last = new int[26];
+        char[] cs = s.toCharArray();
+        int n = cs.length, maxCnt = 0;
+        for (int i = 0; i < n; i++) {
+            int c = cs[i] - 'a';
+            maxCnt = Math.max(maxCnt, ++cnt[c]);
+            last[c] = i;
+        }
+        Integer[] ids = new Integer[26];
+        Arrays.setAll(ids, i -> i);
+        Arrays.sort(ids, Comparator.comparingInt(a -> last[a]));
+        StringBuilder sb = new StringBuilder();
+        for (int id : ids) {
+            if (cnt[id] == maxCnt) {
+                sb.append((char) (id + 'a'));
             }
         }
         return sb.toString();

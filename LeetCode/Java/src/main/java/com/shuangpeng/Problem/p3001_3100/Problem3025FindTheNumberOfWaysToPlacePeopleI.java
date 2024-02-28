520d1231d8ff3509281a1c1d85609f20d8cc6c41
shuangpengpang <15913598312@163.com>
2024-02-28 17:57:09 +0800

    人员站位的方案数I（3025）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3025FindTheNumberOfWaysToPlacePeopleI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3025FindTheNumberOfWaysToPlacePeopleI.java
@@ -14,14 +14,10 @@ public class Problem3025FindTheNumberOfWaysToPlacePeopleI {
         Arrays.sort(points, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
         int ans = 0, n = points.length;
         for (int i = n - 1; i >= 0; i--) {
-            int j = i + 1;
-            while (j < n && points[j][1] > points[i][1]) {
-                j++;
-            }
             int p = -1;
-            for (int k = j; k < n && p < points[i][1]; k++) {
-                if (points[k][1] > p && points[k][1] <= points[i][1]) {
-                    p = points[k][1];
+            for (int j = i + 1; j < n && p < points[i][1]; j++) {
+                if (points[j][1] > p && points[j][1] <= points[i][1]) {
+                    p = points[j][1];
                     ans++;
                 }
             }

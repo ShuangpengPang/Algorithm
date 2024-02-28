1a39c1217b2f7bff6f4e9156730d08c0bde0d84e
shuangpengpang <15913598312@163.com>
2024-02-28 18:57:38 +0800

    Alice和Bob玩鲜花游戏（3021）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3021AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,14 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3021AliceAndBobPlayingFlowerGame（Alice和Bob玩鲜花游戏）
+ * @date 2024/2/28 6:03 PM
+ */
+public class Problem3021AliceAndBobPlayingFlowerGame {
+
+    public long flowerGame(int n, int m) {
+        return (long) (n >> 1) * (m + 1 >> 1) + (long) (n + 1 >> 1) * (m >> 1);
+    }
+}

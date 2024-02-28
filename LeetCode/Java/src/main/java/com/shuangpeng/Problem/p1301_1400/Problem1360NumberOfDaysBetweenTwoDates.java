15e0c2edac2fbcd3572231a7572929a5af7ae5aa
shuangpengpang <15913598312@163.com>
2024-02-28 23:06:43 +0800

    日期之间隔几天（1360）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1360NumberOfDaysBetweenTwoDates.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1360NumberOfDaysBetweenTwoDates.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +11,7 @@ package com.shuangpeng.Problem.p1301_1400;
  */
 public class Problem1360NumberOfDaysBetweenTwoDates {
 
-    public int daysBetweenDates(String date1, String date2) {
+    public int daysBetweenDates0(String date1, String date2) {
         return Math.abs(getDays(date1) - getDays(date2));
     }
 
@@ -22,5 +25,11 @@ public class Problem1360NumberOfDaysBetweenTwoDates {
             day += i == 2 ? (leap ? 29 : 28) : (i < 8 && (i & 1) == 1 || i >= 8 && (i & 1) == 0 ? 31 : 30);
         }
         return day + Integer.parseInt(strs[2]);
+    }
+
+    public int daysBetweenDates(String date1, String date2) {
+        LocalDate localDate1 = LocalDate.parse(date1, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate localDate2 = LocalDate.parse(date2, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return (int) Math.abs(localDate1.toEpochDay() - localDate2.toEpochDay());
     }
 }

c97f9a7b76700b8853bd4e0d613290807c70b44b
shuangpengpang <15913598312@163.com>
2024-02-29 23:37:09 +0800

    有多少小于当前数字的数字（1365）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1365HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1301_1400/Problem1365HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -1,5 +1,8 @@
 package com.shuangpeng.Problem.p1301_1400;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author ShuangPengPang
  * @version 1.0
@@ -8,7 +11,7 @@ package com.shuangpeng.Problem.p1301_1400;
  */
 public class Problem1365HowManyNumbersAreSmallerThanTheCurrentNumber {
 
-    public int[] smallerNumbersThanCurrent(int[] nums) {
+    public int[] smallerNumbersThanCurrent0(int[] nums) {
         int n = nums.length;
         int[] cnt = new int[101];
         for (int num : nums) {
@@ -22,6 +25,27 @@ public class Problem1365HowManyNumbersAreSmallerThanTheCurrentNumber {
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
             ans[i] = preSum[nums[i]];
+        }
+        return ans;
+    }
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        Integer[] ids = new Integer[n];
+        Arrays.setAll(ids, i -> i);
+        Arrays.sort(ids, Comparator.comparingInt(e -> nums[e]));
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = left + (right - left >> 1);
+                if (nums[ids[mid]] < nums[i]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans[i] = left;
         }
         return ans;
     }

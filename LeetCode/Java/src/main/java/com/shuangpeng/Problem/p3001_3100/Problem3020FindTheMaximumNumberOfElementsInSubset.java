0c17f6a402d69602d0ed717ac4a0f99f3ee99400
shuangpengpang <15913598312@163.com>
2024-03-02 10:59:39 +0800

    子集中元素的最大数量（3020）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3020FindTheMaximumNumberOfElementsInSubset.java
@@ -0,0 +1,34 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3020FindTheMaximumNumberOfElementsInSubset（子集中元素的最大数量）
+ * @date 2024/3/1 7:49 PM
+ */
+public class Problem3020FindTheMaximumNumberOfElementsInSubset {
+
+    public int maximumLength(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (long num : nums) {
+            map.merge(num, 1, Integer::sum);
+        }
+        int ans = 0;
+        for (long num : map.keySet()) {
+            if (num == 1) {
+                int cnt = map.get(num);
+                ans = Math.max(ans, cnt + (cnt & 1) - 1);
+                continue;
+            }
+            int cnt = 1;
+            for (long i = num, j = i * i; map.get(i) > 1 && map.getOrDefault(j, 0) > 0; i = j, j = j * j) {
+                cnt += 2;
+            }
+            ans = Math.max(ans, cnt);
+        }
+        return ans;
+    }
+}

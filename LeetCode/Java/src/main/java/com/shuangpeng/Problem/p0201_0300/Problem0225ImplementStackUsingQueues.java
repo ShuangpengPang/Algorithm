4030d33ff674ce7075cc9e31b112b640b9757289
shuangpengpang <15913598312@163.com>
2024-03-03 00:30:21 +0800

    用队列实现栈（225）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0225ImplementStackUsingQueues.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0225ImplementStackUsingQueues.java
@@ -10,40 +10,39 @@ import java.util.Queue;
  * @date 2024/1/24 12:10 AM
  */
 public class Problem0225ImplementStackUsingQueues {
-}
 
-class MyStack {
+    class MyStack {
 
-    private Queue<Integer> q;
-    private int topNum;
+        private Queue<Integer> q;
+        private int topNum;
 
-    public MyStack() {
-        q = new ArrayDeque<>();
-    }
-
-    public void push(int x) {
-        topNum = x;
-        q.offer(x);
-    }
-
-    public int pop() {
-        int size = q.size();
-        topNum = -1;
-        for (int i = 1; i < size; i++) {
-            topNum = q.poll();
-            q.offer(topNum);
+        public MyStack() {
+            q = new ArrayDeque<>();
         }
-        return q.poll();
-    }
 
-    public int top() {
-        return topNum;
-    }
+        public void push(int x) {
+            topNum = x;
+            q.offer(x);
+        }
 
-    public boolean empty() {
-        return q.isEmpty();
+        public int pop() {
+            int size = q.size();
+            topNum = -1;
+            for (int i = 1; i < size; i++) {
+                topNum = q.poll();
+                q.offer(topNum);
+            }
+            return q.poll();
+        }
+
+        public int top() {
+            return topNum;
+        }
+
+        public boolean empty() {
+            return q.isEmpty();
+        }
     }
-}
 
 /**
  * Your MyStack object will be instantiated and called as such:
@@ -53,3 +52,35 @@ class MyStack {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
+}
+
+class Problem0225ImplementStackUsingQueues0 {
+
+    class MyStack {
+
+        private Queue<Integer> q;
+
+        public MyStack() {
+            q = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            q.offer(x);
+            for (int i = q.size() - 2; i >= 0; i--) {
+                q.offer(q.poll());
+            }
+        }
+
+        public int pop() {
+            return q.poll();
+        }
+
+        public int top() {
+            return q.peek();
+        }
+
+        public boolean empty() {
+            return q.isEmpty();
+        }
+    }
+}

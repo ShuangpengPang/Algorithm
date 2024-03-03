a35b198ee1ac16e074c191f78300300bad92a031
shuangpengpang <15913598312@163.com>
2024-03-03 19:47:47 +0800

    按距离统计房屋对数目I（3015）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3015CountTheNumberOfHousesAtACertainDistanceI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3015CountTheNumberOfHousesAtACertainDistanceI.java
@@ -60,3 +60,68 @@ public class Problem3015CountTheNumberOfHousesAtACertainDistanceI {
         }
     }
 }
+
+class Problem3015CountTheNumberOfHousesAtACertainDistanceI0 {
+    public long[] countOfPairs(int n, int x, int y) {
+        if (x > y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        diff = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            add(1, i - 1, 1);
+            add(1, n - i, 1);
+            if (x + 1 >= y) {
+                continue;
+            }
+            if (i <= x) {
+                update(i, x, y, n);
+            } else if (i >= y) {
+                update(n + 1 - i, n + 1 - y, n + 1 - x, n);
+            } else if (i < (x + y) / 2) {
+                update2(i, x, y, n);
+            } else if (i > (x + y + 1) / 2) {
+                update2(n + 1 - i, n + 1 - y, n + 1 - x, n);
+            }
+        }
+
+        long[] ans = new long[n];
+        long sumD = 0;
+        for (int i = 0; i < n; i++) {
+            sumD += diff[i + 1];
+            ans[i] = sumD;
+        }
+        return ans;
+    }
+
+    private int[] diff;
+
+    private void add(int l, int r, int v) {
+        if (l > r) return;
+        diff[l] += v;
+        diff[r + 1] -= v;
+    }
+
+    private void update(int i, int x, int y, int n) {
+        add(y - i, n - i, -1); // 撤销 [y,n]
+        int dec = y - x - 1; // 缩短的距离
+        add(y - i - dec, n - i - dec, 1);
+
+        int j = (x + y + 1) / 2 + 1;
+        add(j - i, y - 1 - i, -1); // 撤销 [j, y-1]
+        add(x - i + 2, x - i + y - j + 1, 1);
+    }
+
+    private void update2(int i, int x, int y, int n) {
+        add(y - i, n - i, -1); // 撤销 [y,n]
+        int dec = (y - i) - (i - x + 1); // 缩短的距离
+        add(y - i - dec, n - i - dec, 1);
+
+        int j = i + (y - x + 1) / 2 + 1;
+        add(j - i, y - 1 - i, -1); // 撤销 [j, y-1]
+        add(i - x + 2, i - x + y - j + 1, 1);
+    }
+}

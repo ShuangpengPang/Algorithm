3de90672b061b2b7dfd29a2568e76011951a3da5
shuangpengpang <15913598312@163.com>
2024-03-06 00:29:03 +0800

    判断能否形成等差数列（1502）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1502CanMakeArithmeticProgressionFromSequence.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1502CanMakeArithmeticProgressionFromSequence.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p1501_1600;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author ShuangPengPang
@@ -10,10 +12,33 @@ import java.util.Arrays;
  */
 public class Problem1502CanMakeArithmeticProgressionFromSequence {
 
-    public boolean canMakeArithmeticProgression(int[] arr) {
+    public boolean canMakeArithmeticProgression0(int[] arr) {
         Arrays.sort(arr);
         for (int i = 2; i < arr.length; i++) {
             if (arr[i] - arr[i - 1] != arr[i - 1] - arr[i - 2]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            set.add(num);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        if (min == max) {
+            return true;
+        }
+        int n = arr.length, d = (max - min) / (n - 1);
+        if (set.size() != n || min + d * (n - 1) != max) {
+            return false;
+        }
+        for (int i = min + d; i < max; i += d) {
+            if (!set.contains(i)) {
                 return false;
             }
         }

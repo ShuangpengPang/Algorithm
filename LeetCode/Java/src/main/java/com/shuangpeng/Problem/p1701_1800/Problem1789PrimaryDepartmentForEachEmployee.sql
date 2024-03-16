be033fa16d167c1129b620bad36a75f6e6ac49b5
shuangpengpang <15913598312@163.com>
2024-03-16 14:34:45 +0800

    员工的直属部门（1789）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1789PrimaryDepartmentForEachEmployee.sql
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1789PrimaryDepartmentForEachEmployee.sql
@@ -4,3 +4,22 @@ select employee_id, department_id
 from Employee e
 where primary_flag = 'Y' or (select count(1) from Employee where employee_id = e.employee_id) = 1
 ;
+
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y' or employee_id in (
+    select employee_id
+    from Employee
+    group by employee_id
+    having count(1) = 1
+)
+;
+
+with tmp as (
+    select employee_id, department_id, primary_flag, count(1) over (partition by employee_id) cnt
+    from Employee
+)
+select employee_id, department_id
+from tmp
+where primary_flag = 'Y' or cnt = 1
+;

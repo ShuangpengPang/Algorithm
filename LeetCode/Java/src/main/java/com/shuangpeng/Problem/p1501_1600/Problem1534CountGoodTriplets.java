958f535233a5c7b570371b61a2fdfec459526b16
shuangpengpang <15913598312@163.com>
2024-03-18 11:57:13 +0800

    统计好三元组（1534）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1534CountGoodTriplets.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1534CountGoodTriplets.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1501_1600;
  */
 public class Problem1534CountGoodTriplets {
 
-    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+    public int countGoodTriplets0(int[] arr, int a, int b, int c) {
         int n = arr.length, ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -22,5 +22,68 @@ public class Problem1534CountGoodTriplets {
             }
         }
         return ans;
+    }
+
+    public int countGoodTriplets1(int[] arr, int a, int b, int c) {
+        int n = arr.length, maxValue = 0;
+        for (int num : arr) {
+            maxValue = Math.max(maxValue, num);
+        }
+        int[] cnt = new int[maxValue + 2];
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (Math.abs(arr[j] - arr[k]) <= b) {
+                    int left = Math.max(0, Math.max(arr[j] - a, arr[k] - c));
+                    int right = Math.min(maxValue, Math.min(arr[j] + a, arr[k] + c));
+                    if (left <= right) {
+                        ans += cnt[right + 1] - cnt[left];
+                    }
+                }
+            }
+            for (int i = arr[j] + 1; i <= maxValue + 1; i++) {
+                cnt[i]++;
+            }
+        }
+        return ans;
+    }
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n = arr.length, maxValue = 0;
+        for (int num : arr) {
+            maxValue = Math.max(maxValue, num);
+        }
+        int[] bit = new int[maxValue + 2];
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (Math.abs(arr[j] - arr[k]) <= b) {
+                    int left = Math.max(0, Math.max(arr[j] - a, arr[k] - c));
+                    int right = Math.min(maxValue, Math.min(arr[j] + a, arr[k] + c));
+                    if (left <= right) {
+                        ans += query(bit, right + 1) - query(bit, left);
+                    }
+                }
+            }
+            increment(bit, arr[j] + 1);
+        }
+        return ans;
+    }
+
+    private int query(int[] bit, int x) {
+        int ans = 0;
+        while (x != 0) {
+            ans += bit[x];
+            x ^= x & -x;
+        }
+        return ans;
+    }
+
+    private void increment(int[] bit, int x) {
+        int n = bit.length;
+        while (x < n) {
+            bit[x]++;
+            x += x & -x;
+        }
     }
 }

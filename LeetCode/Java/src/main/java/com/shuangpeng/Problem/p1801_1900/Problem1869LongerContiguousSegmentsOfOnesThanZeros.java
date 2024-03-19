293e222d9a361df77b18abf7634f61d2776626c2
shuangpengpang <15913598312@163.com>
2024-03-19 18:43:21 +0800

    哪种连续子字符串更长（1869）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1801_1900/Problem1869LongerContiguousSegmentsOfOnesThanZeros.java
@@ -0,0 +1,24 @@
+package com.shuangpeng.Problem.p1801_1900;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem1869LongerContiguousSegmentsOfOnesThanZeros（哪种连续子字符串更长）
+ * @date 2024/3/19 5:48 PM
+ */
+public class Problem1869LongerContiguousSegmentsOfOnesThanZeros {
+
+    public boolean checkZeroOnes(String s) {
+        int[] cnt = new int[2];
+        int n = s.length();
+        for (int i = 0, count = 0; i < n; i++) {
+            count++;
+            char c = s.charAt(i);
+            if (i == n - 1 || s.charAt(i + 1) != c) {
+                cnt[c - '0'] = Math.max(cnt[c - '0'], count);
+                count = 0;
+            }
+        }
+        return cnt[1] > cnt[0];
+    }
+}

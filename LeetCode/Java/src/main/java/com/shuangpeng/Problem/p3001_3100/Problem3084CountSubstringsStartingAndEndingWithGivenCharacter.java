9d244c38733596fda671b24215553b0dfbcdf900
shuangpengpang <15913598312@163.com>
2024-03-25 23:58:55 +0800

    统计以给定字符开头和结尾的子字符串总数（3084）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3084CountSubstringsStartingAndEndingWithGivenCharacter.java
@@ -0,0 +1,20 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3084CountSubstringsStartingAndEndingWithGivenCharacter（统计以给定字符开头和结尾的子字符串总数）
+ * @date 2024/3/25 11:57 PM
+ */
+public class Problem3084CountSubstringsStartingAndEndingWithGivenCharacter {
+
+    public long countSubstrings(String s, char c) {
+        long cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                cnt++;
+            }
+        }
+        return cnt * (cnt + 1) >> 1;
+    }
+}

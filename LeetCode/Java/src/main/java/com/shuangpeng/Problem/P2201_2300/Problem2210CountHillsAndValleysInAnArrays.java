5ebad00663dd58ca077650c43187b891028c28b5
shuangpengpang <15913598312@163.com>
2024-03-26 16:10:32 +0800

    统计数组中峰和谷的数量（2210）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/P2201_2300/Problem2210CountHillsAndValleysInAnArrays.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/P2201_2300/Problem2210CountHillsAndValleysInAnArrays.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.P2201_2300;
  */
 public class Problem2210CountHillsAndValleysInAnArrays {
 
-    public int countHillValley(int[] nums) {
+    public int countHillValley0(int[] nums) {
         int n = nums.length, ans = 0;
         int i = 1;
         while (i < n && nums[i] == nums[0]) {
@@ -26,6 +26,27 @@ public class Problem2210CountHillsAndValleysInAnArrays {
             }
             if (i < n) {
                 ans++;
+            }
+        }
+        return ans;
+    }
+
+    public int countHillValley(int[] nums) {
+        int n = nums.length, ans = 0;
+        boolean isHill = false, isValley = false;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                if (isValley) {
+                    ans++;
+                    isValley = false;
+                }
+                isHill = true;
+            } else if (nums[i] < nums[i - 1]) {
+                if (isHill) {
+                    ans++;
+                    isHill = false;
+                }
+                isValley = true;
             }
         }
         return ans;

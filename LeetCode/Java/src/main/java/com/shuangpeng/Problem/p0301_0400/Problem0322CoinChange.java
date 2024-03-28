68711c3363d3025e3629da8e2b552ac77fc8a3c1
shuangpengpang <15913598312@163.com>
2024-03-28 16:54:18 +0800

    零钱兑换（322）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0322CoinChange.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0322CoinChange.java
@@ -68,24 +68,50 @@ public class Problem0322CoinChange {
         }
         return -1;
     }
-
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        int N = Integer.MAX_VALUE >> 1;
-        Arrays.fill(dp, N);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i >= coin) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
-        }
-        return dp[amount] == N ? -1 : dp[amount];
-    }
 }
 
 class Problem0322CoinChange0 {
+    private int[] coins;
+    private int[][] memo;
+
+    public int coinChange0(int[] coins, int amount) {
+        this.coins = coins;
+        int n = coins.length;
+        memo = new int[n][amount + 1];
+        for (int[] row : memo)
+            Arrays.fill(row, -1); // -1 表示没有访问过
+        int ans = dfs(n - 1, amount);
+        return ans < Integer.MAX_VALUE / 2 ? ans : -1;
+    }
+
+    private int dfs(int i, int c) {
+        if (i < 0) return c == 0 ? 0 : Integer.MAX_VALUE / 2; // 除 2 是防止下面 + 1 溢出
+        if (memo[i][c] != -1) return memo[i][c];
+        if (c < coins[i]) return memo[i][c] = dfs(i - 1, c);
+        return memo[i][c] = Math.min(dfs(i - 1, c), dfs(i, c - coins[i]) + 1);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length, N = Integer.MAX_VALUE >> 1;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int[] a : dp) {
+            Arrays.fill(a, N);
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i - 1]) {
+                    int k = j - coins[i - 1];
+                    dp[i][j] = Math.min(dp[i][j], Math.min(dp[i - 1][k], dp[i][k]) + 1);
+                }
+            }
+        }
+        return dp[n][amount] == N ? -1 : dp[n][amount];
+    }
+}
+
+class Problem0322CoinChange1 {
 
     private static final int N = Integer.MAX_VALUE >> 1;
 
@@ -113,24 +139,31 @@ class Problem0322CoinChange0 {
     }
 }
 
-class Problem0322CoinChange1 {
-    private int[] coins;
-    private int[][] memo;
+class Problem0322CoinChange2 {
 
-    public int coinChange(int[] coins, int amount) {
-        this.coins = coins;
-        int n = coins.length;
-        memo = new int[n][amount + 1];
-        for (int[] row : memo)
-            Arrays.fill(row, -1); // -1 表示没有访问过
-        int ans = dfs(n - 1, amount);
+    public int coinChange0(int[] coins, int amount) {
+        int[] f = new int[amount + 1];
+        Arrays.fill(f, Integer.MAX_VALUE / 2); // 除 2 是防止下面 + 1 溢出
+        f[0] = 0;
+        for (int x : coins)
+            for (int c = x; c <= amount; ++c)
+                f[c] = Math.min(f[c], f[c - x] + 1);
+        int ans = f[amount];
         return ans < Integer.MAX_VALUE / 2 ? ans : -1;
     }
 
-    private int dfs(int i, int c) {
-        if (i < 0) return c == 0 ? 0 : Integer.MAX_VALUE / 2; // 除 2 是防止下面 + 1 溢出
-        if (memo[i][c] != -1) return memo[i][c];
-        if (c < coins[i]) return memo[i][c] = dfs(i - 1, c);
-        return memo[i][c] = Math.min(dfs(i - 1, c), dfs(i, c - coins[i]) + 1);
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        int N = Integer.MAX_VALUE >> 1;
+        Arrays.fill(dp, N);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == N ? -1 : dp[amount];
     }
 }

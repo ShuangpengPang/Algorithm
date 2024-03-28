40a529b99c8d6aa76742ae26740677970029fe9a
shuangpengpang <15913598312@163.com>
2024-03-28 15:41:21 +0800

    零钱兑换（322）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0322CoinChange.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0301_0400/Problem0322CoinChange.java
@@ -112,3 +112,25 @@ class Problem0322CoinChange0 {
         return memo[amount] = Math.min(N, ans + 1);
     }
 }
+
+class Problem0322CoinChange1 {
+    private int[] coins;
+    private int[][] memo;
+
+    public int coinChange(int[] coins, int amount) {
+        this.coins = coins;
+        int n = coins.length;
+        memo = new int[n][amount + 1];
+        for (int[] row : memo)
+            Arrays.fill(row, -1); // -1 表示没有访问过
+        int ans = dfs(n - 1, amount);
+        return ans < Integer.MAX_VALUE / 2 ? ans : -1;
+    }
+
+    private int dfs(int i, int c) {
+        if (i < 0) return c == 0 ? 0 : Integer.MAX_VALUE / 2; // 除 2 是防止下面 + 1 溢出
+        if (memo[i][c] != -1) return memo[i][c];
+        if (c < coins[i]) return memo[i][c] = dfs(i - 1, c);
+        return memo[i][c] = Math.min(dfs(i - 1, c), dfs(i, c - coins[i]) + 1);
+    }
+}

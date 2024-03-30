680ed95f4d018502140dd6354adb73c990440410
shuangpengpang <15913598312@163.com>
2024-03-31 00:37:59 +0800

    到目标字符串的最短距离（2515）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2501_2600/Problem2515ShortestDistanceToTargetStringInACircularArray.java
@@ -0,0 +1,33 @@
+package com.shuangpeng.Problem.p2501_2600;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2515ShortestDistanceToTargetStringInACircularArray（到目标字符串的最短距离）
+ * @date 2024/3/31 12:32 AM
+ */
+public class Problem2515ShortestDistanceToTargetStringInACircularArray {
+
+    public int closetTarget(String[] words, String target, int startIndex) {
+        int n = words.length, ans = n;
+        for (int i = startIndex, j = 0; j < n; i++, j++) {
+            if (i == n) {
+                i = 0;
+            }
+            if (words[i].equals(target)) {
+                ans = Math.min(ans, j);
+                break;
+            }
+        }
+        for (int i = startIndex, j = 0; j < n; i--, j++) {
+            if (i == -1) {
+                i = n - 1;
+            }
+            if (words[i].equals(target)) {
+                ans = Math.min(ans, j);
+                break;
+            }
+        }
+        return ans == n ? -1 : ans;
+    }
+}

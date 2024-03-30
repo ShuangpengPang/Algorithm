e5251818ac1963270169ccfdadd8e9cf64c77817
shuangpengpang <15913598312@163.com>
2024-03-30 13:39:17 +0800

    需要添加的硬币的最小数量（2952）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2952MinimumNumberOfCoinsToBeAdded.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2952MinimumNumberOfCoinsToBeAdded.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class Problem2952MinimumNumberOfCoinsToBeAdded {
 
-    public int minimumAddedCoins(int[] coins, int target) {
+    public int minimumAddedCoins0(int[] coins, int target) {
         Arrays.sort(coins);
         int n = coins.length, x = 1, cnt = 0;
         int i = 0;
@@ -20,6 +20,21 @@ public class Problem2952MinimumNumberOfCoinsToBeAdded {
                 x <<= 1;
             } else {
                 x += coins[i];
+                i++;
+            }
+        }
+        return cnt;
+    }
+
+    public int minimumAddedCoins(int[] coins, int target) {
+        Arrays.sort(coins);
+        int n = coins.length, cnt = 0;
+        for (int i = 0, j = 1; j <= target;) {
+            if (i >= n || coins[i] > j) {
+                cnt++;
+                j <<= 1;
+            } else {
+                j += coins[i];
                 i++;
             }
         }

c9d9e4de171d8c57a89f6cedfc1f505a16acfd27
shuangpengpang <15913598312@163.com>
2024-03-30 17:46:03 +0800

    数组异或操作（1486）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1486XorOperationInAnArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1401_1500/Problem1486XorOperationInAnArray.java
@@ -8,11 +8,21 @@ package com.shuangpeng.Problem.p1401_1500;
  */
 public class Problem1486XorOperationInAnArray {
 
-    public int xorOperation(int n, int start) {
+    public int xorOperation0(int n, int start) {
         int ans = 0, max = start + (n << 1);
         for (int i = start; i < max; i += 2) {
             ans ^= i;
         }
         return ans;
+    }
+
+    public int xorOperation(int n, int start) {
+        int s = start >> 1, e = s + n - 1;
+        return (getResult(e) ^ (s > 1 ? getResult(s - 1) : 0)) << 1 | ((start & 1) & (n & 1));
+    }
+
+    private int getResult(int x) {
+        int[] map = {x, 1, x + 1, 0};
+        return map[x & 3];
     }
 }

8f470489365b9e68a0a1d8b4f0638ff712af272c
shuangpengpang <15913598312@163.com>
2024-04-07 15:09:29 +0800

    美丽下标对的数目（2748）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2701_2800/Problem2748NumberOfBeautifulPairs.java
@@ -0,0 +1,41 @@
+package com.shuangpeng.Problem.p2701_2800;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2748NumberOfBeautifulPairs（美丽下标对的数目）
+ * @date 2024/4/7 12:05 PM
+ */
+public class Problem2748NumberOfBeautifulPairs {
+
+    public int countBeautifulPairs(int[] nums) {
+        int[] cnt = new int[10];
+        int ans = 0;
+        for (int num : nums) {
+            int d = num % 10;
+            for (int i = 1; i < 10; i++) {
+                if (gcd(d, i) == 1) {
+                    ans += cnt[i];
+                }
+            }
+            cnt[getFirstDigit(num)]++;
+        }
+        return ans;
+    }
+
+    private int getFirstDigit(int x) {
+        while (x > 9) {
+            x /= 10;
+        }
+        return x;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+}

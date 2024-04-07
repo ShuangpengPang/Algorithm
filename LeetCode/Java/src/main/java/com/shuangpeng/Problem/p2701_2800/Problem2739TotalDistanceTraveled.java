6fa34ecbace3d560187fc82fd200a383126b00aa
shuangpengpang <15913598312@163.com>
2024-04-07 15:59:14 +0800

    总行驶距离（2739）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2701_2800/Problem2739TotalDistanceTraveled.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.Problem.p2701_2800;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem2739TotalDistanceTraveled（总行驶距离）
+ * @date 2024/4/7 3:23 PM
+ */
+public class Problem2739TotalDistanceTraveled {
+
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        int ans = 0;
+        while (additionalTank > 0 && mainTank >= 5) {
+            int num = Math.min(additionalTank, mainTank / 5);
+            ans += num * 5;
+            mainTank -= num << 2;
+            additionalTank -= num;
+        }
+        return (ans + mainTank) * 10;
+    }
+}

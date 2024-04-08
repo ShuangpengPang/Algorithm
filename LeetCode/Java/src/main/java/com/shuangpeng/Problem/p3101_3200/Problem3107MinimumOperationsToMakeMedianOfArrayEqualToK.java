dc74b789c0b92f7035e8cf46c346121006453300
shuangpengpang <15913598312@163.com>
2024-04-08 19:16:55 +0800

    使数组中位数等于K的最少操作数（3107）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3101_3200/Problem3107MinimumOperationsToMakeMedianOfArrayEqualToK.java
@@ -0,0 +1,28 @@
+package com.shuangpeng.Problem.p3101_3200;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3107MinimumOperationsToMakeMedianOfArrayEqualToK（使数组中位数等于K的最少操作数）
+ * @date 2024/4/8 7:01 PM
+ */
+public class Problem3107MinimumOperationsToMakeMedianOfArrayEqualToK {
+
+    public long minOperationsToMakeMedianK(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length, m = n >> 1;
+        long ans = 0;
+        if (nums[m] > k) {
+            for (int i = m; i >= 0 && nums[i] > k; i--) {
+                ans += nums[i] - k;
+            }
+        } else {
+            for (int i = m; i < n && nums[i] < k; i++) {
+                ans += k - nums[i];
+            }
+        }
+        return ans;
+    }
+}

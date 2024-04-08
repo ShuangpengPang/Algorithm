6cb8cf731619c7b1e545db55959acaa9f0fbfa5e
shuangpengpang <15913598312@163.com>
2024-04-08 15:31:25 +0800

    换水问题II（3100）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3100WaterBottlesII.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3100WaterBottlesII（换水问题II）
+ * @date 2024/4/8 3:28 PM
+ */
+public class Problem3100WaterBottlesII {
+
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = 0, empty = 0;
+        while (numBottles + empty >= numExchange) {
+            ans += numBottles;
+            empty += numBottles - numExchange;
+            numBottles = 1;
+            numExchange++;
+        }
+        return ans + numBottles;
+    }
+}

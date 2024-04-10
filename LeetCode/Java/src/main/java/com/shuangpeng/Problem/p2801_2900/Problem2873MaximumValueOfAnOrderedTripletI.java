26e0fbf2db4761a5d9e6a70fe412b9d7806efea8
shuangpengpang <15913598312@163.com>
2024-04-10 16:19:06 +0800

    有序三元组中的最大值I（2873）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2873MaximumValueOfAnOrderedTripletI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2801_2900/Problem2873MaximumValueOfAnOrderedTripletI.java
@@ -8,6 +8,20 @@ package com.shuangpeng.Problem.p2801_2900;
  */
 public class Problem2873MaximumValueOfAnOrderedTripletI {
 
+    public long maximumTripletValue0(int[] nums) {
+        int n = nums.length;
+        int[] suffix = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = Math.max(nums[i], suffix[i + 1]);
+        }
+        long ans = 0;
+        for (int i = 0, max = 0; i < n - 1; i++) {
+            ans = Math.max(ans, (long) (max - nums[i]) * suffix[i + 1]);
+            max = Math.max(max, nums[i]);
+        }
+        return ans;
+    }
+
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
         long minValue = Math.min(nums[0], nums[1]), maxValue = Math.max(nums[0], nums[1]);

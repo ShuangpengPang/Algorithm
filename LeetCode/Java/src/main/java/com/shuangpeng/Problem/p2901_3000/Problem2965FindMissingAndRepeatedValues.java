5e2eaca54f01de3183c61d6a133d0aad93710386
shuangpengpang <15913598312@163.com>
2024-04-15 17:49:30 +0800

    找出缺失和重复的数字（2965）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2965FindMissingAndRepeatedValues.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p2901_3000/Problem2965FindMissingAndRepeatedValues.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p2901_3000;
  */
 public class Problem2965FindMissingAndRepeatedValues {
 
-    public int[] findMissingAndRepeatedValues(int[][] grid) {
+    public int[] findMissingAndRepeatedValues0(int[][] grid) {
         int n = grid.length, N = n * n;
         boolean[] visited = new boolean[N + 1];
         int a = 0, sum = 0;
@@ -23,5 +23,42 @@ public class Problem2965FindMissingAndRepeatedValues {
             }
         }
         return new int[]{a, a + ((N + 1) * N >> 1) - sum};
+    }
+
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length, N = n * n, xor = 0;
+        for (int[] r : grid) {
+            for (int num : r) {
+                xor ^= num;
+            }
+        }
+        xor ^= (N & 1) == 0 ? N : 1;
+        int lowBit = xor & -xor,sum1 = 0, sum2 = 0;
+        for (int[] r : grid) {
+            for (int num : r) {
+                if ((num & lowBit) == 0) {
+                    sum1 ^= num;
+                } else {
+                    sum2 ^= num;
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            if ((i & lowBit) == 0) {
+                sum1 ^= i;
+            } else {
+                sum2 ^= i;
+            }
+        }
+        for (int[] r : grid) {
+            for (int num : r) {
+                if (num == sum1) {
+                    return new int[]{sum1, sum2};
+                } else if (num == sum2) {
+                    return new int[]{sum2, sum1};
+                }
+            }
+        }
+        return new int[2];
     }
 }

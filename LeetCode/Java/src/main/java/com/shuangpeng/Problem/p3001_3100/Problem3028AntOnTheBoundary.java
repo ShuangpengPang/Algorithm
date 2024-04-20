2bbe7694e2574f545e1b3d1b3a6162469a0c2df8
shuangpengpang <15913598312@163.com>
2024-04-20 17:32:00 +0800

    边界上的蚂蚁（3028）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3028AntOnTheBoundary.java
@@ -0,0 +1,20 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3028AntOnTheBoundary（边界上的蚂蚁）
+ * @date 2024/4/20 5:11 PM
+ */
+public class Problem3028AntOnTheBoundary {
+
+    public int returnToBoundaryCount(int[] nums) {
+        int ans = 0;
+        for (int n = nums.length, i = 1, s = nums[0]; i < n; i++) {
+            if ((s += nums[i]) == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}

5f0c26959ad11377c0f70600bb19c3a35dee1297
shuangpengpang <15913598312@163.com>
2024-04-22 19:24:45 +0800

    字符串及其反转中是否存在同一子字符串（3083）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3083ExistenceOfASubstringInAStringAndItsReverse.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3083ExistenceOfASubstringInAStringAndItsReverse.java
@@ -8,14 +8,28 @@ package com.shuangpeng.Problem.p3001_3100;
  */
 public class Problem3083ExistenceOfASubstringInAStringAndItsReverse {
 
-    public boolean isSubstringPresent(String s) {
+    public boolean isSubstringPresent0(String s) {
         boolean[][] set = new boolean[26][26];
         char[] cs = s.toCharArray();
         for (int n = cs.length, i = 1; i < n; i++) {
-            if (cs[i - 1] == cs[i] || set[cs[i] - 'a'][cs[i - 1] - 'a']) {
+            set[cs[i - 1] - 'a'][cs[i] - 'a'] = true;
+            if (set[cs[i] - 'a'][cs[i - 1] - 'a']) {
                 return true;
             }
-            set[cs[i - 1] - 'a'][cs[i] - 'a'] = true;
+        }
+        return false;
+    }
+
+    public boolean isSubstringPresent(String s) {
+        char[] cs = s.toCharArray();
+        int n = cs.length, N = Integer.MAX_VALUE;
+        int[] set = new int[26];
+        for (int i = 1; i < n; i++) {
+            int x = cs[i] - 'a', y = cs[i - 1] - 'a';
+            set[x] |= 1 << y;
+            if ((set[y] >> x & 1) == 1) {
+                return true;
+            }
         }
         return false;
     }

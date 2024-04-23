b4bf010282385580fe60d04dfcf7c0c2289c282b
shuangpengpang <15913598312@163.com>
2024-04-23 20:13:36 +0800

    输入单词需要的最少按键次数I（3014）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3001_3100/Problem3014MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,26 @@
+package com.shuangpeng.Problem.p3001_3100;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3014MinimumNumberOfPushesToTypeWordI（输入单词需要的最少按键次数I）
+ * @date 2024/4/23 3:51 PM
+ */
+public class Problem3014MinimumNumberOfPushesToTypeWordI {
+
+    public int minimumPushes(String word) {
+        char[] cs = word.toCharArray();
+        int[] cnt = new int[26];
+        for (char c : cs) {
+            cnt[c - 'a']++;
+        }
+        Arrays.sort(cnt);
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans += cnt[i] * ((26 - i + 7) / 8);
+        }
+        return ans;
+    }
+}

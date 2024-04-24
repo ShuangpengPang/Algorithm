9e6de92466e3cdf79c3634f6149d40ca5735403d
shuangpengpang <15913598312@163.com>
2024-04-24 14:36:14 +0800

    替换字符可以得到的最晚时间（3114）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3101_3200/Problem3114LatestTimeYouCanObtainAfterReplacingCharacters.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3101_3200/Problem3114LatestTimeYouCanObtainAfterReplacingCharacters.java
@@ -8,6 +8,21 @@ package com.shuangpeng.Problem.p3101_3200;
  */
 public class Problem3114LatestTimeYouCanObtainAfterReplacingCharacters {
 
+    public String findLatestTime0(String s) {
+        char[] cs = s.toCharArray();
+        for (int h = 11; ; h--) {
+            if (cs[0] != '?' && cs[0] - '0' != h / 10 || cs[1] != '?' && cs[1] - '0' != h % 10) {
+                continue;
+            }
+            for (int m = 59; m >= 0; m--) {
+                if (cs[3] != '?' && cs[3] - '0' != m / 10 || cs[4] != '?' && cs[4] - '0' != m % 10) {
+                    continue;
+                }
+                return String.format("%02d:%02d", h, m);
+            }
+        }
+    }
+
     public String findLatestTime(String s) {
         char[] cs = s.toCharArray();
         if (cs[0] == '?') {

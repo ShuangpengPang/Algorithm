7f15c0156ef0e082ee3b3de49ef742ae925106c4
shuangpengpang <15913598312@163.com>
2024-04-25 12:12:09 +0800

    LCP 18 早餐组合

--- LeetCode/Java/src/main/java/com/shuangpeng/lcp/LCP18BreakFastNumber.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcp/LCP18BreakFastNumber.java
@@ -10,6 +10,24 @@ import java.util.Arrays;
  */
 public class LCP18BreakFastNumber {
 
+    public int breakfastNumber0(int[] staple, int[] drinks, int x) {
+        Arrays.sort(drinks);
+        int N = (int) 1e9 + 7, ans = 0;
+        for (int s : staple) {
+            int d = x - s, left = 0, right = drinks.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left >> 1);
+                if (drinks[mid] <= d) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans = (ans + left) % N;
+        }
+        return ans;
+    }
+
     public int breakfastNumber(int[] staple, int[] drinks, int x) {
         Arrays.sort(staple);
         Arrays.sort(drinks);

51959eab04bc3cdf44b23b356d62418ee7f97a52
shuangpengpang <15913598312@163.com>
2024-04-29 11:55:27 +0800

    LCR 019 验证回文串II

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR019ValidPalindrome.java
@@ -0,0 +1,29 @@
+package com.shuangpeng.lcr;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR019ValidPalindrome（验证回文串II）
+ * @date 2024/4/29 11:44 AM
+ */
+public class LCR019ValidPalindrome {
+
+    public boolean validPalindrome(String s) {
+        char[] cs = s.toCharArray();
+        for (int i = 0, j = cs.length - 1; i < j; i++, j--) {
+            if (cs[i] != cs[j]) {
+                return check(cs, i + 1, j) || check(cs, i, j - 1);
+            }
+        }
+        return true;
+    }
+
+    private boolean check(char[] cs, int s, int e) {
+        for (int i = s, j = e; i < j; i++, j--) {
+            if (cs[i] != cs[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

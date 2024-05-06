d8a306b7c4bc6fc1c6c61c3e3bea293ca5d13714
shuangpengpang <15913598312@163.com>
2024-05-06 14:19:05 +0800

    LCR 012 寻找数组的中心下标

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR012PivotIndex.java
@@ -0,0 +1,25 @@
+package com.shuangpeng.lcr;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR012PivotIndex（寻找数组的中心下标）
+ * @date 2024/5/6 12:16 PM
+ */
+public class LCR012PivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int sum = 0, n = nums.length;
+        for (int num : nums) {
+            sum += num;
+        }
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            if (left << 1 == sum - nums[i]) {
+                return i;
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+}

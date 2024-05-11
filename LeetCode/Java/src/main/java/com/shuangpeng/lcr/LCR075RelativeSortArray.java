d07cbcd5184edb96b22e0fc76d3900175249e7f5
shuangpengpang <15913598312@163.com>
2024-05-11 15:48:53 +0800

    LCR 075 数组的相对排序

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR075RelativeSortArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR075RelativeSortArray.java
@@ -13,7 +13,7 @@ import java.util.Map;
  */
 public class LCR075RelativeSortArray {
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public int[] relativeSortArray0(int[] arr1, int[] arr2) {
         int n1 = arr1.length, n2 = arr2.length;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n2; i++) {
@@ -27,5 +27,30 @@ public class LCR075RelativeSortArray {
             ans[i] = arr[i];
         }
         return ans;
+    }
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int maxValue = 0;
+        for (int num : arr1) {
+            maxValue = Math.max(maxValue, num);
+        }
+        int[] count = new int[maxValue + 1];
+        for (int num : arr1) {
+            count[num]++;
+        }
+        int index = 0;
+        for (int num : arr2) {
+            while (count[num] > 0) {
+                arr1[index++] = num;
+                count[num]--;
+            }
+        }
+        for (int i = 0; i <= maxValue; i++) {
+            while (count[i] > 0) {
+                arr1[index++] = i;
+                count[i]--;
+            }
+        }
+        return arr1;
     }
 }

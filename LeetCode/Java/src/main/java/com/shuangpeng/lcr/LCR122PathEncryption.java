efee5ff247ac8b1dbd2e6b57bc81b127c71a646b
shuangpengpang <15913598312@163.com>
2024-05-12 19:58:44 +0800

    LCR 122 路径加密

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR122PathEncryption.java
@@ -0,0 +1,20 @@
+package com.shuangpeng.lcr;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR122PathEncryption（路径加密）
+ * @date 2024/5/12 7:57 PM
+ */
+public class LCR122PathEncryption {
+
+    public String pathEncryption(String path) {
+        char[] cs = path.toCharArray();
+        for (int n = cs.length, i = 0; i < n; i++) {
+            if (cs[i] == '.') {
+                cs[i] = ' ';
+            }
+        }
+        return new String(cs);
+    }
+}

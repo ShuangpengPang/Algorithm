613bafb590a0d1589db59f54512fc1e31f384be5
shuangpengpang <15913598312@163.com>
2024-05-13 18:43:47 +0800

    LCR 125 图书整理II

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR125BookManagementII.java
@@ -0,0 +1,40 @@
+package com.shuangpeng.lcr;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR125BookManagementII（图书整理II）
+ * @date 2024/5/13 6:22 PM
+ */
+public class LCR125BookManagementII {
+}
+
+class CQueue {
+
+    Queue<Integer> q;
+
+    public CQueue() {
+        q = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        q.offer(value);
+    }
+
+    public int deleteHead() {
+        if (q.isEmpty()) {
+            return -1;
+        }
+        return q.poll();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */

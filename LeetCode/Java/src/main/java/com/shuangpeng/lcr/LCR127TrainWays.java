193285cb72026c2b3bb4563d69ba70b161db3b00
shuangpengpang <15913598312@163.com>
2024-05-13 18:20:59 +0800

    LCR 127 跳跃训练

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR127TrainWays.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR127TrainWays.java
@@ -8,7 +8,7 @@ package com.shuangpeng.lcr;
  */
 public class LCR127TrainWays {
 
-    public int trainWays(int num) {
+    public int trainWays0(int num) {
         int N = (int) 1e9 + 7;
         int last1 = 0, last = 1, t = 1;
         for (int i = 0; i < num; i++) {
@@ -17,5 +17,41 @@ public class LCR127TrainWays {
             last1 = t;
         }
         return last;
+    }
+
+    public int trainWays(int num) {
+        int[][] a = {{0, 1}};
+        int[][] b = {{0, 1}, {1, 1}}, c = matrixExp(b, num);
+        int[][] ans = matrixMultiple(a, c);
+        return ans[0][1];
+    }
+
+    private int[][] matrixExp(int[][] matrix, int e) {
+        int n = matrix.length;
+        int[][] ans = new int[n][n];
+        for (int i = 0;i < n; i++) {
+            ans[i][i] = 1;
+        }
+        while (e != 0) {
+            if ((e & 1) == 1) {
+                ans = matrixMultiple(ans, matrix);
+            }
+            e >>= 1;
+            matrix = matrixMultiple(matrix, matrix);
+        }
+        return ans;
+    }
+
+    private int[][] matrixMultiple(int[][] a, int [][] b) {
+        int m = a.length, n = b[0].length, N = (int) 1e9 + 7;
+        int[][] c = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < b.length; j++) {
+                for (int k = 0; k < n; k++) {
+                    c[i][k] = (int) ((c[i][k] + (long) a[i][j] * b[j][k]) % N);
+                }
+            }
+        }
+        return c;
     }
 }

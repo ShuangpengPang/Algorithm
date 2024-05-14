ccfd8e15bcaad2d3c6673f31101624ca5b027195
shuangpengpang <15913598312@163.com>
2024-05-14 17:00:45 +0800

    LCR 101 分割等和子集

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR101CanPartition.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR101CanPartition.java
@@ -27,3 +27,41 @@ public class LCR101CanPartition {
         return dp[target];
     }
 }
+
+class LCR101CanPartition0 {
+
+    boolean[] vis;
+    public boolean canPartition(int[] nums) {
+        //背包问题 - 从物品选择出价值是sum / 2的商品
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        if(sum % 2 == 1){
+            return false;
+        }
+        int n = nums.length;
+        int target = sum / 2;
+        vis = new boolean[target + 1];
+        return dfs(nums,0, 0, target);
+    }
+    public boolean dfs(int[] nums, int idx, int cur, int target){
+        if(cur == target){
+            //商品重量 = target
+            return true;
+        }
+        if(cur > target || vis[cur]){
+            return false;
+        }
+        vis[cur] = true;
+        for(int i = idx; i < nums.length; i++){
+            cur += nums[i];
+            if(dfs(nums, i + 1, cur, target)){
+                return true;
+            }
+            cur -= nums[i];
+        }
+        return false;
+    }
+
+}

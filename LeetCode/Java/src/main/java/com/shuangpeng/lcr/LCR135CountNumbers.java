ce9dbcd22f69df69b779c4a9eaf7cbd3edb39bb1
shuangpengpang <15913598312@163.com>
2024-05-14 16:43:16 +0800

    LCR 135 报数

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR135CountNumbers.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR135CountNumbers.java
@@ -26,3 +26,32 @@ public class LCR135CountNumbers {
         return ans;
     }
 }
+
+class LCR135CountNumbers0 {
+
+    int[] res;
+    int nine = 0, count = 0, start, cnt;
+    char[] num, loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public int[] countNumbers(int cnt) {
+        this.cnt = cnt;
+        res = new int[(int)Math.pow(10, cnt) - 1];
+        num = new char[cnt];
+        start = cnt - 1;
+        dfs(0);
+        return res;
+    }
+    void dfs(int x) {
+        if(x == cnt) {
+            String s = String.valueOf(num).substring(start);
+            if(!s.equals("0")) res[count++] = Integer.parseInt(s);
+            if(cnt - start == nine) start--;
+            return;
+        }
+        for(char i : loop) {
+            if(i == '9') nine++;
+            num[x] = i;
+            dfs(x + 1);
+        }
+        nine--;
+    }
+}

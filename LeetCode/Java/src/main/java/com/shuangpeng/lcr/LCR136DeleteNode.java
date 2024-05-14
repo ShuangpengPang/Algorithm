c393f952bcbacf9d851fb8cd83a5db0c7e83bcd3
shuangpengpang <15913598312@163.com>
2024-05-14 12:19:12 +0800

    LCR 136 删除链表的节点

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR136DeleteNode.java
@@ -0,0 +1,23 @@
+package com.shuangpeng.lcr;
+
+import com.shuangpeng.common.ListNode;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR136DeleteNode（删除链表的节点）
+ * @date 2024/5/14 12:11 PM
+ */
+public class LCR136DeleteNode {
+
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head), node = dummy;
+        while (node.next != null && node.next.val != val) {
+            node = node.next;
+        }
+        if (node.next != null) {
+            node.next = node.next.next;
+        }
+        return dummy.next;
+    }
+}

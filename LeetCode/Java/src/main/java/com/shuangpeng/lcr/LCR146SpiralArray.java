3dbca24d207836ebe51f59841beca0c316cef937
shuangpengpang <15913598312@163.com>
2024-05-16 15:14:55 +0800

    LCR 146 螺旋遍历二维数组

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR146SpiralArray.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR146SpiralArray.java
@@ -41,3 +41,29 @@ public class LCR146SpiralArray {
         return ans;
     }
 }
+
+class LCR146SpiralArray0 {
+
+    private static int[] dirs = {0, 1, 0, -1, 0};
+
+    public int[] spiralArray(int[][] array) {
+        if (array.length == 0 || array[0].length == 0) {
+            return new int[0];
+        }
+        int m = array.length, n = array[0].length, total = m * n;
+        int[] ans = new int[total];
+        boolean[][] visited = new boolean[m][n];
+        int r = 0, c = 0, d = 0;
+        for (int i = 0; i < total; i++) {
+            ans[i] = array[r][c];
+            visited[r][c] = true;
+            int nr = r + dirs[d], nc = c + dirs[d + 1];
+            if (nr < 0 || nr == m || nc < 0 || nc == n || visited[nr][nc]) {
+                d = (d + 1) % 4;
+            }
+            r += dirs[d];
+            c += dirs[d + 1];
+        }
+        return ans;
+    }
+}

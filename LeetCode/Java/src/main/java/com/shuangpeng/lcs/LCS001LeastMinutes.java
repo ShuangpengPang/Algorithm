3e1335dbfc86770af8c6176e86ac3e873c21ddae
shuangpengpang <15913598312@163.com>
2024-05-20 15:57:11 +0800

    LCS 001 下载插件

--- LeetCode/Java/src/main/java/com/shuangpeng/lcs/LCS001LeastMinutes.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcs/LCS001LeastMinutes.java
@@ -8,11 +8,21 @@ package com.shuangpeng.lcs;
  */
 public class LCS001LeastMinutes {
 
-    public int leastMinutes(int n) {
+    public int leastMinutes0(int n) {
         int ans = n;
         for (int i = 2, cnt = 1; i <= n; i <<= 1, cnt++) {
             ans = Math.min(ans, cnt + (n + i - 1) / i);
         }
         return ans;
+    }
+
+    public int leastMinutes(int n) {
+        int x = (int) (Math.log(n * Math.log(2)) / Math.log(2));
+        return Math.min(getCount(n, x), getCount(n, x + 1));
+    }
+
+    private int getCount(int n, int x) {
+        int p = (int) Math.pow(2, x);
+        return x + (n + p - 1) / p;
     }
 }

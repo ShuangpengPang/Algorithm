8d61625a310bccfc58b85badd5518b63f5023252
shuangpengpang <15913598312@163.com>
2024-05-22 12:02:49 +0800

    LCS 002 完成一半题目

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcs/LCS002HalfQuestions.java
@@ -0,0 +1,34 @@
+package com.shuangpeng.lcs;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCS002HalfQuestions（完成一半题目）
+ * @date 2024/5/22 11:02 AM
+ */
+public class LCS002HalfQuestions {
+
+    public int halfQuestions(int[] questions) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int q : questions) {
+            map.merge(q, 1, Integer::sum);
+        }
+        int n = map.size(), index = 0;
+        int[] cnt = new int[n];
+        for (int c : map.values()) {
+            cnt[index++] = c;
+        }
+        Arrays.sort(cnt);
+        int m = questions.length >> 1, types = 0;
+        int p = n - 1;
+        while (m > 0) {
+            types++;
+            m -= cnt[p--];
+        }
+        return types;
+    }
+}

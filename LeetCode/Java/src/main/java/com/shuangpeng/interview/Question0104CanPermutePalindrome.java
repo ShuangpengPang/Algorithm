f3ebad84343903c1129cc89fb65f73c6e81c5965
shuangpengpang <15913598312@163.com>
2024-05-24 17:29:36 +0800

    面试题 01.04 回文排列

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0104CanPermutePalindrome.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0104CanPermutePalindrome.java
@@ -20,7 +20,7 @@ public class Question0104CanPermutePalindrome {
 //        return (mask & (mask - 1)) == 0;
 //    }
 
-    public boolean canPermutePalindrome(String s) {
+    public boolean canPermutePalindrome0(String s) {
         Map<Character, Integer> map = new HashMap<>();
         int cnt = 0;
         for (char c : s.toCharArray()) {
@@ -28,5 +28,21 @@ public class Question0104CanPermutePalindrome {
             cnt += (map.get(c) << 1) - 1;
         }
         return cnt < 2;
+    }
+
+    public boolean canPermutePalindrome(String s) {
+        long high = 0, low = 0;
+        for (char c : s.toCharArray()) {
+            if (c < 64) {
+                low ^= 1L << c;
+            } else {
+                high ^= 1L << c - 64;
+            }
+        }
+        if (high == low) {
+            return low == 0;
+        }
+        low |= high;
+        return low == (low & -low);
     }
 }

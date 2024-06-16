c08d7684a7e42a6d744b620b4d35cb8caf7f3f91
shuangpengpang <15913598312@163.com>
2024-06-16 15:27:48 +0800

    LCR 022. 环形链表II

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR022DetectCycle.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR022DetectCycle.java
@@ -10,6 +10,26 @@ import com.shuangpeng.common.ListNode;
  */
 public class LCR022DetectCycle {
 
+    public ListNode detectCycle0(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        do {
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast && fast != null && fast.next != null);
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
     public ListNode detectCycle(ListNode head) {
         if (head == null) {
             return null;

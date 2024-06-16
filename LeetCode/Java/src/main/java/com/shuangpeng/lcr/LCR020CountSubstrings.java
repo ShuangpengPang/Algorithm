80cb1fef0d353b83cdc9250f44913abef60cf0b0
shuangpengpang <15913598312@163.com>
2024-06-16 18:48:07 +0800

    LCR 020. 回文子串

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR020CountSubstrings.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR020CountSubstrings.java
@@ -8,7 +8,7 @@ package com.shuangpeng.lcr;
  */
 public class LCR020CountSubstrings {
 
-    public int countSubstrings(String s) {
+    public int countSubstrings0(String s) {
         char[] cs = s.toCharArray();
         int n = cs.length, ans = n;
         int[][] dp = new int[n][n];
@@ -19,6 +19,20 @@ public class LCR020CountSubstrings {
             for (int j = i + 2; j < n; j++) {
                 dp[i][j] = cs[i] == cs[j] ? dp[i + 1][j - 1] : 0;
                 ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+
+    public int countSubstrings(String s) {
+        char[] cs = s.toCharArray();
+        int n = cs.length, ans = 0;
+        for (int i = 0; i < (n << 1) - 1; i++) {
+            int l = i >> 1, r = i + 1 >> 1;
+            while (l >= 0 && r < n && cs[l] == cs[r]) {
+                ans++;
+                l--;
+                r++;
             }
         }
         return ans;

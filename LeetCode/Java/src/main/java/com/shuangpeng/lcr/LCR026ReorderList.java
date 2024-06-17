b57070e5cab4c6ff4539320f6a6a46ee5bd49432
shuangpengpang <15913598312@163.com>
2024-06-17 16:27:26 +0800

    LCR 026. 重排链表

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR026ReorderList.java
@@ -0,0 +1,39 @@
+package com.shuangpeng.lcr;
+
+import com.shuangpeng.common.ListNode;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR026ReorderList（重排链表）
+ * @date 2024/6/17 4:08 PM
+ */
+public class LCR026ReorderList {
+
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null, node = slow.next;
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        slow.next = null;
+        ListNode node1 = head, node2 = prev;
+        while (node2 != null)  {
+            ListNode next1 = node1.next, next2 = node2.next;
+            node1.next = node2;
+            node2.next = next1;
+            node1 = next1;
+            node2 = next2;
+        }
+    }
+}

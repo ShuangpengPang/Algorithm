eb5a6deffe9998d324bcc56f1bb18ececf13ec46
shuangpengpang <15913598312@163.com>
2024-06-17 15:17:31 +0800

    LCR 028. 扁平化多级双向链表

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/LCR028Flatten.java
@@ -0,0 +1,43 @@
+package com.shuangpeng.lcr;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR028Flatten（扁平化多级双向链表）
+ * @date 2024/6/17 2:18 PM
+ */
+public class LCR028Flatten {
+
+    private Node current;
+
+    public Node flatten(Node head) {
+        if (head == null) {
+            return null;
+        }
+        current = new Node();
+        dfs(head);
+        head.prev = null;
+        return head;
+    }
+
+    private void dfs(Node head) {
+        if (head == null) {
+            return;
+        }
+        current.next = head;
+        current.child = null;
+        head.prev = current;
+        current = head;
+        Node child = head.child, next = head.next;
+        dfs(child);
+        dfs(next);
+    }
+}
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};

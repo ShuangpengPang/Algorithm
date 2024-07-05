9f82496beb3bab87c4b1835d1fdcafe0e9e9132c
shuangpengpang <15913598312@163.com>
2024-07-05 15:45:59 +0800

    LCR 096. 交错字符串

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/p001_100/LCR096IsInterleave.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p001_100/LCR096IsInterleave.java
@@ -8,7 +8,7 @@ package com.shuangpeng.lcr.p001_100;
  */
 public class LCR096IsInterleave {
 
-    public boolean isInterleave(String s1, String s2, String s3) {
+    public boolean isInterleave0(String s1, String s2, String s3) {
         int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
         if (n1 + n2 != n3) {
             return false;
@@ -26,5 +26,26 @@ public class LCR096IsInterleave {
             }
         }
         return dp[n1 & 1][n2];
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
+        if (n1 + n2 != n3) {
+            return false;
+        }
+        char[] cs1 = s1.toCharArray(), cs2 = s2.toCharArray(), cs3 = s3.toCharArray();
+        boolean[] dp = new boolean[n2 + 1];
+        dp[0] = true;
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0, k = i - 1; j <= n2; j++, k++) {
+                if (i > 0) {
+                    dp[j] = dp[j] && cs1[i - 1] == cs3[k];
+                }
+                if (j > 0) {
+                    dp[j] = dp[j] || dp[j - 1] && cs2[j - 1] == cs3[k];
+                }
+            }
+        }
+        return dp[n2];
     }
 }

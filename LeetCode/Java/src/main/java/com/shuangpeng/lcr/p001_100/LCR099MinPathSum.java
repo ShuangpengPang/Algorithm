f81f22b89329da0d501fe1121d44b53fdd362341
shuangpengpang <15913598312@163.com>
2024-07-06 11:27:15 +0800

    LCR 099. 最小路径和

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p001_100/LCR099MinPathSum.java
@@ -0,0 +1,26 @@
+package com.shuangpeng.lcr.p001_100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR099MinPathSum
+ * @date 2024/7/6 11:13 AM
+ */
+public class LCR099MinPathSum {
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = grid[0][i] + dp[i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+}

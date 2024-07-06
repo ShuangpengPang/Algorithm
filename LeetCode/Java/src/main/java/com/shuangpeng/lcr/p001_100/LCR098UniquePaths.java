8565a759db71cb83f78b1691443ce1f365972552
shuangpengpang <15913598312@163.com>
2024-07-06 12:06:39 +0800

    LCR 098. 不同路径

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p001_100/LCR098UniquePaths.java
@@ -0,0 +1,21 @@
+package com.shuangpeng.lcr.p001_100;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR098UniquePaths（不同路径）
+ * @date 2024/7/6 11:50 AM
+ */
+public class LCR098UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}

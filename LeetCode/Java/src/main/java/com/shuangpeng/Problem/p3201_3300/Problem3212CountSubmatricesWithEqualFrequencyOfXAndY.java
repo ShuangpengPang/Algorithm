32e415277139b63ad7b3d7457b5e23ded21184a5
shuangpengpang <15913598312@163.com>
2024-07-18 20:48:42 +0800

    统计X和Y频数相等的子矩阵数量（3212）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3212CountSubmatricesWithEqualFrequencyOfXAndY.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3212CountSubmatricesWithEqualFrequencyOfXAndY.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p3201_3300;
  */
 public class Problem3212CountSubmatricesWithEqualFrequencyOfXAndY {
 
-    public int numberOfSubmatrices(char[][] grid) {
+    public int numberOfSubmatrices0(char[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[][] dp = new int[n][2];
         int ans = 0;
@@ -23,6 +23,25 @@ public class Problem3212CountSubmatricesWithEqualFrequencyOfXAndY {
                 dp[i][0] += x;
                 dp[i][1] += y;
                 if (dp[i][0] > 0 && dp[i][0] == dp[i][1]) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int numberOfSubmatrices(char[][] grid) {
+        int n = grid[0].length, ans = 0;
+        int[][] dp = new int[n][2];
+        for (char[] row : grid) {
+            int sum0 = 0, sum1 = 0;
+            for (int i = 0; i < n; i++) {
+                if (row[i] != '.') {
+                    dp[i][row[i] & 1]++;
+                }
+                sum0 += dp[i][0];
+                sum1 += dp[i][1];
+                if (sum0 > 0 && sum0 == sum1) {
                     ans++;
                 }
             }

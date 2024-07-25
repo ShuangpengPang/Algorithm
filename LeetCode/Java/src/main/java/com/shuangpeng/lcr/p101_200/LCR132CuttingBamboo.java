d03407eef0f2709b993b46e79f421bcb010d4f59
shuangpengpang <15913598312@163.com>
2024-07-25 19:35:40 +0800

    LCR 132. 砍竹子 II

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR132CuttingBamboo.java
@@ -0,0 +1,29 @@
+package com.shuangpeng.lcr.p101_200;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR132CuttingBamboo（LCR 132. 砍竹子 II）
+ * @date 2024/7/25 7:04 PM
+ */
+public class LCR132CuttingBamboo {
+
+    public int cuttingBamboo(int bamboo_len) {
+        if (bamboo_len == 2) {
+            return 1;
+        }
+        if (bamboo_len == 3) {
+            return 2;
+        }
+        int p = (bamboo_len - 2) / 3;
+        long ans = 1, N = (long) 1e9 + 7, x = 3;
+        while (p != 0) {
+            if ((p & 1) == 1) {
+                ans = (ans * x) % N;
+            }
+            x = (x * x) % N;
+            p >>= 1;
+        }
+        return (int) (ans * ((bamboo_len - 2) % 3 + 2) % N);
+    }
+}

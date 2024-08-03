9095f18a09766b3e6581dca6885dfe50304819f2
shuangpengpang <15913598312@163.com>
2024-08-03 13:37:53 +0800

    LCR 155. 将二叉搜索树转化为排序的双向链表

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR155TreeToDoublyList.java
@@ -0,0 +1,45 @@
+package com.shuangpeng.lcr.p101_200;
+
+import com.shuangpeng.common.Node;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR155TreeToDoublyList（LCR 155. 将二叉搜索树转化为排序的双向链表）
+ * @date 2024/8/3 10:32 AM
+ */
+public class LCR155TreeToDoublyList {
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node node = root, last = root;
+        Node dummy = new Node(), prev = dummy;
+        while (node != null) {
+            last = node;
+            if (node.left != null) {
+                Node cur = node.left;
+                while (cur.right != null && cur.right != node) {
+                    cur = cur.right;
+                }
+                if (cur.right == null) {
+                    cur.right = node;
+                    node = node.left;
+                } else {
+                    prev = node;
+                    node.left = cur;
+                    node = node.right;
+                }
+            } else {
+                prev.right = node;
+                node.left = prev;
+                prev = node;
+                node = node.right;
+            }
+        }
+        last.right = dummy.right;
+        dummy.right.left = last;
+        return dummy.right;
+    }
+}

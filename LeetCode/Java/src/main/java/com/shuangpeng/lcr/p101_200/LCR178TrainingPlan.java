db1864bccbd18ae96d8d17fae5983e5bdf6511ea
shuangpengpang <15913598312@163.com>
2024-08-11 16:34:11 +0800

    LCR 178. 训练计划 VI

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR178TrainingPlan.java
@@ -0,0 +1,26 @@
+package com.shuangpeng.lcr.p101_200;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR178TrainingPlan（LCR 178. 训练计划 VI）
+ * @date 2024/8/11 4:32 PM
+ */
+public class LCR178TrainingPlan {
+
+    public int trainingPlan(int[] actions) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : actions) {
+            map.merge(a, 1, Integer::sum);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}

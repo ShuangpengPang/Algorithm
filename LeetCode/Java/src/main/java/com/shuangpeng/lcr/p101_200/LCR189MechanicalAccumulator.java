616f18df840c9001fde3b88470ca3ba6fd4efc1f
shuangpengpang <15913598312@163.com>
2024-08-16 12:23:09 +0800

    LCR 189. 设计机械累加器

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR189MechanicalAccumulator.java
@@ -0,0 +1,85 @@
+package com.shuangpeng.lcr.p101_200;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: LCR189MechanicalAccumulator（LCR 189. 设计机械累加器）
+ * @date 2024/8/16 10:59 AM
+ */
+public class LCR189MechanicalAccumulator {
+
+    public int mechanicalAccumulator(int target) {
+        return dfs(target);
+    }
+
+    private int dfs(int target) {
+        boolean flag = target > 0 && (target += dfs(target - 1)) > 0;
+        return target;
+    }
+}
+
+class LCR189MechanicalAccumulator0 {
+
+    public int mechanicalAccumulator(int target) {
+        int ans = 0, A = target, B = target + 1;
+        boolean flag;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        flag = ((B & 1) > 0) && (ans += A) > 0;
+        A <<= 1;
+        B >>= 1;
+
+        return ans >> 1;
+    }
+}

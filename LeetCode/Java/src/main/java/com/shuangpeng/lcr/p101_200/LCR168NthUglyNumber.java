805fb057f66fe3ee1ddc088fc7a13939b7c3d16b
shuangpengpang <15913598312@163.com>
2024-08-17 11:46:15 +0800

    LCR 168. 丑数

--- LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR168NthUglyNumber.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/lcr/p101_200/LCR168NthUglyNumber.java
@@ -10,7 +10,7 @@ import java.util.PriorityQueue;
  */
 public class LCR168NthUglyNumber {
 
-    public int nthUglyNumber(int n) {
+    public int nthUglyNumber0(int n) {
         PriorityQueue<Long> pq = new PriorityQueue<>();
         pq.offer(1L);
         long ans = 0;
@@ -24,5 +24,24 @@ public class LCR168NthUglyNumber {
             pq.offer(ans * 5);
         }
         return (int) ans;
+    }
+
+    public int nthUglyNumber(int n) {
+        int[] p = {0, 0, 0}, factors = {2, 3, 5};
+        long[] dp = new long[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            long num = Long.MAX_VALUE;
+            for (int j = 0; j < 3; j++) {
+                num = Math.min(num, dp[p[j]] * factors[j]);
+            }
+            for (int j = 0; j < 3; j++) {
+                if (dp[p[j]] * factors[j] == num) {
+                    p[j]++;
+                }
+            }
+            dp[i] = num;
+        }
+        return (int) dp[n - 1];
     }
 }

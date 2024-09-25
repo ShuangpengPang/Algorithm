0c65b98aa616deb7b510ed3911f1062fb60cb653
shuangpengpang <15913598312@163.com>
2024-09-25 15:07:28 +0800

    面试题 08.08. 有重复字符串的排列组合

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0808Permutation.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0808Permutation.java
@@ -1,6 +1,7 @@
 package com.shuangpeng.interview;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,6 +34,33 @@ public class Question0808Permutation {
                 cnt[i]--;
                 dfs(cs, cnt, index + 1, ans);
                 cnt[i]++;
+            }
+        }
+    }
+}
+
+class Question0808Permutation0 {
+
+    public String[] permutation(String S) {
+        char[] cs = S.toCharArray();
+        Arrays.sort(cs);
+        List<String> ans = new ArrayList<>();
+        dfs(cs, new char[cs.length], 0, new boolean[cs.length], ans);
+        return ans.toArray(new String[ans.size()]);
+    }
+
+    private void dfs(char[] origin, char[] cs, int index, boolean[] visited, List<String> ans) {
+        int n = cs.length;
+        if (index == n) {
+            ans.add(new String(cs));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && (i == 0 || visited[i - 1] || origin[i] != origin[i - 1])) {
+                visited[i] = true;
+                cs[index] = origin[i];
+                dfs(origin, cs, index + 1, visited, ans);
+                visited[i] = false;
             }
         }
     }

c1fc38252bbfbe67aff626815e62125ea124c60e
shuangpengpang <15913598312@163.com>
2024-10-10 15:24:26 +0800

    面试题 08.14. 布尔运算

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0814CountEval.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question0814CountEval.java
@@ -8,7 +8,7 @@ package com.shuangpeng.interview;
  */
 public class Question0814CountEval {
 
-    public int countEval(String s, int result) {
+    public int countEval0(String s, int result) {
         int n = s.length();
         int[] ans = dfs(s.toCharArray(), 0, n - 1, new int[n][n][]);
         return result == 0 ? ans[0] : ans[1];
@@ -47,6 +47,43 @@ public class Question0814CountEval {
             }
         }
         return memo[start][end] = ans;
+    }
+
+    public int countEval(String s, int result) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        char[] cs = s.toCharArray();
+        int[][][] dp = new int[n][n][];
+        for (int i = n - 1; i >= 0; i--) {
+            int val = cs[i] - '0';
+            dp[i][i] = new int[2];
+            dp[i][i][0] = 1 ^ val;
+            dp[i][i][1] = 0 ^ val;
+            for (int j = i + 2; j < n; j += 2) {
+                int[] ans = new int[2];
+                for (int k = i; k < j; k += 2) {
+                    int[] left = dp[i][k], right = dp[k + 2][j];
+                    switch (cs[k + 1]) {
+                        case '&':
+                            ans[0] += left[0] * (right[0] + right[1]) + left[1] * right[0];
+                            ans[1] += left[1] * right[1];
+                            break;
+                        case '|':
+                            ans[0] += left[0] * right[0];
+                            ans[1] += left[1] * (right[0] + right[1]) + left[0] * right[1];
+                            break;
+                        default:
+                            ans[0] += left[0] * right[0] + left[1] * right[1];
+                            ans[1] += left[0] * right[1] + left[1] * right[0];
+                            break;
+                    }
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][n - 1][result];
     }
 
 //    public static void main(String[] args) {

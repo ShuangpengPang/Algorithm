26e2448b21c58bc270505c818ea779501aaf377b
shuangpengpang <15913598312@163.com>
2024-10-11 11:17:55 +0800

    面试题 10.09. 排序矩阵查找

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1009SearchMatrix.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1009SearchMatrix.java
@@ -8,7 +8,7 @@ package com.shuangpeng.interview;
  */
 public class Question1009SearchMatrix {
 
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix0(int[][] matrix, int target) {
         if (matrix.length == 0) {
             return false;
         }
@@ -23,5 +23,25 @@ public class Question1009SearchMatrix {
             }
         }
         return false;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        return dfs(matrix, 0, matrix.length - 1, 0, matrix[0].length - 1, target);
+    }
+
+    private boolean dfs(int[][] matrix, int r1, int r2, int c1, int c2, int target) {
+        if (r1 > r2 || c1 > c2) {
+            return false;
+        }
+        int r = r1 + (r2 - r1 >> 1), c = c1 + (c2 - c1 >> 1);
+        if (target < matrix[r][c]) {
+            return dfs(matrix, r1, r - 1, c1, c2, target) || dfs(matrix, r, r2, c1, c - 1, target);
+        } else if (target > matrix[r][c]) {
+            return dfs(matrix, r1, r, c + 1, c2, target) || dfs(matrix, r + 1, r2, c1, c2, target);
+        }
+        return true;
     }
 }

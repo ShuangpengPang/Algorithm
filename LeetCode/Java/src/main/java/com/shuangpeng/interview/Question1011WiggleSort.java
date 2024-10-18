8566c99c7e65b8dfacc1824d88b04584c5794bb8
shuangpengpang <15913598312@163.com>
2024-10-18 10:53:42 +0800

    面试题 10.11. 峰与谷

--- LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1011WiggleSort.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1011WiggleSort.java
@@ -10,11 +10,22 @@ import java.util.Arrays;
  */
 public class Question1011WiggleSort {
 
-    public void wiggleSort(int[] nums) {
+    public void wiggleSort0(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < n; i += 2) {
             if (i + 1 < n) {
+                nums[i] = nums[i] ^ nums[i + 1];
+                nums[i + 1] = nums[i] ^ nums[i + 1];
+                nums[i] = nums[i] ^ nums[i + 1];
+            }
+        }
+    }
+
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if ((i & 1) == 0 && nums[i] < nums[i + 1] || (i & 1) == 1 && nums[i] > nums[i + 1]) {
                 nums[i] = nums[i] ^ nums[i + 1];
                 nums[i + 1] = nums[i] ^ nums[i + 1];
                 nums[i] = nums[i] ^ nums[i + 1];

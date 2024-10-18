6a878362fad92b56a9a95cdd6119c167f8f6f30b
shuangpengpang <15913598312@163.com>
2024-10-18 10:47:08 +0800

    面试题 10.11. 峰与谷

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/interview/Question1011WiggleSort.java
@@ -0,0 +1,24 @@
+package com.shuangpeng.interview;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Question1011WiggleSort（面试题 10.11. 峰与谷）
+ * @date 2024/10/18 10:22 AM
+ */
+public class Question1011WiggleSort {
+
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i += 2) {
+            if (i + 1 < n) {
+                nums[i] = nums[i] ^ nums[i + 1];
+                nums[i + 1] = nums[i] ^ nums[i + 1];
+                nums[i] = nums[i] ^ nums[i + 1];
+            }
+        }
+    }
+}

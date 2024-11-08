d82304759ed96e19f76d92052a4e775fc7502fb8
shuangpengpang <15913598312@163.com>
2024-11-08 16:17:05 +0800

    K次乘运算后的最终数组I（3264）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3264FindArrayStateAfterKMultiplicationOperationsI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3264FindArrayStateAfterKMultiplicationOperationsI.java
@@ -10,7 +10,7 @@ import java.util.PriorityQueue;
  */
 public class Problem3264FindArrayStateAfterKMultiplicationOperationsI {
 
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
+    public int[] getFinalState0(int[] nums, int k, int multiplier) {
         if (multiplier == 1) {
             return nums;
         }
@@ -25,5 +25,50 @@ public class Problem3264FindArrayStateAfterKMultiplicationOperationsI {
             pq.offer(idx);
         }
         return nums;
+    }
+
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        if (multiplier == 1) {
+            return nums;
+        }
+        int max = 0, n = nums.length;
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> (int) (a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+            pq.offer(new long[]{nums[i], i});
+        }
+        long N = (long) 1e9 + 7;
+        while (k > 0 && pq.peek()[0] <= max) {
+            long[] pair = pq.poll();
+            pair[0] *= multiplier;
+            nums[(int) pair[1]] = (int) (pair[0] % N);
+            pq.offer(pair);
+            k--;
+        }
+        int m = k % n;
+        for (int i = 0; i < m; i++) {
+            long[] pair = pq.poll();
+            nums[(int) pair[1]] = (int) (pair[0] * multiplier % N);
+        }
+        long p = fastPower(multiplier, k / n);
+        for (int i = 0; i < n; i++) {
+            nums[i] = (int) (nums[i] * p % N);
+        }
+        return nums;
+    }
+
+    private long fastPower(long num, int power) {
+        long N = (long) 1e9 + 7;
+        long ans = 1;
+        while (power != 0) {
+            if ((power & 1) == 1) {
+                ans = ans * num % N;
+            }
+            num = num * num % N;
+            power >>= 1;
+        }
+        return ans;
     }
 }

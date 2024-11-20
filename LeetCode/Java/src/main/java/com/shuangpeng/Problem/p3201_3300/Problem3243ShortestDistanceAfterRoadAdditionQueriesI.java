bae367e8aaec67a055ad59f7b38d70abfea76e0a
shuangpengpang <15913598312@163.com>
2024-11-20 14:03:24 +0800

    新增道路查询后的最短距离I（3243）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3243ShortestDistanceAfterRoadAdditionQueriesI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3243ShortestDistanceAfterRoadAdditionQueriesI.java
@@ -50,7 +50,7 @@ public class Problem3243ShortestDistanceAfterRoadAdditionQueriesI {
         return ans;
     }
 
-    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+    public int[] shortestDistanceAfterQueries1(int n, int[][] queries) {
         List<Integer>[] g = new List[n];
         Arrays.setAll(g, i -> new ArrayList<>());
         for (int i = 1; i < n; i++) {
@@ -88,5 +88,28 @@ public class Problem3243ShortestDistanceAfterRoadAdditionQueriesI {
             dis++;
         }
         return dis;
+    }
+
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        int[] dp = new int[n];
+        Arrays.setAll(dp, i -> i);
+        List<Integer>[] from = new List[n];
+        Arrays.setAll(from, i -> new ArrayList<>());
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            int x = queries[i][0], y = queries[i][1];
+            from[y].add(x);
+            if (dp[x] + 1 < dp[y]) {
+                for (int j = y; j < n; j++) {
+                    dp[j] = Math.min(dp[j], dp[j - 1] + 1);
+                    for (int f : from[j]) {
+                        dp[j] = Math.min(dp[j], dp[f] + 1);
+                    }
+                }
+            }
+            ans[i] = dp[n - 1];
+        }
+        return ans;
     }
 }

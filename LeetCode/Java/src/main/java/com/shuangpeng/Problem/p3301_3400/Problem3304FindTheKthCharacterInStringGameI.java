bf4790dc63a1f137fe98bf4c0beef4402c1bcf08
shuangpengpang <15913598312@163.com>
2024-11-26 16:58:38 +0800

    找出第K个字符I（3304）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3304FindTheKthCharacterInStringGameI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3304FindTheKthCharacterInStringGameI.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p3301_3400;
  */
 public class Problem3304FindTheKthCharacterInStringGameI {
 
-    public char kthCharacter(int k) {
+    public char kthCharacter0(int k) {
         StringBuilder sb = new StringBuilder("a");
         while (sb.length() < k) {
             int n = sb.length();
@@ -17,5 +17,57 @@ public class Problem3304FindTheKthCharacterInStringGameI {
             }
         }
         return sb.charAt(sb.length() - 1);
+    }
+
+    public char kthCharacter(int k) {
+        int cnt = 0;
+        while (k != 0) {
+            int p = (int) (Math.log(k) / Math.log(2));
+            int num = (int) Math.pow(2, p);
+            int m = k % num;
+            cnt += m == 0 ? p : 1;
+            k = m;
+        }
+        return (char) ('a' + cnt % 26);
+    }
+}
+
+class Problem3304FindTheKthCharacterInStringGameI0 {
+
+    public char kthCharacter(int k) {
+        return dfs(k, 1 << Integer.SIZE - Integer.numberOfLeadingZeros(k - 1));
+    }
+
+    private char dfs(int k, int num) {
+        if (k == 1) {
+            return 'a';
+        }
+        int cnt = 0;
+        if (k > num) {
+            cnt++;
+            k -= num;
+        }
+        return (char) (cnt + dfs(k, num >> 1));
+    }
+}
+
+class Problem3304FindTheKthCharacterInStringGameI1 {
+
+    public char kthCharacter(int k) {
+        return (char) ('a' + Integer.bitCount(k - 1));
+    }
+}
+
+class Solution {
+    public char kthCharacter(int k) {
+        k--;
+        char ans = 'a';
+        for (int i = 31 - Integer.numberOfLeadingZeros(k); i >= 0; i--) {
+            if (k >= (1 << i)) { // k 在右半边
+                ans++;
+                k -= (1 << i);
+            }
+        }
+        return ans;
     }
 }

9147d1e339977497e629be5c2761aeb23d2c8984
shuangpengpang <15913598312@163.com>
2024-11-27 11:54:41 +0800

    交替组II（3208）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3208AlternatingGroupsII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3201_3300/Problem3208AlternatingGroupsII.java
@@ -8,6 +8,25 @@ package com.shuangpeng.Problem.p3201_3300;
  */
 public class Problem3208AlternatingGroupsII {
 
+    public int numberOfAlternatingGroups0(int[] colors, int k) {
+        int n = colors.length, m = n + k - 1;
+        int[] arr = new int[m];
+        System.arraycopy(colors, 0, arr, 0, n);
+        for (int i = n; i < m; i++) {
+            arr[i] = colors[i - n];
+        }
+        int ans = 0;
+        for (int i = 0, j = 1; j < m; j++) {
+            if (arr[j] == arr[j - 1]) {
+                i = j;
+            }
+            if (j - i + 1 >= k) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
     public int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length, ans = 0;
         for (int i = 0, j = 0; j < n + k - 1; j++) {

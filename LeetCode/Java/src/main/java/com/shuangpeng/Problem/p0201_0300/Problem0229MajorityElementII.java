5b22b3f754cc38b9d8ca2b2537197b8d5d617ecc
shuangpengpang <15913598312@163.com>
2025-02-24 15:30:19 +0800

    多次元素II（229）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0229MajorityElementII.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p0201_0300/Problem0229MajorityElementII.java
@@ -3,6 +3,12 @@ package com.shuangpeng.Problem.p0201_0300;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * @description:
+ * @param null : 
+ * @return null
+ * @date 2025/2/24 3:29 PM
+ **/
 public class Problem0229MajorityElementII {
 
     public List<Integer> majorityElement0(int[] nums) {
@@ -94,5 +100,42 @@ public class Problem0229MajorityElementII {
             ans.add(second);
         }
         return ans;
+    }
+}
+
+class Problem0229MajorityElementII0 {
+
+    public List<Integer> majorityElement(int[] nums) {
+        int x = 0, y = 0, cntX = 0, cntY = 0;
+        for (int i = 0, n = nums.length; i < n; i++) {
+            if (cntX == 0 && nums[i] != y || nums[i] == x) {
+                x = nums[i];
+                cntX++;
+            } else if (cntY == 0 && nums[i] != x || nums[i] == y) {
+                y = nums[i];
+                cntY++;
+            } else {
+                --cntX;
+                --cntY;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        if (cntX > 0 && check(nums, x)) {
+            ans.add(x);
+        }
+        if (cntY > 0 && check(nums, y)) {
+            ans.add(y);
+        }
+        return ans;
+    }
+
+    private boolean check(int[] nums, int num) {
+        int cnt = 0;
+        for (int x : nums) {
+            if (x == num) {
+                cnt++;
+            }
+        }
+        return cnt * 3 > nums.length;
     }
 }

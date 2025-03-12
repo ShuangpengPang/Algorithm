aac99004b739689cfaa50c085055bbb6ad56986b
shuangpengpang <15913598312@163.com>
2025-03-12 15:42:06 +0800

    变长子数组求和（3427）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3401_3500/Problem3427SumOfVariableLengthSubarrays.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3401_3500/Problem3427SumOfVariableLengthSubarrays.java
@@ -8,13 +8,28 @@ package com.shuangpeng.Problem.p3401_3500;
  */
 public class Problem3427SumOfVariableLengthSubarrays {
 
-    public int subarraySum(int[] nums) {
+    public int subarraySum0(int[] nums) {
         int n = nums.length;
         int[] preSum = new int[n + 1];
         int ans = 0;
         for (int i = 0; i < n; i++) {
             preSum[i + 1] = preSum[i] + nums[i];
             ans += preSum[i + 1] - preSum[Math.max(0, i - nums[i])];
+        }
+        return ans;
+    }
+
+    public int subarraySum(int[] nums) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            diff[Math.max(0, i - nums[i])]++;
+            diff[i + 1]--;
+        }
+        int ans = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += diff[i];
+            ans += nums[i] * cnt;
         }
         return ans;
     }

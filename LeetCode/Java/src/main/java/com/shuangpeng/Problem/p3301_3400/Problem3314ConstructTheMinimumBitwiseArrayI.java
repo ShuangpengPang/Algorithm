923d5f36a4f9e2e5bd203d2a3b67d38fa9dffac3
shuangpengpang <15913598312@163.com>
2025-03-24 18:22:01 +0800

    构造最小位运算数组I（3314）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3314ConstructTheMinimumBitwiseArrayI.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3314ConstructTheMinimumBitwiseArrayI.java
@@ -10,12 +10,22 @@ import java.util.List;
  */
 public class Problem3314ConstructTheMinimumBitwiseArrayI {
 
-    public int[] minBitwiseArray(List<Integer> nums) {
+    public int[] minBitwiseArray0(List<Integer> nums) {
         int n = nums.size();
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
             int x = nums.get(i), y = x + 1, z = y & -y;
             ans[i] = (x & y) | ((z >> 1) - 1);
+        }
+        return ans;
+    }
+
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i), y = ((~x) & -(~x)) >> 1;
+            ans[i] = x == 2 ? -1 : x ^ y;
         }
         return ans;
     }

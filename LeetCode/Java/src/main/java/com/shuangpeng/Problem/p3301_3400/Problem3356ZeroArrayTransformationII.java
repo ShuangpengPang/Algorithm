0d26af4f3afe1e05c4b19f77ceee4dae12027f90
shuangpengpang <15913598312@163.com>
2025-04-01 14:09:30 +0800

    零数组变换II（3356）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3356ZeroArrayTransformationII.java
@@ -0,0 +1,52 @@
+package com.shuangpeng.Problem.p3301_3400;
+
+import java.util.Arrays;
+
+/**
+ * @author ShuangPengPang
+ * @version 1.0
+ * @description: Problem3356ZeroArrayTransformationII（零数组变换II）
+ * @date 2025/4/1 11:40
+ */
+public class Problem3356ZeroArrayTransformationII {
+
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int left = 0, right = queries.length - 1;
+        int n = nums.length;
+        boolean isAllZero = true;
+        for (int num : nums) {
+            if (num != 0) {
+                isAllZero = false;
+                break;
+            }
+        }
+        if (isAllZero) {
+            return 0;
+        }
+        int[] diff = new int[n + 1];
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            Arrays.fill(diff, 0);
+            if (!check(nums, queries, diff, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left == queries.length ? -1 : left + 1;
+    }
+
+    private boolean check(int[] nums, int[][] queries, int[] diff, int k) {
+        for (int i = 0; i <= k; i++) {
+            diff[queries[i][0]] += queries[i][2];
+            diff[queries[i][1] + 1] -= queries[i][2];
+        }
+        for (int i = 0, s = 0, n = nums.length; i < n; i++) {
+            s += diff[i];
+            if (nums[i] > s) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

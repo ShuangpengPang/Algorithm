0fb972d120e3be539dade48cfd222992c9511b7b
shuangpengpang <15913598312@163.com>
2025-04-03 11:32:59 +0800

    最小数组和（3366）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3366MinimumArraySum.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3366MinimumArraySum.java
@@ -1,6 +1,8 @@
 package com.shuangpeng.Problem.p3301_3400;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ShuangPengPang
@@ -45,5 +47,109 @@ public class Problem3366MinimumArraySum {
             }
         }
         return ans;
+    }
+}
+
+class Problem3366MinimumArraySum0 {
+    public int minArraySum(int[] nums, int k, int op1, int op2) {
+        int[][] f = new int[op1 + 1][op2 + 1];
+        for (int x : nums) {
+            for (int p = op1; p >= 0; p--) {
+                for (int q = op2; q >= 0; q--) {
+                    int res = f[p][q] + x;
+                    if (p > 0) {
+                        res = Math.min(res, f[p - 1][q] + (x + 1) / 2);
+                    }
+                    if (q > 0 && x >= k) {
+                        res = Math.min(res, f[p][q - 1] + x - k);
+                        if (p > 0) {
+                            int y = (x + 1) / 2 >= k ? (x + 1) / 2 - k : (x - k + 1) / 2;
+                            res = Math.min(res, f[p - 1][q - 1] + y);
+                        }
+                    }
+                    f[p][q] = res;
+                }
+            }
+        }
+        return f[op1][op2];
+    }
+}
+
+class Problem3366MinimumArraySum1 {
+    public int minArraySum(int[] nums, int k, int op1, int op2) {
+        Arrays.sort(nums);
+        int high = lowerBound(nums, k * 2 - 1);
+        int low = lowerBound(nums, k);
+
+        // 在 [2k-1,∞) 中的数，直接先除再减（从大到小操作）
+        for (int i = nums.length - 1; i >= high; i--) {
+            if (op1 > 0) {
+                nums[i] = (nums[i] + 1) / 2;
+                op1--;
+            }
+            if (op2 > 0) {
+                nums[i] -= k;
+                op2--;
+            }
+        }
+
+        // 在 [k,2k-2] 中的数，先把小的数 -k
+        Map<Integer, Integer> cnt = new HashMap<>();
+        int odd = 0;
+        for (int i = low; i < high; i++) {
+            if (op2 > 0) {
+                nums[i] -= k;
+                if (k % 2 > 0 && nums[i] % 2 > 0) {
+                    // nums[i] 原来是偶数，后面有机会把这次 -k 操作留给奇数，得到更小的答案
+                    cnt.merge(nums[i], 1, Integer::sum); // cnt[nums[i]]++
+                }
+                op2--;
+            } else {
+                odd += nums[i] % 2; // 没有执行 -k 的奇数
+            }
+        }
+
+        // 重新排序（注：这里可以改用合并两个有序数组的做法）
+        Arrays.sort(nums, 0, high);
+
+        int ans = 0;
+        if (k % 2 > 0) {
+            // 调整，对于 [k,2k-2] 中 -k 后还要再 /2 的数，如果原来是偶数，改成给奇数 -k 再 /2，这样答案可以减一
+            for (int i = high - op1; i < high && odd > 0; i++) {
+                int x = nums[i];
+                if (cnt.containsKey(x)) {
+                    if (cnt.merge(x, -1, Integer::sum) == 0) { // --cnt[x] == 0
+                        cnt.remove(x);
+                    }
+                    odd--;
+                    ans--;
+                }
+            }
+        }
+
+        // 最后，从大到小执行操作 1
+        for (int i = high - 1; i >= 0 && op1 > 0; i--) {
+            nums[i] = (nums[i] + 1) / 2;
+            op1--;
+        }
+
+        for (int x : nums) {
+            ans += x;
+        }
+        return ans;
+    }
+
+    // 见 https://www.bilibili.com/video/BV1AP41137w7/
+    private int lowerBound(int[] nums, int target) {
+        int left = -1, right = nums.length;
+        while (left + 1 < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return right;
     }
 }

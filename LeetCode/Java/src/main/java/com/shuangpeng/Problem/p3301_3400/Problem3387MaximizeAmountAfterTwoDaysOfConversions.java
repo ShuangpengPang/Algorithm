fb1083bff23d96bd94c2302e80b06e786de3ac36
shuangpengpang <15913598312@163.com>
2025-04-07 18:43:31 +0800

    两天自由外汇交易后的最大货币数（3387）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3387MaximizeAmountAfterTwoDaysOfConversions.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3387MaximizeAmountAfterTwoDaysOfConversions.java
@@ -3,6 +3,7 @@ package com.shuangpeng.Problem.p3301_3400;
 import javafx.util.Pair;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -85,4 +86,46 @@ public class Problem3387MaximizeAmountAfterTwoDaysOfConversions {
 //        Problem3387MaximizeAmountAfterTwoDaysOfConversions a = new Problem3387MaximizeAmountAfterTwoDaysOfConversions();
 //        a.maxAmount(initialCurrency, list1, rates1, list2, rates2);
 //    }
+}
+
+class Problem3387MaximizeAmountAfterTwoDaysOfConversions0 {
+
+    public double maxAmount(String initialCurrency, List<List<String>> pairs1, double[] rates1, List<List<String>> pairs2, double[] rates2) {
+        Map<String, Double> day1Amount = calcAmount(pairs1, rates1, initialCurrency);
+        Map<String, Double> day2Amount = calcAmount(pairs2, rates2, initialCurrency);
+
+        double ans = 0;
+        for (Map.Entry<String, Double> e : day2Amount.entrySet()) {
+            ans = Math.max(ans, day1Amount.getOrDefault(e.getKey(), 0.0) / e.getValue());
+        }
+        return ans;
+    }
+
+    private Map<String, Double> calcAmount(List<List<String>> pairs, double[] rates, String initialCurrency) {
+        Map<String, List<Pair<String, Double>>> g = new HashMap<>();
+        for (int i = 0; i < pairs.size(); i++) {
+            String x = pairs.get(i).get(0);
+            String y = pairs.get(i).get(1);
+            double r = rates[i];
+            g.computeIfAbsent(x, k -> new ArrayList<>()).add(new Pair(y, r));
+            g.computeIfAbsent(y, k -> new ArrayList<>()).add(new Pair(x, 1 / r));
+        }
+
+        Map<String, Double> amount = new HashMap<>();
+        dfs(initialCurrency, 1, g, amount);
+        return amount;
+    }
+
+    private void dfs(String x, double curAmount, Map<String, List<Pair<String, Double>>> g, Map<String, Double> amount) {
+        amount.put(x, curAmount);
+        if (!g.containsKey(x)) {
+            return;
+        }
+        for (Pair<String, Double> p : g.get(x)) {
+            // 每个节点只需递归一次（重复递归算出来的结果是一样的，因为题目保证汇率没有矛盾）
+            if (!amount.containsKey(p.getKey())) {
+                dfs(p.getKey(), curAmount * p.getValue(), g, amount);
+            }
+        }
+    }
 }

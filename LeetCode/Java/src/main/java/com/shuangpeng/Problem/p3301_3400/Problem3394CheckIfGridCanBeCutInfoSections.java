668b86dad83a1239dae469bf70ad41a9c5d71c58
shuangpengpang <15913598312@163.com>
2025-04-08 17:28:58 +0800

    判断网格图能否被切割成块（3394）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3394CheckIfGridCanBeCutInfoSections.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p3301_3400/Problem3394CheckIfGridCanBeCutInfoSections.java
@@ -1,5 +1,6 @@
 package com.shuangpeng.Problem.p3301_3400;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeSet;
 
@@ -39,4 +40,72 @@ public class Problem3394CheckIfGridCanBeCutInfoSections {
 //        int[][] r = {{0,0,2,2},{2,0,4,2},{4,0,6,2},{0,2,3,4},{0,4,1,6},{1,4,3,6},{3,2,4,6},{4,2,5,6},{5,2,6,6}};
 //        a.checkValidCuts(6, r);
 //    }
+}
+
+class Problem3394CheckIfGridCanBeCutInfoSections0 {
+
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        int m = rectangles.length;
+        int[][] a = new int[m][2], b = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            int[] r = rectangles[i];
+            a[i][0] = r[0];
+            a[i][1] = r[2];
+            b[i][0] = r[1];
+            b[i][1] = r[3];
+        }
+        return check(a) || check(b);
+    }
+
+    private boolean check(int[][] a) {
+        Arrays.sort(a, Comparator.comparingInt(e -> e[0]));
+        int n = a.length, cnt = 0;
+        for (int i = 0, r = -1; i < n && cnt < 3; i++) {
+            if (a[i][0] >= r) {
+                cnt++;
+            }
+            r = Math.max(r, a[i][1]);
+        }
+        return cnt == 3;
+    }
+}
+
+class Problem3394CheckIfGridCanBeCutInfoSections1 {
+    static int bits = 30;
+    static int mask = (1 << bits) - 1;
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+
+        int m = rectangles.length;
+        long[] start = new long[m];
+
+        for (int i = 0; i < m; i++)
+            start[i] = ((long) rectangles[i][0] << bits) + rectangles[i][2];
+
+        Arrays.sort(start);
+        if(validate(start))
+            return true;
+
+        for (int i = 0; i < m; i++)
+            start[i] = ((long) rectangles[i][1] << bits) + rectangles[i][3];
+
+        Arrays.sort(start);
+        return validate(start);
+    }
+
+    private boolean validate(long[] nums) {
+
+        int count = 0;
+        int maxRight = -1;
+
+        for (long x: nums) {
+            int left = (int) (x >> bits), right = (int) (x & mask);
+
+            if (left >= maxRight && ++count == 3)
+                return true;
+
+            maxRight = Math.max(maxRight, right);
+        }
+
+        return false;
+    }
 }

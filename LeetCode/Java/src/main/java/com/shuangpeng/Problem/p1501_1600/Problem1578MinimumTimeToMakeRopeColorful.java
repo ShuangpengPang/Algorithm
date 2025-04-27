6b5a3e293a4f71d73abd480625f97ca80bb0af49
shuangpengpang <15913598312@163.com>
2025-04-27 13:41:58 +0800

    使绳子变成彩色的最短时间（1578）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1578MinimumTimeToMakeRopeColorful.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1501_1600/Problem1578MinimumTimeToMakeRopeColorful.java
@@ -8,7 +8,7 @@ package com.shuangpeng.Problem.p1501_1600;
  */
 public class Problem1578MinimumTimeToMakeRopeColorful {
 
-    public int minCost(String colors, int[] neededTime) {
+    public int minCost0(String colors, int[] neededTime) {
         char[] cs = colors.toCharArray();
         int n = cs.length, cnt = 0, s = 0, mx = Integer.MIN_VALUE;
         int ans = 0;
@@ -23,6 +23,26 @@ public class Problem1578MinimumTimeToMakeRopeColorful {
                 cnt = 0;
                 s = 0;
                 mx = Integer.MIN_VALUE;
+            }
+        }
+        return ans;
+    }
+
+    public int minCost(String colors, int[] neededTime) {
+        char[] cs = colors.toCharArray();
+        int n = cs.length, maxTime = 0, ans = 0;
+        char preColor = '-';
+        for (int i = 0; i < n; i++) {
+            if (cs[i] == preColor) {
+                if (neededTime[i] > maxTime) {
+                    ans += maxTime;
+                    maxTime = neededTime[i];
+                } else {
+                    ans += neededTime[i];
+                }
+            } else {
+                preColor = cs[i];
+                maxTime = neededTime[i];
             }
         }
         return ans;

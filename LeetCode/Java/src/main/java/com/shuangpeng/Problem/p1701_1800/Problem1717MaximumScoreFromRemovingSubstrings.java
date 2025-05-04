ef754dc67cb0b4e21adbff804471f2bccf3ec854
shuangpengpang <15913598312@163.com>
2025-05-04 21:03:50 +0800

    删除子字符串的最大得分（1717）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1717MaximumScoreFromRemovingSubstrings.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1717MaximumScoreFromRemovingSubstrings.java
@@ -39,3 +39,31 @@ public class Problem1717MaximumScoreFromRemovingSubstrings {
         return ans;
     }
 }
+
+class Problem1717MaximumScoreFromRemovingSubstrings0 {
+
+    public int maximumGain(String s, int x, int y) {
+        return x>y? getMax(s,'a','b',x,y):getMax(s,'b','a',y,x);
+    }
+
+    private int getMax(String s, char a,char b,int x,int y){
+        int c1 = 0,c2 = 0,ans = 0;
+        s=s+"c";
+        for (char c : s.toCharArray()) {
+            if(c == a){
+                c1++;
+            }else if(c == b){
+                if(c1 == 0){
+                    c2++;
+                }else {
+                    ans+=x;
+                    c1--;
+                }
+            }else {
+                ans+=(y*Math.min(c1,c2));
+                c1 = 0;c2 = 0;
+            }
+        }
+        return ans;
+    }
+}

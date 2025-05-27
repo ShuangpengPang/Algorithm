d3a2726582d7ab86fcb2c95b4b2cea98ed231886
shuangpengpang <15913598312@163.com>
2025-05-27 18:21:02 +0800

    解码异或后的排列（1734）

--- LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1734DecodeXORedPermutation.java
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1701_1800/Problem1734DecodeXORedPermutation.java
@@ -9,7 +9,7 @@ package com.shuangpeng.Problem.p1701_1800;
  */
 public class Problem1734DecodeXORedPermutation {
 
-    public int[] decode(int[] encoded) {
+    public int[] decode0(int[] encoded) {
         int n = encoded.length + 1, x = n, y = 0;
         for (int i = 1; i < n; i++) {
             y ^= encoded[i - 1];
@@ -19,6 +19,22 @@ public class Problem1734DecodeXORedPermutation {
         ans[0] = x;
         for (int i = 1; i < n; i++) {
             ans[i] = ans[i - 1] ^ encoded[i - 1];
+        }
+        return ans;
+    }
+
+    public int[] decode(int[] encoded) {
+        int n = encoded.length, x = n + 1;
+        for (int i = 0; i < n; i += 2) {
+            x ^= encoded[i];
+        }
+        for (int i = 1; i <= n; i++) {
+            x ^= i;
+        }
+        int[] ans = new int[n + 1];
+        ans[n] = x;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i + 1] ^ encoded[i];
         }
         return ans;
     }

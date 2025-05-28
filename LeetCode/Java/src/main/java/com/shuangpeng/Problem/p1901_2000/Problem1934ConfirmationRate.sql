6cc8d020276f234f4eeb26109fae825fbc8b3e12
shuangpengpang <15913598312@163.com>
2025-05-28 17:55:04 +0800

    确认率（1934）

---
+++ LeetCode/Java/src/main/java/com/shuangpeng/Problem/p1901_2000/Problem1934ConfirmationRate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below（确认率）
+
+select s.user_id, round(if(c.user_id is null, 0, sum(if(c.action = 'confirmed', 1, 0)) / count(*)), 2) as confirmation_rate
+from Signups s
+    left join Confirmations c on s.user_id = c.user_id
+group by s.user_id
+;
